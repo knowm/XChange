4b5c2e05f59e4555c948e4b79a9de93fbd8de9b3
timmolter <tim.molter@gmail.com>
2012-04-08 16:38:16 +0200

    trivial formatting changes

--- xchange/src/main/java/com/xeiam/xchange/utils/CharsetUtils.java
+++ xchange/src/main/java/com/xeiam/xchange/utils/CharsetUtils.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (C) 2012 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.xeiam.xchange.utils;
-
-import com.xeiam.xchange.streaming.websocket.CloseFrame;
-import com.xeiam.xchange.streaming.websocket.exceptions.InvalidDataException;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
@@ -10,13 +28,15 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
 
+import com.xeiam.xchange.streaming.websocket.CloseFrame;
+import com.xeiam.xchange.streaming.websocket.exceptions.InvalidDataException;
+
 public class CharsetUtils {
 
   public static CodingErrorAction codingErrorAction = CodingErrorAction.REPORT;
 
   /**
    * @param input The string to be converted
-   *
    * @return UTF-8 encoding in bytes
    */
   public static byte[] toByteArrayUtf8(String input) {
@@ -30,7 +50,6 @@ public class CharsetUtils {
 
   /**
    * @param input The string to be converted
-   *
    * @return ASCII encoding in bytes
    */
   public static byte[] toByteArrayAscii(String input) {
@@ -44,7 +63,6 @@ public class CharsetUtils {
 
   /**
    * @param bytes The byte array
-   *
    * @return An ASCII encoded version of the byte array
    */
   public static String toStringAscii(byte[] bytes) {
@@ -52,12 +70,13 @@ public class CharsetUtils {
   }
 
   /**
-   * <p>Constructs a new String by decoding the specified subarray of bytes using the specified charset. The length of the new String is a function of the charset, and hence may not be equal to the length of the subarray</p>
-   *
-   * @param bytes  The bytes to be decoded into characters
+   * <p>
+   * Constructs a new String by decoding the specified subarray of bytes using the specified charset. The length of the new String is a function of the charset, and hence may not be equal to the length of the subarray
+   * </p>
+   * 
+   * @param bytes The bytes to be decoded into characters
    * @param offset The index of the first byte to decode
    * @param length The number of bytes to decode
-   *
    * @return The encoded String
    */
   public static String toStringAscii(byte[] bytes, int offset, int length) {
@@ -74,12 +93,13 @@ public class CharsetUtils {
   }
 
   /**
-   * <p>Constructs a new String by decoding the specified subarray of bytes using the specified charset. The length of the new String is a function of the charset, and hence may not be equal to the length of the subarray</p>
-   *
-   * @param bytes  The bytes to be decoded into characters
+   * <p>
+   * Constructs a new String by decoding the specified subarray of bytes using the specified charset. The length of the new String is a function of the charset, and hence may not be equal to the length of the subarray
+   * </p>
+   * 
+   * @param bytes The bytes to be decoded into characters
    * @param offset The index of the first byte to decode
    * @param length The number of bytes to decode
-   *
    * @return The encoded String
    * @throws com.xeiam.xchange.streaming.websocket.exceptions.InvalidDataException If something goes wrong
    */
--- xchange/src/main/java/com/xeiam/xchange/utils/MoneyUtils.java
+++ xchange/src/main/java/com/xeiam/xchange/utils/MoneyUtils.java
@@ -1,24 +1,24 @@
 package com.xeiam.xchange.utils;
 
+import java.math.RoundingMode;
+
 import org.joda.money.BigMoney;
 import org.joda.money.CurrencyUnit;
 
-import java.math.RoundingMode;
-
 /**
- * <p>Utilities to provide the following to application:</p>
+ * <p>
+ * Utilities to provide the following to application:
+ * </p>
  * <ul>
  * <li>Various shortcuts to common currencies from Joda Money</li>
  * </ul>
- *
+ * 
  * @since 0.0.1
- *         
  */
 public class MoneyUtils {
 
   /**
    * @param value A general-purpose currency and value representation (e.g. "USD 3210.12345678")
-   *
    * @return A standard fiat currency BigMoney that can handle complex calculations and display using a scale inferred from the minor part
    * @see org.joda.money.Money For a simpler approach for fiat currencies not requiring precise calculations (e.g. display only)
    */
@@ -28,10 +28,10 @@ public class MoneyUtils {
 
   /**
    * @param value A general-purpose currency and value representation (e.g. "BTC 3210.1234567800")
-   *
    * @return A standard Bitcoin currency BigMoney that can handle complex calculations using a scale of 12 regardless of the minor part
    */
   public static BigMoney parseBitcoin(String value) {
+
     BigMoney unscaled = BigMoney.parse(value);
     if (!unscaled.getCurrencyUnit().getCode().equals("BTC")) {
       throw new IllegalArgumentException("Must have BTC currency code");
@@ -41,10 +41,10 @@ public class MoneyUtils {
 
   /**
    * @param value A whole number of satoshis (e.g. 1 provides the same as "BTC 0.00000001")
-   *
    * @return A standard Bitcoin currency BigMoney that can handle complex calculations using a scale of 12 regardless of the minor part
    */
   public static BigMoney fromSatoshi(long value) {
+
     BigMoney unscaled = BigMoney.ofScale(CurrencyUnit.getInstance("BTC"), value, 8);
     if (!unscaled.getCurrencyUnit().getCode().equals("BTC")) {
       throw new IllegalArgumentException("Must have BTC currency code");
@@ -53,6 +53,7 @@ public class MoneyUtils {
   }
 
   public static String formatBitcoin(BigMoney value) {
+
     if (!value.getCurrencyUnit().getCode().equals("BTC")) {
       throw new IllegalArgumentException("Must have BTC currency code");
     }
