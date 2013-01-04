a13f6f7bc66213ae7315de41e4c3b131e9a418f5
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-01-04 07:44:03 +0100

    Refactor: separate rest parameter handling from the rest

---
+++ xchange-core/src/main/java/com/xeiam/xchange/proxy/AllParams.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2013 Matija Mazi
+ * Copyright (C) 2013 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.proxy;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Matija Mazi <br/>
+ */
+public class AllParams {
+  @SuppressWarnings("unchecked")
+  private static final List<Class<? extends Annotation>> PARAM_ANNOTATION_CLASSES = Arrays.asList(QueryParam.class, PathParam.class, FormParam.class, HeaderParam.class);
+
+  private Map<Class<? extends Annotation>, Params> paramsMap;
+
+  private AllParams(Map<Class<? extends Annotation>, Params> paramsMap) {
+    this.paramsMap = paramsMap;
+  }
+
+  static AllParams createInstance(Method method, Object[] args) {
+     return new AllParams(createParamsMap(method, args));
+  }
+
+  private static Map<Class<? extends Annotation>, Params> createParamsMap(Method method, Object[] args) {
+    Map<Class<? extends Annotation>, Params> params = createEmptyMap();
+    Annotation[][] paramAnnotations = method.getParameterAnnotations();
+    for (int i = 0; i < paramAnnotations.length; i++) {
+      Annotation[] paramAnns = paramAnnotations[i];
+      for (Annotation paramAnn : paramAnns) {
+        String paramName = getParamName(paramAnn);
+        if (paramName != null) {
+          params.get(paramAnn.annotationType()).add(paramName, args[i]);
+        }
+      }
+    }
+    return params;
+  }
+
+  private static String getParamName(Annotation queryParam) {
+
+    for (Class<? extends Annotation> annotationClass : PARAM_ANNOTATION_CLASSES) {
+      String paramName = getValueOrNull(annotationClass, queryParam);
+      if (paramName != null) {
+        return paramName;
+      }
+    }
+    // This is not one of the annotations in PARAM_ANNOTATION_CLASSES.
+    return null;
+  }
+
+  private static <T extends Annotation> String getValueOrNull(Class<T> annotationClass, Annotation ann) {
+    if (!annotationClass.isInstance(ann)) {
+      return null;
+    }
+    try {
+      return (String) ann.getClass().getMethod("value").invoke(ann);
+    } catch (Exception e) {
+      throw new RuntimeException("Annotation " + annotationClass + " has no element 'value'.");
+    }
+  }
+
+  private static Map<Class<? extends Annotation>, Params> createEmptyMap() {
+
+    Map<Class<? extends Annotation>, Params> map = new HashMap<Class<? extends Annotation>, Params>();
+    for (Class<? extends Annotation> annotationClass : PARAM_ANNOTATION_CLASSES) {
+      map.put(annotationClass, new Params());
+    }
+    return map;
+  }
+
+  String getPath(String methodPath) {
+    return paramsMap.get(PathParam.class).applyToPath(methodPath);
+  }
+
+  String getPostBody() {
+    return paramsMap.get(FormParam.class).asFormEncodedPostBody();
+  }
+
+  Map<String, String> getHttpHeaders() {
+    return paramsMap.get(HeaderParam.class).getAsHttpHeaders();
+  }
+
+  String getQueryString() {
+    return paramsMap.get(QueryParam.class).asQueryString();
+  }
+}
--- xchange-core/src/main/java/com/xeiam/xchange/proxy/RestInvocationHandler.java
+++ xchange-core/src/main/java/com/xeiam/xchange/proxy/RestInvocationHandler.java
@@ -25,43 +25,38 @@ package com.xeiam.xchange.proxy;
 import com.xeiam.xchange.utils.HttpTemplate;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import javax.ws.rs.*;
-import java.lang.annotation.Annotation;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
 * @author Matija Mazi <br/>
 */
 public class RestInvocationHandler implements InvocationHandler {
-  @SuppressWarnings("unchecked")
-  private static final List<Class<? extends Annotation>> PARAM_ANNOTATION_CLASSES = Arrays.asList(QueryParam.class, PathParam.class, FormParam.class, HeaderParam.class);
 
   private final HttpTemplate httpTemplate;
   private final ObjectMapper mapper;
   private final String intfacePath;
-  private final String url;
+  private final String baseUrl;
 
   public RestInvocationHandler(HttpTemplate httpTemplate, ObjectMapper mapper, Class<?> restInterface, String url) {
 
     this.httpTemplate = httpTemplate;
     this.mapper = mapper;
     this.intfacePath = restInterface.getAnnotation(Path.class).value();
-    this.url = url;
+    this.baseUrl = url;
   }
 
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
-    String path = method.getAnnotation(Path.class).value();
     Class<?> returnType = method.getReturnType();
-    Map<Class<? extends Annotation>, Params> params = getArgumentMap(method, args);
-    Params pathParams = params.get(PathParam.class);
-    path = pathParams.applyToPath(path);
+    AllParams params = AllParams.createInstance(method, args);
+    String path = method.getAnnotation(Path.class).value();
+    path = params.getPath(path);
     if (method.isAnnotationPresent(GET.class)) {
       return getForJsonObject(path, returnType, params);
     } else if (method.isAnnotationPresent(POST.class)) {
@@ -71,75 +66,26 @@ public class RestInvocationHandler implements InvocationHandler {
     }
   }
 
-  private static Map<Class<? extends Annotation>, Params> getArgumentMap(Method method, Object[] args) {
-    Map<Class<? extends Annotation>, Params> params = createParamsMap();
-    Annotation[][] paramAnnotations = method.getParameterAnnotations();
-    for (int i = 0; i < paramAnnotations.length; i++) {
-      Annotation[] paramAnns = paramAnnotations[i];
-      for (Annotation paramAnn : paramAnns) {
-        String paramName = getParamName(paramAnn);
-        if (paramName != null) {
-          params.get(paramAnn.annotationType()).add(paramName, args[i]);
-        }
-      }
-    }
-    return params;
-  }
-
-  private static String getParamName(Annotation queryParam) {
-
-    for (Class<? extends Annotation> annotationClass : PARAM_ANNOTATION_CLASSES) {
-      String paramName = getValueOrNull(annotationClass, queryParam);
-      if (paramName != null) {
-        return paramName;
-      }
-    }
-    // This is not one of the annotations in PARAM_ANNOTATION_CLASSES.
-    return null;
-  }
-
-  private static <T extends Annotation> String getValueOrNull(Class<T> annotationClass, Annotation ann) {
-    if (!annotationClass.isInstance(ann)) {
-      return null;
-    }
-    try {
-      return (String) ann.getClass().getMethod("value").invoke(ann);
-    } catch (Exception e) {
-      throw new RuntimeException("Annotation " + annotationClass + " has no element 'value'.");
-    }
-  }
-
-  private static Map<Class<? extends Annotation>, Params> createParamsMap() {
-
-    Map<Class<? extends Annotation>, Params> map = new HashMap<Class<? extends Annotation>, Params>();
-    for (Class<? extends Annotation> annotationClass : PARAM_ANNOTATION_CLASSES) {
-      map.put(annotationClass, new Params());
-    }
-    return map;
-  }
-
-  private String getUrl(String method) {
+  private String getUrl(String method, String queryString) {
 
     // todo: make more robust in terms of path separator ('/') handling
-    return String.format("%s/%s/%s", url, intfacePath, method);
+    String completeUrl = String.format("%s/%s/%s", this.baseUrl, intfacePath, method);
+    if (queryString != null && !queryString.isEmpty()) {
+      completeUrl += "?" + queryString;
+    }
+    return completeUrl;
   }
 
-  private <T> T getForJsonObject(String methodPath, Class<T> returnType, Map<Class<? extends Annotation>, Params> params) {
+  private <T> T getForJsonObject(String methodPath, Class<T> returnType, AllParams params) {
 
-    String url = getUrl(methodPath);
-    Params queryParams = params.get(QueryParam.class);
-    if (queryParams != null) {
-      url += "?" + queryParams.asQueryString();
-    }
+    String queryString = params.getQueryString();
+    String url = getUrl(methodPath, queryString);
 
     return httpTemplate.getForJsonObject(url, returnType, mapper, new HashMap<String, String>());
   }
 
-  private <T> T postForJsonObject(String methodPath, Class<T> returnType, Map<Class<? extends Annotation>, Params> params) {
+  private <T> T postForJsonObject(String methodPath, Class<T> returnType, AllParams allParams) {
 
-    String postBody = params.get(FormParam.class).asFormEncodedPostBody();
-    Map<String, String> asHttpHeaders = params.get(HeaderParam.class).getAsHttpHeaders();
-
-    return httpTemplate.postForJsonObject(getUrl(methodPath), returnType, postBody, mapper, asHttpHeaders);
+    return httpTemplate.postForJsonObject(getUrl(methodPath, null), returnType, allParams.getPostBody(), mapper, allParams.getHttpHeaders());
   }
 }
