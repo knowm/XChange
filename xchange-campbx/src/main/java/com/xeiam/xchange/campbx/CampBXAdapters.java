e5586347878096578527df6181164f8451486efa
Tim Molter <tim.molter@gmail.com>
2013-01-06 23:56:52 +0100

    campBX imll cleaned up

---
+++ xchange-campbx/src/main/java/com/xeiam/xchange/campbx/CampBXAdapters.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (C) 2012 Xeiam LLC http://xeiam.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.campbx;
+
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.money.BigMoney;
+import org.joda.money.CurrencyUnit;
+
+import com.xeiam.xchange.campbx.dto.marketdata.CampBXOrderBook;
+import com.xeiam.xchange.campbx.dto.marketdata.CampBXTicker;
+import com.xeiam.xchange.dto.Order;
+import com.xeiam.xchange.dto.marketdata.OrderBook;
+import com.xeiam.xchange.dto.marketdata.Ticker;
+import com.xeiam.xchange.dto.marketdata.TickerBuilder;
+import com.xeiam.xchange.dto.trade.LimitOrder;
+import com.xeiam.xchange.utils.MoneyUtils;
+
+/**
+ * Various adapters for converting from CampBX DTOs to XChange DTOs
+ */
+public class CampBXAdapters {
+
+  /**
+   * Adapts a com.xeiam.xchange.bitstamp.api.model.OrderBook to a OrderBook Object
+   * 
+   * @param orderBook
+   * @param currency
+   * @param tradableIdentifier
+   * @return
+   */
+  public static OrderBook adaptOrders(CampBXOrderBook orderBook, String currency, String tradableIdentifier) {
+
+    List<LimitOrder> asks = createOrders(tradableIdentifier, currency, Order.OrderType.ASK, orderBook.getAsks());
+    List<LimitOrder> bids = createOrders(tradableIdentifier, currency, Order.OrderType.BID, orderBook.getBids());
+    return new OrderBook(asks, bids);
+  }
+
+  private static List<LimitOrder> createOrders(String tradableIdentifier, String currency, Order.OrderType orderType, List<List<BigDecimal>> orders) {
+
+    List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
+    for (List<BigDecimal> ask : orders) {
+      checkArgument(ask.size() == 2, "Expected a pair (price, amount) but got {0} elements.", ask.size());
+      limitOrders.add(createOrder(tradableIdentifier, currency, ask, orderType));
+    }
+    return limitOrders;
+  }
+
+  private static LimitOrder createOrder(String tradableIdentifier, String currency, List<BigDecimal> priceAndAmount, Order.OrderType orderType) {
+
+    return new LimitOrder(orderType, priceAndAmount.get(1), tradableIdentifier, currency, BigMoney.of(CurrencyUnit.USD, priceAndAmount.get(0)));
+  }
+
+  private static void checkArgument(boolean argument, String msgPattern, Object... msgArgs) {
+
+    if (!argument) {
+      throw new IllegalArgumentException(MessageFormat.format(msgPattern, msgArgs));
+    }
+  }
+
+  /**
+   * Adapts a CampBXTicker to a Ticker Object
+   * 
+   * @param campbxTicker
+   * @param currency
+   * @param tradableIdentifier
+   * @return
+   */
+  public static Ticker adaptTicker(CampBXTicker campbxTicker, String currency, String tradableIdentifier) {
+
+    BigMoney last = MoneyUtils.parseFiat(currency + " " + campbxTicker.getLast());
+    BigMoney bid = MoneyUtils.parseFiat(currency + " " + campbxTicker.getBid());
+    BigMoney ask = MoneyUtils.parseFiat(currency + " " + campbxTicker.getAsk());
+
+    return TickerBuilder.newInstance().withTradableIdentifier(tradableIdentifier).withLast(last).withBid(bid).withAsk(ask).build();
+
+  }
+
+}
--- xchange-campbx/src/main/java/com/xeiam/xchange/campbx/service/marketdata/polling/CampBXPollingMarketDataService.java
+++ xchange-campbx/src/main/java/com/xeiam/xchange/campbx/service/marketdata/polling/CampBXPollingMarketDataService.java
@@ -22,36 +22,28 @@
  */
 package com.xeiam.xchange.campbx.service.marketdata.polling;
 
-import java.math.BigDecimal;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import org.joda.money.BigMoney;
-import org.joda.money.CurrencyUnit;
 
 import com.xeiam.xchange.CurrencyPair;
 import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.NotAvailableFromExchangeException;
 import com.xeiam.xchange.campbx.CampBX;
-import com.xeiam.xchange.dto.Order;
+import com.xeiam.xchange.campbx.CampBXAdapters;
+import com.xeiam.xchange.campbx.CampBXUtils;
+import com.xeiam.xchange.campbx.dto.marketdata.CampBXOrderBook;
+import com.xeiam.xchange.campbx.dto.marketdata.CampBXTicker;
 import com.xeiam.xchange.dto.marketdata.OrderBook;
 import com.xeiam.xchange.dto.marketdata.Ticker;
-import com.xeiam.xchange.dto.marketdata.TickerBuilder;
 import com.xeiam.xchange.dto.marketdata.Trades;
-import com.xeiam.xchange.dto.trade.LimitOrder;
 import com.xeiam.xchange.proxy.RestProxyFactory;
 import com.xeiam.xchange.service.BasePollingExchangeService;
 import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
+import com.xeiam.xchange.utils.Assert;
 
 /**
  * @author Matija Mazi <br/>
  */
 public class CampBXPollingMarketDataService extends BasePollingExchangeService implements PollingMarketDataService {
-
-  private static final CurrencyUnit BTC = CurrencyUnit.of("BTC");
-  private static final CurrencyUnit USD = CurrencyUnit.of("USD");
-  private static final List<CurrencyPair> CURRENCY_PAIRS = Arrays.asList(CurrencyPair.BTC_USD);
 
   private final CampBX campbx;
 
@@ -67,65 +59,50 @@ public class CampBXPollingMarketDataService extends BasePollingExchangeService i
   }
 
   @Override
-  public List<CurrencyPair> getExchangeSymbols() {
-
-    return CURRENCY_PAIRS;
-  }
-
-  @Override
   public Ticker getTicker(String tradableIdentifier, String currency) {
 
-    checkArgument(tradableIdentifier.equals(BTC.getCode()));
-    checkArgument(currency.equals(USD.getCode()));
-    com.xeiam.xchange.campbx.dto.marketdata.CampBXTicker tck = campbx.getTicker();
-    return new TickerBuilder().withAsk(BigMoney.of(USD, tck.getAsk())).withBid(BigMoney.of(USD, tck.getBid())).withLast(BigMoney.of(USD, tck.getLast())).build();
+    verify(tradableIdentifier, currency);
+    CampBXTicker campbxTicker = campbx.getTicker();
+
+    return CampBXAdapters.adaptTicker(campbxTicker, currency, tradableIdentifier);
   }
 
   @Override
   public OrderBook getPartialOrderBook(String tradableIdentifier, String currency) {
 
-    return getFullOrderBook(tradableIdentifier, currency);
+    throw new NotAvailableFromExchangeException();
   }
 
   @Override
   public OrderBook getFullOrderBook(String tradableIdentifier, String currency) {
 
-    com.xeiam.xchange.campbx.dto.marketdata.CampBXOrderBook orderBook = campbx.getOrderBook();
-    List<LimitOrder> asks = createOrders(tradableIdentifier, currency, Order.OrderType.ASK, orderBook.getAsks());
-    List<LimitOrder> bids = createOrders(tradableIdentifier, currency, Order.OrderType.BID, orderBook.getBids());
-    return new OrderBook(asks, bids);
-  }
+    CampBXOrderBook orderBook = campbx.getOrderBook();
 
-  private List<LimitOrder> createOrders(String tradableIdentifier, String currency, Order.OrderType orderType, List<List<BigDecimal>> orders) {
-
-    List<LimitOrder> limitOrders = new ArrayList<LimitOrder>();
-    for (List<BigDecimal> ask : orders) {
-      checkArgument(ask.size() == 2, "Expected a pair (price, amount) but got {0} elements.", ask.size());
-      limitOrders.add(createOrder(tradableIdentifier, currency, ask, orderType));
-    }
-    return limitOrders;
-  }
-
-  private LimitOrder createOrder(String tradableIdentifier, String currency, List<BigDecimal> priceAndAmount, Order.OrderType orderType) {
-
-    return new LimitOrder(orderType, priceAndAmount.get(1), tradableIdentifier, currency, BigMoney.of(CurrencyUnit.USD, priceAndAmount.get(0)));
+    return CampBXAdapters.adaptOrders(orderBook, currency, tradableIdentifier);
   }
 
   @Override
   public Trades getTrades(String tradableIdentifier, String currency) {
 
-    throw new UnsupportedOperationException("getTrades not supported.");
+    throw new NotAvailableFromExchangeException();
   }
 
-  private static void checkArgument(boolean argument) {
+  /**
+   * Verify
+   * 
+   * @param tradableIdentifier
+   * @param currency
+   */
+  private void verify(String tradableIdentifier, String currency) {
 
-    checkArgument(argument, "Illegal argument.");
+    Assert.notNull(tradableIdentifier, "tradableIdentifier cannot be null");
+    Assert.notNull(currency, "currency cannot be null");
+    Assert.isTrue(CampBXUtils.isValidCurrencyPair(new CurrencyPair(tradableIdentifier, currency)), "currencyPair is not valid:" + tradableIdentifier + " " + currency);
   }
 
-  private static void checkArgument(boolean argument, String msgPattern, Object... msgArgs) {
+  @Override
+  public List<CurrencyPair> getExchangeSymbols() {
 
-    if (!argument) {
-      throw new IllegalArgumentException(MessageFormat.format(msgPattern, msgArgs));
-    }
+    return CampBXUtils.CURRENCY_PAIRS;
   }
 }
