555a7b31bbfec87b45d248e8accd95c11feb6d16
Tim Molter <tim.molter@gmail.com>
2013-01-21 20:41:44 +0100

    started roughing in BitcoinCentral marketdata API

--- xchange-examples/src/main/java/com/xeiam/xchange/examples/bitcoincentral/marketdata/TradesDemo.java
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/bitcoincentral/marketdata/TradesDemo.java
@@ -22,53 +22,31 @@
  */
 package com.xeiam.xchange.examples.bitcoincentral.marketdata;
 
-import java.util.List;
-
-import com.xeiam.xchange.CurrencyPair;
+import com.xeiam.xchange.Currencies;
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
-import com.xeiam.xchange.ExchangeSpecification;
 import com.xeiam.xchange.bitcoincentral.BitcoinCentralExchange;
-import com.xeiam.xchange.dto.marketdata.OrderBook;
-import com.xeiam.xchange.dto.marketdata.Ticker;
 import com.xeiam.xchange.dto.marketdata.Trades;
 import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
 
 /**
- * <p>
- * Example showing the following:
- * </p>
- * <ul>
- * <li>Connect to Bitcoin Central exchange with authentication</li>
- * <li>View account balance</li>
- * <li>Get the bitcoin deposit address</li> Please provide your username and password as program arguments.
- * </ul>
+ * Demonstrate requesting Order Book at Bitstamp
  */
-public class BitcoinCentralMarketDataDemo {
+public class TradesDemo {
 
   public static void main(String[] args) {
 
-    ExchangeSpecification exSpec = new ExchangeSpecification(BitcoinCentralExchange.class);
-    exSpec.setUri("https://en.bitcoin-central.net");
+    // Use the factory to get Bitstamp exchange API using default settings
+    Exchange bitcoinCentralExchange = ExchangeFactory.INSTANCE.createExchange(BitcoinCentralExchange.class.getName());
 
-    Exchange btcCentral = ExchangeFactory.INSTANCE.createExchange(exSpec);
+    // Interested in the public polling market data feed (no authentication)
+    PollingMarketDataService marketDataService = bitcoinCentralExchange.getPollingMarketDataService();
 
-    PollingMarketDataService service = btcCentral.getPollingMarketDataService();
+    // Get the latest trade data for BTC/CAD
+    Trades trades = marketDataService.getTrades(Currencies.BTC, Currencies.EUR);
 
-    List<CurrencyPair> symbols = service.getExchangeSymbols();
-    System.out.println("symbols = " + symbols);
-
-    OrderBook fullOrderBook = service.getFullOrderBook("BTC", "EUR");
-    System.out.println("fullOrderBook = " + fullOrderBook);
-
-    OrderBook partialOrderBook = service.getPartialOrderBook("BTC", "EUR");
-    System.out.println("partialOrderBook = " + partialOrderBook);
-
-    Ticker ticker = service.getTicker("BTC", "EUR");
-    System.out.println("ticker = " + ticker);
-
-    Trades trades = service.getTrades("BTC", "EUR");
-    System.out.println("trades = " + trades);
+    System.out.println(trades.toString());
 
   }
+
 }
