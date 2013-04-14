94f003a9063eadd4c03c13406623e92b3483ea70
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-04-14 12:36:41 +0200

    Fixed bug https://github.com/timmolter/XChange/issues/73

--- xchange-btce/src/main/java/com/xeiam/xchange/btce/BTCEAdapters.java
+++ xchange-btce/src/main/java/com/xeiam/xchange/btce/BTCEAdapters.java
@@ -188,7 +188,7 @@ public final class BTCEAdapters {
       OrderType orderType = o.getType() == BTCEOrder.Type.buy ? OrderType.BID : OrderType.ASK;
       String[] pair = o.getPair().split("_");
       String currency = pair[1].toUpperCase();
-      BigMoney price = BigMoney.of(CurrencyUnit.of(currency), o.getAmount());
+      BigMoney price = BigMoney.of(CurrencyUnit.of(currency), o.getRate());
       os.add(new LimitOrder(orderType, o.getAmount(), pair[0].toUpperCase(), currency, Long.toString(id), price));
     }
     return new OpenOrders(os);
