05e0f1913c96daaea144f901d7bd4f745ce7a1af
Macarse <macarse@gmail.com>
2013-04-23 21:52:39 -0300

    Removed java_websocket from the repo and added it as mvn dep

--- xchange-core/src/main/java/org/java_websocket/handshake/ClientHandshakeBuilder.java
+++
@@ -1,6 +0,0 @@
-package org.java_websocket.handshake;
-
-public interface ClientHandshakeBuilder extends HandshakeBuilder, ClientHandshake {
-
-  public void setResourceDescriptor(String resourcedescriptor);
-}
