1e4b5837fd8eda9a6167be4c5a9d3203414499cd
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-05-13 16:49:36 +0200

    Fix this (Use Base64; this was originally copied from the wrong implementation, hence the error). Still doesn't work, unfortunately.

--- xchange-mtgox/src/main/java/com/xeiam/xchange/mtgox/v2/service/account/polling/MtGoxV2Digest.java
+++ xchange-mtgox/src/main/java/com/xeiam/xchange/mtgox/v2/service/account/polling/MtGoxV2Digest.java
@@ -1,7 +1,27 @@
+/**
+ * Copyright (C) 2012 - 2013 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.xeiam.xchange.mtgox.v2.service.account.polling;
 
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -9,11 +29,9 @@ import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.RestInvocationParams;
+import si.mazi.rescu.utils.Base64;
 
 /**
  * @author Matija Mazi <br/>
@@ -22,8 +40,6 @@ import si.mazi.rescu.RestInvocationParams;
  * TODO! this doesn't work yet.
  */
 public class MtGoxV2Digest implements ParamsDigest {
-
-  private static final Logger log = LoggerFactory.getLogger(MtGoxV2Digest.class);
 
   private static final String HMAC_SHA_512 = "HmacSHA512";
   private final Mac mac;
@@ -37,11 +53,11 @@ public class MtGoxV2Digest implements ParamsDigest {
   private MtGoxV2Digest(String secretKeyBase64) throws IllegalArgumentException {
 
     try {
-      SecretKey secretKey = new SecretKeySpec(secretKeyBase64.getBytes("UTF-8"), HMAC_SHA_512);
+      SecretKey secretKey = new SecretKeySpec(Base64.decode(secretKeyBase64.getBytes()), HMAC_SHA_512);
       mac = Mac.getInstance(HMAC_SHA_512);
       mac.init(secretKey);
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException("Illegal encoding, check the code.", e);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Could not decode Base 64 string", e);
     } catch (InvalidKeyException e) {
       throw new IllegalArgumentException("Invalid key for hmac initialization.", e);
     } catch (NoSuchAlgorithmException e) {
@@ -54,20 +70,11 @@ public class MtGoxV2Digest implements ParamsDigest {
     return secretKeyBase64 == null ? null : new MtGoxV2Digest(secretKeyBase64);
   }
 
-  @Override
   public String digestParams(RestInvocationParams restInvocationParams) {
 
-    try {
-      String postBody = restInvocationParams.getRequestBody();
-      String methodPath = restInvocationParams.getMethodPath();
-      log.debug("methodPath = {}", methodPath);
-      mac.update(methodPath.getBytes("UTF-8"));
-      mac.update((byte) 0);
-      mac.update(postBody.getBytes("UTF-8"));
-      return String.format("%0128x", new BigInteger(1, mac.doFinal()));
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException("Illegal encoding, check the code.", e);
-    }
-    // return Base64.encodeBytes(mac.doFinal()).trim();
+    mac.update(restInvocationParams.getMethodPath().getBytes());
+    mac.update(new byte[]{0});
+    mac.update(restInvocationParams.getRequestBody().getBytes());
+    return Base64.encodeBytes(mac.doFinal()).trim();
   }
 }
