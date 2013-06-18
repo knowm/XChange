d97d4bc9f60c9d948f816e62c9a3f8ab510ac86d
Tim Molter <tim.molter@gmail.com>
2013-06-18 19:15:30 +0200

    removed redundant class. Issue #134

--- xchange-examples/src/main/java/com/xeiam/xchange/examples/mtgox/v2/service/trade/streaming/MtGoxWebSocketTradeDemo.java
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/mtgox/v2/service/trade/streaming/MtGoxWebSocketTradeDemo.java
@@ -32,7 +32,7 @@ import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.examples.mtgox.v2.MtGoxV2ExamplesUtils;
 import com.xeiam.xchange.mtgox.v2.dto.account.polling.MtGoxAccountInfo;
 import com.xeiam.xchange.mtgox.v2.dto.account.streaming.MtGoxWalletUpdate;
-import com.xeiam.xchange.mtgox.v2.dto.trade.streaming.MtGoxOpenOrder;
+import com.xeiam.xchange.mtgox.v2.dto.trade.polling.MtGoxOpenOrder;
 import com.xeiam.xchange.mtgox.v2.dto.trade.streaming.MtGoxOrderCanceled;
 import com.xeiam.xchange.mtgox.v2.dto.trade.streaming.MtGoxTradeLag;
 import com.xeiam.xchange.mtgox.v2.service.streaming.MtGoxStreamingConfiguration;
