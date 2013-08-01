8dc6a158591525def2610d038a587dfd8065b169
veken0m <veken0m@gmail.com>
2013-07-31 20:07:04 -0400

    BTCChina example and test classes

---
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/btcchina/marketdata/DepthDemo.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2012 - 2013 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.examples.btcchina.marketdata;
+
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeFactory;
+import com.xeiam.xchange.currency.Currencies;
+import com.xeiam.xchange.dto.marketdata.OrderBook;
+import com.xeiam.xchange.service.polling.PollingMarketDataService;
+import com.xeiam.xchange.btcchina.BTCChinaExchange;
+
+/**
+ * Demonstrate requesting Order Book at BTC China
+ */
+public class DepthDemo {
+
+  public static void main(String[] args) {
+
+    // Use the factory to get the BTCChina exchange API using default settings
+    Exchange btcchina = ExchangeFactory.INSTANCE.createExchange(BTCChinaExchange.class.getName());
+
+    // Interested in the public polling market data feed (no authentication)
+    PollingMarketDataService marketDataService = btcchina.getPollingMarketDataService();
+
+    // Get the latest order book data for BTC/CNY
+    OrderBook orderBook = marketDataService.getFullOrderBook(Currencies.BTC, Currencies.CNY);
+
+    // System.out.println(orderBook.toString());
+
+  }
+
+}
---
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/btcchina/marketdata/TradesDemo.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2012 - 2013 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.examples.btcchina.marketdata;
+
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeFactory;
+import com.xeiam.xchange.currency.Currencies;
+import com.xeiam.xchange.dto.marketdata.Trades;
+import com.xeiam.xchange.service.polling.PollingMarketDataService;
+import com.xeiam.xchange.btcchina.BTCChinaExchange;
+
+/**
+ * Demonstrate requesting Trades at BTC China
+ */
+public class TradesDemo {
+
+  public static void main(String[] args) {
+
+    // Use the factory to get the VirtEx exchange API using default settings
+    Exchange btcchina = ExchangeFactory.INSTANCE.createExchange(BTCChinaExchange.class.getName());
+
+    // Interested in the public polling market data feed (no authentication)
+    PollingMarketDataService marketDataService = btcchina.getPollingMarketDataService();
+
+    // Get the latest trade data for BTC/CNY
+    Trades trades = marketDataService.getTrades(Currencies.BTC, Currencies.CNY);
+
+    System.out.println(trades.toString());
+
+  }
+
+}
