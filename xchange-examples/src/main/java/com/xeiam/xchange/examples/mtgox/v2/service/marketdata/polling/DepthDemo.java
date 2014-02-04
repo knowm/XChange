2c88cdd8bbd85008dca34b068e5ca72967d79644
Tim Molter <tim.molter@gmail.com>
2014-02-04 13:50:50 +0100

    added call to raw()

--- xchange-examples/src/main/java/com/xeiam/xchange/examples/mtgox/v2/service/marketdata/polling/DepthDemo.java
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/mtgox/v2/service/marketdata/polling/DepthDemo.java
@@ -21,6 +21,9 @@
  */
 package com.xeiam.xchange.examples.mtgox.v2.service.marketdata.polling;
 
+import java.io.IOException;
+import java.util.List;
+
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.currency.Currencies;
@@ -32,53 +35,51 @@ import com.xeiam.xchange.mtgox.v2.dto.marketdata.MtGoxDepthWrapper;
 import com.xeiam.xchange.mtgox.v2.service.polling.MtGoxMarketDataServiceRaw;
 import com.xeiam.xchange.service.polling.PollingMarketDataService;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
 /**
  * Test requesting depth at MtGox
  */
 public class DepthDemo {
 
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        // Use the factory to get the version 2 MtGox exchange API using default settings
-        Exchange mtGoxExchange = ExchangeFactory.INSTANCE.createExchange(MtGoxExchange.class.getName());
+    // Use the factory to get the version 2 MtGox exchange API using default settings
+    Exchange mtGoxExchange = ExchangeFactory.INSTANCE.createExchange(MtGoxExchange.class.getName());
 
-        // Interested in the public market data feed (no authentication)
-        PollingMarketDataService marketDataService = mtGoxExchange.getPollingMarketDataService();
-        generic(marketDataService);
+    // Interested in the public market data feed (no authentication)
+    PollingMarketDataService marketDataService = mtGoxExchange.getPollingMarketDataService();
+    generic(marketDataService);
+    raw((MtGoxMarketDataServiceRaw) marketDataService);
 
+  }
 
-    }
+  private static void generic(PollingMarketDataService marketDataService) throws IOException {
 
-    private static void generic(PollingMarketDataService marketDataService) throws IOException {
-        // Get the current orderbook
-        OrderBook orderBook = marketDataService.getOrderBook(Currencies.BTC, Currencies.SEK);
+    // Get the current orderbook
+    OrderBook orderBook = marketDataService.getOrderBook(Currencies.BTC, Currencies.SEK);
 
-        System.out.println("Current Order Book size for BTC / SEK: " + (orderBook.getAsks().size() + orderBook.getBids().size()));
+    System.out.println("Current Order Book size for BTC / SEK: " + (orderBook.getAsks().size() + orderBook.getBids().size()));
 
-        System.out.println("First Ask: " + orderBook.getAsks().get(0).toString());
+    System.out.println("First Ask: " + orderBook.getAsks().get(0).toString());
 
-        System.out.println("First Bid: " + orderBook.getBids().get(0).toString());
+    System.out.println("First Bid: " + orderBook.getBids().get(0).toString());
 
-        System.out.println(orderBook.toString());
-    }
+    System.out.println(orderBook.toString());
+  }
 
-    private static void raw(MtGoxMarketDataServiceRaw marketDataService) throws IOException {
-        // Get the current orderbook
-        MtGoxDepthWrapper orderBook = marketDataService.getMtGoxOrderBook(Currencies.BTC, Currencies.SEK, null);
-        List<LimitOrder> asks = MtGoxAdapters.adaptOrders(orderBook.getMtGoxDepth().getAsks(), Currencies.SEK, "ask", "");
-        List<LimitOrder> bids = MtGoxAdapters.adaptOrders(orderBook.getMtGoxDepth().getBids(), Currencies.SEK, "bid", "");
+  private static void raw(MtGoxMarketDataServiceRaw marketDataService) throws IOException {
 
-        System.out.println("Current Order Book size for BTC / SEK: " + (asks.size() + bids.size()));
+    // Get the current orderbook
+    MtGoxDepthWrapper orderBook = marketDataService.getMtGoxOrderBook(Currencies.BTC, Currencies.SEK, null);
+    List<LimitOrder> asks = MtGoxAdapters.adaptOrders(orderBook.getMtGoxDepth().getAsks(), Currencies.SEK, "ask", "");
+    List<LimitOrder> bids = MtGoxAdapters.adaptOrders(orderBook.getMtGoxDepth().getBids(), Currencies.SEK, "bid", "");
 
-        System.out.println("First Ask: " + asks.get(0).toString());
+    System.out.println("Current Order Book size for BTC / SEK: " + (asks.size() + bids.size()));
 
-        System.out.println("First Bid: " + asks.get(0).toString());
+    System.out.println("First Ask: " + asks.get(0).toString());
 
-        System.out.println(orderBook.toString());
-    }
+    System.out.println("First Bid: " + asks.get(0).toString());
+
+    System.out.println(orderBook.toString());
+  }
 
 }
