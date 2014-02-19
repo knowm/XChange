633beabd18678e93412a1d6dd152eda55b49ffd6
James Edwards <james.p.edwards42@gmail.com>
2014-02-18 23:11:51 -0800

    remove keys

--- xchange-examples/src/main/java/com/xeiam/xchange/examples/coinbase/CoinbaseDemoUtils.java
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/coinbase/CoinbaseDemoUtils.java
@@ -32,8 +32,8 @@ public class CoinbaseDemoUtils {
   public static Exchange createExchange() {
 
     ExchangeSpecification exSpec = new CoinbaseExchange().getDefaultExchangeSpecification();
-    exSpec.setApiKey("4uCFoLlM6cbarnyI");
-    exSpec.setSecretKey("YgzIUavPEsAqwx7kXofXriccTBWF0Etx");
+    exSpec.setApiKey("");
+    exSpec.setSecretKey("");
     return ExchangeFactory.INSTANCE.createExchange(exSpec);
   }
 }
