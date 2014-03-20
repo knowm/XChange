10f8f767bceac9cff182218cefcd921345a56bc1
Jonathan Heusser <jonathan.heusser@gmail.com>
2014-03-20 20:58:14 +0000

    Transaction DTOs

---
+++ xchange-btcchina/src/main/java/com/xeiam/xchange/btcchina/dto/trade/request/BTCChinaTransactionsRequest.java
@@ -0,0 +1,21 @@
+package com.xeiam.xchange.btcchina.dto.trade.request;
+
+import com.xeiam.xchange.btcchina.dto.BTCChinaRequest;
+
+public class BTCChinaTransactionsRequest extends BTCChinaRequest {
+	/**
+	 * Constructor
+	 * 
+	 * @param orderId
+	 */
+	public BTCChinaTransactionsRequest() {
+
+		method = "getTransactions";
+		params = "[]";
+	}
+
+	@Override
+	public String toString() {
+		return String.format("BTCChinaTransactionsRequest{id=%d, method=%s, params=%s}", id, method, params);
+	}
+}
