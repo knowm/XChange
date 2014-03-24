b4071c433bb4fe03dca5be1d6b83582ed899185f
Tim Molter <tim.molter@gmail.com>
2014-03-24 08:37:38 +0100

    fix compile error

--- xchange-examples/src/main/java/com/xeiam/xchange/examples/bitfinex/marketdata/TradesDemo.java
+++ xchange-examples/src/main/java/com/xeiam/xchange/examples/bitfinex/marketdata/TradesDemo.java
@@ -62,7 +62,7 @@ public class TradesDemo {
   private static void raw(BitfinexMarketDataServiceRaw marketDataService) throws IOException {
 
     // Get the latest trade data for BTC/USD
-    BitfinexTrade[] trades = marketDataService.getBitfinexTrades("btcusd");
+    BitfinexTrade[] trades = marketDataService.getBitfinexTrades("btcusd", System.currentTimeMillis() / 1000 - 120);
     System.out.println("Trades, default. Size= " + trades.length);
     System.out.println(Arrays.toString(trades));
   }
