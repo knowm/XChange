f8b98c5de7f7a164e39758faacf79a24a717a2fe
Benedict Wong <bennydictwong@gmail.com>
2014-05-20 17:35:58 +0800

    Cryptsy Polling Services added

---
+++ xchange-cryptsy/src/main/java/com/xeiam/xchange/cryptsy/service/polling/CryptsyBasePollingService.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2012 - 2014 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.cryptsy.service.polling;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import si.mazi.rescu.ParamsDigest;
+import si.mazi.rescu.RestProxyFactory;
+
+import com.xeiam.xchange.ExchangeException;
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.cryptsy.Cryptsy;
+import com.xeiam.xchange.cryptsy.CryptsyAdapters;
+import com.xeiam.xchange.cryptsy.CryptsyCurrencyUtils;
+import com.xeiam.xchange.cryptsy.dto.CryptsyGenericReturn;
+import com.xeiam.xchange.cryptsy.dto.marketdata.CryptsyGetMarketsReturn;
+import com.xeiam.xchange.cryptsy.service.CryptsyBaseService;
+import com.xeiam.xchange.cryptsy.service.CryptsyHmacPostBodyDigest;
+import com.xeiam.xchange.currency.CurrencyPair;
+
+/**
+ * @author ObsessiveOrange
+ */
+public class CryptsyBasePollingService<T extends Cryptsy> extends CryptsyBaseService {
+  
+  private final Logger                  logger         = LoggerFactory.getLogger(CryptsyBasePollingService.class);
+  
+  public static final Set<CurrencyPair> CURRENCY_PAIRS = new HashSet<CurrencyPair>();
+  
+  private static final long             START_MILLIS   = 1356998400000L;                                                               // Jan 1st, 2013 in milliseconds from epoch
+  // counter for the nonce
+  private static final AtomicInteger    lastNonce      = new AtomicInteger((int) ((System.currentTimeMillis() - START_MILLIS) / 250L));
+  
+  protected final String                apiKey;
+  protected final T                     cryptsy;
+  protected final ParamsDigest          signatureCreator;
+  
+  /**
+   * Constructor
+   * 
+   * @param exchangeSpecification The {@link ExchangeSpecification}
+   */
+  public CryptsyBasePollingService(Class<T> cryptsyType, ExchangeSpecification exchangeSpecification) {
+  
+    super(exchangeSpecification);
+    
+    this.cryptsy = RestProxyFactory.createProxy(cryptsyType, exchangeSpecification.getSslUri());
+    this.apiKey = exchangeSpecification.getApiKey();
+    this.signatureCreator = CryptsyHmacPostBodyDigest.createInstance(exchangeSpecification.getSecretKey());
+  }
+  
+  @SuppressWarnings({ "unchecked", "rawtypes" })
+  @Override
+  public Collection<CurrencyPair> getExchangeSymbols() throws IOException {
+  
+    if (CURRENCY_PAIRS.isEmpty()) {
+      CryptsyGetMarketsReturn OverallMarketData = new CryptsyMarketDataService(exchangeSpecification).getCryptsyMarkets();
+      
+      CURRENCY_PAIRS.addAll(CryptsyAdapters.adaptCurrencyPairs(OverallMarketData));
+      
+      HashMap[] marketSets = CryptsyAdapters.adaptMarketSets(OverallMarketData);
+      CryptsyCurrencyUtils.marketIDs_CurrencyPairs = marketSets[0];
+      CryptsyCurrencyUtils.currencyPairs_MarketIDs = marketSets[1];
+    }
+    
+    return CURRENCY_PAIRS;
+  }
+  
+  protected int nextNonce() {
+  
+    int nextNonce = lastNonce.incrementAndGet();
+    logger.debug("nextNonce in CryptsyBaseService: " + nextNonce);
+    
+    return nextNonce;
+  }
+  
+  public static void checkResult(CryptsyGenericReturn<?> info) {
+  
+    if (!info.isSuccess()) {
+      throw new ExchangeException("Cryptsy returned an error: " + info.getError());
+    }
+    else if (info.getError() != null) {
+      throw new ExchangeException("Got error message: " + info.getError());
+    }
+  }
+  
+}
---
+++ xchange-cryptsy/src/main/java/com/xeiam/xchange/cryptsy/service/polling/CryptsyMarketDataService.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2012 - 2014 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.xeiam.xchange.cryptsy.service.polling;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.cryptsy.CryptsyAdapters;
+import com.xeiam.xchange.cryptsy.CryptsyCurrencyUtils;
+import com.xeiam.xchange.cryptsy.dto.marketdata.CryptsyGetMarketsReturn;
+import com.xeiam.xchange.cryptsy.dto.marketdata.CryptsyMarketTradesReturn;
+import com.xeiam.xchange.cryptsy.dto.marketdata.CryptsyOrderBookReturn;
+import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.ExchangeInfo;
+import com.xeiam.xchange.dto.marketdata.OrderBook;
+import com.xeiam.xchange.dto.marketdata.Ticker;
+import com.xeiam.xchange.dto.marketdata.Trades;
+import com.xeiam.xchange.service.polling.PollingMarketDataService;
+
+/**
+ * @author ObsessiveOrange
+ */
+public class CryptsyMarketDataService extends CryptsyMarketDataServiceRaw implements PollingMarketDataService {
+  
+  /**
+   * @param exchangeSpecification The {@link ExchangeSpecification}
+   */
+  public CryptsyMarketDataService(ExchangeSpecification exchangeSpecification) {
+  
+    super(exchangeSpecification);
+  }
+  
+  @Override
+  public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
+  
+    verify(currencyPair);
+    
+    CryptsyGetMarketsReturn marketsReturnData = super.getCryptsyMarkets();
+    
+    return CryptsyAdapters.adaptTicker(marketsReturnData, currencyPair);
+  }
+  
+  /**
+   * Get market depth from exchange
+   * 
+   * @param tradableIdentifier The identifier to use (e.g. BTC or GOOG). First currency of the pair
+   * @param currency The currency of interest, null if irrelevant. Second currency of the pair
+   * @param args Optional arguments. Exchange-specific. This implementation assumes:
+   *          Integer value from 1 to 2000 -> get corresponding number of items
+   * @return The OrderBook
+   * @throws IOException
+   */
+  @Override
+  public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
+  
+    verify(currencyPair);
+    
+    CryptsyOrderBookReturn orderBookReturnData = super.getCryptsyOrderBook(CryptsyCurrencyUtils.convertToMarketID(currencyPair));
+    
+    return CryptsyAdapters.adaptOrderBook(orderBookReturnData, currencyPair);
+  }
+  
+  /**
+   * Get recent trades from exchange
+   * 
+   * @param tradableIdentifier The identifier to use (e.g. BTC or GOOG)
+   * @param currency The currency of interest
+   * @param args Optional arguments. This implementation assumes
+   *          args[0] is integer value limiting number of trade items to get.
+   *          -1 or missing -> use default 2000 max fetch value
+   *          int from 1 to 2000 -> use API v.3 to get corresponding number of trades
+   * @return Trades object
+   * @throws IOException
+   */
+  @Override
+  public Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
+  
+    verify(currencyPair);
+    
+    CryptsyMarketTradesReturn tradesReturnData = super.getCryptsyTrades(CryptsyCurrencyUtils.convertToMarketID(currencyPair));
+    
+    return CryptsyAdapters.adaptTrades(tradesReturnData, currencyPair);
+  }
+  
+  @Override
+  public ExchangeInfo getExchangeInfo() throws IOException {
+  
+    return new ExchangeInfo((List<CurrencyPair>) getExchangeSymbols());
+  }
+  
+}
