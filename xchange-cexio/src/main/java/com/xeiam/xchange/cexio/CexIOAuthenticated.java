dfea5be4d4b139c1695e051f3008b34e31b754f3
Michael Lagace <veken0m@gmail.com>
2014-05-25 16:51:30 -0400

    [GHash.IO] piggy-back CEX.IO module to access GHash.IO mining API

--- xchange-cexio/src/main/java/com/xeiam/xchange/cexio/CexIOAuthenticated.java
+++ xchange-cexio/src/main/java/com/xeiam/xchange/cexio/CexIOAuthenticated.java
@@ -35,6 +35,8 @@ import javax.ws.rs.core.MediaType;
 import si.mazi.rescu.ParamsDigest;
 
 import com.xeiam.xchange.cexio.dto.account.CexIOBalanceInfo;
+import com.xeiam.xchange.cexio.dto.account.GHashIOHashrate;
+import com.xeiam.xchange.cexio.dto.account.GHashIOWorkers;
 import com.xeiam.xchange.cexio.dto.trade.CexIOOpenOrders;
 import com.xeiam.xchange.cexio.dto.trade.CexIOOrder;
 
@@ -65,5 +67,14 @@ public interface CexIOAuthenticated {
   @Path("place_order/{ident}/{currency}/")
   CexIOOrder placeOrder(@PathParam("ident") String tradeableIdentifier, @PathParam("currency") String currency, @FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer,
       @FormParam("nonce") long nonce, @FormParam("type") CexIOOrder.Type type, @FormParam("price") BigDecimal price, @FormParam("amount") BigDecimal amount) throws IOException;
+  
+  // GHash.IO calls
+  @POST
+  @Path("ghash.io/hashrate")
+  GHashIOHashrate getHashrate(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer, @FormParam("nonce") long nonce) throws IOException;
+  
+  @POST
+  @Path("ghash.io/workers")
+  GHashIOWorkers getWorkers(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer, @FormParam("nonce") long nonce) throws IOException;
 
 }
