eae78c8c7c23d12a5359f20ce911b6dd6c4e2158
Benedict Wong <bennydictwong@gmail.com>
2014-05-27 14:21:38 +0800

    Fixed BTC Central tests, added BTCCentral marketData example

--- xchange-btccentral/src/main/java/com/xeiam/xchange/btccentral/BTCCentralAdapters.java
+++ xchange-btccentral/src/main/java/com/xeiam/xchange/btccentral/BTCCentralAdapters.java
@@ -1,7 +1,15 @@
 package com.xeiam.xchange.btccentral;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralMarketDepth;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralMarketOrder;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralTicker;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralTrade;
 import com.xeiam.xchange.currency.CurrencyPair;
-import com.xeiam.xchange.btccentral.dto.marketdata.*;
 import com.xeiam.xchange.dto.Order.OrderType;
 import com.xeiam.xchange.dto.marketdata.OrderBook;
 import com.xeiam.xchange.dto.marketdata.Ticker;
@@ -9,87 +17,88 @@ import com.xeiam.xchange.dto.marketdata.Trade;
 import com.xeiam.xchange.dto.marketdata.Trades;
 import com.xeiam.xchange.dto.trade.LimitOrder;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 /**
  * @author kpysniak
  */
 public class BTCCentralAdapters {
-
+  
   /**
    * Singleton
    */
-  private BTCCentralAdapters() { }
-
+  private BTCCentralAdapters() {
+  
+  }
+  
   /**
    * Adapts a BTCCentralTicker to a Ticker Object
-   *
+   * 
    * @param btcCentralTicker The exchange specific ticker
    * @param currencyPair (e.g. BTC/USD)
    * @return The ticker
    */
   public static Ticker adaptTicker(BTCCentralTicker btcCentralTicker, CurrencyPair currencyPair) {
-
+  
     BigDecimal bid = btcCentralTicker.getBid();
     BigDecimal ask = btcCentralTicker.getAsk();
     BigDecimal high = btcCentralTicker.getHigh();
     BigDecimal low = btcCentralTicker.getLow();
+    BigDecimal last = btcCentralTicker.getPrice();
     BigDecimal volume = btcCentralTicker.getVolume();
     Date timestamp = new Date(btcCentralTicker.getAt() * 1000L);
-
-    return Ticker.TickerBuilder.newInstance().withCurrencyPair(currencyPair)
-        .withBid(bid).withAsk(ask).withHigh(high).withLow(low).withVolume(volume)
-        .withTimestamp(timestamp).build();
+    
+    return Ticker.TickerBuilder.newInstance().withCurrencyPair(currencyPair).withBid(bid).withAsk(ask).withHigh(high).withLow(low).withLast(last)
+        .withVolume(volume).withTimestamp(timestamp).build();
   }
-
+  
   /**
-   *
+   * 
    * @param marketDepth
    * @param currencyPair
    * @return new order book
    */
   public static OrderBook adaptMarketDepth(BTCCentralMarketDepth marketDepth, CurrencyPair currencyPair) {
+  
     List<LimitOrder> asks = adaptMarketOrderToLimitOrder(marketDepth.getAsks(), OrderType.ASK, currencyPair);
     List<LimitOrder> bids = adaptMarketOrderToLimitOrder(marketDepth.getBids(), OrderType.BID, currencyPair);
-
+    
     // TODO
     // What timestamp should be used? Latest/Earliest/Current one?
     return new OrderBook(new Date(), asks, bids);
   }
-
+  
   /**
-   *
+   * 
    * @param btcCentralMarketOrders
    * @param orderType
    * @param currencyPair
    */
-  private static List<LimitOrder> adaptMarketOrderToLimitOrder(BTCCentralMarketOrder[] btcCentralMarketOrders,
-                                                               OrderType orderType, CurrencyPair currencyPair) {
-
-    List<LimitOrder> orders = new ArrayList<LimitOrder>(btcCentralMarketOrders.length);
-
+  private static List<LimitOrder> adaptMarketOrderToLimitOrder(List<BTCCentralMarketOrder> btcCentralMarketOrders, OrderType orderType,
+      CurrencyPair currencyPair) {
+  
+    List<LimitOrder> orders = new ArrayList<LimitOrder>(btcCentralMarketOrders.size());
+    
     for (BTCCentralMarketOrder btcCentralMarketOrder : btcCentralMarketOrders) {
-      LimitOrder limitOrder = new LimitOrder(orderType, btcCentralMarketOrder.getAmount(), currencyPair, null, new Date(btcCentralMarketOrder.getTimestamp()), btcCentralMarketOrder.getPrice());
+      LimitOrder limitOrder =
+          new LimitOrder(orderType, btcCentralMarketOrder.getAmount(), currencyPair, null, new Date(btcCentralMarketOrder.getTimestamp()),
+              btcCentralMarketOrder.getPrice());
       orders.add(limitOrder);
     }
-
+    
     return orders;
   }
-
+  
   public static Trades adaptTrade(BTCCentralTrade[] btcCentralTrades, CurrencyPair currencyPair) {
+  
     List<Trade> trades = new ArrayList<Trade>();
-
+    
     for (BTCCentralTrade btcCentralTrade : btcCentralTrades) {
-      Trade trade = new Trade(null, btcCentralTrade.getTraded_btc(), currencyPair,
-          btcCentralTrade.getPrice(), new Date(btcCentralTrade.getCreated_at_int()),
-          btcCentralTrade.getUuid().toString(), btcCentralTrade.getUuid().toString());
-
+      Trade trade =
+          new Trade(null, btcCentralTrade.getTraded_btc(), currencyPair, btcCentralTrade.getPrice(), new Date(btcCentralTrade.getCreated_at_int()),
+              btcCentralTrade.getUuid().toString(), btcCentralTrade.getUuid().toString());
+      
       trades.add(trade);
     }
-
+    
     return new Trades(trades, Trades.TradeSortType.SortByTimestamp);
   }
 }
---
+++ xchange-btccentral/src/test/java/com/xeiam/xchange/btccentral/AdaptersTest.java
@@ -0,0 +1,84 @@
+package com.xeiam.xchange.btccentral;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralMarketDepth;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralTicker;
+import com.xeiam.xchange.btccentral.dto.marketdata.BTCCentralTrade;
+import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.marketdata.OrderBook;
+import com.xeiam.xchange.dto.marketdata.Ticker;
+import com.xeiam.xchange.dto.marketdata.Trades;
+
+/**
+ * @author ObsessiveOrange
+ */
+public class AdaptersTest {
+  
+  @Test
+  public void testBTCCentralTickerRequest() throws JsonParseException, JsonMappingException, IOException {
+  
+    // Read in the JSON from the example resources
+    InputStream is = AdaptersTest.class.getResourceAsStream("/Example_TickerData.json");
+    
+    // Use Jackson to parse it
+    ObjectMapper mapper = new ObjectMapper();
+    BTCCentralTicker btcCentralTicker = mapper.readValue(is, BTCCentralTicker.class);
+    
+    Ticker genericTicker = BTCCentralAdapters.adaptTicker(btcCentralTicker, CurrencyPair.BTC_EUR);
+    
+    assertEquals(genericTicker.getAsk(), new BigDecimal("20.4"));
+    assertEquals(genericTicker.getBid(), new BigDecimal("20.1"));
+    assertEquals(genericTicker.getHigh(), new BigDecimal("20.74"));
+    assertEquals(genericTicker.getLow(), new BigDecimal("20.2"));
+    assertEquals(genericTicker.getLast(), new BigDecimal("20.2"));
+    assertEquals(genericTicker.getVolume(), new BigDecimal("148.80193218"));
+    
+  }
+  
+  @Test
+  public void testBTCCentralDepthRequest() throws JsonParseException, JsonMappingException, IOException {
+  
+    // Read in the JSON from the example resources
+    InputStream is = AdaptersTest.class.getResourceAsStream("/Example_DepthData.json");
+    
+    // Use Jackson to parse it
+    ObjectMapper mapper = new ObjectMapper();
+    BTCCentralMarketDepth btcCentralMarketDepth = mapper.readValue(is, BTCCentralMarketDepth.class);
+    
+    OrderBook genericOrderBook = BTCCentralAdapters.adaptMarketDepth(btcCentralMarketDepth, CurrencyPair.BTC_EUR);
+    
+    assertEquals(genericOrderBook.getAsks().get(0).getTradableAmount(), new BigDecimal("0.48762"));
+    assertEquals(genericOrderBook.getAsks().get(0).getLimitPrice(), new BigDecimal("24.48996"));
+    assertEquals(genericOrderBook.getBids().get(0).getTradableAmount(), new BigDecimal("0.77372456"));
+    assertEquals(genericOrderBook.getBids().get(0).getLimitPrice(), new BigDecimal("24.05"));
+    
+  }
+  
+  @Test
+  public void testBTCCentralTradesRequest() throws JsonParseException, JsonMappingException, IOException {
+  
+    // Read in the JSON from the example resources
+    InputStream is = AdaptersTest.class.getResourceAsStream("/Example_TradesData.json");
+    
+    // Use Jackson to parse it
+    ObjectMapper mapper = new ObjectMapper();
+    BTCCentralTrade[] btcCentralTrades = mapper.readValue(is, BTCCentralTrade[].class);
+    
+    Trades genericTrades = BTCCentralAdapters.adaptTrade(btcCentralTrades, CurrencyPair.BTC_EUR);
+    
+    assertEquals(genericTrades.getTrades().get(0).getPrice(), new BigDecimal("5.0"));
+    assertEquals(genericTrades.getTrades().get(0).getTradableAmount(), new BigDecimal("980.0"));
+    
+  }
+  
+}
