7dd2d0704c44ad183a1afa8ccc512ee5c23cfb0d
Rafał Krupiński <rafal@krupinski.info.pl>
2014-07-04 00:46:37 +0200

    Add PLN to BitcurexFunds

--- xchange-bitcurex/src/main/java/com/xeiam/xchange/bitcurex/dto/marketdata/BitcurexFunds.java
+++ xchange-bitcurex/src/main/java/com/xeiam/xchange/bitcurex/dto/marketdata/BitcurexFunds.java
@@ -6,22 +6,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BitcurexFunds {
 	private final BigDecimal eurs;
+    private final BigDecimal plns;
 	private final BigDecimal btcs;
 	private final String address;
 	private final String error;
 	
-	  public BitcurexFunds( @JsonProperty("eurs") BigDecimal eurs, 
-			                @JsonProperty("btcs") BigDecimal btcs, 
+	  public BitcurexFunds( @JsonProperty("eurs") BigDecimal eurs,
+                            @JsonProperty("plns") BigDecimal plns,
+			                @JsonProperty("btcs") BigDecimal btcs,
 			                @JsonProperty("address") String address,
-			                @JsonProperty("error") String error ) 
+			                @JsonProperty("error") String error )
 	  {
 		    this.eurs = eurs;
+            this.plns = plns;
 		    this.btcs = btcs;
 		    this.address = address;
 		    this.error = error;
 	  }
 
 	  public BigDecimal getEurs() { return eurs; }
+      public BigDecimal getPlns() { return plns; }
 	  public BigDecimal getBtcs() { return btcs; }
 	  public String getAddress() { return address; }
 	  public String getError() { return error; }
