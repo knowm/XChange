48cbb492a09c2a68e1129ea27f2883642f0ec99f
Zach Holmes <shortkeys@gmail.com>
2014-07-29 20:57:27 -0400

    Prepared Poloniex for Pull Request
    Added license, fixed inconsistencies, commented out attempt at
    authenticated methods

--- xchange-poloniex/src/main/java/com/xeiam/xchange/poloniex/service/polling/PoloniexAccountService.java
+++ xchange-poloniex/src/main/java/com/xeiam/xchange/poloniex/service/polling/PoloniexAccountService.java
@@ -1,17 +1,36 @@
+/**
+ * The MIT License
+ * Copyright (c) 2012 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.xeiam.xchange.poloniex.service.polling;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
 
 import com.xeiam.xchange.ExchangeException;
 import com.xeiam.xchange.ExchangeSpecification;
 import com.xeiam.xchange.NotAvailableFromExchangeException;
 import com.xeiam.xchange.NotYetImplementedForExchangeException;
 import com.xeiam.xchange.dto.account.AccountInfo;
-import com.xeiam.xchange.dto.trade.Wallet;
 import com.xeiam.xchange.service.polling.PollingAccountService;
-
 
 /**
  * @author Zach Holmes
@@ -22,15 +41,17 @@ public class PoloniexAccountService extends PoloniexAccountServiceRaw implements
   public PoloniexAccountService(ExchangeSpecification exchangeSpecification) {
 
     super(exchangeSpecification);
-    // TODO Auto-generated constructor stub
   }
 
   @Override
   public AccountInfo getAccountInfo() throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
 
-    List<Wallet> wallets = getWallets();
-    
-    return new AccountInfo(null, wallets);
+    /*
+     * List<Wallet> wallets = getWallets();
+     * return new AccountInfo(null, wallets);
+     */
+
+    throw new NotYetImplementedForExchangeException();
   }
 
   @Override
--- xchange-poloniex/src/main/java/com/xeiam/xchange/poloniex/service/polling/PoloniexAccountServiceRaw.java
+++ xchange-poloniex/src/main/java/com/xeiam/xchange/poloniex/service/polling/PoloniexAccountServiceRaw.java
@@ -1,10 +1,28 @@
+/**
+ * The MIT License
+ * Copyright (c) 2012 Xeiam LLC http://xeiam.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.xeiam.xchange.poloniex.service.polling;
 
-import java.util.List;
-
 import com.xeiam.xchange.ExchangeSpecification;
-import com.xeiam.xchange.dto.trade.Wallet;
-import com.xeiam.xchange.poloniex.PoloniexAdapters;
 import com.xeiam.xchange.poloniex.PoloniexAuthenticated;
 
 /**
@@ -18,11 +36,11 @@ public class PoloniexAccountServiceRaw extends PoloniexBasePollingService<Poloni
     super(PoloniexAuthenticated.class, exchangeSpecification);
   }
 
-  public List<Wallet> getWallets() {
-
-    List<Wallet> wallets = PoloniexAdapters.adaptPoloniexBalances(poloniex.getBalances(apiKey, signatureCreator, "returnBalances", String.valueOf(nextNonce())));
-    // System.out.println("wallets: " + wallets);
-    return wallets;
-  }
+  /*
+   * public List<Wallet> getWallets() {
+   * List<Wallet> wallets = PoloniexAdapters.adaptPoloniexBalances(poloniex.getBalances(apiKey, signatureCreator, "returnBalances", String.valueOf(nextNonce())));
+   * return wallets;
+   * }
+   */
 
 }
