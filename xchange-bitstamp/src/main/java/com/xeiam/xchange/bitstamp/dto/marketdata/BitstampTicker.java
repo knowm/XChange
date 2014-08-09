d8707d1da4e17b9b8da0af896f827d80ca7c3eda
Gregor Gololicic <gregor@gololicic.si>
2014-08-09 18:52:43 +0200

    added vwap paramter to the Ticker

--- xchange-bitstamp/src/main/java/com/xeiam/xchange/bitstamp/dto/marketdata/BitstampTicker.java
+++ xchange-bitstamp/src/main/java/com/xeiam/xchange/bitstamp/dto/marketdata/BitstampTicker.java
@@ -13,6 +13,7 @@ public final class BitstampTicker {
   private final BigDecimal high;
   private final BigDecimal low;
   private final BigDecimal volume;
+  private final BigDecimal vwap;
   private final BigDecimal bid;
   private final BigDecimal ask;
   private final long timestamp;
@@ -24,20 +25,21 @@ public final class BitstampTicker {
    * @param high
    * @param low
    * @param volume
+   * @param vwap
    * @param bid
    * @param ask
    */
   public BitstampTicker(@JsonProperty("last") BigDecimal last, @JsonProperty("high") BigDecimal high, @JsonProperty("low") BigDecimal low, @JsonProperty("volume") BigDecimal volume,
-      @JsonProperty("bid") BigDecimal bid, @JsonProperty("ask") BigDecimal ask, @JsonProperty("timestamp") long timestamp) {
+                        @JsonProperty("vwap") BigDecimal vwap, @JsonProperty("bid") BigDecimal bid, @JsonProperty("ask") BigDecimal ask, @JsonProperty("timestamp") long timestamp) {
 
     this.last = last;
     this.high = high;
     this.low = low;
     this.volume = volume;
+    this.vwap = vwap;
     this.bid = bid;
     this.ask = ask;
     this.timestamp = timestamp;
-
   }
 
   public BigDecimal getLast() {
@@ -60,6 +62,11 @@ public final class BitstampTicker {
     return volume;
   }
 
+  public BigDecimal getVwap() {
+
+    return vwap;
+  }
+
   public BigDecimal getBid() {
 
     return bid;
@@ -78,7 +85,7 @@ public final class BitstampTicker {
   @Override
   public String toString() {
 
-    return "BitstampTicker [last=" + last + ", high=" + high + ", low=" + low + ", volume=" + volume + ", bid=" + bid + ", ask=" + ask + ", timestamp=" + timestamp + "]";
+    return "BitstampTicker [last=" + last + ", high=" + high + ", low=" + low + ", volume=" + volume + ", vwap=" + vwap +", bid=" + bid + ", ask=" + ask + ", timestamp=" + timestamp + "]";
   }
 
 }
