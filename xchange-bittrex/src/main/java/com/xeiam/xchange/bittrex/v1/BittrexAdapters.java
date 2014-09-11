f493d8a6c91295a5616a40a698ec40d6c7472020
Zach Holmes <shortkeys@gmail.com>
2014-09-10 23:51:57 -0400

    [Bittrex] Fixed trade history limit price

--- xchange-bittrex/src/main/java/com/xeiam/xchange/bittrex/v1/BittrexAdapters.java
+++ xchange-bittrex/src/main/java/com/xeiam/xchange/bittrex/v1/BittrexAdapters.java
@@ -154,7 +154,7 @@ public final class BittrexAdapters {
 
     OrderType orderType = trade.getOrderType().equalsIgnoreCase("LIMIT_BUY") ? OrderType.BID : OrderType.ASK;
     BigDecimal amount = trade.getQuantity().subtract(trade.getQuantityRemaining());
-    BigDecimal price = trade.getPrice();
+    BigDecimal price = trade.getLimit();
     Date date = BittrexUtils.toDate(trade.getTimeStamp());
     String tradeId = String.valueOf(trade.getOrderUuid());
 
