cc84a5e6e41e5abe65a4bfaaf3b7226962ae229f
Zach Holmes <shortkeys@gmail.com>
2014-09-23 17:09:36 -0400

    [BitVC] Fix account info adapter
    Loan LTC was not being returned, so adaptAccountInfo failed when it
    tried to subtract a null value. I added a sweep that checks every
    balance to see if it's null. If a balance is null, it sets it to
    BigDecimal.ZERO.

--- xchange-bitvc/src/main/java/com/xeiam/xchange/bitvc/dto/account/BitVcAccountInfo.java
+++ xchange-bitvc/src/main/java/com/xeiam/xchange/bitvc/dto/account/BitVcAccountInfo.java
@@ -6,19 +6,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class BitVcAccountInfo {
 
-  private final BigDecimal availableCny;
-  private final BigDecimal availableBtc;
-  private final BigDecimal availableLtc;
-  private final BigDecimal frozenCny;
-  private final BigDecimal frozenBtc;
-  private final BigDecimal frozenLtc;
-  private final BigDecimal loanCny;
-  private final BigDecimal loanBtc;
-  private final BigDecimal loanLtc;
+  private BigDecimal availableCny;
+  private BigDecimal availableBtc;
+  private BigDecimal availableLtc;
+  private BigDecimal frozenCny;
+  private BigDecimal frozenBtc;
+  private BigDecimal frozenLtc;
+  private BigDecimal loanCny;
+  private BigDecimal loanBtc;
+  private BigDecimal loanLtc;
 
-  public BitVcAccountInfo(@JsonProperty("available_cny") final BigDecimal availableCnyDisplay, @JsonProperty("available_btc") final BigDecimal availableBtcDisplay,
-      @JsonProperty("available_ltc") final BigDecimal availableLtcDisplay, @JsonProperty("frozen_cny") final BigDecimal frozenCnyDisplay,
-      @JsonProperty("frozen_btc") final BigDecimal frozenBtcDisplay, @JsonProperty("frozen_ltc") final BigDecimal frozenLtcDisplay, @JsonProperty("loan_cny") final BigDecimal loanCnyDisplay,
+  public BitVcAccountInfo(@JsonProperty("available_cny") final BigDecimal availableCnyDisplay,
+      @JsonProperty("available_btc") final BigDecimal availableBtcDisplay, @JsonProperty("available_ltc") final BigDecimal availableLtcDisplay,
+      @JsonProperty("frozen_cny") final BigDecimal frozenCnyDisplay, @JsonProperty("frozen_btc") final BigDecimal frozenBtcDisplay,
+      @JsonProperty("frozen_ltc") final BigDecimal frozenLtcDisplay, @JsonProperty("loan_cny") final BigDecimal loanCnyDisplay,
       @JsonProperty("loan_btc") final BigDecimal loanBtcDisplay, @JsonProperty("loan_ltc") final BigDecimal loanLtcDisplay) {
 
     this.availableCny = availableCnyDisplay;
@@ -32,49 +33,102 @@ public class BitVcAccountInfo {
     this.loanLtc = loanLtcDisplay;
   }
 
-  public BigDecimal getAvailableCnyDisplay() {
+  public BigDecimal getAvailableCny() {
 
     return availableCny;
   }
 
-  public BigDecimal getAvailableBtcDisplay() {
+  public void setAvailableCny(BigDecimal availableCny) {
+
+    this.availableCny = availableCny;
+  }
+
+  public BigDecimal getAvailableBtc() {
 
     return availableBtc;
   }
 
-  public BigDecimal getAvailableLtcDisplay() {
+  public void setAvailableBtc(BigDecimal availableBtc) {
+
+    this.availableBtc = availableBtc;
+  }
+
+  public BigDecimal getAvailableLtc() {
 
     return availableLtc;
   }
 
-  public BigDecimal getFrozenCnyDisplay() {
+  public void setAvailableLtc(BigDecimal availableLtc) {
+
+    this.availableLtc = availableLtc;
+  }
+
+  public BigDecimal getFrozenCny() {
 
     return frozenCny;
   }
 
-  public BigDecimal getFrozenBtcDisplay() {
+  public void setFrozenCny(BigDecimal frozenCny) {
+
+    this.frozenCny = frozenCny;
+  }
+
+  public BigDecimal getFrozenBtc() {
 
     return frozenBtc;
   }
 
-  public BigDecimal getFrozenLtcDisplay() {
+  public void setFrozenBtc(BigDecimal frozenBtc) {
+
+    this.frozenBtc = frozenBtc;
+  }
+
+  public BigDecimal getFrozenLtc() {
 
     return frozenLtc;
   }
 
-  public BigDecimal getLoanCnyDisplay() {
+  public void setFrozenLtc(BigDecimal frozenLtc) {
+
+    this.frozenLtc = frozenLtc;
+  }
+
+  public BigDecimal getLoanCny() {
 
     return loanCny;
   }
 
-  public BigDecimal getLoanBtcDisplay() {
+  public void setLoanCny(BigDecimal loanCny) {
+
+    this.loanCny = loanCny;
+  }
+
+  public BigDecimal getLoanBtc() {
 
     return loanBtc;
   }
 
-  public BigDecimal getLoanLtcDisplay() {
+  public void setLoanBtc(BigDecimal loanBtc) {
+
+    this.loanBtc = loanBtc;
+  }
+
+  public BigDecimal getLoanLtc() {
 
     return loanLtc;
+  }
+
+  public void setLoanLtc(BigDecimal loanLtc) {
+
+    this.loanLtc = loanLtc;
+  }
+
+  @Override
+  public String toString() {
+
+    return "BitVcAccountInfo [availableCny=" + availableCny + ", availableBtc=" + availableBtc + ", availableLtc=" + availableLtc + ", frozenCny="
+        + frozenCny + ", frozenBtc=" + frozenBtc + ", frozenLtc=" + frozenLtc + ", loanCny=" + loanCny + ", loanBtc=" + loanBtc + ", loanLtc="
+        + loanLtc + "]";
   }
 
 }
