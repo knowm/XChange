9a87be98a428cba3aef318d24ea3953f14c3a793
Zach Holmes <shortkeys@gmail.com>
2014-09-23 20:20:59 -0400

    Improve HitBTC
    Added HitbtcException to handle error message responses
    
    Fixed trade adapter for cases where the trade id is null
    
    Amended digestParams to remove need for DatatypeConverter

--- xchange-hitbtc/src/main/java/com/xeiam/xchange/hitbtc/service/HitbtcHmacDigest.java
+++ xchange-hitbtc/src/main/java/com/xeiam/xchange/hitbtc/service/HitbtcHmacDigest.java
@@ -13,6 +13,7 @@ import si.mazi.rescu.RestInvocation;
 
 public class HitbtcHmacDigest implements ParamsDigest {
 
+  private static final char[] hexArray = "0123456789ABCDEF".toCharArray();
   private static final String HMAC_SHA_512 = "HmacSHA512";
   private final Mac mac;
 
@@ -49,6 +50,17 @@ public class HitbtcHmacDigest implements ParamsDigest {
 
     mac.update(message.getBytes());
 
-    return DatatypeConverter.printHexBinary(mac.doFinal()).toLowerCase();
+    return bytesToHex(mac.doFinal()).toLowerCase();
+  }
+
+  private static String bytesToHex(byte[] bytes) {
+
+    char[] hexChars = new char[bytes.length * 2];
+    for (int j = 0; j < bytes.length; j++) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = hexArray[v >>> 4];
+      hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    }
+    return new String(hexChars);
   }
 }
