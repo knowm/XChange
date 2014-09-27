5ed8d275fba91878a0a05fb1e9289b074056513d
Zach Holmes <shortkeys@gmail.com>
2014-09-27 13:50:39 -0400

    BitVC improvements
    Return BigDecimal.ZERO instead of null if balance is not received to
    avoid errors in adaptAccountInfo
    
    Added error handling to getAccountInfo

--- xchange-bitvc/src/main/java/com/xeiam/xchange/bitvc/dto/account/BitVcAccountInfo.java
+++ xchange-bitvc/src/main/java/com/xeiam/xchange/bitvc/dto/account/BitVcAccountInfo.java
@@ -15,11 +15,12 @@ public class BitVcAccountInfo {
   private final BigDecimal loanCny;
   private final BigDecimal loanBtc;
   private final BigDecimal loanLtc;
+  private final String message;
 
   public BitVcAccountInfo(@JsonProperty("available_cny") final BigDecimal availableCnyDisplay, @JsonProperty("available_btc") final BigDecimal availableBtcDisplay,
       @JsonProperty("available_ltc") final BigDecimal availableLtcDisplay, @JsonProperty("frozen_cny") final BigDecimal frozenCnyDisplay,
       @JsonProperty("frozen_btc") final BigDecimal frozenBtcDisplay, @JsonProperty("frozen_ltc") final BigDecimal frozenLtcDisplay, @JsonProperty("loan_cny") final BigDecimal loanCnyDisplay,
-      @JsonProperty("loan_btc") final BigDecimal loanBtcDisplay, @JsonProperty("loan_ltc") final BigDecimal loanLtcDisplay) {
+      @JsonProperty("loan_btc") final BigDecimal loanBtcDisplay, @JsonProperty("loan_ltc") final BigDecimal loanLtcDisplay, @JsonProperty("message") final String message) {
 
     this.availableCny = availableCnyDisplay;
     this.availableBtc = availableBtcDisplay;
@@ -30,51 +31,57 @@ public class BitVcAccountInfo {
     this.loanCny = loanCnyDisplay;
     this.loanBtc = loanBtcDisplay;
     this.loanLtc = loanLtcDisplay;
+    this.message = message;
   }
 
   public BigDecimal getAvailableCnyDisplay() {
 
-    return availableCny;
+    return availableCny == null ? BigDecimal.ZERO : availableCny;
   }
 
   public BigDecimal getAvailableBtcDisplay() {
 
-    return availableBtc;
+    return availableBtc == null ? BigDecimal.ZERO : availableBtc;
   }
 
   public BigDecimal getAvailableLtcDisplay() {
 
-    return availableLtc;
+    return availableLtc == null ? BigDecimal.ZERO : availableLtc;
   }
 
   public BigDecimal getFrozenCnyDisplay() {
 
-    return frozenCny;
+    return frozenCny == null ? BigDecimal.ZERO : frozenCny;
   }
 
   public BigDecimal getFrozenBtcDisplay() {
 
-    return frozenBtc;
+    return frozenBtc == null ? BigDecimal.ZERO : frozenBtc;
   }
 
   public BigDecimal getFrozenLtcDisplay() {
 
-    return frozenLtc;
+    return frozenLtc == null ? BigDecimal.ZERO : frozenLtc;
   }
 
   public BigDecimal getLoanCnyDisplay() {
 
-    return loanCny;
+    return loanCny == null ? BigDecimal.ZERO : loanCny;
   }
 
   public BigDecimal getLoanBtcDisplay() {
 
-    return loanBtc;
+    return loanBtc == null ? BigDecimal.ZERO : loanBtc;
   }
 
   public BigDecimal getLoanLtcDisplay() {
 
-    return loanLtc;
+    return loanLtc == null ? BigDecimal.ZERO : loanLtc;
+  }
+
+  public String getMessage() {
+
+    return message;
   }
 
 }
