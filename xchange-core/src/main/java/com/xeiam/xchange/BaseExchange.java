c1449df3df754a9c19d955037ae569f0c4f4fe2d
JH <jonathan.heusser@gmail.com>
2014-10-01 18:49:53 +0100

    Merge of existing and default specs when instantiating exchange

--- xchange-core/src/main/java/com/xeiam/xchange/BaseExchange.java
+++ xchange-core/src/main/java/com/xeiam/xchange/BaseExchange.java
@@ -1,5 +1,7 @@
 package com.xeiam.xchange;
 
+import java.util.Map;
+
 import com.xeiam.xchange.service.polling.PollingAccountService;
 import com.xeiam.xchange.service.polling.PollingMarketDataService;
 import com.xeiam.xchange.service.polling.PollingTradeService;
@@ -49,9 +51,13 @@ public abstract class BaseExchange implements Exchange {
       if (exchangeSpecification.getPlainTextUri() == null) {
         exchangeSpecification.setPlainTextUri(defaultSpecification.getPlainTextUri());
       }
-      if (exchangeSpecification.getExchangeSpecificParameters() == null || exchangeSpecification.getExchangeSpecificParameters().size() <= 0) {
-        exchangeSpecification.setExchangeSpecificParameters(defaultSpecification.getExchangeSpecificParameters());
+      // add default value unless it is overriden by current spec
+      for(Map.Entry<String, Object> entry : defaultSpecification.getExchangeSpecificParameters().entrySet()) {
+        if(exchangeSpecification.getExchangeSpecificParametersItem(entry.getKey()) == null) {
+          exchangeSpecification.setExchangeSpecificParametersItem(entry.getKey(), entry.getValue());
+        }
       }
+
       this.exchangeSpecification = exchangeSpecification;
     }
 
