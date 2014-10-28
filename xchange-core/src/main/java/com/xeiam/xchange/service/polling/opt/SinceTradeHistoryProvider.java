363c57c9349ba2c25717db4a07c6cb66d8b505b9
Rafał Krupiński <rafal@krupinski.info.pl>
2014-10-28 15:24:32 +0100

    Introduce parametrized getTradeService

---
+++ xchange-core/src/main/java/com/xeiam/xchange/service/polling/opt/SinceTradeHistoryProvider.java
@@ -0,0 +1,15 @@
+package com.xeiam.xchange.service.polling.opt;
+
+import com.xeiam.xchange.ExchangeException;
+import com.xeiam.xchange.dto.trade.UserTrade;
+import com.xeiam.xchange.dto.trade.UserTrades;
+
+import java.io.IOException;
+
+/**
+ * Optional interface implemented by clients that support getting trade history limited to some number of entries
+ */
+public interface SinceTradeHistoryProvider {
+
+  public UserTrades getTradeHistory(UserTrade last) throws ExchangeException, IOException;
+}
