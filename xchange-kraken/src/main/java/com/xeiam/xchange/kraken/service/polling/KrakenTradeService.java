7223058c91772f7ee456da307c298b69dfada006
Rafał Krupiński <rkrupinski+github@gmail.com>
2014-11-07 18:24:53 +0100

    [Kraken] Implement parametrized TradeService.getTradeHistory

--- xchange-kraken/src/main/java/com/xeiam/xchange/kraken/service/polling/KrakenTradeService.java
+++ xchange-kraken/src/main/java/com/xeiam/xchange/kraken/service/polling/KrakenTradeService.java
@@ -1,7 +1,9 @@
 package com.xeiam.xchange.kraken.service.polling;
 
 import java.io.IOException;
+import java.util.Date;
 
+import com.xeiam.xchange.service.polling.opt.TradeHistoryTimeSpan;
 import si.mazi.rescu.SynchronizedValueFactory;
 
 import com.xeiam.xchange.ExchangeSpecification;
@@ -11,6 +13,8 @@ import com.xeiam.xchange.dto.trade.OpenOrders;
 import com.xeiam.xchange.dto.trade.UserTrades;
 import com.xeiam.xchange.kraken.KrakenAdapters;
 import com.xeiam.xchange.service.polling.PollingTradeService;
+
+import javax.annotation.Nullable;
 
 public class KrakenTradeService extends KrakenTradeServiceRaw implements PollingTradeService {
 
@@ -46,12 +50,54 @@ public class KrakenTradeService extends KrakenTradeServiceRaw implements Polling
   @Override
   public UserTrades getTradeHistory(Object... args) throws IOException {
 
-    return KrakenAdapters.adaptTradesHistory(super.getKrakenTradeHistory());
+    Long start = null;
+    Long end = null;
+
+    if (args.length == 1) {
+      Object arg0 = args[0];
+      if (arg0 instanceof TradeHistoryParam) {
+        TradeHistoryParam param = (TradeHistoryParam) arg0;
+        if (param.from != null)
+          start = param.from.getTime();
+        if (param.to != null)
+          end = param.to.getTime();
+      }
+    }
+
+    return KrakenAdapters.adaptTradesHistory(getKrakenTradeHistory(null, false, start, end, null));
   }
 
   @Override
   public Object createTradeHistoryParams() {
-    return null;
+    return new TradeHistoryParam();
   }
 
+  public static class TradeHistoryParam implements TradeHistoryTimeSpan{
+    @Nullable
+    private Date from;
+    @Nullable
+    private Date to;
+
+    public TradeHistoryParam() {
+    }
+
+    public TradeHistoryParam(Date from) {
+      this.from = from;
+    }
+
+    public TradeHistoryParam(Date from, Date to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    @Override
+    public void setToTime(Date to) {
+      this.to = to;
+    }
+
+    @Override
+    public void setFromTime(Date from) {
+      this.from = from;
+    }
+  }
 }
