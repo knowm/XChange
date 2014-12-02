395766f4e0e58a48eab654490b29e114a14c8054
mazi <matija.mazi@gmail.com>
2014-12-02 14:26:13 +0100

    Bitstamp: additional api methods: list unconfirmed deposits, list recent withdrawal requests. Improve exceptions.

---
+++ xchange-bitstamp/src/main/java/com/xeiam/xchange/bitstamp/util/WithdrawalRequestStatusDeserializer.java
@@ -0,0 +1,12 @@
+package com.xeiam.xchange.bitstamp.util;
+
+import com.xeiam.xchange.bitstamp.dto.account.WithdrawalRequest;
+import com.xeiam.xchange.utils.jackson.EnumIntDeserializer;
+
+/**
+ * @author Matija Mazi
+ */
+public class WithdrawalRequestStatusDeserializer extends EnumIntDeserializer<WithdrawalRequest.Status> {
+
+  public WithdrawalRequestStatusDeserializer() { super(WithdrawalRequest.Status.class); }
+}
