81b7545c3d86af6f7458737e945ee11aa49fe940
Felipe Micaroni Lalli <micaroni@gmail.com>
2014-12-11 18:04:01 -0200

    [bitcointoyou] BitcoinToYou full implementation.

---
+++ xchange-bitcointoyou/src/main/java/com/xeiam/xchange/bitcointoyou/service/polling/account/BitcoinToYouAccountService.java
@@ -0,0 +1,51 @@
+package com.xeiam.xchange.bitcointoyou.service.polling.account;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.NotAvailableFromExchangeException;
+import com.xeiam.xchange.bitcointoyou.BitcoinToYouAdapters;
+import com.xeiam.xchange.dto.account.AccountInfo;
+import com.xeiam.xchange.service.polling.PollingAccountService;
+
+/**
+ * @author Matija Mazi
+ * @author Felipe Micaroni Lalli
+ */
+public class BitcoinToYouAccountService extends BitcoinToYouAccountServiceRaw implements PollingAccountService {
+
+  /**
+   * Constructor
+   *
+   * @param exchangeSpecification
+   *          The {@link com.xeiam.xchange.ExchangeSpecification}
+   */
+  public BitcoinToYouAccountService(ExchangeSpecification exchangeSpecification) {
+
+    super(exchangeSpecification);
+  }
+
+  @Override
+  public AccountInfo getAccountInfo() throws IOException {
+
+    return BitcoinToYouAdapters.adaptAccountInfo(getBitcoinToYouBalance(), exchangeSpecification.getUserName());
+  }
+
+  @Override
+  public String withdrawFunds(String currency, BigDecimal amount, String address) throws IOException {
+
+    throw new NotAvailableFromExchangeException();
+  }
+
+  /**
+   * This returns the currently set deposit address. It will not generate a
+   * new address (ie. repeated calls will return the same address).
+   */
+  @Override
+  public String requestDepositAddress(String currency, String... arguments) throws IOException {
+
+    throw new NotAvailableFromExchangeException();
+
+  }
+}
