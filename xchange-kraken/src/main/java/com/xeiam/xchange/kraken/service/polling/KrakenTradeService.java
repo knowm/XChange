951790beca4cb6e3b0ad00750f1b3a6f460a1104
Rafał Krupiński <rkrupinski+github@gmail.com>
2014-12-18 02:37:55 +0100

    [kraken] Migrate MarketMetadataService

--- xchange-kraken/src/main/java/com/xeiam/xchange/kraken/service/polling/KrakenTradeService.java
+++ xchange-kraken/src/main/java/com/xeiam/xchange/kraken/service/polling/KrakenTradeService.java
@@ -5,12 +5,14 @@ import com.xeiam.xchange.ExchangeSpecification;
 import com.xeiam.xchange.NotAvailableFromExchangeException;
 import com.xeiam.xchange.NotYetImplementedForExchangeException;
 import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.marketdata.BaseMarketMetadata;
 import com.xeiam.xchange.dto.marketdata.MarketMetadata;
 import com.xeiam.xchange.dto.trade.LimitOrder;
 import com.xeiam.xchange.dto.trade.MarketOrder;
 import com.xeiam.xchange.dto.trade.OpenOrders;
 import com.xeiam.xchange.dto.trade.UserTrades;
 import com.xeiam.xchange.kraken.KrakenAdapters;
+import com.xeiam.xchange.kraken.dto.marketdata.KrakenAssetPair;
 import com.xeiam.xchange.service.polling.PollingTradeService;
 import com.xeiam.xchange.service.polling.trade.TradeHistoryParamOffset;
 import com.xeiam.xchange.service.polling.trade.TradeHistoryParams;
@@ -19,8 +21,12 @@ import com.xeiam.xchange.service.polling.trade.DefaultTradeHistoryParamsTimeSpan
 import si.mazi.rescu.SynchronizedValueFactory;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+
+import static com.xeiam.xchange.utils.ConfigurationManager.CFG_MGR;
 
 public class KrakenTradeService extends KrakenTradeServiceRaw implements PollingTradeService {
 
@@ -86,7 +92,21 @@ public class KrakenTradeService extends KrakenTradeServiceRaw implements Polling
    * @see com.xeiam.xchange.dto.marketdata.MarketMetadata
    */
   @Override public Map<CurrencyPair, ? extends MarketMetadata> getMarketMetadata() throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
-    throw new NotAvailableFromExchangeException();
+    Map<CurrencyPair, BaseMarketMetadata> result = new HashMap<CurrencyPair, BaseMarketMetadata>();
+
+    Map<String, KrakenAssetPair> assetPairs = getKrakenAssetPairs().getAssetPairMap();
+    for (Map.Entry<String, KrakenAssetPair> e : assetPairs.entrySet()) {
+      String krakenPair = e.getKey();
+      CurrencyPair pair = KrakenAdapters.adaptCurrencyPair(krakenPair);
+
+      KrakenAssetPair assetPair = e.getValue();
+      BigDecimal amountMinimum = CFG_MGR.getBigDecimalProperty(KEY_ORDER_SIZE_MIN_DEFAULT).setScale(assetPair.getVolumeLotScale(), BigDecimal.ROUND_UNNECESSARY);
+      BaseMarketMetadata metadata = new BaseMarketMetadata(amountMinimum, assetPair.getPairScale());
+
+      result.put(pair, metadata);
+    }
+
+    return result;
   }
 
   private static Long getTime(Date date) {
