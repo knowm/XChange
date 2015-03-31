bfc0c349f79ceff7f909d034d5a21f040e5e3941
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2015-03-30 19:35:22 -0700

    Add objects for TransHistory result and return.

---
+++ xchange-btce/src/main/java/com/xeiam/xchange/btce/v3/dto/trade/BTCETransHistoryResult.java
@@ -0,0 +1,20 @@
+package com.xeiam.xchange.btce.v3.dto.trade;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Peter N. Steinmetz
+ *         Date: 3/30/15
+ *         Time: 3:19 PM
+ */
+public class BTCETransHistoryResult {
+
+  private BigDecimal amount;
+  private String currency;
+
+
+  private final Long timestamp = 0L;
+
+
+
+}
---
+++ xchange-btce/src/main/java/com/xeiam/xchange/btce/v3/dto/trade/BTCETransHistoryReturn.java
@@ -0,0 +1,9 @@
+package com.xeiam.xchange.btce.v3.dto.trade;
+
+/**
+ * @author Peter N. Steinmetz
+ *         Date: 3/30/15
+ *         Time: 3:19 PM
+ */
+public class BTCETransHistoryReturn {
+}
