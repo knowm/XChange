2b6af0a308e2ccef9edc3435b1f43884f2720bb6
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2015-04-04 10:57:35 -0700

    Add retrieval of transaction history taking a TransHistoryParams object.

--- xchange-btce/src/main/java/com/xeiam/xchange/btce/v3/service/polling/BTCETradeService.java
+++ xchange-btce/src/main/java/com/xeiam/xchange/btce/v3/service/polling/BTCETradeService.java
@@ -7,10 +7,7 @@ import java.util.Map;
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.btce.v3.BTCEAdapters;
 import com.xeiam.xchange.btce.v3.BTCEAuthenticated;
-import com.xeiam.xchange.btce.v3.dto.trade.BTCECancelOrderResult;
-import com.xeiam.xchange.btce.v3.dto.trade.BTCEOrder;
-import com.xeiam.xchange.btce.v3.dto.trade.BTCEPlaceOrderResult;
-import com.xeiam.xchange.btce.v3.dto.trade.BTCETradeHistoryResult;
+import com.xeiam.xchange.btce.v3.dto.trade.*;
 import com.xeiam.xchange.btce.v3.service.polling.trade.params.BTCETradeHistoryParams;
 import com.xeiam.xchange.btce.v3.service.polling.trade.params.BTCETransHistoryParams;
 import com.xeiam.xchange.currency.CurrencyPair;
@@ -103,7 +100,7 @@ public class BTCETradeService extends BTCETradeServiceRaw implements PollingTrad
       pair = String.format("%s_%s", tradableIdentifier, transactionCurrency).toLowerCase();
     }
     Map<Long, BTCETradeHistoryResult> resultMap = getBTCETradeHistory(null, numberOfTransactions, id, id, BTCEAuthenticated.SortOrder.DESC, null,
-        null, pair);
+       null, pair);
     return BTCEAdapters.adaptTradeHistory(resultMap);
   }
 
@@ -181,8 +178,63 @@ public class BTCETradeService extends BTCETradeServiceRaw implements PollingTrad
 
   @Override
   public com.xeiam.xchange.service.polling.trade.params.TradeHistoryParams createTradeHistoryParams() {
-
     return new BTCETradeHistoryParams();
+  }
+
+  /**
+   * Retrieve TransHistory.
+   *
+   * :TODO: Return could be abstracted in a fashion similar to UserTrades and also used
+   *  in additional service for BitStamp exchange.
+   *
+   * @param params
+   * @return Map of transaction id to BTCETransHistoryResult
+   */
+  public Map<Long, BTCETransHistoryResult> getTransHistory(BTCETransHistoryParams params) throws ExchangeException, IOException {
+
+    Long offset = null;
+    Long count = null;
+    Long startId = null;
+    Long endId = null;
+    BTCEAuthenticated.SortOrder sort = BTCEAuthenticated.SortOrder.DESC;
+    Long startTime = null;
+    Long endTime = null;
+
+    if (params instanceof TradeHistoryParamPaging) {
+      TradeHistoryParamPaging pagingParams = (TradeHistoryParamPaging) params;
+      Integer pageLength = pagingParams.getPageLength();
+      Integer pageNumber = pagingParams.getPageNumber();
+      if (pageNumber == null) {
+        pageNumber = 0;
+      }
+
+      if (pageLength != null) {
+        count = pageLength.longValue();
+        offset = (long) (pageLength * pageNumber);
+      } else {
+        offset = pageNumber.longValue();
+      }
+    }
+
+    if (params instanceof TradeHistoryParamsIdSpan) {
+      TradeHistoryParamsIdSpan idParams = (TradeHistoryParamsIdSpan) params;
+      startId = nullSafeToLong(idParams.getStartId());
+      endId = nullSafeToLong(idParams.getEndId());
+    }
+
+    if (params instanceof TradeHistoryParamsTimeSpan) {
+      TradeHistoryParamsTimeSpan timeParams = (TradeHistoryParamsTimeSpan) params;
+      startTime = nullSafeUnixTime(timeParams.getStartTime());
+      endTime = nullSafeUnixTime(timeParams.getEndTime());
+    }
+
+    if (params instanceof BTCETransHistoryParams) {
+      sort = ((BTCETransHistoryParams) params).getSortOrder();
+    }
+
+    Map<Long, BTCETransHistoryResult> resultMap = getBTCETransHistory(offset, count, startId, endId, sort, startTime, endTime);
+
+    return resultMap;
   }
 
 }
