6adb6826ac5dc9ffea595036892441ea6251a4c5
Ying Zhe Chong <zh3zh3@gmail.com>
2015-04-12 22:14:15 -0700

    Adding Coinbase Exchange

---
+++ xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/CoinbaseExExchange.java
@@ -0,0 +1,42 @@
+package com.xeiam.xchange.coinbaseex;
+
+import com.xeiam.xchange.BaseExchange;
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.coinbaseex.service.polling.CoinbaseExMarketDataService;
+import com.xeiam.xchange.utils.nonce.CurrentTimeNonceFactory;
+import si.mazi.rescu.SynchronizedValueFactory;
+
+/**
+ * Created by Yingzhe on 4/1/2015.
+ */
+public class CoinbaseExExchange extends BaseExchange implements Exchange {
+
+  private SynchronizedValueFactory<Long> nonceFactory = new CurrentTimeNonceFactory();
+
+  @Override
+  public void applySpecification(ExchangeSpecification exchangeSpecification) {
+
+    super.applySpecification(exchangeSpecification);
+
+    this.pollingMarketDataService = new CoinbaseExMarketDataService(this);
+  }
+
+  @Override
+  public ExchangeSpecification getDefaultExchangeSpecification() {
+
+    ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
+    exchangeSpecification.setSslUri("https://api.exchange.coinbase.com");
+    exchangeSpecification.setHost("api.exchange.coinbase.com");
+    exchangeSpecification.setPort(80);
+    exchangeSpecification.setExchangeName("CoinbaseEx");
+    exchangeSpecification.setExchangeDescription("Coinbase Exchange is a Bitcoin exchange recently launched in January 2015");
+    return exchangeSpecification;
+  }
+
+  @Override
+  public SynchronizedValueFactory<Long> getNonceFactory() {
+
+    return nonceFactory;
+  }
+}
---
+++ xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/dto/marketdata/CoinbaseExProductTicker.java
@@ -0,0 +1,59 @@
+package com.xeiam.xchange.coinbaseex.dto.marketdata;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Yingzhe on 4/4/2015.
+ */
+public class CoinbaseExProductTicker {
+
+  private final String tradeId;
+  private final BigDecimal price;
+  private final BigDecimal size;
+  private Date time;
+
+  public CoinbaseExProductTicker(@JsonProperty("trade_id") String tradeId, @JsonProperty("price") BigDecimal price, @JsonProperty("size") BigDecimal size, @JsonProperty("time") String time) {
+
+    this.tradeId = tradeId;
+    this.price = price;
+    this.size = size;
+    try {
+      // Need to explicitly parse the date again as the timestamp from API contains microseconds
+      time = time.substring(0, 23);
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+      dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+      this.time = dateFormat.parse(time);
+    } catch (ParseException e) {
+      this.time = null;
+    }
+  }
+
+  public Date getTime() {
+
+    return time;
+  }
+
+  public String getTradeId() {
+
+    return tradeId;
+  }
+
+  public BigDecimal getPrice() {
+
+    return price;
+  }
+
+  public BigDecimal getSize() {
+
+    return size;
+  }
+
+}
