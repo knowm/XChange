bc5286c3e20a40060a7011db48ff167bfd9ebdbe
Matija Mazi <matija.mazi@gmail.com>
2015-04-28 10:37:34 +0200

    Improve nonce creation. Not using a nonce factory though since the creation is centralized in CointraderRequest anyway.

--- xchange-cointrader/src/main/java/com/xeiam/xchange/cointrader/dto/CointraderRequest.java
+++ xchange-cointrader/src/main/java/com/xeiam/xchange/cointrader/dto/CointraderRequest.java
@@ -2,6 +2,7 @@ package com.xeiam.xchange.cointrader.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
@@ -11,10 +12,22 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CointraderRequest {
 
+  @JsonIgnore(true)
+  private static long lastNonceTs = 0;
+
   @JsonProperty private String t;
 
-  // todo: use a nonce factory to avoid clashes
   public CointraderRequest() {
-    t = String.format("%tc", new Date());
+    long nextNonceTs = getCurrentTimestamp();
+    if (lastNonceTs == nextNonceTs) {
+      nextNonceTs++;
+    }
+    lastNonceTs = nextNonceTs;
+    t = String.format("%tc", new Date(lastNonceTs * 1000));
+  }
+
+  /** Current timestamp in seconds */
+  private long getCurrentTimestamp() {
+    return System.currentTimeMillis() / 1000;
   }
 }
