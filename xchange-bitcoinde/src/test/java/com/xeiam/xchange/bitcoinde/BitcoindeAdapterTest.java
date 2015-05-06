0b64ce2b0c1cba591d996456118aaa743160d46e
Matthew Downey <matthewdowney20@gmail.com>
2015-05-05 18:48:51 -0700

    Fixed BitcoindeAdapterTest to work with sorted OrderBook

--- xchange-bitcoinde/src/test/java/com/xeiam/xchange/bitcoinde/BitcoindeAdapterTest.java
+++ xchange-bitcoinde/src/test/java/com/xeiam/xchange/bitcoinde/BitcoindeAdapterTest.java
@@ -36,10 +36,12 @@ public class BitcoindeAdapterTest {
     // Create a generic OrderBook object from a Bitcoinde specific OrderBook
     OrderBook orderBook = BitcoindeAdapters.adaptOrderBook(bitcoindeOrderBook, CurrencyPair.BTC_EUR);
 
+System.out.println(orderBook.getBids().get(0).getLimitPrice().toString());
+System.out.println(orderBook.getBids().get(0).getTradableAmount());
     // verify all fields are filled correctly
-    assertThat(orderBook.getBids().get(0).getLimitPrice().toString()).isEqualTo("200");
+    assertThat(orderBook.getBids().get(0).getLimitPrice().toString()).isEqualTo("222.5");
     assertThat(orderBook.getBids().get(0).getType()).isEqualTo(OrderType.BID);
-    assertThat(orderBook.getBids().get(0).getTradableAmount()).isEqualTo(new BigDecimal("10"));
+    assertThat(orderBook.getBids().get(0).getTradableAmount()).isEqualTo(new BigDecimal("0.35"));
     assertThat(orderBook.getBids().get(0).getCurrencyPair()).isEqualTo(CurrencyPair.BTC_EUR);
   }
 
