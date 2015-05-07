f744a23209b17a5340c4e1e5f426beec35903743
Jonathan Heusser <jonathan.heusser@gmail.com>
2015-05-07 18:45:44 +0100

    BitVcFutures market data and raw services

---
+++ xchange-huobi/src/main/java/com/xeiam/xchange/huobi/service/polling/BitVcFuturesMarketDataService.java
@@ -0,0 +1,45 @@
+package com.xeiam.xchange.huobi.service.polling;
+
+import java.io.IOException;
+
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.marketdata.OrderBook;
+import com.xeiam.xchange.dto.marketdata.Ticker;
+import com.xeiam.xchange.dto.marketdata.Trades;
+import com.xeiam.xchange.huobi.BitVcFuturesAdapter;
+import com.xeiam.xchange.huobi.FuturesContract;
+import com.xeiam.xchange.service.polling.marketdata.PollingMarketDataService;
+
+public class BitVcFuturesMarketDataService extends BitVcFuturesMarketDataServiceRaw implements PollingMarketDataService {
+
+  /**
+   * Constructor
+   *
+   * @param exchange
+   * @param contract 
+   */
+  public BitVcFuturesMarketDataService(Exchange exchange, FuturesContract contract) {
+
+    super(exchange, contract);
+  }
+
+  @Override
+  public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
+
+    return BitVcFuturesAdapter.adaptTicker(getBitVcTicker(currencyPair.baseSymbol.toLowerCase()), currencyPair);
+  }
+
+  @Override
+  public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
+
+    return BitVcFuturesAdapter.adaptOrderBook(getBitVcDepth(currencyPair.baseSymbol.toLowerCase()), currencyPair);
+  }
+
+  @Override
+  public Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
+
+    return BitVcFuturesAdapter.adaptTrades(getBitVcTrades(currencyPair.baseSymbol.toLowerCase()), currencyPair);
+  }
+
+}
