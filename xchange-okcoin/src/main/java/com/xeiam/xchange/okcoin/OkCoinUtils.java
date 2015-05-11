e544ec6b2d549c504497a5b8e9555f227c618066
Jonathan Heusser <jonathan.heusser@gmail.com>
2015-05-11 09:29:12 +0100

    Extract futures error codes for OkCoin

--- xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/OkCoinUtils.java
+++ xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/OkCoinUtils.java
@@ -40,6 +40,65 @@ public class OkCoinUtils {
       return "Order price differs from current market price too much";
     case (10216):
       return "Non-public API";
+
+    /** Futures error codes */
+    case(20001):    
+      return "User does not exist";
+    case(20002):    
+      return "Account frozen";
+    case(20003):    
+      return "Account frozen due to liquidation";
+    case(20004):    
+      return "Futures account frozen";
+    case(20005):    
+      return "User futures account does not exist";
+    case(20006):    
+      return "Required field missing";
+    case(20007):    
+      return "Illegal parameter";
+    case(20008):    
+      return "Futures account balance is too low";
+    case(20009):    
+      return "Future contract status error";
+    case(20010):    
+      return "Risk rate ratio does not exist";
+    case(20011):    
+      return "Risk rate higher than 90% before opening position";
+    case(20012):    
+      return "Risk rate higher than 90% after opening position";
+    case(20013):    
+      return "Temporally no counter party price";
+    case(20014):    
+      return "System error";
+    case(20015):    
+      return "Order does not exist";
+    case(20016):    
+      return "Close amount bigger than your open positions";
+    case(20017):    
+      return "Not authorized/illegal operation";
+    case(20018):    
+      return "Order price differ more than 5% from the price in the last minute";
+    case(20019):    
+      return "IP restricted from accessing the resource";
+    case(20020):    
+      return "secretKey does not exist";
+    case(20021):    
+      return "Index information does not exist";
+    case(20022):    
+      return "Wrong API interface (Cross margin mode shall call cross margin API, fixed margin mode shall call fixed margin API)";
+    case(20023):    
+      return "Account in fixed-margin mode";
+    case(20024):    
+      return "Signature does not match";
+    case(20025):    
+      return "Leverage rate error";
+    case(20026):    
+      return "API Permission Error";
+    case(20027):    
+      return "no transaction record";
+    case(20028):    
+      return "no such contract";
+
     default:
       return "Unknown error";
     }
