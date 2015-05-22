24032ef561cf72aad3823a1ea5964890339e5e86
Jonathan Heusser <jonathan.heusser@gmail.com>
2015-05-22 16:27:27 +0100

    Initial commit of coinbase digest

--- xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/service/CoinbaseExDigest.java
+++ xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/service/CoinbaseExDigest.java
@@ -2,18 +2,14 @@ package com.xeiam.xchange.coinbaseex.service;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 
 import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.HeaderParam;
-import javax.xml.bind.DatatypeConverter;
 
 import si.mazi.rescu.RestInvocation;
 import si.mazi.rescu.utils.Base64;
 
 import com.xeiam.xchange.service.BaseParamsDigest;
-import com.xeiam.xchange.utils.DigestUtils;
 
 
 public class CoinbaseExDigest extends BaseParamsDigest {
@@ -22,39 +18,18 @@ public class CoinbaseExDigest extends BaseParamsDigest {
 
 		super(secretKey, HMAC_SHA_256);
 	}
-	
-	public static byte[] hexStringToByteArray(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
-	}
 
 	public static CoinbaseExDigest createInstance(String secretKey) {
 
-		byte[] decode2 = null;
-		byte[] printBase64Binary = null;
-		try {
-			printBase64Binary = DatatypeConverter.parseBase64Binary(secretKey);
-			
-			decode2 = Base64.decode(secretKey);
+		byte[] decodedSecretKey = null;
+		try {			
+			decodedSecretKey = Base64.decode(secretKey);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("Cannot decode secret key");
 		}
-		return secretKey == null ? null : new CoinbaseExDigest(new String(decode2));
-//		return secretKey == null ? null : new CoinbaseExDigest(secretKey);
+
+		return secretKey == null ? null : new CoinbaseExDigest(new String(decodedSecretKey));
 	}
-//	
-//    public static String generateSignature(String secret_key, String timestamp, String method, String endpoint_url, String body) throws NoSuchAlgorithmException, InvalidKeyException, CloneNotSupportedException {
-//        String prehash = timestamp + method.toUpperCase() + endpoint_url + body;
-//        byte[] secretDecoded = Base64.getDecoder().decode(secret_key);
-//
-//     
-//    }
 
 	@Override
 	public String digestParams(RestInvocation restInvocation) {
@@ -63,23 +38,16 @@ public class CoinbaseExDigest extends BaseParamsDigest {
 				restInvocation.getParamValue(HeaderParam.class, "CB-ACCESS-TIMESTAMP").toString() +
 				restInvocation.getHttpMethod() + 
 				"/" + restInvocation.getMethodPath();
-		
+
 		if(restInvocation.getRequestBody() != null) {
 			message += restInvocation.getRequestBody();
 		}
-		
-		message = "1GET/accounts";
 
-		System.out.println("Message: " + message);
+		//		message = "1GET/accounts";
 
 		Mac mac256 = getMac();
 		mac256.update(message.getBytes());
 
-		
-//		return DatatypeConverter.printBase64Binary(mac256.doFinal(message.getBytes()));
-		return Base64.encodeBytes(new BigInteger(mac256.doFinal()).toByteArray());
-//		return Base64.encodeBytes(new BigInteger(1, mac256.doFinal()).toByteArray());
-//		return Base64.encodeBytes((new BigInteger(mac256.doFinal())).toByteArray());
-//		return String.format("%064x", new BigInteger(1, mac256.doFinal()));
+		return Base64.encodeBytes(new BigInteger(1, mac256.doFinal()).toByteArray());
 	}
 }
