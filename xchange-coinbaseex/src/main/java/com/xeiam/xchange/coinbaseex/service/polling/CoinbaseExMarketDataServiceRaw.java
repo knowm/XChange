683d3deb2ea8c6a7a1b1566f96797677b4045c22
Jonathan Heusser <jonathan.heusser@gmail.com>
2015-05-22 16:27:58 +0100

    Organise Imports

--- xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/service/polling/CoinbaseExMarketDataServiceRaw.java
+++ xchange-coinbaseex/src/main/java/com/xeiam/xchange/coinbaseex/service/polling/CoinbaseExMarketDataServiceRaw.java
@@ -1,14 +1,13 @@
 package com.xeiam.xchange.coinbaseex.service.polling;
 
+import java.io.IOException;
+
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.coinbaseex.CoinbaseEx;
-import com.xeiam.xchange.coinbaseex.dto.marketdata.CoinbaseExProduct;
 import com.xeiam.xchange.coinbaseex.dto.marketdata.CoinbaseExProductBook;
 import com.xeiam.xchange.coinbaseex.dto.marketdata.CoinbaseExProductStats;
 import com.xeiam.xchange.coinbaseex.dto.marketdata.CoinbaseExProductTicker;
 import com.xeiam.xchange.currency.CurrencyPair;
-
-import java.io.IOException;
 
 /**
  * Created by Yingzhe on 4/6/2015.
