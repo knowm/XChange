876c98a31174692349941aee6c84173002a7a896
Radu <radu.miron.iulian@gmail.com>
2015-06-17 21:20:40 +0300

    [coinbaseex] move test to Integration class

--- xchange-coinbaseex/src/test/java/com/xeiam/xchange/coinbaseex/CoinbaseExExchangeTest.java
+++ xchange-coinbaseex/src/test/java/com/xeiam/xchange/coinbaseex/CoinbaseExExchangeTest.java
@@ -15,15 +15,4 @@ public class CoinbaseExExchangeTest {
   public void coinbaseShouldBeInstantiatedWithoutAnExceptionWhenUsingDefaultSpecification() {
     ExchangeFactory.INSTANCE.createExchange(CoinbaseExExchange.class.getCanonicalName());
   }
-
-  @Test
-  public void testCoinbaseCurrencies() throws IOException {
-    CoinbaseExExchange exchange = new CoinbaseExExchange();
-    exchange.applySpecification(exchange.getDefaultExchangeSpecification());
-
-    assertThat(exchange.getPollingMarketDataService().getExchangeSymbols()).containsAll(
-        Arrays.asList(new CurrencyPair("BTC/USD"), new CurrencyPair("BTC/GBP"), new CurrencyPair("BTC/EUR"))
-    );
-  }
-
 }
