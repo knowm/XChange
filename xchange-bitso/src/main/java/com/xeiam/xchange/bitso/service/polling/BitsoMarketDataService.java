dfe27cf033f4b1c139a8efc7b96e4dc4b11c889d
Bryan Hernandez <bryan4887@gmail.com>
2015-06-17 19:46:11 -0500

    Added an intelligent retry method in BitsoMarketDataService for when you get an error from polling too fast.
    	modified:   xchange-bitso/src/main/java/com/xeiam/xchange/bitso/service/polling/BitsoMarketDataService.java

--- xchange-bitso/src/main/java/com/xeiam/xchange/bitso/service/polling/BitsoMarketDataService.java
+++ xchange-bitso/src/main/java/com/xeiam/xchange/bitso/service/polling/BitsoMarketDataService.java
@@ -2,8 +2,14 @@ package com.xeiam.xchange.bitso.service.polling;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.bitso.BitsoAdapters;
+import com.xeiam.xchange.bitso.dto.marketdata.BitsoOrderBook;
+import com.xeiam.xchange.bitso.dto.marketdata.BitsoTicker;
+import com.xeiam.xchange.bitso.dto.marketdata.BitsoTransaction;
 import com.xeiam.xchange.currency.CurrencyPair;
 import com.xeiam.xchange.dto.marketdata.OrderBook;
 import com.xeiam.xchange.dto.marketdata.Ticker;
@@ -17,23 +23,93 @@ import com.xeiam.xchange.service.polling.marketdata.PollingMarketDataService;
  * @author Piotr Ładyżyński
  */
 public class BitsoMarketDataService extends BitsoMarketDataServiceRaw implements PollingMarketDataService {
+  
+  private static final Logger logger = LoggerFactory.getLogger(BitsoMarketDataService.class);
+  private final String tooManyRequestsErrorMessage = "{code=200, message=Too many requests} (HTTP status code: 200)"; // could potentially change in the future.
+
+  public interface PollingFunction<T> {
+    public T doPolling(Object...args) throws IOException;
+  }
 
   public BitsoMarketDataService(Exchange exchange) {
     super(exchange);
   }
+  
+  private <T> T retry(PollingFunction<T> pFunc, int maxNumRetries, int waitTimeInSeconds, String expectedErrorMessage) {
+    T obj = null;
+    
+    int numTries = 0;
+    boolean ranOutOfTries = numTries == maxNumRetries;
+    
+    do {
+      try {
+        numTries++;
+        obj = pFunc.doPolling();
+      } catch (Exception e) {
+        String message = e.getMessage();
+        if (message.equals(expectedErrorMessage)) {
+          logger.warn("Received error: {}", message);
+          logger.warn("Waiting {} seconds and trying again.", waitTimeInSeconds);
+          
+          try {
+            Thread.sleep(waitTimeInSeconds * 1000); 
+          } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+          }
+        } else {
+          logger.warn("Returning null.  Received error: {}.", e);
+          break;
+        }
+      }
+      ranOutOfTries = numTries == maxNumRetries;
+    } while (!ranOutOfTries && obj == null);
+    
+    if (ranOutOfTries) {
+      logger.warn("Retried {} times.  Giving up and returning null.", maxNumRetries);
+    }
+    
+    return obj;
+  }
 
   @Override
   public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
-    return BitsoAdapters.adaptTicker(getBitsoTicker(), currencyPair);
+    PollingFunction<BitsoTicker> tickerPollingFunc = new PollingFunction<BitsoTicker>() {
+
+      @Override
+      public BitsoTicker doPolling(Object...args) throws IOException {
+        return getBitsoTicker();
+      }
+      
+    };
+    BitsoTicker ticker = retry(tickerPollingFunc, 20, 5, tooManyRequestsErrorMessage);
+    return BitsoAdapters.adaptTicker(ticker, currencyPair);
   }
 
   @Override
   public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
-    return BitsoAdapters.adaptOrderBook(getBitsoOrderBook(), currencyPair, 1000);
+    PollingFunction<BitsoOrderBook> obPollingFunc = new PollingFunction<BitsoOrderBook>() {
+
+      @Override
+      public BitsoOrderBook doPolling(Object...args) throws IOException {
+        return getBitsoOrderBook();
+      }
+      
+    };
+    BitsoOrderBook bitsoOrderBook = retry(obPollingFunc, 20, 5, tooManyRequestsErrorMessage);
+    return BitsoAdapters.adaptOrderBook(bitsoOrderBook, currencyPair, 1000);
   }
 
   @Override
   public Trades getTrades(CurrencyPair currencyPair, Object... args) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
-    return BitsoAdapters.adaptTrades(getBitsoTransactions(args), currencyPair);
+    PollingFunction<BitsoTransaction[]> transactionPollingFunc = new PollingFunction<BitsoTransaction[]>() {
+
+      @Override
+      public BitsoTransaction[] doPolling(Object...args) throws IOException {
+        return getBitsoTransactions(args);
+      }
+      
+    };
+    BitsoTransaction[] bitsoTransactions = retry(transactionPollingFunc, 20, 5, tooManyRequestsErrorMessage);
+    return BitsoAdapters.adaptTrades(bitsoTransactions, currencyPair);
   }
 }
