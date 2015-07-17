f18461049d39e4061ee34a63c79a031809076b74
Douggie <douggie@melvilleclarke.com>
2015-07-17 18:40:52 +0100

    fixes #1000

--- xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/service/streaming/MonitorTask.java
+++ xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/service/streaming/MonitorTask.java
@@ -1,27 +1,34 @@
 package com.xeiam.xchange.okcoin.service.streaming;
 
 import java.util.TimerTask;
+import java.util.concurrent.RejectedExecutionException;
 
-class MonitorTask extends TimerTask {  
-  private long  startTime = System.currentTimeMillis();
-  private int checkTime = 5000;
+class MonitorTask extends TimerTask {
+  private long startTime = System.currentTimeMillis();
+  private final int checkTime = 5000;
   private WebSocketBase client = null;
-  
-  public void updateTime(){
+
+  public void updateTime() {
     startTime = System.currentTimeMillis();
   }
-  
-  public MonitorTask(WebSocketBase client){
+
+  public MonitorTask(WebSocketBase client) {
     this.client = client;
   }
-  
-  public void run() {  
-    if(System.currentTimeMillis() - startTime > checkTime){
+
+  @Override
+  public void run() {
+    if (System.currentTimeMillis() - startTime > checkTime) {
       client.setStatus(false);
 
       client.reConnect();
-    } 
-    
-    client.sendPing();
-  }  
+    }
+    try {
+      client.sendPing();
+    } catch (RejectedExecutionException reject) {
+      client.setStatus(false);
+
+      client.reConnect();
+    }
+  }
 }
