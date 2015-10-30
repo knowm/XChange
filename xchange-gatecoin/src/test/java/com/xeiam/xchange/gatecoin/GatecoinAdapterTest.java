1de51ca1450984c7ef655a25515f65e29be65223
Matija Mazi <matija.mazi@gmail.com>
2015-10-30 20:00:00 +0100

    Fix tests

--- xchange-gatecoin/src/test/java/com/xeiam/xchange/gatecoin/GatecoinAdapterTest.java
+++ xchange-gatecoin/src/test/java/com/xeiam/xchange/gatecoin/GatecoinAdapterTest.java
@@ -1,19 +1,15 @@
 package com.xeiam.xchange.gatecoin;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.offset;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import com.xeiam.xchange.gatecoin.dto.marketdata.GatecoinTicker;
-
 import com.xeiam.xchange.currency.CurrencyPair;
 import com.xeiam.xchange.dto.Order.OrderType;
 import com.xeiam.xchange.dto.account.AccountInfo;
@@ -21,6 +17,7 @@ import com.xeiam.xchange.dto.marketdata.Ticker;
 import com.xeiam.xchange.dto.marketdata.Trades;
 import com.xeiam.xchange.dto.trade.UserTrades;
 import com.xeiam.xchange.gatecoin.dto.account.Results.GatecoinBalanceResult;
+import com.xeiam.xchange.gatecoin.dto.marketdata.GatecoinTicker;
 import com.xeiam.xchange.gatecoin.dto.marketdata.Results.GatecoinTickerResult;
 import com.xeiam.xchange.gatecoin.dto.marketdata.Results.GatecoinTransactionResult;
 import com.xeiam.xchange.gatecoin.dto.trade.Results.GatecoinTradeHistoryResult;
@@ -46,7 +43,7 @@ public class GatecoinAdapterTest {
     assertThat(accountInfo.getWallets().get(0).getCurrency()).isEqualTo("BTC");
     assertThat(accountInfo.getWallets().get(0).getBalance().toString()).isEqualTo("2.94137538");
     assertThat(accountInfo.getWallets().get(0).getAvailable().toString()).isEqualTo("2.94137538");
-    assertThat(accountInfo.getWallets().get(0).getFrozen().toString()).isEqualTo("0");    
+    assertThat(accountInfo.getWallets().get(0).getFrozen().toString()).isEqualTo("0");
   }
 
   @Test
@@ -62,9 +59,9 @@ public class GatecoinAdapterTest {
     Trades trades = GatecoinAdapters.adaptTrades(transactions.getTransactions(), CurrencyPair.BTC_USD);
     assertThat(trades.getTrades().size()).isEqualTo(2);
     assertThat(trades.getlastID()).isEqualTo(1392484);
-   
+
     assertThat(trades.getTrades().get(0).getId()).isEqualTo("1392449");
-    assertThat(trades.getTrades().get(0).getPrice().toString()).isEqualTo("229.39");  
+    assertThat(trades.getTrades().get(0).getPrice().toString()).isEqualTo("229.39");
     assertThat(trades.getTrades().get(0).getCurrencyPair()).isEqualTo(CurrencyPair.BTC_USD);
   }
 
@@ -79,14 +76,13 @@ public class GatecoinAdapterTest {
     GatecoinTickerResult gatecoinTickerResult = mapper.readValue(is, GatecoinTickerResult.class);
 
     GatecoinTicker[] gatecoinTicker = gatecoinTickerResult.getTicker();
-    
+
     Ticker ticker = GatecoinAdapters.adaptTicker(gatecoinTicker, CurrencyPair.BTC_USD);
 
     assertThat(ticker.getLast().toString()).isEqualTo("241.58");
     assertThat(ticker.getBid().toString()).isEqualTo("241.58");
     assertThat(ticker.getAsk().toString()).isEqualTo("242");
-    assertThat(ticker.getVolume()).isEqualTo(new BigDecimal("158.12"));   
-    
+    assertThat(ticker.getVolume()).isEqualTo(new BigDecimal("158.12"));
   }
 
   @Test
@@ -102,11 +98,8 @@ public class GatecoinAdapterTest {
     UserTrades userTradeHistory = GatecoinAdapters.adaptTradeHistory(gatecointradeHistory);
 
     assertThat(userTradeHistory.getUserTrades().get(0).getId()).isEqualTo("24579");
-    assertThat(userTradeHistory.getUserTrades().get(0).getType()).isEqualTo(OrderType.BID);
+    assertThat(userTradeHistory.getUserTrades().get(0).getType()).isEqualTo(OrderType.ASK);
     assertThat(userTradeHistory.getUserTrades().get(0).getPrice().toString()).isEqualTo("150");
-    assertThat(userTradeHistory.getUserTrades().get(0).getFeeAmount().toString()).isEqualTo("0.0015");
-
-   
-
+    assertThat(userTradeHistory.getUserTrades().get(0).getFeeAmount().doubleValue()).isEqualTo(0.0015 * 0.222 * 150, offset(0.0001));
   }
 }
