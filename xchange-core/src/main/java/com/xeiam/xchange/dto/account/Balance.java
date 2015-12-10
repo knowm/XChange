f3b8c51a8395392700375203b07a75a4852a0163
Baffo32 <baffo32@gmail.com>
2015-12-10 08:50:52 -0500

    [core] add Balance#getAvailableForWithdrawal() #1117

--- xchange-core/src/main/java/com/xeiam/xchange/dto/account/Balance.java
+++ xchange-core/src/main/java/com/xeiam/xchange/dto/account/Balance.java
@@ -144,6 +144,16 @@ public final class Balance implements Comparable<Balance> {
   }
 
   /**
+   * Returns the amount of the <code>currency</code> in this balance that may be withdrawn.  Equal to <code>available - borrowed</code>.
+   *
+   * @return the amount that is available to withdraw.
+   */
+  public BigDecimal getAvailableForWithdrawal() {
+
+    return getAvailable().subtract(getBorrowed());
+  }
+
+  /**
    * Returns the frozen amount of the <code>currency</code> in this balance that is locked in trading.
    *
    * @return the amount that is locked in open orders.
