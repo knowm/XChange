2db7f93b5ca4d51b83473dc5bacdba1fb40b4781
Douggie <douggie@melvilleclarke.com>
2016-03-01 16:02:51 +0100

    fixes #1188

--- xchange-core/src/main/java/com/xeiam/xchange/dto/Order.java
+++ xchange-core/src/main/java/com/xeiam/xchange/dto/Order.java
@@ -21,10 +21,10 @@ public abstract class Order {
      * Selling order (the trader is providing the base currency)
      */
     ASK, /**
-     * Close short position (the trader is providing the base currency)
+     * This is to close a short position when trading crypto currency derivatives such as swaps, futures for CFD's.
      */
     EXIT_ASK, /**
-     * Close long position (the trader is providing the base currency)
+     * This is to close a long position when trading crypto currency derivatives such as swaps, futures for CFD's.
      */
     EXIT_BID
   }
