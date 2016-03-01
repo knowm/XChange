88c7c0e32a96da56c6f437f8e0752901093b0e7e
Douggie <douggie@melvilleclarke.com>
2016-03-01 16:41:41 +0100

    fixes  #1201

--- xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/service/polling/OkCoinFuturesTradeService.java
+++ xchange-okcoin/src/main/java/com/xeiam/xchange/okcoin/service/polling/OkCoinFuturesTradeService.java
@@ -82,16 +82,20 @@ public class OkCoinFuturesTradeService extends OkCoinTradeServiceRaw implements 
 
   @Override
   public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
-
-    long orderId = futuresTrade(OkCoinAdapters.adaptSymbol(marketOrder.getCurrencyPair()), marketOrder.getType() == OrderType.BID ? "1" : "2", "0",
-        marketOrder.getTradableAmount().toPlainString(), futuresContract, 1, leverRate).getOrderId();
-    return String.valueOf(orderId);
+    long orderId;
+    if (marketOrder.getType() == OrderType.BID || marketOrder.getType() == OrderType.ASK) {
+      orderId = futuresTrade(OkCoinAdapters.adaptSymbol(marketOrder.getCurrencyPair()), marketOrder.getType() == OrderType.BID ? "1" : "2", "0",
+          marketOrder.getTradableAmount().toPlainString(), futuresContract, 1, leverRate).getOrderId();
+      return String.valueOf(orderId);
+    } else
+      return liquidateMarketOrder(marketOrder);
   }
 
   /** Liquidate long or short contract (depending on market order order type) using a market order */
   public String liquidateMarketOrder(MarketOrder marketOrder) throws IOException {
 
-    long orderId = futuresTrade(OkCoinAdapters.adaptSymbol(marketOrder.getCurrencyPair()), marketOrder.getType() == OrderType.BID ? "3" : "4", "0",
+    long orderId = futuresTrade(OkCoinAdapters.adaptSymbol(marketOrder.getCurrencyPair()),
+        marketOrder.getType() == OrderType.BID || marketOrder.getType() == OrderType.EXIT_BID ? "3" : "4", "0",
         marketOrder.getTradableAmount().toPlainString(), futuresContract, 1, leverRate).getOrderId();
     return String.valueOf(orderId);
   }
