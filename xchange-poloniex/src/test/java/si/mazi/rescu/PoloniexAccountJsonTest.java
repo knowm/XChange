a4a1b55c7a845693c94e8e39f81a04517c696e5c
Matija Mazi <matija.mazi@gmail.com>
2016-04-07 15:09:48 +0200

    Poloniex: throw exceptions

---
+++ xchange-poloniex/src/test/java/si/mazi/rescu/PoloniexAccountJsonTest.java
@@ -0,0 +1,41 @@
+package si.mazi.rescu;
+
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import si.mazi.rescu.serialization.jackson.JacksonConfigureListener;
+import si.mazi.rescu.serialization.jackson.JacksonMapper;
+import si.mazi.rescu.serialization.jackson.JacksonResponseReader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xeiam.xchange.poloniex.PoloniexAuthenticated;
+import com.xeiam.xchange.poloniex.PoloniexException;
+
+public class PoloniexAccountJsonTest {
+
+  public static final JacksonMapper DUMMY = new JacksonMapper(new JacksonConfigureListener() {
+    @Override public void configureObjectMapper(ObjectMapper objectMapper) { }
+  });
+
+  @Test
+  public void testBalancesError() throws Exception {
+
+    InvocationResult invocationResult = new InvocationResult(
+        "{\"error\":\"Invalid API key\\/secret pair.\"}", 200
+    );
+
+    Method apiMethod = PoloniexAuthenticated.class.getDeclaredMethod(
+        "returnCompleteBalances", String.class, ParamsDigest.class, SynchronizedValueFactory.class);
+    RestMethodMetadata balances = RestMethodMetadata.create(apiMethod, "", "");
+
+    try {
+      new JacksonResponseReader(DUMMY, false).read(invocationResult, balances);
+      Assert.assertTrue("Should have failed.", false);
+    } catch (PoloniexException e) {
+      Assert.assertTrue(e.getMessage().contains("Invalid API key/secret pair."));
+    }
+  }
+
+}
