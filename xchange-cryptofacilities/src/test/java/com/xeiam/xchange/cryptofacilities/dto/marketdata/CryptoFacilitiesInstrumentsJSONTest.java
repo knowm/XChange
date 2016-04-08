70bb39611284b53725b9345552cf3ff7df27f66c
Panchen <Panchen@users.noreply.github.com>
2016-04-08 13:10:17 +0100

    Instruments test TZ fix
    Make test work on a build machine in any TZ.

--- xchange-cryptofacilities/src/test/java/com/xeiam/xchange/cryptofacilities/dto/marketdata/CryptoFacilitiesInstrumentsJSONTest.java
+++ xchange-cryptofacilities/src/test/java/com/xeiam/xchange/cryptofacilities/dto/marketdata/CryptoFacilitiesInstrumentsJSONTest.java
@@ -5,6 +5,8 @@ import static org.fest.assertions.api.Assertions.assertThat;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
@@ -21,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class CryptoFacilitiesInstrumentsJSONTest {
 
 	@Test
-	public void testUnmarshal() throws IOException {
+	public void testUnmarshal() throws IOException, ParseException {
 
 	    // Read in the JSON from the example resources
 	    InputStream is = CryptoFacilitiesInstrumentsJSONTest.class.getResourceAsStream("/marketdata/example-instruments-data.json");
@@ -46,16 +48,9 @@ public class CryptoFacilitiesInstrumentsJSONTest {
 	    assertThat(ct.getType()).isEqualTo("futures");
 	    assertThat(ct.getTickSize()).isEqualTo(new BigDecimal("0.01"));
 	    
-	    Calendar cal = new GregorianCalendar();
 	    //2016-04-29 17:00:00
-	    cal.set(Calendar.YEAR, 2016);
-	    cal.set(Calendar.MONTH, Calendar.APRIL);
-	    cal.set(Calendar.DAY_OF_MONTH, 29);
-	    cal.set(Calendar.HOUR_OF_DAY, 17);
-	    cal.set(Calendar.MINUTE, 0);
-	    cal.set(Calendar.SECOND, 0);
-	    cal.set(Calendar.MILLISECOND, 0);
-	    assertThat(ct.getLastTradingTime()).isEqualTo(cal.getTime());
+	    SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+	    assertThat(ct.getLastTradingTime()).isEqualTo(DATE_FORMAT.parse("2016-04-29T16:00:00.000Z"));
 	    
 	  }
 
