ed0f5220b1d2227b96896fae3479549706ebdd68
Matija Mazi <matija.mazi@gmail.com>
2016-06-02 10:07:59 +0200

    ItBit: handle exceptional returns properly

--- xchange-itbit/src/main/java/org/knowm/xchange/itbit/v1/ItBitAuthenticated.java
+++ xchange-itbit/src/main/java/org/knowm/xchange/itbit/v1/ItBitAuthenticated.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.knowm.xchange.itbit.v1.dto.ItBitException;
 import org.knowm.xchange.itbit.v1.dto.account.ItBitAccountInfoReturn;
 import org.knowm.xchange.itbit.v1.dto.account.ItBitDepositRequest;
 import org.knowm.xchange.itbit.v1.dto.account.ItBitDepositResponse;
@@ -31,56 +32,57 @@ public interface ItBitAuthenticated extends ItBit {
 
   @GET
   @Path("/markets/{ident}{currency}/ticker")
-  ItBitTicker getTicker(@PathParam("ident") String tradeableIdentifier, @PathParam("currency") String currency) throws IOException;
+  ItBitTicker getTicker(@PathParam("ident") String tradeableIdentifier, @PathParam("currency") String currency) throws IOException, ItBitException;
 
   @GET
   @Path("wallets?userId={userId}")
   ItBitAccountInfoReturn[] getInfo(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
-      @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("userId") String userId) throws IOException;
+      @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("userId") String userId) throws IOException, ItBitException;
 
   @GET
   @Path("wallets/{walletId}")
   ItBitAccountInfoReturn getWallet(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
-      @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId) throws IOException;
+      @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId) throws IOException, ItBitException;
 
   @GET
   @Path("wallets/{walletId}/orders")
   ItBitOrder[] getOrders(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @QueryParam("instrument") String instrument,
       @QueryParam("page") String page, @QueryParam("perPage") String perPage, @QueryParam("status") String status,
-      @PathParam("walletId") String walletId) throws IOException;
+      @PathParam("walletId") String walletId) throws IOException, ItBitException;
 
   @GET
   @Path("wallets/{walletId}/orders/{orderId}")
   ItBitOrder getOrder(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId,
-      @PathParam("orderId") String orderId) throws IOException;
+      @PathParam("orderId") String orderId) throws IOException, ItBitException;
 
   @POST
   @Path("wallets/{walletId}/orders")
   @Consumes(MediaType.APPLICATION_JSON)
   ItBitOrder postOrder(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId,
-      ItBitPlaceOrderRequest request) throws IOException;
+      ItBitPlaceOrderRequest request) throws IOException, ItBitException;
 
   /** Returns empty body, return object is always null */
   @DELETE
   @Path("wallets/{walletId}/orders/{orderId}")
   Object cancelOrder(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId,
-      @PathParam("orderId") String orderId) throws IOException;
+      @PathParam("orderId") String orderId) throws IOException, ItBitException;
 
   @POST
   @Path("wallets/{walletId}/cryptocurrency_withdrawals")
   @Consumes(MediaType.APPLICATION_JSON)
   ItBitWithdrawalResponse requestWithdrawal(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId,
-      ItBitWithdrawalRequest request) throws IOException;
+      ItBitWithdrawalRequest request) throws IOException, ItBitException;
 
   @POST
   @Path("wallets/{walletId}/cryptocurrency_deposits")
   @Consumes(MediaType.APPLICATION_JSON)
   ItBitDepositResponse requestDeposit(@HeaderParam("Authorization") ParamsDigest signer, @HeaderParam("X-Auth-Timestamp") long timestamp,
       @HeaderParam("X-Auth-Nonce") SynchronizedValueFactory<Long> valueFactory, @PathParam("walletId") String walletId, ItBitDepositRequest request)
-      throws IOException;
+      throws IOException, ItBitException;
+
 }
