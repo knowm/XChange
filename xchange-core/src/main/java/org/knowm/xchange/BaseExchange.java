knowm/XChange

commit c1073a930e230fe9de7e7c2d64d10554e11621d8

issue 1240 - BTER currency pairs through meta data, refactored JSON structure as well.

xchange-core/src/main/java/org/knowm/xchange/BaseExchange.java
@@ -153,7 +153,7 @@ public abstract class BaseExchange implements Exchange {
 
   @Override
   public List<CurrencyPair> getExchangeSymbols() {
-    return new ArrayList<CurrencyPair>(getExchangeMetaData().getCurrencyPairMetaDataMap().keySet());
+    return new ArrayList<CurrencyPair>(getExchangeMetaData().getCurrencyPairs().keySet());
   }
 
   public String getMetaDataFileName(ExchangeSpecification exchangeSpecification) {

xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenExchange.java
@@ -54,7 +54,7 @@ public class KrakenExchange extends BaseExchange implements Exchange {
     // TODO take all the info gathered above and create a `ExchangeMetaData` object via a new method in `*Adapters` class
     // exchangeMetaData = *Adapters.adaptToExchangeMetaData(blah, blah);
 
-    KrakenUtils.buildExchangeSymbols(getExchangeMetaData().getCurrencyPairMetaDataMap());
+    KrakenUtils.buildExchangeSymbols(getExchangeMetaData().getCurrencyPairs());
 
     super.remoteInit();
   }

commit 1bef6a6768936e19338846733af3261d9805082b

Add basic Kraken remote init. Fixes broken Kraken currency handling introduced in c1073a930e2.

xchange-core/src/main/java/org/knowm/xchange/BaseExchange.java
@@ -126,7 +126,7 @@ public abstract class BaseExchange implements Exchange {
   @Override
   public void remoteInit() throws IOException, ExchangeException {
     logger.info(
-        "No remote initialization implemented for {}. The exchange meta data for this exchange is loaded from a json file containing hard-coded exchange meta-data. This may or may not be OK for you, and you should understand exactly how this works. Each exchange can either 1) rely on the hard-coded json file that comes packaged with XChange's jar, 2) provide your own override json file, 3) properly implement the `remoteInit()` method for the exchange (please submit a pull request so the whole community can benefit) or 4) a combination of hard-coded JSON and remote API calls. For more info see: https://github.com/timmolter/XChange/wiki/Design-Notes",
+        "No remote initialization implemented for {}. The exchange meta data for this exchange is loaded from a json file containing hard-coded exchange meta-data. This may or may not be OK for you, and you should understand exactly how this works. Each exchange can either 1) rely on the hard-coded json file that comes packaged with XChange's jar, 2) provide your own override json file, 3) properly implement the `remoteInit()` method for the exchange (please submit a pull request so the whole community can benefit) or 4) a combination of hard-coded JSON and remote API calls. For more info see: https://github.com/timmolter/XChange/wiki/Design-Notes#exchange-metadata",
         exchangeSpecification.getExchangeName());
   }
 

xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenAdapters.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -20,11 +21,16 @@ import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.dto.marketdata.Trades.TradeSortType;
+import org.knowm.xchange.dto.meta.CurrencyMetaData;
+import org.knowm.xchange.dto.meta.CurrencyPairMetaData;
+import org.knowm.xchange.dto.meta.ExchangeMetaData;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.dto.trade.OpenOrders;
 import org.knowm.xchange.dto.trade.UserTrade;
 import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.kraken.dto.account.KrakenDepositAddress;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenAsset;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenAssetPair;
 import org.knowm.xchange.kraken.dto.marketdata.KrakenDepth;
 import org.knowm.xchange.kraken.dto.marketdata.KrakenPublicOrder;
 import org.knowm.xchange.kraken.dto.marketdata.KrakenPublicTrade;
@@ -236,5 +242,25 @@ public class KrakenAdapters {
 
     List<String> orderIds = orderResponse.getTransactionIds();
     return (orderIds == null || orderIds.isEmpty()) ? "" : orderIds.get(0);
+  }
+
+  public static ExchangeMetaData adaptToExchangeMetaData(Map<String, KrakenAssetPair> krakenPairs, Map<String, KrakenAsset> krakenAssets) {
+    Map<CurrencyPair, CurrencyPairMetaData> pairs = new HashMap<>();
+    for (String krakenPairCode : krakenPairs.keySet()) {
+      KrakenAssetPair krakenPair = krakenPairs.get(krakenPairCode);
+      pairs.put(adaptCurrencyPair(krakenPairCode), adaptPair(krakenPair));
+    }
+
+    Map<Currency, CurrencyMetaData> currencies = new HashMap<>();
+    for (String krakenAssetCode : krakenAssets.keySet()) {
+      KrakenAsset krakenAsset = krakenAssets.get(krakenAssetCode);
+      currencies.put(KrakenUtils.addCurrencyAndGetCode(krakenAssetCode), new CurrencyMetaData(krakenAsset.getScale()));
+    }
+    return new ExchangeMetaData(pairs, currencies, null, null, true);
+  }
+
+  private static CurrencyPairMetaData adaptPair(KrakenAssetPair krakenPair) {
+    // TODO
+    return null;
   }
 }

xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenExchange.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import org.knowm.xchange.BaseExchange;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenAssetPairs;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenAssets;
 import org.knowm.xchange.kraken.service.polling.KrakenAccountService;
 import org.knowm.xchange.kraken.service.polling.KrakenMarketDataService;
+import org.knowm.xchange.kraken.service.polling.KrakenMarketDataServiceRaw;
 import org.knowm.xchange.kraken.service.polling.KrakenTradeService;
 import org.knowm.xchange.utils.nonce.CurrentTimeNonceFactory;
 
@@ -46,16 +49,10 @@ public class KrakenExchange extends BaseExchange implements Exchange {
 
   @Override
   public void remoteInit() throws IOException {
-
-    // TODO Implement this.
-    //KrakenAssetPairs  currencies = ((KrakenMarketDataServiceRaw) pollingMarketDataService).getKrakenAssetPairs(CurrencyPair... currencyPairs) ();
+    KrakenAssetPairs assetPairs = ((KrakenMarketDataServiceRaw) pollingMarketDataService).getKrakenAssetPairs();
+    KrakenAssets assets = ((KrakenMarketDataServiceRaw) pollingMarketDataService).getKrakenAssets();
     // other endpoints?
     // hard-coded meta data from json file not available at an endpoint?
-    // TODO take all the info gathered above and create a `ExchangeMetaData` object via a new method in `*Adapters` class
-    // exchangeMetaData = *Adapters.adaptToExchangeMetaData(blah, blah);
-
-    KrakenUtils.buildExchangeSymbols(getExchangeMetaData().getCurrencyPairs());
-
-    super.remoteInit();
+    exchangeMetaData = KrakenAdapters.adaptToExchangeMetaData(assetPairs.getAssetPairMap(), assets.getAssetPairMap());
   }
 }

xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenUtils.java
@@ -1,14 +1,10 @@
 package org.knowm.xchange.kraken;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
-import org.knowm.xchange.dto.meta.CurrencyPairMetaData;
 import org.knowm.xchange.exceptions.ExchangeException;
 
 /**
@@ -52,21 +48,7 @@ public class KrakenUtils {
     throw new ExchangeException("Kraken does not support the currency code " + currency);
   }
 
-  public static void buildExchangeSymbols(Map<CurrencyPair, CurrencyPairMetaData> map) {
-
-    List<CurrencyPair> currencyPairs = new ArrayList<CurrencyPair>();
-
-    final Set<CurrencyPair> krakenCurrencyPairs = map.keySet();
-    for (CurrencyPair krakenCurrencyPair : krakenCurrencyPairs) {
-
-      Currency tradeCurrency = addCurrencyAndGetCode(krakenCurrencyPair.base.toString());
-      Currency priceCurrency = addCurrencyAndGetCode(krakenCurrencyPair.counter.toString());
-
-      currencyPairs.add(new CurrencyPair(tradeCurrency, priceCurrency));
-    }
-  }
-
-  private static Currency addCurrencyAndGetCode(String krakenCurrencyString) {
+  public static Currency addCurrencyAndGetCode(String krakenCurrencyString) {
 
     Currency currencyCode = KrakenAdapters.adaptCurrency(krakenCurrencyString);
     if (krakenCurrencyString.startsWith("X")) {