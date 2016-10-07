36db8f628edbc9d8513153405e7d62d822664e97
Jonathan Heusser <jonathan.heusser@gmail.com>
2016-10-07 15:33:32 +0100

    Initial commit of Gemini

---
+++ xchange-gemini/src/main/java/org/knowm/xchange/gemini/v1/dto/marketdata/GeminiDepth.java
@@ -0,0 +1,40 @@
+package org.knowm.xchange.gemini.v1.dto.marketdata;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GeminiDepth {
+
+  private final GeminiLevel[] asks;
+  private final GeminiLevel[] bids;
+
+  /**
+   * Constructor
+   * 
+   * @param asks
+   * @param bids
+   */
+  public GeminiDepth(@JsonProperty("asks") GeminiLevel[] asks, @JsonProperty("bids") GeminiLevel[] bids) {
+
+    this.asks = asks;
+    this.bids = bids;
+  }
+
+  public GeminiLevel[] getAsks() {
+
+    return asks;
+  }
+
+  public GeminiLevel[] getBids() {
+
+    return bids;
+  }
+
+  @Override
+  public String toString() {
+
+    return "GeminiDepth [asks=" + Arrays.toString(asks) + ", bids=" + Arrays.toString(bids) + "]";
+  }
+
+}
---
+++ xchange-gemini/src/main/java/org/knowm/xchange/gemini/v1/dto/marketdata/GeminiLevel.java
@@ -0,0 +1,49 @@
+package org.knowm.xchange.gemini.v1.dto.marketdata;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GeminiLevel {
+
+  private final BigDecimal price;
+  private final BigDecimal amount;
+  private final BigDecimal timestamp;
+
+  /**
+   * Constructor
+   * 
+   * @param price
+   * @param amount
+   * @param timestamp
+   */
+  public GeminiLevel(@JsonProperty("price") BigDecimal price, @JsonProperty("amount") BigDecimal amount,
+      @JsonProperty("timestamp") BigDecimal timestamp) {
+
+    this.price = price;
+    this.amount = amount;
+    this.timestamp = timestamp;
+  }
+
+  public BigDecimal getPrice() {
+
+    return price;
+  }
+
+  public BigDecimal getAmount() {
+
+    return amount;
+  }
+
+  public BigDecimal getTimestamp() {
+
+    return timestamp;
+  }
+
+  @Override
+  public String toString() {
+
+    return "BitfinexLevel [price=" + price + ", amount=" + amount + ", timestamp=" + timestamp + "]";
+  }
+
+}
