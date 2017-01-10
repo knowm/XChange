7ad52d4d644df9e7c359bdadcddc3325528e7453
Tim Molter <tim@knowm.org>
2017-01-10 09:07:24 +0100

    Currency Pair demo for issue #1357

---
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/therock/TheRockMetaDataDemo.java
@@ -0,0 +1,26 @@
+package org.knowm.xchange.examples.therock;
+
+import java.io.IOException;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.meta.ExchangeMetaData;
+import org.knowm.xchange.therock.TheRockExchange;
+
+public class TheRockMetaDataDemo {
+
+  public static void main(String[] args) throws IOException {
+
+    // Use the factory to get The Rock exchange API using default settings
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(TheRockExchange.class.getName());
+    ExchangeMetaData exchangeMetaData = exchange.getExchangeMetaData();
+    System.out.println(exchangeMetaData.toString());
+
+    for (CurrencyPair currencyPair : exchangeMetaData.getCurrencyPairs().keySet()) {
+      System.out.println(currencyPair.toString());
+    }
+
+  }
+
+}
