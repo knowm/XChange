2f8a6e0758dec9ec24bb2c8008f8595da1dc70f0
Zdenek Dolezal <zdenek.dolezal@gmail.com>
2017-01-19 23:19:59 +0100

    NEW: Coinmate exchange added.

---
+++ exchange-coinmate/src/main/java/info/bitrich/xchange/coinmate/CoinmateStreamingExchange.java
@@ -0,0 +1,39 @@
+package info.bitrich.xchange.coinmate;
+
+import info.bitrich.xchangestream.core.StreamingExchange;
+import info.bitrich.xchangestream.core.StreamingMarketDataService;
+import info.bitrich.xchangestream.pusher.PusherStreamingService;
+import io.reactivex.Completable;
+import org.knowm.xchange.coinmate.CoinmateExchange;
+
+public class CoinmateStreamingExchange extends CoinmateExchange implements StreamingExchange {
+    private static final String API_KEY = "af76597b6b928970fbb0";
+    private final PusherStreamingService streamingService;
+
+    private CoinmateStreamingMarketDataService streamingMarketDataService;
+
+    public CoinmateStreamingExchange() {
+        streamingService = new PusherStreamingService(API_KEY);
+    }
+
+    @Override
+    protected void initServices() {
+        super.initServices();
+        streamingMarketDataService = new CoinmateStreamingMarketDataService(streamingService);
+    }
+
+    @Override
+    public Completable connect() {
+        return streamingService.connect();
+    }
+
+    @Override
+    public Completable disconnect() {
+        return streamingService.disconnect();
+    }
+
+    @Override
+    public StreamingMarketDataService getStreamingMarketDataService() {
+        return streamingMarketDataService;
+    }
+}
