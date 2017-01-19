8d8933edea7cc1dd852af66be8cb08b6a0253404
Zdenek Dolezal <zdenek.dolezal@gmail.com>
2017-01-19 21:46:46 +0100

    NEW: Tests for Bitstamp market data service.

--- exchange-bitstamp/src/test/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataServiceTest.java
+++ exchange-bitstamp/src/test/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataServiceTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -16,11 +18,11 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 public class BitstampStreamingMarketDataServiceTest {
@@ -61,4 +63,24 @@ public class BitstampStreamingMarketDataServiceTest {
         });
     }
 
+    @Test
+    public void testGetTrades() throws Exception {
+        // Given order book in JSON
+        String trade = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("trade.json").toURI())));
+
+        when(streamingService.subscribeChannel(any(), anyList())).thenReturn(Observable.just(trade));
+
+        Trade expected = new Trade(Order.OrderType.ASK, new BigDecimal("34.390000000000001"), CurrencyPair.BTC_USD, new BigDecimal("914.38999999999999"), new Date(1484858423000L), "177827396");
+
+        // Call get order book observable
+        TestObserver<Trade> test = marketDataService.getTrades(CurrencyPair.BTC_USD).test();
+
+        // We get order book object in correct order
+        test.assertValue(expected);
+    }
+
+    @Test(expected = NotAvailableFromExchangeException.class)
+    public void testGetTicker() throws Exception {
+        marketDataService.getTicker(CurrencyPair.BTC_USD).test();
+    }
 }
