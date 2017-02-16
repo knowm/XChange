6b26be8b60014c276f132928b9ecd6502838c2b6
Achterhoeker <ronald@berp.nl>
2017-02-16 12:10:04 +0100

    Update ItBitTradeService.java

--- xchange-itbit/src/main/java/org/knowm/xchange/itbit/v1/service/ItBitTradeService.java
+++ xchange-itbit/src/main/java/org/knowm/xchange/itbit/v1/service/ItBitTradeService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Date;
 
 import org.knowm.xchange.Exchange;
