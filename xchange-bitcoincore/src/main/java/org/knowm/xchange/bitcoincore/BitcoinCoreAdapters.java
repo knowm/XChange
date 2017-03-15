c8ced33146edcfe393daec1bdda7bfff6adf88c5
ww3456 <ww3456@gmail.com>
2017-03-15 21:38:12 +0000

    [BitcoinCore] connection to wallet's RPC interface to retreive balance information

---
+++ xchange-bitcoincore/src/main/java/org/knowm/xchange/bitcoincore/BitcoinCoreAdapters.java
@@ -0,0 +1,18 @@
+package org.knowm.xchange.bitcoincore;
+
+import java.math.BigDecimal;
+
+import org.knowm.xchange.bitcoincore.dto.account.BitcoinCoreBalanceResponse;
+import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.dto.account.AccountInfo;
+import org.knowm.xchange.dto.account.Balance;
+import org.knowm.xchange.dto.account.Wallet;
+
+public class BitcoinCoreAdapters {
+  public static AccountInfo adaptAccountInfo(BitcoinCoreBalanceResponse available, BitcoinCoreBalanceResponse unconfirmed) {
+    BigDecimal total = available.getAmount().add(unconfirmed.getAmount());
+    Balance btc = new Balance(Currency.BTC, total, available.getAmount(), unconfirmed.getAmount());
+    Wallet wallet = new Wallet(btc);
+    return new AccountInfo(wallet);
+  }
+}
