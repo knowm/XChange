9354289811c9e5bf443d5c68f71f73e459b62f16
ww3456 <ww3456@gmail.com>
2017-03-27 19:08:26 +0100

    [TrueFX] ticker implementation

---
+++ xchange-truefx/src/main/java/org/knowm/xchange/truefx/service/TrueFxMarketDataServiceRaw.java
@@ -0,0 +1,59 @@
+package org.knowm.xchange.truefx.service;
+
+import java.io.IOException;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.service.BaseExchangeService;
+import org.knowm.xchange.truefx.TrueFxPublic;
+import org.knowm.xchange.truefx.dto.marketdata.TrueFxTicker;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+
+import si.mazi.rescu.ClientConfig;
+import si.mazi.rescu.RestProxyFactory;
+import si.mazi.rescu.serialization.jackson.DefaultJacksonObjectMapperFactory;
+import si.mazi.rescu.serialization.jackson.JacksonObjectMapperFactory;
+
+public class TrueFxMarketDataServiceRaw extends BaseExchangeService {
+
+  private final TrueFxPublic trueFx;
+
+  protected TrueFxMarketDataServiceRaw(Exchange exchange) {
+    super(exchange);
+
+    final ClientConfig config = new ClientConfig();
+    config.setJacksonObjectMapperFactory(factory);
+
+    trueFx = RestProxyFactory.createProxy(TrueFxPublic.class, exchange.getExchangeSpecification().getPlainTextUri(), config);
+  }
+
+  public TrueFxTicker getTicker(CurrencyPair pair) throws IOException {
+    return trueFx.getTicker(pair);
+  }
+
+  public ObjectMapper createObjectMapper() {
+    ObjectMapper mapper = factory.createObjectMapper();
+    factory.configureObjectMapper(mapper);
+    return mapper;
+  }
+
+  private final JacksonObjectMapperFactory factory = new DefaultJacksonObjectMapperFactory() {
+    @Override
+    protected ObjectMapper createInstance() {
+      return new CsvMapper();
+    }
+
+    @Override
+    public void configureObjectMapper(ObjectMapper mapper) {
+      super.configureObjectMapper(mapper);
+
+      final SimpleModule customDeserializer = new SimpleModule(TrueFxTicker.class.getSimpleName(), Version.unknownVersion());
+      customDeserializer.addDeserializer(TrueFxTicker.class, new TrueFxTicker.TrueFxTickerDeserializer());
+      mapper.registerModule(customDeserializer);
+    }
+  };
+}
