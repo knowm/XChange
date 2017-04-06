35c9406bc71261b9d901760465f177a74e7bc4b7
Matija Mazi <matija.mazi@gmail.com>
2017-04-06 16:35:14 +0200

    Add Generic API for funding information implementation to Coinfloor

--- xchange-coinfloor/src/main/java/org/knowm/xchange/coinfloor/service/CoinfloorAccountService.java
+++ xchange-coinfloor/src/main/java/org/knowm/xchange/coinfloor/service/CoinfloorAccountService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.coinfloor.CoinfloorAdapters;
@@ -11,8 +12,12 @@ import org.knowm.xchange.coinfloor.dto.account.CoinfloorBalance;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.account.AccountInfo;
+import org.knowm.xchange.dto.account.FundingRecord;
+import org.knowm.xchange.exceptions.ExchangeException;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.service.account.AccountService;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 
 public class CoinfloorAccountService extends CoinfloorAccountServiceRaw implements AccountService {
   public CoinfloorAccountService(Exchange exchange) {
@@ -38,5 +43,15 @@ public class CoinfloorAccountService extends CoinfloorAccountServiceRaw implemen
   @Override
   public String requestDepositAddress(Currency currency, String... args) throws NotAvailableFromExchangeException {
     throw new NotAvailableFromExchangeException();
+  }
+
+  @Override
+  public TradeHistoryParams createFundingHistoryParams() {
+    throw new NotYetImplementedForExchangeException();
+  }
+
+  @Override
+  public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+    throw new NotYetImplementedForExchangeException();
   }
 }
