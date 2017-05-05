3cac373d0a14dc8d0127b73bd496549155788d51
Mikelle <mikhwall@gmail.com>
2017-05-05 14:36:27 +0300

    Implemented tests for dto marketdata

---
+++ xchange-dsx/src/test/java/org/known/xchange/dsx/DSXAdapterTest.java
@@ -0,0 +1,19 @@
+package org.known.xchange.dsx;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * Test for DSXAdapter class
+ *
+ * @author Mikhail Wall
+ */
+public class DSXAdapterTest {
+
+  @Test
+  public void testOrderAdapterWithDepth() throws IOException {
+
+
+  }
+}
