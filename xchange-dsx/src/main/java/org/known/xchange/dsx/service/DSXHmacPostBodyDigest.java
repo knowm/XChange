e86c2534105a44a33327f24cec6d93ba35e77d2e
Mikelle <mikhwall@gmail.com>
2017-05-08 15:10:28 +0300

    added more comments to code

--- xchange-dsx/src/main/java/org/known/xchange/dsx/service/DSXHmacPostBodyDigest.java
+++ xchange-dsx/src/main/java/org/known/xchange/dsx/service/DSXHmacPostBodyDigest.java
@@ -37,25 +37,9 @@ public class DSXHmacPostBodyDigest extends BaseParamsDigest {
       String postBody = restInvocation.getRequestBody();
       Mac mac = getMac();
       mac.update(postBody.getBytes("UTF-8"));
-      //mac.update(restInvocation.getParamValue(FormParam.class, "nonce").toString().getBytes());
-      //byte[] res = mac.doFinal();
       return DatatypeConverter.printBase64Binary(mac.doFinal());
-      //System.out.println(DatatypeConverter.printBase64Binary(mac.doFinal()));
-      //return String.format("%064x", new BigInteger(1, mac.doFinal()));
     } catch (UnsupportedEncodingException e) {
       throw new RuntimeException("Illegal encoding, check the code.", e);
     }
-//
-//    String digest;
-//    byte[] signature;
-//
-//    Mac sha512 = getMac();
-//
-//    sha512.update(restInvocation.getRequestBody().getBytes());
-//
-//    signature = sha512.doFinal();
-//    digest = Base64.encodeBytes(signature);
-//
-//    return digest;
   }
 }
--- xchange-dsx/src/main/java/org/known/xchange/dsx/service/DSXMarketDataService.java
+++ xchange-dsx/src/main/java/org/known/xchange/dsx/service/DSXMarketDataService.java
@@ -31,21 +31,39 @@ public class DSXMarketDataService extends DSXMarketDataServiceRaw implements Mar
     super(exchange);
   }
 
+  /**
+   * Get ticker from exchange
+   *
+   * @param currencyPair
+   * @param args
+   * @return The ticker
+   * @throws IOException
+   */
   @Override
   public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
 
     String pairs = DSXAdapters.getPair(currencyPair);
     DSXTickerWrapper dsxTickerWrapper = getDSXTicker(pairs);
 
+    // Adapt to XChange DTOs
     return DSXAdapters.adaptTicker(dsxTickerWrapper.getTicker(DSXAdapters.getPair(currencyPair)), currencyPair);
   }
 
+  /**
+   * Get market depth from exchange
+   *
+   * @param currencyPair Currency pair for getting info abount
+   * @param args Optional arguments. Exchange-specific
+   * @return The OrderBook
+   * @throws IOException
+   */
   @Override
   public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
 
     String pairs = DSXAdapters.getPair(currencyPair);
     DSXOrderbookWrapper dsxOrderbookWrapper = getDSXOrderbook(pairs);
 
+    // Adapt to XChange DTOs
     List<LimitOrder> asks = DSXAdapters.adaptOrders(dsxOrderbookWrapper.getOrderbook(DSXAdapters.getPair(currencyPair)).getAsks(), currencyPair,
         "ask", "");
     List<LimitOrder> bids = DSXAdapters.adaptOrders(dsxOrderbookWrapper.getOrderbook(DSXAdapters.getPair(currencyPair)).getBids(), currencyPair,
@@ -54,6 +72,14 @@ public class DSXMarketDataService extends DSXMarketDataServiceRaw implements Mar
     return new OrderBook(null, asks, bids);
   }
 
+  /**
+   * Get recent trades from exchange
+   *
+   * @param currencyPair
+   * @param args Optional arguments. Exchange-specific. This implementation assumes args[0] is integer value
+   * @return Trades object
+   * @throws IOException
+   */
   @Override
   public Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
 
