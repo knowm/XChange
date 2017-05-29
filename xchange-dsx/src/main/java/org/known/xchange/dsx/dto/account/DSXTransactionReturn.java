4de00c978f6dd2810ef85842dca7410c0bd514d1
André Reiter <mail@areiter.net>
2017-05-29 16:32:11 +0200

    implemented funding history for DSX
    lets be final

--- xchange-dsx/src/main/java/org/known/xchange/dsx/dto/account/DSXTransactionReturn.java
+++ xchange-dsx/src/main/java/org/known/xchange/dsx/dto/account/DSXTransactionReturn.java
@@ -1,5 +1,6 @@
 package org.known.xchange.dsx.dto.account;
 
+import java.util.List;
 import java.util.Map;
 
 import org.known.xchange.dsx.dto.DSXReturn;
@@ -10,11 +11,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Mikhail Wall
  */
 
-public class DSXTransactionReturn extends DSXReturn<Map<Long, DSXTransaction>> {
+public class DSXTransactionReturn extends DSXReturn<List<DSXTransaction>> {
 
-  public DSXTransactionReturn(@JsonProperty("success") boolean success, @JsonProperty("return") Map<Long, DSXTransaction> value,
+  public DSXTransactionReturn(@JsonProperty("success") boolean success, @JsonProperty("return") Map<String, List<DSXTransaction>> value,
       @JsonProperty("error") String error) {
 
-    super(success, value, error);
+    super(success, value.get("transactions"), error);
   }
 }
