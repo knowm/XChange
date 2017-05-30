58267fca0fd3b11a8e3b8002cc809486dd5f57dd
André Reiter <mail@areiter.net>
2017-05-30 16:59:15 +0200

    support for crypto and fiat withdrawals @ dsx

--- xchange-dsx/src/main/java/org/knowm/xchange/dsx/dto/account/DSXFiatWithdrawReturn.java
+++ xchange-dsx/src/main/java/org/knowm/xchange/dsx/dto/account/DSXFiatWithdrawReturn.java
@@ -1,7 +1,5 @@
 package org.knowm.xchange.dsx.dto.account;
 
-import java.util.Map;
-
 import org.knowm.xchange.dsx.dto.DSXReturn;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,11 +8,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Mikhail Wall
  */
 
-public class DSXFiatWithdrawReturn extends DSXReturn<Map<Long, DSXFiatWithdraw>> {
+public class DSXFiatWithdrawReturn extends DSXReturn<DSXFiatWithdraw> {
 
-  public DSXFiatWithdrawReturn(@JsonProperty("success") boolean success, @JsonProperty("return") Map<Long, DSXFiatWithdraw> value,
+  public DSXFiatWithdrawReturn(@JsonProperty("success") boolean success, @JsonProperty("return") DSXFiatWithdraw value,
       @JsonProperty("error") String error) {
-
     super(success, value, error);
   }
 }
