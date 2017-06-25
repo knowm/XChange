74d07dda0be39984ecd86a97f148e5dd69329e97
npomfret <nick-github@snowmonkey.co.uk>
2017-06-25 22:10:50 +0100

    implemented getTradeHistory for BleutradeTrade

--- xchange-bleutrade/src/main/java/org/knowm/xchange/bleutrade/service/BleutradeTradeServiceRaw.java
+++ xchange-bleutrade/src/main/java/org/knowm/xchange/bleutrade/service/BleutradeTradeServiceRaw.java
@@ -1,8 +1,5 @@
 package org.knowm.xchange.bleutrade.service;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bleutrade.BleutradeException;
 import org.knowm.xchange.bleutrade.BleutradeUtils;
@@ -12,6 +9,10 @@ import org.knowm.xchange.bleutrade.dto.trade.BleutradeOpenOrdersReturn;
 import org.knowm.xchange.bleutrade.dto.trade.BleutradePlaceOrderReturn;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.exceptions.ExchangeException;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+
+import java.io.IOException;
+import java.util.List;
 
 public class BleutradeTradeServiceRaw extends BleutradeBaseService {
 
@@ -84,6 +85,55 @@ public class BleutradeTradeServiceRaw extends BleutradeBaseService {
       return response.getResult();
     } catch (BleutradeException e) {
       throw new ExchangeException(e);
+    }
+  }
+
+  public List<BleutradeOpenOrder> getTrades(TradeHistoryParams params) throws IOException {
+    BleutradeTradeHistoryParams bleutradeTradeHistoryParams;
+    if (params instanceof BleutradeTradeHistoryParams) {
+      bleutradeTradeHistoryParams = (BleutradeTradeHistoryParams) params;
+    } else {
+      bleutradeTradeHistoryParams = BleutradeTradeHistoryParams.ALL;
+    }
+
+    try {
+      BleutradeOpenOrdersReturn response = bleutrade.getTrades(apiKey, signatureCreator, exchange.getNonceFactory(),
+          bleutradeTradeHistoryParams.market,
+          bleutradeTradeHistoryParams.orderStatus,
+          bleutradeTradeHistoryParams.orderType
+      );
+
+      if (!response.getSuccess()) {
+        throw new ExchangeException(response.getMessage());
+      }
+
+      return response.getResult();
+    } catch (BleutradeException e) {
+      throw new ExchangeException(e);
+    }
+  }
+
+  public static class BleutradeTradeHistoryParams implements TradeHistoryParams {
+    public static final BleutradeTradeHistoryParams ALL = new BleutradeTradeHistoryParams("ALL", "ALL", "ALL");
+    /**
+     * DIVIDEND_DIVISOR or ALL
+     */
+    public final String market;
+
+    /**
+     * ALL, OK, OPEN, CANCELED
+     */
+    public final String orderStatus;
+
+    /**
+     * ALL, BUY, SELL
+     */
+    public final String orderType;
+
+    public BleutradeTradeHistoryParams(String market, String orderStatus, String orderType) {
+      this.market = market;
+      this.orderStatus = orderStatus;
+      this.orderType = orderType;
     }
   }
 
