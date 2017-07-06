ffbd5eef9f52f204c89b5022eabb414d089cee4b
DrawFun <dzf353@gmail.com>
2017-07-06 13:52:26 +0800

    Accoding to bitstamp practice, move MarketData RestProxy from BaseService down to JubiMarketDataServiceRaw, which can decouple different types of proxies(market data, account, trade...).

--- xchange-jubi/src/main/java/org/knowm/xchange/jubi/service/JubiMarketDataServiceRaw.java
+++ xchange-jubi/src/main/java/org/knowm/xchange/jubi/service/JubiMarketDataServiceRaw.java
@@ -5,46 +5,34 @@ import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.jubi.Jubi;
 import org.knowm.xchange.jubi.dto.marketdata.JubiTicker;
 import org.knowm.xchange.jubi.dto.marketdata.JubiTrade;
+import si.mazi.rescu.RestProxyFactory;
 
 import java.io.IOException;
 
 /**
  * Created by Yingzhe on 3/17/2015.
+ * Market Data Service.
  */
-public class JubiMarketDataServiceRaw extends JubiBaseService<Jubi> {
+public class JubiMarketDataServiceRaw extends JubiBaseService {
 
-  /**
-   * Constructor for JubiMarketDataServiceRaw
-   *
-   * @param exchange The {@link org.knowm.xchange.Exchange}
-   */
+  private final Jubi jubi;
+
   public JubiMarketDataServiceRaw(Exchange exchange) {
-
-    super(Jubi.class, exchange);
+    super(exchange);
+    this.jubi = RestProxyFactory.createProxy(Jubi.class, exchange.getExchangeSpecification().getSslUri());
   }
 
-  /**
-   * Gets ticker from Jubi
-   *
-   * @param baseCurrency Base currency
-   * @param targetCurrency Target currency
-   * @return JubiTicker object
-   * @throws java.io.IOException
-   */
   public JubiTicker getJubiTicker(String baseCurrency, String targetCurrency) throws IOException {
-
     // Base currency needs to be in lower case, otherwise API throws an error
     for (CurrencyPair cp : exchange.getExchangeSymbols()) {
       if (cp.base.getCurrencyCode().equalsIgnoreCase(baseCurrency) && cp.counter.getCurrencyCode().equalsIgnoreCase(targetCurrency)) {
         return this.jubi.getTicker(baseCurrency.toLowerCase());
       }
     }
-
     return null;
   }
 
   public JubiTrade[] getJubiTrades(CurrencyPair currencyPair, Object[] args) throws IOException {
-
     return  (args != null && args.length > 0 && args[0] != null && args[0] instanceof Long)
             ? jubi.getTradesSince(currencyPair.base.getCurrencyCode().toLowerCase(), (Long) args[0])
             : jubi.getTrades(currencyPair.base.getCurrencyCode().toLowerCase());
