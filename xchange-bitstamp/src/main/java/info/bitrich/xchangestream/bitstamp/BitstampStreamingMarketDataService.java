6c8126e691b4ec6892149f95c25ff6ca07715d2e
Dolezal Zdenek <Zdenek.Dolezal@airbank.cz>
2017-07-14 12:42:05 +0200

    #11 Xchange dependency updated to latest SNAPSHOT

--- xchange-bitstamp/src/main/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataService.java
+++ xchange-bitstamp/src/main/java/info/bitrich/xchangestream/bitstamp/BitstampStreamingMarketDataService.java
@@ -2,17 +2,18 @@ package info.bitrich.xchangestream.bitstamp;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import info.bitrich.xchangestream.bitstamp.dto.BitstampOrderBook;
 import info.bitrich.xchangestream.bitstamp.dto.BitstampWebSocketTransaction;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import info.bitrich.xchangestream.service.pusher.PusherStreamingService;
 import io.reactivex.Observable;
 import org.knowm.xchange.bitstamp.BitstampAdapters;
-import org.knowm.xchange.bitstamp.dto.marketdata.BitstampOrderBook;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+
 import java.util.Date;
 
 public class BitstampStreamingMarketDataService implements StreamingMarketDataService {
@@ -30,10 +31,10 @@ public class BitstampStreamingMarketDataService implements StreamingMarketDataSe
                 .map(s -> {
                     ObjectMapper mapper = new ObjectMapper();
                     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                    BitstampOrderBook bitstampOrderBook = mapper.readValue(s, BitstampOrderBook.class);
-                    bitstampOrderBook = new BitstampOrderBook(new Date().getTime()/1000L, bitstampOrderBook.getBids(), bitstampOrderBook.getAsks());
+                    BitstampOrderBook orderBook = mapper.readValue(s, BitstampOrderBook.class);
+                    org.knowm.xchange.bitstamp.dto.marketdata.BitstampOrderBook bitstampOrderBook = new org.knowm.xchange.bitstamp.dto.marketdata.BitstampOrderBook(new Date().getTime(), orderBook.getBids(), orderBook.getAsks());
 
-                    return BitstampAdapters.adaptOrderBook(bitstampOrderBook, currencyPair, 1000);
+                    return BitstampAdapters.adaptOrderBook(bitstampOrderBook, currencyPair);
                 });
     }
 
