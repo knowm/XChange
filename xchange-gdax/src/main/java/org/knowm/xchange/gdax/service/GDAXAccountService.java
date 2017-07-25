eb07e7f35b7923108ff41db9b307b682b0193b33
npomfret <nick-github@snowmonkey.co.uk>
2017-07-25 15:45:50 +0100

    [gdax] fixed withdraw funds so that it can be used to withdraw to a crypto wallet (retained the orginal behaviour of 'move funds' by using the new WithdrawFundsParams interface)

--- xchange-gdax/src/main/java/org/knowm/xchange/gdax/service/GDAXAccountService.java
+++ xchange-gdax/src/main/java/org/knowm/xchange/gdax/service/GDAXAccountService.java
@@ -1,9 +1,5 @@
 package org.knowm.xchange.gdax.service;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.List;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
@@ -13,11 +9,16 @@ import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.gdax.GDAXAdapters;
 import org.knowm.xchange.gdax.dto.account.GDAXAccount;
+import org.knowm.xchange.gdax.dto.account.GDAXWithdrawCryptoResponse;
 import org.knowm.xchange.gdax.dto.trade.GDAXSendMoneyResponse;
 import org.knowm.xchange.service.account.AccountService;
 import org.knowm.xchange.service.trade.params.DefaultWithdrawFundsParams;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 import org.knowm.xchange.service.trade.params.WithdrawFundsParams;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class GDAXAccountService extends GDAXAccountServiceRaw implements AccountService {
 
@@ -34,7 +35,31 @@ public class GDAXAccountService extends GDAXAccountServiceRaw implements Account
 
   @Override
   public String withdrawFunds(Currency currency, BigDecimal amount, String address) throws IOException {
+    return withdrawFunds(new DefaultWithdrawFundsParams(address, currency, amount));
+  }
 
+  @Override
+  public String withdrawFunds(WithdrawFundsParams params) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+    if (params instanceof DefaultWithdrawFundsParams) {
+      DefaultWithdrawFundsParams defaultParams = (DefaultWithdrawFundsParams) params;
+      GDAXWithdrawCryptoResponse response = withdrawCrypto(defaultParams.address, defaultParams.amount, defaultParams.currency);
+      return response.id;
+    }
+
+    if (params instanceof GDAXMoveFundsParams) {
+      GDAXMoveFundsParams moveFundsParams = (GDAXMoveFundsParams) params;
+
+      Currency currency = moveFundsParams.currency;
+      String address = moveFundsParams.address;
+      BigDecimal amount = moveFundsParams.amount;
+
+      return moveFunds(currency, address, amount);
+    }
+
+    throw new IllegalStateException("Don't know how to withdraw: " + params);
+  }
+
+  private String moveFunds(Currency currency, String address, BigDecimal amount) throws IOException {
     GDAXAccount[] accounts = getCoinbaseExAccountInfo();
     String accountId = null;
     for (GDAXAccount account : accounts) {
@@ -42,23 +67,17 @@ public class GDAXAccountService extends GDAXAccountServiceRaw implements Account
         accountId = account.getId();
       }
     }
+
     if (accountId == null) {
       throw new ExchangeException("Cannot determine account id for currency " + currency.getCurrencyCode());
     }
+
     GDAXSendMoneyResponse response = sendMoney(accountId, address, amount, currency);
     if (response.getData() != null) {
       return response.getData().getId();
     }
-    return null;
-  }
 
-  @Override
-  public String withdrawFunds(WithdrawFundsParams params) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
-    if (params instanceof DefaultWithdrawFundsParams) {
-      DefaultWithdrawFundsParams defaultParams = (DefaultWithdrawFundsParams) params;
-      return withdrawFunds(defaultParams.currency, defaultParams.amount, defaultParams.address);
-    }
-    throw new IllegalStateException("Don't know how to withdraw: " + params);
+    return null;
   }
 
   @Override
@@ -77,5 +96,17 @@ public class GDAXAccountService extends GDAXAccountServiceRaw implements Account
   public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws IOException {
 
     throw new NotYetImplementedForExchangeException();
+  }
+
+  public static class GDAXMoveFundsParams implements WithdrawFundsParams {
+    public final Currency currency;
+    public final BigDecimal amount;
+    public final String address;
+
+    public GDAXMoveFundsParams(Currency currency, BigDecimal amount, String address) {
+      this.currency = currency;
+      this.amount = amount;
+      this.address = address;
+    }
   }
 }
