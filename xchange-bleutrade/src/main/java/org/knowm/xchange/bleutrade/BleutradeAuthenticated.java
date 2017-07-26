e2d006c4a3b35d15d0e8066db12fbf6e04e98b9d
npomfret <nick-github@snowmonkey.co.uk>
2017-07-26 15:17:11 +0100

    [bleutrade] implemented withDrawFunds

--- xchange-bleutrade/src/main/java/org/knowm/xchange/bleutrade/BleutradeAuthenticated.java
+++ xchange-bleutrade/src/main/java/org/knowm/xchange/bleutrade/BleutradeAuthenticated.java
@@ -3,6 +3,7 @@ package org.knowm.xchange.bleutrade;
 import org.knowm.xchange.bleutrade.dto.account.BleutradeBalanceReturn;
 import org.knowm.xchange.bleutrade.dto.account.BleutradeBalancesReturn;
 import org.knowm.xchange.bleutrade.dto.account.BleutradeDepositAddressReturn;
+import org.knowm.xchange.bleutrade.dto.account.BleutradeWithdrawReturn;
 import org.knowm.xchange.bleutrade.dto.trade.BleutradeCancelOrderReturn;
 import org.knowm.xchange.bleutrade.dto.trade.BleutradeOpenOrdersReturn;
 import org.knowm.xchange.bleutrade.dto.trade.BleutradePlaceOrderReturn;
@@ -17,6 +18,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 @Path("v2")
 @Produces(MediaType.APPLICATION_JSON)
@@ -64,4 +66,9 @@ public interface BleutradeAuthenticated extends Bleutrade {
   BluetradeExecutedTradesWrapper getTrades(@QueryParam("apikey") String apiKey, @HeaderParam("apisign") ParamsDigest signature,
       @QueryParam("nonce") SynchronizedValueFactory<Long> nonce,
       @QueryParam("market") String market, @QueryParam("orderstatus") String orderStatus, @QueryParam("ordertype") String orderType) throws IOException, BleutradeException;
+
+  @GET
+  @Path("/account/withdraw")
+  BleutradeWithdrawReturn withdraw(@QueryParam("apikey") String apiKey, @HeaderParam("apisign") ParamsDigest signature, @QueryParam("nonce") SynchronizedValueFactory<Long> nonce,
+                                   @QueryParam("currency") String currency, @QueryParam("quantity") BigDecimal quantity, @QueryParam("address") String address) throws IOException, BleutradeException;
 }
