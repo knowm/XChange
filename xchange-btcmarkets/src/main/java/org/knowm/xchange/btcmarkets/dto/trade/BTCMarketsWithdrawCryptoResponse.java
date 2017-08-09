e300ed64b27b57a78f31a56854e5ed93bd053d18
npomfret <nick-github@snowmonkey.co.uk>
2017-08-09 10:23:09 +0100

    [btcmarkets] improved parse of response data for withdrawFunds

--- xchange-btcmarkets/src/main/java/org/knowm/xchange/btcmarkets/dto/trade/BTCMarketsWithdrawCryptoResponse.java
+++ xchange-btcmarkets/src/main/java/org/knowm/xchange/btcmarkets/dto/trade/BTCMarketsWithdrawCryptoResponse.java
@@ -3,11 +3,27 @@ package org.knowm.xchange.btcmarkets.dto.trade;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.knowm.xchange.btcmarkets.dto.BTCMarketsBaseResponse;
 
+import java.math.BigDecimal;
+
 public class BTCMarketsWithdrawCryptoResponse extends BTCMarketsBaseResponse {
     public final String status;
+    public final String fundTransferId;
+    public final String description;
+    public final String currency;
+    public final BigDecimal amount;
+    public final BigDecimal fee;
+    public final Long creationTime;
 
-    public BTCMarketsWithdrawCryptoResponse(Boolean success, String errorMessage, Integer errorCode, @JsonProperty("status") String status) {
+    public BTCMarketsWithdrawCryptoResponse(@JsonProperty("success") Boolean success, @JsonProperty("errorMessage") String errorMessage, @JsonProperty("errorCode") Integer errorCode, @JsonProperty("status") String status,
+                                            @JsonProperty("fundTransferId") String fundTransferId, @JsonProperty("description") String description, @JsonProperty("currency") String currency, @JsonProperty("amount") BigDecimal amount,
+                                            @JsonProperty("fee") BigDecimal fee, @JsonProperty("creationTime") Long creationTime) {
         super(success, errorMessage, errorCode);
         this.status = status;
+        this.fundTransferId = fundTransferId;
+        this.description = description;
+        this.currency = currency;
+        this.amount = amount;
+        this.fee = fee;
+        this.creationTime = creationTime;
     }
 }
