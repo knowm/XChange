72dea489f6e857ca21c42abcd8ad37a0939ac19f
mnapier <markwnapier@gmail.com>
2017-08-10 21:52:27 -0400

    Fixed comment

--- xchange-examples/src/main/java/org/knowm/xchange/examples/cryptopia/marketdata/CryptopiaTickerDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/cryptopia/marketdata/CryptopiaTickerDemo.java
@@ -12,7 +12,7 @@ import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
 /**
- * Demonstrate requesting Ticker at Cryptopia. You can access both the raw data from Bitstamp or the XChange generic DTO data format.
+ * Demonstrate requesting Ticker at Cryptopia. You can access both the raw data from Cryptopia or the XChange generic DTO data format.
  */
 public class CryptopiaTickerDemo {
 
