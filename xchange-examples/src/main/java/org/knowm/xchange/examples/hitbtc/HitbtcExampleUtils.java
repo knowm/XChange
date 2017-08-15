3a7c65e430a1a7cf738eb48d3de49a72f88595ae
Eric Anderson <AndersonAnderson@gmail.com>
2017-08-15 16:18:24 -0600

    cleanup

--- xchange-examples/src/main/java/org/knowm/xchange/examples/hitbtc/HitbtcExampleUtils.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/hitbtc/HitbtcExampleUtils.java
@@ -10,8 +10,8 @@ public class HitbtcExampleUtils {
   public static Exchange createExchange() {
 
     ExchangeSpecification exchangeSpecification = new ExchangeSpecification(HitbtcExchange.class);
-    exchangeSpecification.setApiKey("4ac1e9b53c64980c0a3c464c7d9262fc");
-    exchangeSpecification.setSecretKey("d03dea317063b7cf4c74ae212735e948");
+    exchangeSpecification.setApiKey("");
+    exchangeSpecification.setSecretKey("");
 
     return ExchangeFactory.INSTANCE.createExchange(exchangeSpecification);
   }
