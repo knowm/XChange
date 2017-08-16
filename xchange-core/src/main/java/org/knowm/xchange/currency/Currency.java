980d7a51523bb1d006371836afb0a5677d1c37a6
npomfret <nick-github@snowmonkey.co.uk>
2017-08-16 15:58:50 +0100

    added IOTA currency and some common currency pairs

--- xchange-core/src/main/java/org/knowm/xchange/currency/Currency.java
+++ xchange-core/src/main/java/org/knowm/xchange/currency/Currency.java
@@ -105,6 +105,7 @@ public class Currency implements Comparable<Currency> {
   public static final Currency ILS = createCurrency("ILS", "Israeli New Sheqel", null);
   public static final Currency INR = createCurrency("INR", "Indian Rupee", null);
   public static final Currency IOC = createCurrency("IOC", "I/OCoin", null);
+  public static final Currency IOT = createCurrency("IOT", "IOTA", null);
   public static final Currency IQD = createCurrency("IQD", "Iraqi Dinar", null);
   public static final Currency IRR = createCurrency("IRR", "Iranian Rial", null);
   public static final Currency ISK = createCurrency("ISK", "Icelandic Króna", null);
