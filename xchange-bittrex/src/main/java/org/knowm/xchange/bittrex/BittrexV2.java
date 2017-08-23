d7939cbc8bfd0ef370dc644cd5cb52722b03ffbc
simonkeliuotis <s.keliuotis@gmail.com>
2017-08-23 17:45:02 +0300

    [bittrex] create chart data dto classes, code formating

--- xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/BittrexV2.java
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/BittrexV2.java
@@ -1,6 +1,6 @@
 package org.knowm.xchange.bittrex;
 
-import org.knowm.xchange.bittrex.dto.marketdata.BittrexChartData;
+import java.io.IOException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,12 +8,14 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.knowm.xchange.bittrex.dto.marketdata.BittrexChartDataResponse;
+
 @Path("v2.0")
 @Produces(MediaType.APPLICATION_JSON)
 public interface BittrexV2 {
 
-    @GET
-    @Path("pub/market/getTicks")
-    BittrexChartData[] getChartData(@QueryParam("marketName") String market, @QueryParam("tickInterval") String tickInterval);
+  @GET
+  @Path("pub/market/getticks")
+  BittrexChartDataResponse getChartData(@QueryParam("marketname") String market, @QueryParam("tickinterval") String tickInterval) throws IOException;
 
 }
