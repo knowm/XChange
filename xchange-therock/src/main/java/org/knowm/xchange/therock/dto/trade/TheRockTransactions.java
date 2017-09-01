25a30db44a0a91f148149829de2f4cacd8feb6c7
npomfret <nick-github@snowmonkey.co.uk>
2017-09-01 23:47:46 +0100

    [therock] implemented getFundingHistory

--- xchange-therock/src/main/java/org/knowm/xchange/therock/dto/trade/TheRockTransactions.java
+++ xchange-therock/src/main/java/org/knowm/xchange/therock/dto/trade/TheRockTransactions.java
@@ -2,15 +2,19 @@ package org.knowm.xchange.therock.dto.trade;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Map;
+
 /**
  * https://api.therocktrading.com/doc/v1/index.html#api-Trading_API-Transactions
  */
 public class TheRockTransactions {
 
   private final TheRockTransaction[] transactions;
+  private final Map meta;
 
-  public TheRockTransactions(@JsonProperty("transactions") TheRockTransaction[] transactions, @JsonProperty("meta") Object ignored) {
+  public TheRockTransactions(@JsonProperty("transactions") TheRockTransaction[] transactions, @JsonProperty("meta") Map meta) {
     this.transactions = transactions;
+    this.meta = meta;
   }
 
   public int getCount() {
