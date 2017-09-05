df590c080c162a3ce96ccb16ad87e06330359ca2
npomfret <nick-github@snowmonkey.co.uk>
2017-09-05 20:15:36 +0100

    [livecoin] first stab at implementing livecoin account and trade services

--- xchange-livecoin/src/main/java/org/knowm/xchange/livecoin/service/LivecoinBaseService.java
+++ xchange-livecoin/src/main/java/org/knowm/xchange/livecoin/service/LivecoinBaseService.java
@@ -2,19 +2,22 @@ package org.knowm.xchange.livecoin.service;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.livecoin.Livecoin;
+import org.knowm.xchange.livecoin.LivecoinDigest;
 import org.knowm.xchange.service.BaseExchangeService;
 import org.knowm.xchange.service.BaseService;
-
 import si.mazi.rescu.RestProxyFactory;
 
 public class LivecoinBaseService<T extends Livecoin> extends BaseExchangeService implements BaseService {
 
-  protected final T coinbaseEx;
+  protected final T service;
+  protected final LivecoinDigest signatureCreator;
+  protected final String apiKey;
 
   public LivecoinBaseService(Class<T> type, Exchange exchange) {
     super(exchange);
 
-    this.coinbaseEx = RestProxyFactory.createProxy(type, exchange.getExchangeSpecification().getSslUri());
+    this.service = RestProxyFactory.createProxy(type, exchange.getExchangeSpecification().getSslUri());
+    this.apiKey = exchange.getExchangeSpecification().getApiKey();
+    this.signatureCreator = LivecoinDigest.createInstance(exchange.getExchangeSpecification().getSecretKey(), apiKey);
   }
-
 }
