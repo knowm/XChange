b877b578b005df6f7652f5c4ad742fc9eb25c126
npomfret <nick-github@snowmonkey.co.uk>
2017-09-05 16:06:16 +0100

    [bitbay] more work on funding history - small improvement in the parse

--- xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/service/BitbayAccountServiceRaw.java
+++ xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/service/BitbayAccountServiceRaw.java
@@ -19,48 +19,52 @@ import java.util.TimeZone;
  */
 public class BitbayAccountServiceRaw extends BitbayBaseService {
 
-    public BitbayAccountServiceRaw(Exchange exchange) {
-        super(exchange);
+  public BitbayAccountServiceRaw(Exchange exchange) {
+    super(exchange);
+  }
+
+  public BitbayAccountInfoResponse getBitbayAccountInfo() throws IOException {
+    BitbayAccountInfoResponse response = bitbayAuthenticated.info(apiKey, sign, exchange.getNonceFactory());
+
+    checkError(response);
+    return response;
+  }
+
+  public List<FundingRecord> history(Currency currency, int limit) {
+    List<Map> history = bitbayAuthenticated.history(apiKey, sign, exchange.getNonceFactory(), currency.getCurrencyCode(), limit);
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+    List<FundingRecord> res = new ArrayList<>();
+    for (Map map : history) {
+      try {
+        FundingRecord.Type type;
+        if (map.get("operation_type").toString().equals("+outside_income"))
+          type = FundingRecord.Type.DEPOSIT;
+        else if (map.get("operation_type").toString().equals("-transfer"))
+          type = FundingRecord.Type.WITHDRAWAL;
+        else
+          continue;
+
+        res.add(new FundingRecord(
+            null,
+            dateFormat.parse(map.get("time").toString()),
+            Currency.getInstance(map.get("currency").toString()),
+            new BigDecimal(map.get("amount").toString()),
+            map.get("id").toString(),
+            null,
+            type,
+            FundingRecord.Status.COMPLETE,
+            null,
+            null,
+            map.get("comment").toString()
+        ));
+      } catch (ParseException e) {
+        throw new IllegalStateException("Should not happen", e);
+      }
     }
 
-    public BitbayAccountInfoResponse getBitbayAccountInfo() throws IOException {
-        BitbayAccountInfoResponse response = bitbayAuthenticated.info(apiKey, sign, exchange.getNonceFactory());
-
-        checkError(response);
-        return response;
-    }
-
-    public List<FundingRecord> history(Currency currency, int limit) {
-        List<Map> history = bitbayAuthenticated.history(apiKey, sign, exchange.getNonceFactory(), currency.getCurrencyCode(), limit);
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-        List<FundingRecord> res = new ArrayList<>();
-        for (Map map : history) {
-            try {
-                FundingRecord.Type type = FundingRecord.Type.WITHDRAWAL;
-                if(map.get("operation_type").toString().equals("+outside_income"))
-                    type = FundingRecord.Type.DEPOSIT;
-
-                res.add(new FundingRecord(
-                    null,
-                        dateFormat.parse(map.get("time").toString()),
-                        Currency.getInstance(map.get("currency").toString()),
-                        new BigDecimal(map.get("amount").toString()),
-                        map.get("id").toString(),
-                        null,
-                        type,
-                        FundingRecord.Status.COMPLETE,
-                        null,
-                        null,
-                        map.get("comment").toString()
-                ));
-            } catch (ParseException e) {
-                throw new IllegalStateException("Should not happen", e);
-            }
-        }
-
-        return res;
-    }
+    return res;
+  }
 }
