2d77473516899dc911af4a3c6776d598717ae9f5
Kolozsy Gábor <kolozsy.gabor@partner.t-systems.hu>
2017-09-06 18:40:07 +0200

    [core] fill out the missing @param tag description

--- xchange-core/src/main/java/org/knowm/xchange/service/account/AccountService.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/account/AccountService.java
@@ -73,7 +73,7 @@ public interface AccountService extends BaseService {
    * Request a digital currency address to fund this account. Allows to fund the exchange account with digital currency from an external address
    *
    * @param currency The digital currency that corresponds to the desired deposit address.
-   * @param args
+   * @param args Necessary argument(s) as a {@code String}
    * @return the internal deposit address to send funds to
    * @throws ExchangeException                     - Indication that the exchange reported some kind of error with the request or response
    * @throws NotAvailableFromExchangeException     - Indication that the exchange does not support the requested function or data
