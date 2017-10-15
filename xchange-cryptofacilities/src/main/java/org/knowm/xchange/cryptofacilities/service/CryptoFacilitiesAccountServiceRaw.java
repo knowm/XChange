e3578848be334ae0e842930e1f075373356da586
Panchen <Panchen@users.noreply.github.com>
2017-10-15 12:11:37 +0100

    [CryptoFacilities] Upgrade to api v3
    Upgrade to /api/v3 (multi-asset) ahead of iminent inverse contract
    change.
    
    NB index symbols have changed.
    E.g. cf-bpi changed to cf-xbt:usd-rt

--- xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/service/CryptoFacilitiesAccountServiceRaw.java
+++ xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/service/CryptoFacilitiesAccountServiceRaw.java
@@ -3,7 +3,6 @@ package org.knowm.xchange.cryptofacilities.service;
 import java.io.IOException;
 
 import org.knowm.xchange.Exchange;
-import org.knowm.xchange.cryptofacilities.dto.account.CryptoFacilitiesAccount;
 import org.knowm.xchange.cryptofacilities.dto.account.CryptoFacilitiesAccounts;
 import org.knowm.xchange.exceptions.ExchangeException;
 
@@ -21,18 +20,6 @@ public class CryptoFacilitiesAccountServiceRaw extends CryptoFacilitiesBaseServi
   public CryptoFacilitiesAccountServiceRaw(Exchange exchange) {
 
     super(exchange);
-  }
-
-  public CryptoFacilitiesAccount getCryptoFacilitiesAccount() throws IOException {
-
-    CryptoFacilitiesAccount cryptoFacilitiesAccount = cryptoFacilities.account(exchange.getExchangeSpecification().getApiKey(), signatureCreator,
-        exchange.getNonceFactory());
-
-    if (cryptoFacilitiesAccount.isSuccess()) {
-      return cryptoFacilitiesAccount;
-    } else {
-      throw new ExchangeException("Error getting CF account info: " + cryptoFacilitiesAccount.getError());
-    }
   }
 
   public CryptoFacilitiesAccounts getCryptoFacilitiesAccounts() throws IOException {
