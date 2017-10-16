eb48c8704768854fcf349d50fa9ff9ef7ea46fc6
André Reiter <mail@areiter.net>
2017-10-16 17:39:10 +0200

    cryptopia - fix for user trade history (order id, fee currency)

--- xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/service/CryptopiaTradeServiceRaw.java
+++ xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/service/CryptopiaTradeServiceRaw.java
@@ -119,10 +119,10 @@ public class CryptopiaTradeServiceRaw {
       Date timestamp = CryptopiaAdapters.convertTimestamp(map.get("TimeStamp").toString());
       String id = map.get("TradeId").toString();
       BigDecimal fee = new BigDecimal(map.get("Fee").toString());
-      String orderId = map.get("TradePairId").toString();
-      Currency feeCcy = null;
+      String orderId = id;
 
       CurrencyPair pair = new CurrencyPair(map.get("Market").toString());
+      Currency feeCcy = pair.counter;
       results.add(new UserTrade(
           type,
           amount,
