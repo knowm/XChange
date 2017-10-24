64097d2f8bda765f376edfdc2900b8a928799ad9
André Reiter <mail@areiter.net>
2017-10-24 12:05:05 +0200

    staged

--- xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountServiceRaw.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/service/BinanceAccountServiceRaw.java
@@ -1,6 +1,7 @@
 package org.knowm.xchange.binance.service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.binance.dto.BinanceException;
@@ -14,6 +15,20 @@ public class BinanceAccountServiceRaw extends BinanceBaseService {
     
     public BinanceAccountInformation account(Long recvWindow, long timestamp) throws BinanceException, IOException {
         return binance.account(recvWindow, timestamp, super.apiKey, super.signatureCreator);
+    }
+    
+    
+    public Object withdraw(String asset, String address, BigDecimal amount, String name, Long recvWindow
+            , long timestamp) throws IOException, BinanceException {
+        return binance.withdraw(asset, address, amount, name, recvWindow, timestamp, super.apiKey, super.signatureCreator);
+    }
+    
+    public Object depositHistory(String asset, Long startTime, Long endTime, Long recvWindow, long timestamp) throws BinanceException, IOException {
+        return binance.depositHistory(asset, startTime, endTime, recvWindow, timestamp, super.apiKey, super.signatureCreator);
+    }
+    
+    public Object withdrawHistory(String asset, Long startTime, Long endTime, Long recvWindow, long timestamp) throws BinanceException, IOException {
+        return binance.withdrawHistory(asset, startTime, endTime, recvWindow, timestamp, super.apiKey, super.signatureCreator);
     }
     
 }
