8bd33aca74456a7a085befa9bb0fc1838a720a36
npomfret <nick-github@snowmonkey.co.uk>
2017-10-24 13:16:12 +0100

    [cexio] started refactoring the API to use application/json rather than form encoded parameters (as per the spec: https://cex.io/rest-api).  I've started with the ArchviedOrders call because that one wasn't working with the previous implemention

--- xchange-cexio/src/main/java/org/knowm/xchange/cexio/service/CexIOAccountServiceRaw.java
+++ xchange-cexio/src/main/java/org/knowm/xchange/cexio/service/CexIOAccountServiceRaw.java
@@ -1,38 +1,24 @@
 package org.knowm.xchange.cexio.service;
 
-import java.io.IOException;
-import java.util.Map;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.cexio.CexIOAuthenticated;
 import org.knowm.xchange.cexio.dto.account.CexIOBalanceInfo;
 import org.knowm.xchange.cexio.dto.account.GHashIOHashrate;
 import org.knowm.xchange.cexio.dto.account.GHashIOWorker;
 import org.knowm.xchange.exceptions.ExchangeException;
-
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.RestProxyFactory;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author timmolter
  */
 public class CexIOAccountServiceRaw extends CexIOBaseService {
 
-  private final CexIOAuthenticated cexIOAuthenticated;
-  private ParamsDigest signatureCreator;
-
-  /**
-   * Constructor
-   *
-   * @param exchange
-   */
   public CexIOAccountServiceRaw(Exchange exchange) {
-
     super(exchange);
-    this.cexIOAuthenticated = RestProxyFactory.createProxy(CexIOAuthenticated.class, exchange.getExchangeSpecification().getSslUri(),
-        getClientConfig());
-    signatureCreator = CexIODigest.createInstance(exchange.getExchangeSpecification().getSecretKey(),
-        exchange.getExchangeSpecification().getUserName(), exchange.getExchangeSpecification().getApiKey());
   }
 
   public CexIOBalanceInfo getCexIOAccountInfo() throws IOException {
