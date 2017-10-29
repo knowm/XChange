7d0cec484021c86730f735867c0c7fe19f004d25
Fabien André <fabien.andre@xion345.info>
2017-10-29 15:59:08 +0100

    Bitstamp - Add support for getFundingHistory(...)
    Funding history retrieval is currently unsupported on Bitstamp,
    BitstampAccountService.createFundingHistoryParams() and
    BitstampAccountService.getFundingHistory(...) both throw
    NotYetImplementedException. Add support for funding history retrieval on
    Bitstamp, implement createFundingHistoryParams() and
    getFundingHistory(...). The getFundingHistor() implementation of this
    commit is not able to retrieve more than one page of transactions
    (currently 1000 entries), and may therefore miss funding events. This is
    however consistent with the current behavior of
    BitstampAccountService.getTradeHistory(...), which is also unable to
    retrieve more than one page of transactions.

--- xchange-bitstamp/src/main/java/org/knowm/xchange/bitstamp/service/BitstampAccountService.java
+++ xchange-bitstamp/src/main/java/org/knowm/xchange/bitstamp/service/BitstampAccountService.java
@@ -2,13 +2,16 @@ package org.knowm.xchange.bitstamp.service;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bitstamp.BitstampAdapters;
 import org.knowm.xchange.bitstamp.dto.account.BitstampDepositAddress;
 import org.knowm.xchange.bitstamp.dto.account.BitstampWithdrawal;
+import org.knowm.xchange.bitstamp.dto.trade.BitstampUserTransaction;
 import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.account.FundingRecord;
 import org.knowm.xchange.exceptions.ExchangeException;
@@ -17,7 +20,11 @@ import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.service.account.AccountService;
 import org.knowm.xchange.service.trade.params.DefaultWithdrawFundsParams;
 import org.knowm.xchange.service.trade.params.RippleWithdrawFundsParams;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamCurrencyPair;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamOffset;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamPaging;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamsSorted;
 import org.knowm.xchange.service.trade.params.WithdrawFundsParams;
 
 /**
@@ -103,11 +110,30 @@ public class BitstampAccountService extends BitstampAccountServiceRaw implements
 
   @Override
   public TradeHistoryParams createFundingHistoryParams() {
-    throw new NotAvailableFromExchangeException();
+    return new BitstampTradeHistoryParams(null, 1000);
   }
 
   @Override
-  public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
-    throw new NotYetImplementedForExchangeException();
+  public List<FundingRecord> getFundingHistory(TradeHistoryParams params)
+      throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+    Long limit = null;
+    CurrencyPair currencyPair = null;
+    Long offset = null;
+    TradeHistoryParamsSorted.Order sort = null;
+    if (params instanceof TradeHistoryParamPaging) {
+      limit = Long.valueOf(((TradeHistoryParamPaging) params).getPageLength());
+    }
+    if (params instanceof TradeHistoryParamCurrencyPair) {
+      currencyPair = ((TradeHistoryParamCurrencyPair) params).getCurrencyPair();
+    }
+    if (params instanceof TradeHistoryParamOffset) {
+      offset = ((TradeHistoryParamOffset) params).getOffset();
+    }
+    if (params instanceof TradeHistoryParamsSorted) {
+      sort = ((TradeHistoryParamsSorted) params).getOrder();
+    }
+    BitstampUserTransaction[] txs = getBitstampUserTransactions(limit, currencyPair, offset, sort == null ? null : sort.toString());
+    return BitstampAdapters.adaptFundingHistory(Arrays.asList(txs));
   }
+
 }
