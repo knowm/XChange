0d38ad5cffa3905125fa375e054938f610270bb8
Mikelle <mikhwall@gmail.com>
2017-11-01 13:19:27 +0300

    [dsx] fixed dsxIrder creating

--- xchange-dsx/src/main/java/org/knowm/xchange/dsx/service/DSXTradeService.java
+++ xchange-dsx/src/main/java/org/knowm/xchange/dsx/service/DSXTradeService.java
@@ -83,7 +83,7 @@ public class DSXTradeService extends DSXTradeServiceRaw implements TradeService 
 
     String pair = DSXAdapters.getPair(limitOrder.getCurrencyPair());
 
-    DSXOrder dsxOrder = new DSXOrder(pair, type, limitOrder.getTradableAmount(), limitOrder.getLimitPrice(),
+    DSXOrder dsxOrder = new DSXOrder(pair, type, limitOrder.getOriginalAmount(), limitOrder.getLimitPrice(),
         3, DSXOrder.OrderType.limit);
 
     DSXTradeResult result = tradeDSX(dsxOrder);
