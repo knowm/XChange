abe3f5d648916e5339d4e04b4c44235297570981
npomfret <nick-github@snowmonkey.co.uk>
2017-11-11 18:03:36 +0000

    [cexio] fix cancel order, calculate average price correctly on tradeHistory, and reformatted code with intellij default formatting

--- xchange-cexio/src/main/java/org/knowm/xchange/cexio/dto/PlaceOrderRequest.java
+++ xchange-cexio/src/main/java/org/knowm/xchange/cexio/dto/PlaceOrderRequest.java
@@ -5,17 +5,13 @@ import org.knowm.xchange.cexio.dto.trade.CexIOOrder;
 import java.math.BigDecimal;
 
 public class PlaceOrderRequest extends CexIORequest {
-    public final String tradeableIdentifier;
-    public final String currency;
-    public final CexIOOrder.Type type;
-    public final BigDecimal price;
-    public final BigDecimal amount;
+  public final CexIOOrder.Type type;
+  public final BigDecimal price;
+  public final BigDecimal amount;
 
-    public PlaceOrderRequest(String tradeableIdentifier, String currency, CexIOOrder.Type type, BigDecimal price, BigDecimal amount) {
-        this.tradeableIdentifier = tradeableIdentifier;
-        this.currency = currency;
-        this.type = type;
-        this.price = price;
-        this.amount = amount;
-    }
+  public PlaceOrderRequest(CexIOOrder.Type type, BigDecimal price, BigDecimal amount) {
+    this.type = type;
+    this.price = price;
+    this.amount = amount;
+  }
 }
