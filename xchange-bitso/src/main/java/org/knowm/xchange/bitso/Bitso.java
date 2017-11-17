c1753918dcefc0f82ad9109af914f920de420c1d
Mike Gerlyand <mike.gerlyand@iag.com.au>
2017-11-16 19:14:35 -1000

    formatting

--- xchange-bitso/src/main/java/org/knowm/xchange/bitso/Bitso.java
+++ xchange-bitso/src/main/java/org/knowm/xchange/bitso/Bitso.java
@@ -2,7 +2,11 @@ package org.knowm.xchange.bitso;
 
 import java.io.IOException;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.knowm.xchange.bitso.dto.BitsoException;
