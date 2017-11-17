47e5b57f0eac8d79b698b1b2a4b6afc9fdf48a77
Mike Gerlyand <mike.gerlyand@iag.com.au>
2017-11-16 19:14:35 -1000

    formatting

--- xchange-bitso/src/test/java/org/knowm/xchange/bitso/TickerFetchIntegration.java
+++ xchange-bitso/src/test/java/org/knowm/xchange/bitso/TickerFetchIntegration.java
@@ -1,5 +1,7 @@
 package org.knowm.xchange.bitso;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
@@ -7,18 +9,16 @@ import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class TickerFetchIntegration {
 
-    @Test
-    public void tickerFetchTest() throws Exception {
+  @Test
+  public void tickerFetchTest() throws Exception {
 
-        Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BitsoExchange.class.getName());
-        exchange.remoteInit();
-        MarketDataService marketDataService = exchange.getMarketDataService();
-        Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_MXN);
-        System.out.println(ticker.toString());
-        assertThat(ticker).isNotNull();
-    }
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BitsoExchange.class.getName());
+    exchange.remoteInit();
+    MarketDataService marketDataService = exchange.getMarketDataService();
+    Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_MXN);
+    System.out.println(ticker.toString());
+    assertThat(ticker).isNotNull();
+  }
 }
