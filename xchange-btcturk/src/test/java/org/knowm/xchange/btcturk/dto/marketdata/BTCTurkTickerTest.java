8feabb9da1c5bcda783be28b411ae10d5d2c0f1a
semihunaldi <semihunaldi@gmail.com>
2017-11-27 14:10:00 +0300

    [btcturk] marketdata api implementation

---
+++ xchange-btcturk/src/test/java/org/knowm/xchange/btcturk/dto/marketdata/BTCTurkTickerTest.java
@@ -0,0 +1,40 @@
+package org.knowm.xchange.btcturk.dto.marketdata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.knowm.xchange.btcturk.BTCTurk;
+import org.knowm.xchange.currency.CurrencyPair;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by semihunaldi on 26/11/2017
+ */
+public class BTCTurkTickerTest {
+
+  @Test
+  public void testUnmarshal() throws IOException {
+
+    // Read in the JSON from the example resources
+    InputStream is = BTCTurkTickerTest.class.getResourceAsStream("/marketdata/example-ticker-data.json");
+    ObjectMapper mapper = new ObjectMapper();
+    BTCTurkTicker btcTurkTicker = mapper.readValue(is, BTCTurkTicker.class);
+
+    // Verify that the example data was unmarshalled correctly
+    assertThat(btcTurkTicker.getPair()).isEqualTo(new BTCTurk.Pair(CurrencyPair.BTC_TRY));
+    assertThat(btcTurkTicker.getHigh()).isEqualTo(new BigDecimal("39436.99"));
+    assertThat(btcTurkTicker.getLast()).isEqualTo(new BigDecimal("38800.02"));
+    assertThat(btcTurkTicker.getTimestamp()).isEqualTo(1511724574L);
+    assertThat(btcTurkTicker.getBid()).isEqualTo(new BigDecimal("38800.02"));
+    assertThat(btcTurkTicker.getVolume()).isEqualTo(new BigDecimal("850.97"));
+    assertThat(btcTurkTicker.getLow()).isEqualTo(new BigDecimal("35501.0"));
+    assertThat(btcTurkTicker.getAsk()).isEqualTo(new BigDecimal("38800.02"));
+    assertThat(btcTurkTicker.getOpen()).isEqualTo(new BigDecimal("35599.0"));
+    assertThat(btcTurkTicker.getAverage()).isEqualTo(new BigDecimal("37909.3"));
+  }
+}
