a52681bda425c29f1983acab596e6de19ed3b399
semihunaldi <semihunaldi@gmail.com>
2017-11-27 14:10:00 +0300

    [btcturk] marketdata api implementation

---
+++ xchange-btcturk/src/main/java/org/knowm/xchange/btcturk/dto/marketdata/BTCTurkTrade.java
@@ -0,0 +1,53 @@
+package org.knowm.xchange.btcturk.dto.marketdata;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author semihunaldi
+ */
+public final class BTCTurkTrade {
+
+  private final long date;
+  private final int tid;
+  private final BigDecimal price;
+  private final BigDecimal amount;
+
+  /**
+   * Constructor
+   *
+   * @param date Unix timestamp date and time
+   * @param tid Transaction id
+   * @param price BTC price
+   * @param amount BTC amount
+   */
+  public BTCTurkTrade(@JsonProperty("date") long date, @JsonProperty("tid") int tid, @JsonProperty("price") BigDecimal price,
+      @JsonProperty("amount") BigDecimal amount) {
+    this.date = date;
+    this.tid = tid;
+    this.price = price;
+    this.amount = amount;
+  }
+
+  public long getDate() {
+    return date;
+  }
+
+  public int getTid() {
+    return tid;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  @Override
+  public String toString() {
+    return "BTCTurkTrade {" + "date=" + date + ", tid='" + tid + '\'' + ", price=" + price + ", amount=" + amount + '}';
+  }
+}
---
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/btcturk/marketdata/BTCTurkTickerDemo.java
@@ -0,0 +1,40 @@
+package org.knowm.xchange.examples.btcturk.marketdata;
+
+import java.io.IOException;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.btcturk.dto.marketdata.BTCTurkTicker;
+import org.knowm.xchange.btcturk.service.BTCTurkMarketDataService;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Ticker;
+import org.knowm.xchange.examples.btcturk.BTCTurkDemoUtils;
+import org.knowm.xchange.service.marketdata.MarketDataService;
+
+/**
+ * @author semihunaldi
+ * Demonstrate requesting Ticker at BTCTurk. You can access both the raw data from BTCTurk or the XChange generic DTO data format.
+ */
+public class BTCTurkTickerDemo {
+
+  public static void main(String[] args) throws IOException {
+
+    // Use the factory to get BTCTurk exchange API using default settings
+    Exchange btcTurk = BTCTurkDemoUtils.createExchange();
+
+    // Interested in the public market data feed (no authentication)
+    MarketDataService marketDataService = btcTurk.getMarketDataService();
+
+    generic(marketDataService);
+    raw((BTCTurkMarketDataService) marketDataService);
+  }
+
+  private static void generic(MarketDataService marketDataService) throws IOException {
+    Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_TRY);
+    System.out.println(ticker.toString());
+  }
+
+  private static void raw(BTCTurkMarketDataService marketDataService) throws IOException {
+    BTCTurkTicker btcTurkTicker = marketDataService.getBTCTurkTicker(CurrencyPair.BTC_TRY);
+    System.out.println(btcTurkTicker.toString());
+  }
+}
