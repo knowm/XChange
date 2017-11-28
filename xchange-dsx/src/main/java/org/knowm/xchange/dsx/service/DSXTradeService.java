e3dc4fea2e89d4800be7df0a599150a52772ebfa
npomfret <nick-github@snowmonkey.co.uk>
2017-11-28 10:21:42 +0000

    [dsx] fix date params for getTradeHistory (the documention of their API is wrong)

--- xchange-dsx/src/main/java/org/knowm/xchange/dsx/service/DSXTradeService.java
+++ xchange-dsx/src/main/java/org/knowm/xchange/dsx/service/DSXTradeService.java
@@ -1,10 +1,5 @@
 package org.knowm.xchange.dsx.service;
 
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -39,6 +34,11 @@ import org.knowm.xchange.service.trade.params.TradeHistoryParamsTimeSpan;
 import org.knowm.xchange.service.trade.params.orders.DefaultOpenOrdersParamCurrencyPair;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
 import org.knowm.xchange.utils.DateUtils;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * @author Mikhail Wall
@@ -135,8 +135,8 @@ public class DSXTradeService extends DSXTradeServiceRaw implements TradeService 
 
     if (params instanceof TradeHistoryParamsTimeSpan) {
       TradeHistoryParamsTimeSpan timeParams = (TradeHistoryParamsTimeSpan) params;
-      since = nullSafeUnixTime(timeParams.getStartTime());
-      end = nullSafeUnixTime(timeParams.getEndTime());
+      since = DateUtils.toMillisNullSafe(timeParams.getStartTime());
+      end = DateUtils.toMillisNullSafe(timeParams.getEndTime());
     }
 
     if (params instanceof TradeHistoryParamCurrencyPair) {
