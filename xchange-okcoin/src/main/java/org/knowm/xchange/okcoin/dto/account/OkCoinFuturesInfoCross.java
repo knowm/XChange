219e22b6511370a01668c54326b3fba857576a8a
sowhynot <30879636+sowhynot@users.noreply.github.com>
2017-12-04 23:48:00 +0000

    #issue-1938 Implement BCH funds retrieval

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/dto/account/OkCoinFuturesInfoCross.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/dto/account/OkCoinFuturesInfoCross.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class OkCoinFuturesInfoCross {
   private final OkcoinFuturesFundsCross btcFunds;
   private final OkcoinFuturesFundsCross ltcFunds;
+  private final OkcoinFuturesFundsCross bchFunds;
 
   public OkCoinFuturesInfoCross(@JsonProperty("btc") final OkcoinFuturesFundsCross btcFunds,
-      @JsonProperty("ltc") final OkcoinFuturesFundsCross ltcFunds) {
+      @JsonProperty("ltc") final OkcoinFuturesFundsCross ltcFunds, @JsonProperty("bch") final OkcoinFuturesFundsCross bchFunds ) {
     this.btcFunds = btcFunds;
     this.ltcFunds = ltcFunds;
+    this.bchFunds = bchFunds;
   }
 
   public OkcoinFuturesFundsCross getBtcFunds() {
@@ -20,4 +22,5 @@ public class OkCoinFuturesInfoCross {
     return ltcFunds;
   }
 
+  public OkcoinFuturesFundsCross getBchFunds() { return bchFunds; }
 }
