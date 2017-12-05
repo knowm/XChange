d893e36cfb9702e2d6c4f5dd9d2ebfb646a18cd3
Thomas Diesler <tdiesler@redhat.com>
2017-12-05 20:43:41 +0100

    [resolves #1947] Version mixup with fasterxml-jackson

--- xchange-coinbase/src/main/java/org/knowm/xchange/coinbase/dto/merchant/CoinbaseOrder.java
+++ xchange-coinbase/src/main/java/org/knowm/xchange/coinbase/dto/merchant/CoinbaseOrder.java
@@ -1,12 +1,16 @@
 package org.knowm.xchange.coinbase.dto.merchant;
 
-// NOTE: Order of imports matters. put fasterxml ones before xchange. Fails when compiling with Maven for some reason.
-
-// @formatter:off
-
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+
+import org.knowm.xchange.coinbase.dto.CoinbaseBaseResponse;
+import org.knowm.xchange.coinbase.dto.marketdata.CoinbaseMoney;
+import org.knowm.xchange.coinbase.dto.merchant.CoinbaseButton.CoinbaseButtonInfo;
+import org.knowm.xchange.coinbase.dto.serialization.CoinbaseCentsDeserializer;
+import org.knowm.xchange.coinbase.dto.serialization.EnumFromStringHelper;
+import org.knowm.xchange.coinbase.dto.serialization.EnumLowercaseJsonSerializer;
+import org.knowm.xchange.utils.jackson.ISO8601DateDeserializer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParser;
@@ -18,16 +22,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import org.knowm.xchange.coinbase.dto.CoinbaseBaseResponse;
-import org.knowm.xchange.coinbase.dto.marketdata.CoinbaseMoney;
-import org.knowm.xchange.coinbase.dto.merchant.CoinbaseButton.CoinbaseButtonInfo;
-import org.knowm.xchange.coinbase.dto.merchant.CoinbaseOrder.CoinbaseOrderStatus.CoinbaseOrderStatusDeserializer;
-import org.knowm.xchange.coinbase.dto.serialization.CoinbaseCentsDeserializer;
-import org.knowm.xchange.coinbase.dto.serialization.EnumFromStringHelper;
-import org.knowm.xchange.coinbase.dto.serialization.EnumLowercaseJsonSerializer;
-import org.knowm.xchange.utils.jackson.ISO8601DateDeserializer;
-
-// @formatter:on
 
 /**
  * @author jamespedwards42
@@ -101,21 +95,22 @@ public class CoinbaseOrder extends CoinbaseBaseResponse {
 
     NEW, COMPLETED, CANCELED;
 
-    static class CoinbaseOrderStatusDeserializer extends JsonDeserializer<CoinbaseOrderStatus> {
-
-      private static final EnumFromStringHelper<CoinbaseOrderStatus> FROM_STRING_HELPER = new EnumFromStringHelper<>(CoinbaseOrderStatus.class);
-
-      @Override
-      public CoinbaseOrderStatus deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-
-        ObjectCodec oc = jsonParser.getCodec();
-        JsonNode node = oc.readTree(jsonParser);
-        String jsonString = node.textValue();
-        return FROM_STRING_HELPER.fromJsonString(jsonString);
-      }
-    }
   }
 
+  static class CoinbaseOrderStatusDeserializer extends JsonDeserializer<CoinbaseOrderStatus> {
+
+    private static final EnumFromStringHelper<CoinbaseOrderStatus> FROM_STRING_HELPER = new EnumFromStringHelper<>(CoinbaseOrderStatus.class);
+
+    @Override
+    public CoinbaseOrderStatus deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+      ObjectCodec oc = jsonParser.getCodec();
+      JsonNode node = oc.readTree(jsonParser);
+      String jsonString = node.textValue();
+      return FROM_STRING_HELPER.fromJsonString(jsonString);
+    }
+  }
+  
   private static class CoinbaseOrderInfo {
 
     private final String id;
