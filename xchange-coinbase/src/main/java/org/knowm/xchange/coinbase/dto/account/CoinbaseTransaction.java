7fc4034a196a5071f75f381957d7656a9936261a
Tim Molter <tim@knowm.org>
2017-12-05 22:07:26 +0100

    [coinbase] clean DTO

--- xchange-coinbase/src/main/java/org/knowm/xchange/coinbase/dto/account/CoinbaseTransaction.java
+++ xchange-coinbase/src/main/java/org/knowm/xchange/coinbase/dto/account/CoinbaseTransaction.java
@@ -1,12 +1,16 @@
 package org.knowm.xchange.coinbase.dto.account;
 
-//NOTE: Order of imports matters. put fasterxml ones before xchange. Fails when compiling with Maven for some reason.
-// @formatter: off
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+
+import org.knowm.xchange.coinbase.dto.CoinbaseBaseResponse;
+import org.knowm.xchange.coinbase.dto.account.CoinbaseTransaction.CoinbaseTransactionStatus.CoinbaseTransactionStatusDeserializer;
+import org.knowm.xchange.coinbase.dto.account.CoinbaseUser.CoinbaseUserInfo;
+import org.knowm.xchange.coinbase.dto.marketdata.CoinbaseMoney;
+import org.knowm.xchange.coinbase.dto.serialization.EnumFromStringHelper;
+import org.knowm.xchange.utils.jackson.ISO8601DateDeserializer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonParser;
@@ -16,16 +20,6 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-import org.knowm.xchange.coinbase.dto.CoinbaseBaseResponse;
-import org.knowm.xchange.coinbase.dto.account.CoinbaseTransaction.CoinbaseTransactionStatus.CoinbaseTransactionStatusDeserializer;
-import org.knowm.xchange.coinbase.dto.account.CoinbaseUser.CoinbaseUserInfo;
-import org.knowm.xchange.coinbase.dto.marketdata.CoinbaseMoney;
-import org.knowm.xchange.coinbase.dto.serialization.EnumFromStringHelper;
-import org.knowm.xchange.utils.jackson.ISO8601DateDeserializer;
-
-// @formatter: on
-
 
 /**
  * @author jamespedwards42
