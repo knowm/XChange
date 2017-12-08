7ffc368317b3f3ee589dd8346842d8e61ed8d3fe
ahmetoz <bilmuhahmet@gmail.com>
2017-12-08 09:21:34 +0300

    [Koinim] - ticker only implementation

---
+++ xchange-koinim/src/main/java/org/knowm/xchange/koinim/service/KoinimMarketDataService.java
@@ -0,0 +1,39 @@
+package org.knowm.xchange.koinim.service;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Ticker;
+import org.knowm.xchange.dto.marketdata.Trades;
+import org.knowm.xchange.exceptions.ExchangeException;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+import org.knowm.xchange.koinim.KoinimAdapters;
+import org.knowm.xchange.service.marketdata.MarketDataService;
+
+import java.io.IOException;
+
+/**
+ * @author ahmetoz
+ */
+public class KoinimMarketDataService extends KoinimMarketDataServiceRaw implements MarketDataService {
+
+  public KoinimMarketDataService(Exchange exchange) {
+    super(exchange);
+  }
+
+  @Override
+  public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
+    return KoinimAdapters.adaptTicker(getKoinimTicker(), currencyPair);
+  }
+
+  @Override
+  public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+    throw new NotAvailableFromExchangeException();
+  }
+
+  @Override
+  public Trades getTrades(CurrencyPair currencyPair, Object... args) throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+    throw new NotAvailableFromExchangeException();
+  }
+}
