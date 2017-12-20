2bcf3125539218a3eba2e0ceb545de9ac3e29ecb
Cristian Lucaci <cristian.lucaci@tora.com>
2017-12-20 12:09:25 +0200

    [BitFlyer] New Implementation

---
+++ xchange-bitflyer/src/test/java/org/knowm/xchange/bitflyer/dto/trade/BitflyerExecutionJSONTest.java
@@ -0,0 +1,32 @@
+package org.knowm.xchange.bitflyer.dto.trade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class BitflyerExecutionJSONTest {
+  @Test
+  public void testUnmarshal() throws IOException {
+
+    // Read in the JSON from the example resources
+    InputStream is = BitflyerExecution.class.getResourceAsStream("/trade/example-execution.json");
+
+    // Use Jackson to parse it
+    ObjectMapper mapper = new ObjectMapper();
+    BitflyerExecution[] response = mapper.readValue(is, BitflyerExecution[].class);
+
+    // then
+    assertThat(response.length).isEqualTo(2);
+
+    assertThat(response[0].getId()).isEqualTo(39287);
+    assertThat(response[0].getSide()).isEqualTo("BUY");
+    assertThat(response[0].getPrice()).isEqualTo(new BigDecimal(31690));
+    assertThat(response[0].getSize()).isEqualTo(new BigDecimal("27.04"));
+  }
+}
