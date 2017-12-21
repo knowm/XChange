73197eccab9cfc927dfaac1a58aca93cce3278e9
Cristian Lucaci <cristian.lucaci@tora.com>
2017-12-21 11:23:23 +0200

    [BitFlyer] Position retrieval implementation

--- xchange-examples/src/main/java/org/knowm/xchange/examples/bitflyer/trade/BitflyerTradeDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/bitflyer/trade/BitflyerTradeDemo.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bitflyer.dto.trade.BitflyerExecution;
+import org.knowm.xchange.bitflyer.dto.trade.BitflyerPosition;
 import org.knowm.xchange.bitflyer.service.BitflyerTradeServiceRaw;
 import org.knowm.xchange.examples.bitflyer.BitflyerDemoUtils;
 import org.knowm.xchange.service.trade.TradeService;
@@ -17,6 +18,7 @@ public class BitflyerTradeDemo {
     TradeService service = exchange.getTradeService();
 
     executionsInfo(service);
+    positionsInfo(service);
   }
 
   private static void executionsInfo(TradeService service) throws IOException {
@@ -26,6 +28,17 @@ public class BitflyerTradeDemo {
     List<BitflyerExecution> executions = serviceRaw.getExecutions();
     System.out.println(executions);
     executions = serviceRaw.getExecutions("BTC_JPY");
+    System.out.println(executions);
+
+  }
+
+  private static void positionsInfo(TradeService service) throws IOException {
+    // Get the margin information
+    BitflyerTradeServiceRaw serviceRaw = (BitflyerTradeServiceRaw) service;
+
+    List<BitflyerPosition> executions = serviceRaw.getPositions();
+    System.out.println(executions);
+    executions = serviceRaw.getPositions("BTC_JPY");
     System.out.println(executions);
 
   }
