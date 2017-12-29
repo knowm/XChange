c927d4698e2346e525da49774cccaa670a552e82
Cristian Lucaci <cristian.lucaci@tora.com>
2017-12-29 12:07:36 +0200

    [Poloniex] add support for TradableBalance information

--- xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/service/PoloniexTradeServiceRaw.java
+++ xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/service/PoloniexTradeServiceRaw.java
@@ -79,6 +79,10 @@ public class PoloniexTradeServiceRaw extends PoloniexBaseService {
     return poloniexAuthenticated.returnAvailableAccountBalances(apiKey, signatureCreator, exchange.getNonceFactory(), account);
   }
 
+  public Map<String, Map<String, BigDecimal>> returnTradableBalances() throws IOException {
+    return poloniexAuthenticated.returnTradableBalances(apiKey, signatureCreator, exchange.getNonceFactory());
+  }
+
   public PoloniexAccountBalance[] returnAllAvailableAccountBalances() throws IOException {
     return poloniexAuthenticated.returnAvailableAccountBalances(apiKey, signatureCreator, exchange.getNonceFactory());
   }
