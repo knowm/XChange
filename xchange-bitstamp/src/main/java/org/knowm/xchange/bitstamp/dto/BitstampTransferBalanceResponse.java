1cd297711b8b7842dbf9668ad1c2836d096dc805
Artur <ajdev@yandex.com>
2017-12-31 07:32:51 +0530

    [Bitstamp] Add transfer sub-account balance to main

---
+++ xchange-bitstamp/src/main/java/org/knowm/xchange/bitstamp/dto/BitstampTransferBalanceResponse.java
@@ -0,0 +1,22 @@
+package org.knowm.xchange.bitstamp.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BitstampTransferBalanceResponse {
+
+  private final String status;
+  private String reason;
+
+  protected BitstampTransferBalanceResponse(@JsonProperty("status") String status, @JsonProperty("reason") String reason) {
+    this.status = status;
+    this.reason = reason;
+  }
+
+  public String getstatus() {
+    return status;
+  }
+
+  public String getReason() {
+    return reason;
+  }
+}
