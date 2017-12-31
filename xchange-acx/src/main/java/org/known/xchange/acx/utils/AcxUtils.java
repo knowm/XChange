4c56e9c8d5e812289e8672b8596da48958afcfbc
Sergey Tselovalnikov <sergeicelov@gmail.com>
2017-12-31 19:57:35 +1100

    [acx] Trading service: get&cancel orders

---
+++ xchange-acx/src/main/java/org/known/xchange/acx/utils/AcxUtils.java
@@ -0,0 +1,10 @@
+package org.known.xchange.acx.utils;
+
+import org.knowm.xchange.currency.CurrencyPair;
+
+public class AcxUtils {
+    public static String getAcxMarket(CurrencyPair currencyPair) {
+        return currencyPair.base.getCurrencyCode().toLowerCase() +
+                currencyPair.counter.getCurrencyCode().toLowerCase();
+    }
+}
