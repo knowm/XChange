2234d10bebcc6489035cc5fcce00c4cd328213cb
hurricup <hurricup@gmail.com>
2017-12-31 17:11:24 +0300

    [BitFinex] OCO orders support

--- xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/dto/trade/BitfinexLimitOrder.java
+++ xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/dto/trade/BitfinexLimitOrder.java
@@ -1,21 +1,27 @@
 package org.knowm.xchange.bitfinex.v1.dto.trade;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.trade.LimitOrder;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * Bitfinex new order entry returns details of order status. If a LimitOrder object of this type is supplied to the trade service orderEntry method it
  * will be populated with this information.
  */
 public class BitfinexLimitOrder extends LimitOrder {
+  private BigDecimal myOcoStopLimit;
 
   private BitfinexOrderStatusResponse response = null;
 
   public BitfinexLimitOrder(OrderType type, BigDecimal originalAmount, CurrencyPair currencyPair, String id, Date timestamp, BigDecimal limitPrice) {
+    this(type, originalAmount, currencyPair, id, timestamp, limitPrice, null);
+  }
+
+  public BitfinexLimitOrder(OrderType type, BigDecimal originalAmount, CurrencyPair currencyPair, String id, Date timestamp, BigDecimal limitPrice, BigDecimal ocoStopLimit) {
     super(type, originalAmount, currencyPair, id, timestamp, limitPrice);
+    myOcoStopLimit = ocoStopLimit;
   }
 
   public void setResponse(BitfinexOrderStatusResponse value) {
@@ -24,6 +30,14 @@ public class BitfinexLimitOrder extends LimitOrder {
 
   public BitfinexOrderStatusResponse getResponse() {
     return response;
+  }
+
+  public BigDecimal getOcoStopLimit() {
+    return myOcoStopLimit;
+  }
+
+  public void setOcoStopLimit(BigDecimal ocoStopLimit) {
+    myOcoStopLimit = ocoStopLimit;
   }
 
   public static class Builder extends LimitOrder.Builder {
