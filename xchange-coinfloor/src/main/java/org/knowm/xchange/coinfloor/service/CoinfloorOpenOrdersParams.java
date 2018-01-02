4183f2f96e282b2d32fc6f04543639d4e38ff7d1
hurricup <hurricup@gmail.com>
2018-01-02 17:44:26 +0300

    [BitFinex] Implemented XChange-side open orders filtering

--- xchange-coinfloor/src/main/java/org/knowm/xchange/coinfloor/service/CoinfloorOpenOrdersParams.java
+++ xchange-coinfloor/src/main/java/org/knowm/xchange/coinfloor/service/CoinfloorOpenOrdersParams.java
@@ -1,11 +1,12 @@
 package org.knowm.xchange.coinfloor.service;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParamCurrencyPair;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParamMultiCurrencyPair;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class CoinfloorOpenOrdersParams implements OpenOrdersParamMultiCurrencyPair, OpenOrdersParamCurrencyPair {
   private Collection<CurrencyPair> pairs = Collections.emptySet();
@@ -29,5 +30,10 @@ public class CoinfloorOpenOrdersParams implements OpenOrdersParamMultiCurrencyPa
   @Override
   public void setCurrencyPair(CurrencyPair value) {
     pair = value;
+  }
+
+  @Override
+  public boolean accept(LimitOrder order) {
+    return OpenOrdersParamCurrencyPair.super.accept(order) || OpenOrdersParamMultiCurrencyPair.super.accept(order);
   }
 }
