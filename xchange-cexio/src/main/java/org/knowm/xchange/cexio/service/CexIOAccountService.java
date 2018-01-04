d28009586bdeadb45967b058ed6bc0a270b74f57
bharris <bryant_harris@yahoo.com>
2018-01-03 23:15:58 -0800

    [cexio] Completing implementation of requestDepositAddress.

--- xchange-cexio/src/main/java/org/knowm/xchange/cexio/service/CexIOAccountService.java
+++ xchange-cexio/src/main/java/org/knowm/xchange/cexio/service/CexIOAccountService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.cexio.CexIOAdapters;
 import org.knowm.xchange.cexio.dto.account.CexIOBalanceInfo;
+import org.knowm.xchange.cexio.dto.account.CexIOCryptoAddress;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.account.FundingRecord;
@@ -40,8 +41,11 @@ public class CexIOAccountService extends CexIOAccountServiceRaw implements Accou
 
   @Override
   public String requestDepositAddress(Currency currency, String... arguments) throws IOException {
+    CexIOCryptoAddress cryptoAddress = getCexIOCryptoAddress(currency.getCurrencyCode());
+    if ( cryptoAddress.getOk().equals("ok"))
+      return cryptoAddress.getData();
 
-    throw new NotAvailableFromExchangeException();
+    throw new IOException(cryptoAddress.getE() + ": " + cryptoAddress.getError());
   }
 
   @Override
