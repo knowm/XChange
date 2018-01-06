d9016f6fe0641a0b6a7a2457dbf090514af3da1d
James Fleming <jamesfleming91@gmail.com>
2018-01-06 17:53:10 +0000

    Minor updates and correction to pom.xml configurtions

--- xchange-bitz/src/main/java/org/xchange/bitz/BitZExchange.java
+++ xchange-bitz/src/main/java/org/xchange/bitz/BitZExchange.java
@@ -3,32 +3,41 @@ package org.xchange.bitz;
 import org.knowm.xchange.BaseExchange;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.service.account.AccountService;
+import org.knowm.xchange.utils.nonce.CurrentTimeNonceFactory;
 import org.xchange.bitz.service.BitZMarketDataService;
+import org.xchange.bitz.service.BitZTradeService;
 
 import si.mazi.rescu.SynchronizedValueFactory;
 
 public class BitZExchange extends BaseExchange implements Exchange {
 
+  private final SynchronizedValueFactory<Long> nonceFactory = new CurrentTimeNonceFactory();
+  
 	@Override
 	protected void initServices() {
 		this.marketDataService = new BitZMarketDataService(this);
+		this.tradeService = new BitZTradeService(this);
 	}
 
 	@Override
 	public ExchangeSpecification getDefaultExchangeSpecification() {
-
-	    ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
-	    exchangeSpecification.setSslUri("https://www.bit-z.com");
-	    exchangeSpecification.setHost("http://www.bit-z.com");
-	    exchangeSpecification.setPort(80);
-	    exchangeSpecification.setExchangeName("Bit-Z");
-	    exchangeSpecification.setExchangeDescription("Bit-Z is a Bitcoin exchange registered in Hong Kong.");
-	    return exchangeSpecification;
+	  ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
+	  exchangeSpecification.setSslUri("https://www.bit-z.com");
+	  exchangeSpecification.setHost("http://www.bit-z.com");
+	  exchangeSpecification.setPort(80);
+	  exchangeSpecification.setExchangeName("Bit-Z");
+	  exchangeSpecification.setExchangeDescription("Bit-Z is a Bitcoin exchange registered in Hong Kong.");
+	  return exchangeSpecification;
 	}
-	
-	//TODO: Implement Method
+
 	@Override
 	public SynchronizedValueFactory<Long> getNonceFactory() {
-	    return null;
+	  return nonceFactory;
 	}
+
+  @Override
+  public AccountService getAccountService() {
+    throw new UnsupportedOperationException("Account Service is not provided by the exchange!");
+  }
 }
