bbb85c21a948cafb59cf6f8303f6a1bfd5c97fae
bharris <bryant_harris@yahoo.com>
2018-01-13 08:04:24 -0800

    [abucoins] Adding withdraw request.

--- xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsAccountService.java
+++ xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsAccountService.java
@@ -7,27 +7,24 @@ import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.abucoins.AbucoinsAdapters;
 import org.knowm.xchange.abucoins.dto.AbucoinsCryptoDepositRequest;
+import org.knowm.xchange.abucoins.dto.AbucoinsCryptoWithdrawalRequest;
 import org.knowm.xchange.abucoins.dto.account.AbucoinsAccount;
 import org.knowm.xchange.abucoins.dto.account.AbucoinsCryptoDeposit;
-import org.knowm.xchange.abucoins.dto.account.AbucoinsPaymentMethod;
+import org.knowm.xchange.abucoins.dto.account.AbucoinsCryptoWithdrawal;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.account.FundingRecord;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.service.account.AccountService;
+import org.knowm.xchange.service.trade.params.DefaultWithdrawFundsParams;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 import org.knowm.xchange.service.trade.params.WithdrawFundsParams;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Author: bryant_harris
  */
-
-public class AbucoinsAccountService extends AbucoinsAccountServiceRaw implements AccountService {
-  private static Logger logger = LoggerFactory.getLogger(AbucoinsAccountService.class);
-        
+public class AbucoinsAccountService extends AbucoinsAccountServiceRaw implements AccountService {        
   /**
    * Constructor
    *
@@ -45,20 +42,8 @@ public class AbucoinsAccountService extends AbucoinsAccountServiceRaw implements
 
   @Override
   public String requestDepositAddress(Currency currency, String... arguments) throws IOException {
-    String currencyStr = currency.getCurrencyCode();
-    String method = null;
-    AbucoinsPaymentMethod[] paymentMethods = getPaymentMethods();
-    for ( AbucoinsPaymentMethod apm : paymentMethods ) {
-      if ( apm.getCurrency().equals(currencyStr)) {
-        method = apm.getType();
-        break;
-      }
-    }
-          
-    if ( method == null )
-      logger.warn("Unable to determine the payment method suitable for " + currency + " this will likely lead to an error");
-
-    AbucoinsCryptoDeposit cryptoDeposit = getAbucoinsCryptoDeposit(new AbucoinsCryptoDepositRequest(currencyStr, method));
+    String method = abucoinsPaymentMethodForCurrency(currency.getCurrencyCode());
+    AbucoinsCryptoDeposit cryptoDeposit = getAbucoinsCryptoDeposit(new AbucoinsCryptoDepositRequest(currency.getCurrencyCode(), method));
     if ( cryptoDeposit.getMessage() != null )
       throw new IOException(cryptoDeposit.getMessage());
           
@@ -67,14 +52,27 @@ public class AbucoinsAccountService extends AbucoinsAccountServiceRaw implements
 
   @Override
   public String withdrawFunds(WithdrawFundsParams params) throws IOException {
-    throw new NotAvailableFromExchangeException();
+    if ( params instanceof DefaultWithdrawFundsParams ) {
+      DefaultWithdrawFundsParams defParams = (DefaultWithdrawFundsParams) params;
+                 
+      String method = abucoinsPaymentMethodForCurrency(defParams.currency.getCurrencyCode());
+      AbucoinsCryptoWithdrawal withdrawal = abucoinsWithdraw( new AbucoinsCryptoWithdrawalRequest(defParams.amount,
+                                                                                                  defParams.currency.getCurrencyCode(),
+                                                                                                  method,
+                                                                                                  defParams.address,
+                                                                                                  null));
+      return withdrawal.getPayoutId();
+    }
+         
+    if ( params == null )
+      throw new IllegalArgumentException("Requires a DefaultWithdrawFundsParams object to describe the withdrawal");
+         
+    throw new IOException("Abucoins only understands DefaultWithdrawFundsParams not " + params.getClass().getName());
   }
 
   @Override
   public String withdrawFunds(Currency currency, BigDecimal amount, String address) throws IOException {
-
-    throw new NotAvailableFromExchangeException();
-
+    return withdrawFunds(new DefaultWithdrawFundsParams(address, currency, amount));
   }
 
   @Override
@@ -83,8 +81,7 @@ public class AbucoinsAccountService extends AbucoinsAccountServiceRaw implements
   }
 
   @Override
-  public List<FundingRecord> getFundingHistory(
-      TradeHistoryParams params) throws IOException {
+  public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws IOException {
     throw new NotYetImplementedForExchangeException();
   }
 }
