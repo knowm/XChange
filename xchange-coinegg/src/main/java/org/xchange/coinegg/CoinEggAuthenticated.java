cf3c11102c136aeeb8f3b00333036f29a2134186
James Fleming <jamesfleming91@gmail.com>
2018-01-14 22:33:17 +0000

    Partial implementation of Trade Service
    	- Place limit order
    	- Cancel order
    	- View Trade History

--- xchange-coinegg/src/main/java/org/xchange/coinegg/CoinEggAuthenticated.java
+++ xchange-coinegg/src/main/java/org/xchange/coinegg/CoinEggAuthenticated.java
@@ -1,6 +1,7 @@
 package org.xchange.coinegg;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -10,6 +11,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.xchange.coinegg.dto.accounts.CoinEggBalance;
+import org.xchange.coinegg.dto.trade.CoinEggTradeAdd;
+import org.xchange.coinegg.dto.trade.CoinEggTradeCancel;
+import org.xchange.coinegg.dto.trade.CoinEggTradeList;
+import org.xchange.coinegg.dto.trade.CoinEggTradeView;
 
 import si.mazi.rescu.ParamsDigest;
 
@@ -22,5 +27,26 @@ public interface CoinEggAuthenticated {
   @Path("balance")
   CoinEggBalance getBalance(@FormParam("key") String apiKey, @FormParam("nonce") long nonce, @FormParam("signature") ParamsDigest signer) throws IOException;
   
+  @POST
+  @Path("trade_list")
+  CoinEggTradeList getTradeList(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer,
+      @FormParam("nonce") long nonce, @FormParam("since") long since, @FormParam("coin") String coin,
+      @FormParam("type ") String type) throws IOException;
+  
+  @POST
+  @Path("trade_view")
+  CoinEggTradeView getTradeView(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer,
+      @FormParam("nonce") long nonce, @FormParam("id") String tradeID, @FormParam("coin") String coin) throws IOException;
+  
+  @POST
+  @Path("trade_cancel")
+  CoinEggTradeCancel getTradeCancel(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer,
+      @FormParam("nonce") long nonce, @FormParam("id") String id, @FormParam("coin") String coin) throws IOException;
+  
+  @POST
+  @Path("trade_add")
+  CoinEggTradeAdd getTradeAdd(@FormParam("key") String apiKey, @FormParam("signature") ParamsDigest signer,
+      @FormParam("nonce") long nonce, @FormParam("amount") BigDecimal amount, @FormParam("price") BigDecimal price,
+      @FormParam("type") String type, @FormParam("coin") String coin) throws IOException;
   
 }
