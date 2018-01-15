8756a9dff2856843bfaff3019c79695a79447952
Travis Collins <TravisCollins@Koverse.com>
2018-01-14 19:40:04 -0500

    [Bitstamp] Implemented getOrder(...) for bitstamp. Returns custom implementation of an Order because Bitstamp provides very little information beyond some price and size related data.

---
+++ xchange-bitstamp/src/main/java/org/knowm/xchange/bitstamp/order/dto/BitstampGenericOrder.java
@@ -0,0 +1,23 @@
+package org.knowm.xchange.bitstamp.order.dto;
+
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class BitstampGenericOrder extends Order {
+
+    public BitstampGenericOrder(
+            OrderType type,
+            BigDecimal originalAmount,
+            CurrencyPair currencyPair,
+            String id, Date timestamp,
+            BigDecimal averagePrice,
+            BigDecimal cumulativeAmount,
+            OrderStatus status) {
+
+        super(type, originalAmount, currencyPair, id, timestamp, averagePrice, cumulativeAmount, status);
+    }
+
+}
