854724507955a10ac66ae9c8533528053043b8af
bharris <bryant_harris@yahoo.com>
2018-01-15 01:52:31 -0800

    [abucoins] Adding javadocs mapping rest calls to 'Raw' java.

--- xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsMarketDataServiceRaw.java
+++ xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsMarketDataServiceRaw.java
@@ -10,44 +10,80 @@ import org.knowm.xchange.abucoins.dto.marketdata.AbucoinsTicker;
 import org.knowm.xchange.abucoins.dto.marketdata.AbucoinsTrade;
 
 /**
+ * <p>Class providing a 1:1 proxy for the Abucoins market related
+ * REST requests.</p>
+ * 
+ * <ul>
+ * <li>{@link #getAbucoinsServerTime GET /time}</li>
+ * <li>{@link #getAbucoinsProducts GET /products}</li>
+ * <li>{@link #getAbucoinsProduct() GET /products/&#123;product-id&#125;}</li>
+ * <li>{@link #getAbucoinsOrderBook GET /products/&#123;product-id&#125;/book}</li>
+ * <li>{@link #getAbucoinsOrderBook(String, AbucoinsOrderBookLevel) GET /products/&#123;product-id&#125;/book?level=&#123;level&#125;}</li>
+ * <li>{@link #getAbucoinsTicker GET /products/&#123;product-id&#125;/ticker}</li>
+ * <li>{@link #getAbucoinsTrades(String) GET /products/&#123;product-id&#125;/trades}</li>
+ * <ol>
  * @author bryant_harris
  */
 public class AbucoinsMarketDataServiceRaw extends AbucoinsBaseService {
-
-
   /**
    * Constructor
    *
    * @param exchange
    */
   public AbucoinsMarketDataServiceRaw(Exchange exchange) {
-
     super(exchange);
-
   }
   
+  /**
+   * Corresponds to <code>GET /time</code>
+   * @return
+   * @throws IOException
+   */
   public AbucoinsServerTime getAbucoinsServerTime() throws IOException {
     return abucoins.getTime();
   }
-  
-  public AbucoinsProduct[] getAbucoinsProducts() throws IOException {
 
+  /**
+   * Corresponds to <code>GET /products</code>
+   * @return
+   * @throws IOException
+   */
+  public AbucoinsProduct[] getAbucoinsProducts() throws IOException {
     return abucoins.getProducts();
   }
   
+  /**
+   * Corresponds to <code>GET /products/{product-id}</code>
+   * @return
+   * @throws IOException
+   */  
   public AbucoinsProduct getAbucoinsProduct(String productID) throws IOException {
-
     return abucoins.getProduct(productID);
   }
   
+  /**
+   * Corresponds to <code>GET /products/{product-id}/book</code>
+   * @return
+   * @throws IOException
+   */
   public AbucoinsOrderBook getAbucoinsOrderBook(String productID) throws IOException {
     return abucoins.getBook(productID);
   }
-  
+
+  /**
+   * Corresponds to <code>GET /products/{product-id}/book?level={level}</code>
+   * @return
+   * @throws IOException
+   */
   public AbucoinsOrderBook getAbucoinsOrderBook(String productID, AbucoinsOrderBookLevel level) throws IOException {
-    return abucoins.getBook(productID);
+    return abucoins.getBook(productID, level.name());
   }
 
+  /**
+   * Corresponds to <code>GET /products/{product-id}/ticker</code>
+   * @return
+   * @throws IOException
+   */
   public AbucoinsTicker getAbucoinsTicker(String productID) throws IOException {
 
     AbucoinsTicker abucoinsTicker = abucoins.getTicker(productID);
@@ -55,6 +91,11 @@ public class AbucoinsMarketDataServiceRaw extends AbucoinsBaseService {
     return abucoinsTicker;
   }
   
+  /**
+   * Corresponds to <code>GET /products/{product-id}/trades</code>
+   * @return
+   * @throws IOException
+   */
   public AbucoinsTrade[] getAbucoinsTrades(String productID) throws IOException {
     return abucoins.getTrades(productID);
   }
