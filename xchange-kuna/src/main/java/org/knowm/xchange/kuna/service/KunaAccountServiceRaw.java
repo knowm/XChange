eba75da157cb6286b222ae6913bc2c05ef1a0a01
datbui <buidat91@yahoo.com>
2018-01-16 15:55:33 +0200

    Kuna public services

---
+++ xchange-kuna/src/main/java/org/knowm/xchange/kuna/service/KunaAccountServiceRaw.java
@@ -0,0 +1,17 @@
+package org.knowm.xchange.kuna.service;
+
+import org.knowm.xchange.Exchange;
+
+/**
+ * @author Dat Bui
+ */
+public class KunaAccountServiceRaw extends KunaBaseService {
+  /**
+   * Constructor.
+   *
+   * @param exchange
+   */
+  protected KunaAccountServiceRaw(Exchange exchange) {
+    super(exchange);
+  }
+}
