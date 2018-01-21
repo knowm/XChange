2c0b1670f08a3da1cf21ee235e1a88bb095a0a23
bharris <bryant_harris@yahoo.com>
2018-01-21 11:27:53 -0800

    [bitbay] Adding support for withdrawing crypto and fiat currencies.

--- xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/service/BitbayAccountServiceRaw.java
+++ xchange-bitbay/src/main/java/org/knowm/xchange/bitbay/service/BitbayAccountServiceRaw.java
@@ -10,13 +10,16 @@ import java.util.Map;
 import java.util.TimeZone;
 
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.bitbay.dto.BitbayBaseResponse;
 import org.knowm.xchange.bitbay.dto.acount.BitbayAccountInfoResponse;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.FundingRecord;
+import org.knowm.xchange.exceptions.ExchangeException;
 
 /**
  * @author Z. Dolezal
  */
+@SuppressWarnings("rawtypes")
 public class BitbayAccountServiceRaw extends BitbayBaseService {
 
   public BitbayAccountServiceRaw(Exchange exchange) {
@@ -30,6 +33,50 @@ public class BitbayAccountServiceRaw extends BitbayBaseService {
     return response;
   }
 
+  /**
+   * Corresponds to <code>POST /transfer</code> end point.
+   * @param currency cryptocurrency to transfer
+   * @param quantity amount of cryptocurrency, which will be transferred
+   * @param address wallet address of receiver
+   * @return Success of transfer
+   * @throws ExchangeException if an error occurred.
+   */
+  public BitbayBaseResponse transfer(Currency currency, BigDecimal quantity, String address) {
+    BitbayBaseResponse resp = bitbayAuthenticated.transfer(apiKey,
+                                                           sign,
+                                                           exchange.getNonceFactory(),
+                                                           currency.getCurrencyCode(),
+                                                           quantity.toString(),
+                                                           address);
+    if ( resp.getMessage() != null)
+      throw new ExchangeException(resp.getMessage());
+    return resp;
+  }
+
+  /**
+   * Corresponds to <code>POST /withdraw</code> end point.
+   * @param currency cryptocurrency to transfer
+   * @param quantity amount of cryptocurrency, which will be transferred
+   * @param account account number on which money would be transferred
+   * @param express true/false
+   * @param bicOrSwiftCode swift/bic number
+   * @return Success of withdraw
+   * @throws ExchangeException if an error occurred.
+   */
+  public BitbayBaseResponse withdraw(Currency currency, BigDecimal quantity, String account, boolean express, String bicOrSwiftCode) {
+    BitbayBaseResponse resp = bitbayAuthenticated.withdraw(apiKey,
+                                                           sign,
+                                                           exchange.getNonceFactory(),
+                                                           currency.getCurrencyCode(),
+                                                           quantity.toString(),
+                                                           account,
+                                                           Boolean.toString(express),
+                                                           bicOrSwiftCode);
+    if ( resp.getMessage() != null)
+      throw new ExchangeException(resp.getMessage());
+    return resp;
+  }
+  
   public List<FundingRecord> history(Currency currency, int limit) {
     List<Map> history = bitbayAuthenticated.history(apiKey, sign, exchange.getNonceFactory(), currency.getCurrencyCode(), limit);
 
