d03485b404df3d369e33c597dd406f85f0ce524f
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-01-25 02:20:28 +0100

    [Kucoin] implement getTradeHistory() call in TradingService
    This might warrant another look. Paging seemed wonky in tests. Time span
    params not implemented yet.

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/dto/KucoinAdapters.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/dto/KucoinAdapters.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order.OrderStatus;
 import org.knowm.xchange.dto.Order.OrderType;
@@ -13,14 +14,18 @@ import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trade;
+import org.knowm.xchange.dto.marketdata.Trades.TradeSortType;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.dto.trade.UserTrade;
+import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.kucoin.dto.account.KucoinUserInfoResponse;
 import org.knowm.xchange.kucoin.dto.marketdata.KucoinDealOrder;
 import org.knowm.xchange.kucoin.dto.marketdata.KucoinOrderBook;
 import org.knowm.xchange.kucoin.dto.marketdata.KucoinTicker;
 import org.knowm.xchange.kucoin.dto.trading.KucoinActiveOrder;
 import org.knowm.xchange.kucoin.dto.trading.KucoinActiveOrders;
+import org.knowm.xchange.kucoin.dto.trading.KucoinDealtOrder;
 
 public class KucoinAdapters {
   public static String adaptCurrencyPair(CurrencyPair pair) {
@@ -86,6 +91,26 @@ public class KucoinAdapters {
         .cumulativeAmount(order.getDealAmount())
         .orderStatus(order.getDealAmount().compareTo(BigDecimal.ZERO) == 0 ?
             OrderStatus.NEW : OrderStatus.PARTIALLY_FILLED)
+        .build();
+  }
+
+  public static UserTrades adaptUserTrades(List<KucoinDealtOrder> orders) {
+    List<UserTrade> trades = new LinkedList<>();
+    orders.stream().forEach(order -> trades.add(adaptUserTrade(order)));
+    return new UserTrades(trades, TradeSortType.SortByTimestamp);
+  }
+
+  private static UserTrade adaptUserTrade(KucoinDealtOrder order) {
+    return new UserTrade.Builder()
+        .currencyPair(new CurrencyPair(order.getCoinType(), order.getCoinTypePair()))
+        .orderId(order.getOid())
+        .originalAmount(order.getAmount())
+        .price(order.getDealPrice())
+        .timestamp(new Date(order.getCreatedAt()))
+        .type(order.getDirection().getOrderType())
+        .feeAmount(order.getFee())
+        .feeCurrency(order.getDirection().equals(KucoinOrderType.BUY)
+            ? new Currency(order.getCoinType()) : new Currency(order.getCoinTypePair()))
         .build();
   }
 }
