472364099c720becbacf3a5a173c93fdac99dd8d
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-01-25 23:25:40 +0100

    [Kucoin] implement requestDepositAddress call in AccountService

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/service/KucoinAccountServiceRaw.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/service/KucoinAccountServiceRaw.java
@@ -3,6 +3,8 @@ package org.knowm.xchange.kucoin.service;
 import java.io.IOException;
 
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.kucoin.dto.account.KucoinDepositAddressResponse;
 import org.knowm.xchange.kucoin.dto.account.KucoinUserInfoResponse;
 
 public class KucoinAccountServiceRaw extends KucoinBaseService {
@@ -13,5 +15,9 @@ public class KucoinAccountServiceRaw extends KucoinBaseService {
   
   KucoinUserInfoResponse userInfo() throws IOException {
     return kucoin.userInfo(apiKey, exchange.getNonceFactory(), signatureCreator);
+  }
+  
+  KucoinDepositAddressResponse walletAddress(Currency cur) throws IOException {
+    return kucoin.walletAddress(apiKey, exchange.getNonceFactory(), signatureCreator, cur.getCurrencyCode());
   }
 }
