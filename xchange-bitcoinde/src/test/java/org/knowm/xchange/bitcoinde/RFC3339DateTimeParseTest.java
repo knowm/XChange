1ad78903f0173a1ffdaf7b7fe5d4b855466fd672
Frank Kaiser <frank@kaiser.de>
2018-01-25 19:52:25 +0100

    Fix failing test

--- xchange-bitcoinde/src/test/java/org/knowm/xchange/bitcoinde/RFC3339DateTimeParseTest.java
+++ xchange-bitcoinde/src/test/java/org/knowm/xchange/bitcoinde/RFC3339DateTimeParseTest.java
@@ -1,16 +1,12 @@
 package org.knowm.xchange.bitcoinde;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
-import org.knowm.xchange.utils.DateUtils;
-
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 public class RFC3339DateTimeParseTest {
 
@@ -23,13 +19,5 @@ public class RFC3339DateTimeParseTest {
 		rfc3339timestamp = "2018-01-26T06:00:00+01:00";
 		Date date = simpleDateFormat.parse(rfc3339timestamp);
 		assertNotNull(date);
-	}
-
-	@Test
-	public void testUtil() throws InvalidFormatException {		
-		// This method does not work
-		Date date= DateUtils.fromRfc3339DateString(rfc3339timestamp);		
-		System.out.println(date);
-//		assertNotNull(date);
 	}
 }
