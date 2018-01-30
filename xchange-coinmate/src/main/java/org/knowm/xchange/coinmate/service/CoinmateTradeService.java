845c69bc866c2588b032baff296536b6e5681340
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2018-01-30 15:10:19 -0500

    [Coinmate] implemented @Override placeStopOrder

--- xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/service/CoinmateTradeService.java
+++ xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/service/CoinmateTradeService.java
@@ -29,10 +29,7 @@ import org.knowm.xchange.coinmate.dto.trade.CoinmateOpenOrders;
 import org.knowm.xchange.coinmate.dto.trade.CoinmateTradeResponse;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
-import org.knowm.xchange.dto.trade.LimitOrder;
-import org.knowm.xchange.dto.trade.MarketOrder;
-import org.knowm.xchange.dto.trade.OpenOrders;
-import org.knowm.xchange.dto.trade.UserTrades;
+import org.knowm.xchange.dto.trade.*;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.service.trade.TradeService;
 import org.knowm.xchange.service.trade.params.CancelOrderByIdParams;
@@ -101,6 +98,11 @@ public class CoinmateTradeService extends CoinmateTradeServiceRaw implements Tra
     }
 
     return Long.toString(response.getData());
+  }
+
+  @Override
+  public String placeStopOrder(StopOrder stopOrder) throws IOException {
+    throw new NotYetImplementedForExchangeException();
   }
 
   @Override
