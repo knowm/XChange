36543b65716b7f636c45f7a3d7d69fa0990fd760
cacofish <cacofish@hotmail.com>
2018-01-31 23:19:54 -0800

    Add paymentId in hitbtc withdraw
    1. Add paymentId in hitbtc withdraw
    2. Code format

--- xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcAccountServiceRaw.java
+++ xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcAccountServiceRaw.java
@@ -1,5 +1,10 @@
 package org.knowm.xchange.hitbtc.v2.service;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.exceptions.ExchangeException;
@@ -8,12 +13,8 @@ import org.knowm.xchange.hitbtc.v2.dto.HitbtcBalance;
 import org.knowm.xchange.hitbtc.v2.dto.HitbtcInternalTransferResponse;
 import org.knowm.xchange.hitbtc.v2.dto.HitbtcTransaction;
 import org.knowm.xchange.hitbtc.v2.dto.HitbtcTransferType;
-import si.mazi.rescu.HttpStatusIOException;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
+import si.mazi.rescu.HttpStatusIOException;
 
 public class HitbtcAccountServiceRaw extends HitbtcBaseService {
 
@@ -21,22 +22,20 @@ public class HitbtcAccountServiceRaw extends HitbtcBaseService {
     super(exchange);
   }
 
-  public String withdrawFundsRaw(Currency currency, BigDecimal amount, String address) throws HttpStatusIOException {
-    Map response = hitbtc.payout(amount, currency.getCurrencyCode(), address);
+  public String withdrawFundsRaw(Currency currency, BigDecimal amount, String address, String paymentId) throws HttpStatusIOException {
+    Map response = hitbtc.payout(amount, currency.getCurrencyCode(), address, paymentId);
 
     /*
-    todo:
-    if there isn't enough money we get a 400 with body:
-
-    {"error":{"code":20001,"message":"Insufficient funds","description":"Check that the funds are sufficient, given commissions"}}
-
-    ...but currently 400 errors don't reach this code
+     * todo: if there isn't enough money we get a 400 with body:
+     * {"error":{"code":20001,"message":"Insufficient funds","description":"Check that the funds are sufficient, given commissions"}} ...but currently
+     * 400 errors don't reach this code
      */
 
     return response.get("id").toString();
   }
 
-  public HitbtcInternalTransferResponse transferFunds(Currency currency, BigDecimal amount, HitbtcTransferType hitbtcTransferType) throws IOException {
+  public HitbtcInternalTransferResponse transferFunds(Currency currency, BigDecimal amount,
+      HitbtcTransferType hitbtcTransferType) throws IOException {
     return hitbtc.transferToTrading(amount, currency.getCurrencyCode(), hitbtcTransferType.getType());
   }
 
