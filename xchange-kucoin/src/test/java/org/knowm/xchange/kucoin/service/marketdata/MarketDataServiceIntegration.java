a5a3f4c8b8b1c99b6f0e1b0745f1a3175afefc01
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-02-02 22:25:54 +0100

    [Kucoin] improve integration tests

--- xchange-kucoin/src/test/java/org/knowm/xchange/kucoin/service/marketdata/MarketDataServiceIntegration.java
+++ xchange-kucoin/src/test/java/org/knowm/xchange/kucoin/service/marketdata/MarketDataServiceIntegration.java
@@ -1,7 +1,6 @@
 package org.knowm.xchange.kucoin.service.marketdata;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.knowm.xchange.Exchange;
@@ -13,22 +12,23 @@ import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.kucoin.KucoinExchange;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
-public class KucoinIntegrationTests {
+public class MarketDataServiceIntegration {
 
-  private Exchange KUCOIN = ExchangeFactory.INSTANCE.createExchange(KucoinExchange.class.getName());
-  private CurrencyPair XRB_BTC = new CurrencyPair("XRB", "BTC");
+  private static final Exchange KUCOIN = ExchangeFactory.INSTANCE.createExchange(KucoinExchange.class.getName());
+  private static final CurrencyPair XRB_BTC = new CurrencyPair("XRB", "BTC");
 
   @Test
-  public void tickerFetchTest() throws Exception {
+  public void testGetTicker() throws Exception {
 
     MarketDataService marketDataService = KUCOIN.getMarketDataService();
     Ticker ticker = marketDataService.getTicker(XRB_BTC);
-    System.out.println(ticker.toString());
-    assertThat(ticker).isNotNull();
+    assertThat(ticker.getAsk()).isGreaterThan(ticker.getBid());
+    assertThat(ticker.getHigh()).isGreaterThan(ticker.getLow());
+    assertThat(ticker.getCurrencyPair()).isEqualTo(XRB_BTC);
   }
 
   @Test
-  public void ordersFetchTest() throws Exception {
+  public void testGetOrderBook() throws Exception {
     
     OrderBook orderBookDefault = KUCOIN.getMarketDataService().getOrderBook(XRB_BTC);
     assertThat(orderBookDefault).isNotNull();
@@ -47,25 +47,14 @@ public class KucoinIntegrationTests {
   }
 
   @Test
-  public void tradesFetchTest() throws Exception {
+  public void testGetTrades() throws Exception {
 
     Trades tradesDefault = KUCOIN.getMarketDataService().getTrades(XRB_BTC);
-    System.out.println(tradesDefault.toString());
     assertThat(tradesDefault.getTrades().size()).isEqualTo(10);
+    tradesDefault.getTrades().forEach(t -> assertThat(t.getCurrencyPair()).isEqualTo(XRB_BTC));
     
     Trades tradesLimit20 = KUCOIN.getMarketDataService().getTrades(XRB_BTC, 20);
-    System.out.println("LIMIT 20");
-    System.out.println(tradesLimit20.toString());
     assertThat(tradesLimit20.getTrades().size()).isEqualTo(20);
-    
-    // couldnt get test case below to work, no idea how the since parameter works
-//    Date since = tradesLimit20.getTrades().stream()
-//        .sorted((t1, t2) -> t2.getTimestamp().compareTo(t1.getTimestamp()))
-//        .findFirst().get().getTimestamp();
-//    Trades tradesSinceLastCall = KUCOIN.getMarketDataService().getTrades(BTC_ETH, 20, since);
-//    System.out.println("WITH SINCE");
-//    System.out.println(since);
-//    System.out.println(tradesSinceLastCall.toString());
-//    assertThat(tradesSinceLastCall.getTrades().size()).isLessThan(20);
+    tradesLimit20.getTrades().forEach(t -> assertThat(t.getCurrencyPair()).isEqualTo(XRB_BTC));
   }
 }
