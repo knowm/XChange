bc5bc487368b098f045a366dbb6f845c34f01dc9
Ryan Fox <ryan@foxrow.com>
2018-02-09 15:13:25 -0600

    [bitmex] implement cancelOrder

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/service/BitmexTradeServiceRaw.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/service/BitmexTradeServiceRaw.java
@@ -72,4 +72,9 @@ public class BitmexTradeServiceRaw extends BitmexBaseService {
     return bitmex.placeOrder(apiKey, exchange.getNonceFactory(), signatureCreator, symbol, orderQuantity.intValue(),
             null, stopPrice, "Stop", executionInstructions);
   }
+
+  public boolean cancelBitmexOrder(String orderID) {
+    List<BitmexPrivateOrder> orders = bitmex.cancelOrder(apiKey, exchange.getNonceFactory(), signatureCreator, orderID);
+    return orders.get(0).getId().equals(orderID);
+  }
 }
