8059edd020f7290230fd6e7dc0ff956a3d0c5e39
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-02-10 23:15:37 +0100

    [Bibox] implement retrieving all markets (used for metadata later)

--- xchange-bibox/src/main/java/org/knowm/xchange/bibox/service/BiboxMarketDataServiceRaw.java
+++ xchange-bibox/src/main/java/org/knowm/xchange/bibox/service/BiboxMarketDataServiceRaw.java
@@ -3,10 +3,12 @@ package org.knowm.xchange.bibox.service;
 import static org.knowm.xchange.bibox.dto.BiboxAdapters.toBiboxPair;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bibox.BiboxException;
 import org.knowm.xchange.bibox.dto.BiboxAdapters;
+import org.knowm.xchange.bibox.dto.marketdata.BiboxMarket;
 import org.knowm.xchange.bibox.dto.marketdata.BiboxTicker;
 import org.knowm.xchange.bibox.dto.trade.BiboxOrderBook;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -16,6 +18,7 @@ public class BiboxMarketDataServiceRaw extends BiboxBaseService {
 
   private static final String TICKER_CMD = "ticker";
   private static final String DEPTH_CMD = "depth";
+  private static final String ALL_TICKERS_CMD = "marketAll";
 
   protected BiboxMarketDataServiceRaw(Exchange exchange) {
     super(exchange);
@@ -32,6 +35,14 @@ public class BiboxMarketDataServiceRaw extends BiboxBaseService {
   public BiboxOrderBook getBiboxOrderBook(CurrencyPair currencyPair, Integer depth) throws IOException {
     try {
       return bibox.orderBook(DEPTH_CMD, BiboxAdapters.toBiboxPair(currencyPair), depth).getResult();
+    } catch (BiboxException e) {
+      throw new ExchangeException(e.getMessage());
+    }
+  }
+
+  public List<BiboxMarket> getAllBiboxTickers() throws IOException {
+    try {
+      return bibox.marketAll(ALL_TICKERS_CMD).getResult();
     } catch (BiboxException e) {
       throw new ExchangeException(e.getMessage());
     }
