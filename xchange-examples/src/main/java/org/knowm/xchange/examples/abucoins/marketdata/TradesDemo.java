9c1be4c27e3b6ef5269e40ae3b7e9f6e5c50ad66
bharris <bryant_harris@yahoo.com>
2018-02-19 11:28:04 -0800

    [examples] Adding abucoins to samples project.

---
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/abucoins/marketdata/TradesDemo.java
@@ -0,0 +1,32 @@
+package org.knowm.xchange.examples.abucoins.marketdata;
+
+import java.io.IOException;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.abucoins.AbucoinsExchange;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Trades;
+import org.knowm.xchange.service.marketdata.MarketDataService;
+
+/**
+ * Author: bryant_harris
+ */
+
+public class TradesDemo {
+
+  public static void main(String[] args) throws IOException {
+
+    // Use the factory to get Cex.IO exchange API using default settings
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(AbucoinsExchange.class.getName());
+
+    // Interested in the public market data feed (no authentication)
+    MarketDataService marketDataService = exchange.getMarketDataService();
+
+    // Get the latest trade data 
+    Trades trades = marketDataService.getTrades(CurrencyPair.BTC_USD);
+    System.out.println("Trades Size= " + trades.getTrades().size());
+    System.out.println(trades.toString());
+  }
+
+}
