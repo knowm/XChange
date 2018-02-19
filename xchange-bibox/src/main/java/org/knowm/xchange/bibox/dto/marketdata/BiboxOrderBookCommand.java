5091f9afb44ce5e115bcc26714b6fdaadf44acdb
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-02-20 00:07:03 +0100

    [Bibox] retrieve all order books

---
+++ xchange-bibox/src/main/java/org/knowm/xchange/bibox/dto/marketdata/BiboxOrderBookCommand.java
@@ -0,0 +1,11 @@
+package org.knowm.xchange.bibox.dto.marketdata;
+
+import org.knowm.xchange.bibox.dto.BiboxCommand;
+
+public class BiboxOrderBookCommand extends BiboxCommand<BiboxOrderBookCommandBody>{
+
+  public BiboxOrderBookCommand(String pair, Integer size) {
+    super("api/depth", new BiboxOrderBookCommandBody(pair, size));
+  }
+
+}
