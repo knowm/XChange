4964b7446e3afc169a21e63b78e9ee43ea90ad00
Your Name <you@example.com>
2018-02-21 22:39:49 +0700

    idex updates

--- xchange-idex/src/test/java/org/knowm/xchange/idex/IdexSMokeTest.kt
+++ xchange-idex/src/test/java/org/knowm/xchange/idex/IdexSMokeTest.kt
@@ -1,13 +1,23 @@
 package org.knowm.xchange.idex
 
 import org.knowm.xchange.*
+import org.knowm.xchange.currency.*
 
 fun main(args: Array<String>) {
     val apiKey = args[0]
-    val apiZekret = if(args.size > 1) args[1] else ""
+    System.err.println("level 1.... grabbing your wallet")
+    val apiZekret = if (args.size > 1) args[1] else ""
 
     val idex = ExchangeFactory.INSTANCE.createExchange(IdexExchange::class.java, apiKey, apiZekret);
     val accountInfo = idex.accountService.accountInfo
 
     System.err.println(accountInfo.toString())
+
+    System.err.println("level 2... looking for gold in your teeth (ticker for ETH_DCN)")
+    val ticker = idex.marketDataService.getTicker(CurrencyPair("ETH", "DCN"));
+    val tickers = ticker.toString()
+    System.err.println(
+            tickers
+    )
+
 }
