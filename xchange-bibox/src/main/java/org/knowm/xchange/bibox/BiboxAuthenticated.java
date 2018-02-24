37f6dc296c95473bcdca3799a2d3d24957ee030f
Oliver Drotleff <oliver.drotleff@gmail.com>
2018-02-24 13:19:26 +0100

    [Bibox] Cancel multiple orders in one call

--- xchange-bibox/src/main/java/org/knowm/xchange/bibox/BiboxAuthenticated.java
+++ xchange-bibox/src/main/java/org/knowm/xchange/bibox/BiboxAuthenticated.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.knowm.xchange.bibox.dto.BiboxMultipleResponses;
 import org.knowm.xchange.bibox.dto.BiboxSingleResponse;
 import org.knowm.xchange.bibox.dto.account.BiboxCoin;
 import org.knowm.xchange.bibox.dto.trade.BiboxOrders;
@@ -79,6 +80,18 @@ public interface BiboxAuthenticated extends Bibox {
   @POST
   @Path("orderpending")
   BiboxSingleResponse<BiboxOrders> orderPendingList(
+      @FormParam(FORM_CMDS) String cmds,
+      @FormParam(FORM_APIKEY) String apiKey,
+      @FormParam(FORM_SIGNATURE) ParamsDigest signature);
+
+  /**
+   * Cancel multiple orders
+   * 
+   * @return multiple times chinese for "cancelled", useless
+   */
+  @POST
+  @Path("orderpending")
+  BiboxMultipleResponses<String> cancelTrades(
       @FormParam(FORM_CMDS) String cmds,
       @FormParam(FORM_APIKEY) String apiKey,
       @FormParam(FORM_SIGNATURE) ParamsDigest signature);
