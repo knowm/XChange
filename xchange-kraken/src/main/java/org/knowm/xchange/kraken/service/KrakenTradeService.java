78a0312b5b85123b6f95a417195f4a4b1b9a14cd
npomfret <nick-github@snowmonkey.co.uk>
2018-02-27 12:18:39 +0000

    [kraken] just a reformat (no functional change)

--- xchange-kraken/src/main/java/org/knowm/xchange/kraken/service/KrakenTradeService.java
+++ xchange-kraken/src/main/java/org/knowm/xchange/kraken/service/KrakenTradeService.java
@@ -1,18 +1,18 @@
 package org.knowm.xchange.kraken.service;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.dto.Order;
-import org.knowm.xchange.dto.trade.*;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
+import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.dto.trade.StopOrder;
+import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.exceptions.ExchangeException;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.kraken.KrakenAdapters;
-import org.knowm.xchange.kraken.dto.trade.KrakenOrder;
 import org.knowm.xchange.service.trade.TradeService;
 import org.knowm.xchange.service.trade.params.CancelOrderByIdParams;
 import org.knowm.xchange.service.trade.params.CancelOrderParams;
