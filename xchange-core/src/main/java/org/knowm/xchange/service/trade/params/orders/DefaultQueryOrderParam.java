a9220c45ab1ed70aa61e3a17ba51b6a7f8635827
aleks-azen <aleksazen@gmail.com>
2018-03-01 15:35:46 -0500

    added default implementation for cancel and query order params

---
+++ xchange-core/src/main/java/org/knowm/xchange/service/trade/params/orders/DefaultQueryOrderParam.java
@@ -0,0 +1,20 @@
+package org.knowm.xchange.service.trade.params.orders;
+
+
+public class DefaultQueryOrderParam implements OrderQueryParams{
+    private String orderId;
+
+    public DefaultQueryOrderParam(String orderId){
+        this.orderId = orderId;
+    }
+
+    @Override
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    @Override
+    public String getOrderId() {
+        return orderId;
+    }
+}
