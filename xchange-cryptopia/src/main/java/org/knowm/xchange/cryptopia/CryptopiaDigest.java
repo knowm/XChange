e5ce562ad2d2a4cb4dfaee11fb84209400a5f0d7
Jim Northrup <jnorthrup@users.noreply.github.com>
2018-03-04 09:53:11 +0700

    [cryptopia] monkeypatch the GetAccountInfo failure

--- xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/CryptopiaDigest.java
+++ xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/CryptopiaDigest.java
@@ -1,63 +1,143 @@
 package org.knowm.xchange.cryptopia;
 
-import java.io.IOException;
+import org.knowm.xchange.service.BaseParamsDigest;
+import si.mazi.rescu.RestInvocation;
+import si.mazi.rescu.SynchronizedValueFactory;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
-import org.knowm.xchange.service.BaseParamsDigest;
-
-import net.iharder.Base64;
-import si.mazi.rescu.RestInvocation;
-import si.mazi.rescu.SynchronizedValueFactory;
-
 public class CryptopiaDigest extends BaseParamsDigest {
 
-  private final SynchronizedValueFactory<Long> nonceFactory;
-  private final String apiKey;
+    private final SynchronizedValueFactory<Long> nonceFactory;
+    private final String apiKey;
+    private final String secretKey;
 
-  private CryptopiaDigest(SynchronizedValueFactory<Long> nonceFactory, String secretKey, String apiKey) throws IOException {
-    super(decodeBase64(secretKey), HMAC_SHA_256);
+    private CryptopiaDigest(SynchronizedValueFactory<Long> nonceFactory, String secretKey, String apiKey) {
+        super(BaseParamsDigest.decodeBase64(secretKey), BaseParamsDigest.HMAC_SHA_256);
 
-    this.nonceFactory = nonceFactory;
-    this.apiKey = apiKey;
-  }
-
-  public static CryptopiaDigest createInstance(SynchronizedValueFactory<Long> nonceFactory, String secretKey, String apiKey) {
-    if (secretKey == null)
-      return null;
-
-    try {
-      return new CryptopiaDigest(nonceFactory, secretKey, apiKey);
-    } catch (Exception e) {
-      throw new IllegalStateException("cannot create digest", e);
+        this.nonceFactory = nonceFactory;
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
     }
-  }
 
-  @Override
-  public String digestParams(RestInvocation restInvocation) {
-    try {
-      String urlMethod = restInvocation.getInvocationUrl();
-      String nonce = String.valueOf(nonceFactory.createValue());
+    public static CryptopiaDigest createInstance(SynchronizedValueFactory<Long> nonceFactory, String secretKey, String apiKey) {
+        if (secretKey == null)
+            return null;
 
-      String body = restInvocation.getRequestBody();
-      String md5 = Base64.encodeBytes(MessageDigest.getInstance("MD5").digest(body.getBytes("UTF-8")));
-
-      String reqSignature =
-          apiKey
-              + "POST"
-              + URLEncoder.encode(urlMethod, StandardCharsets.UTF_8.toString()).toLowerCase()
-              + nonce
-              + md5;
-
-      return "amx "
-          + apiKey
-          + ":"
-          + Base64.encodeBytes(getMac().doFinal(reqSignature.getBytes("UTF-8")))
-          + ":"
-          + nonce;
-    } catch (Exception e) {
-      throw new IllegalStateException("Faile to sign request", e);
+        try {
+            return new CryptopiaDigest(nonceFactory, secretKey, apiKey);
+        } catch (Exception e) {
+            throw new IllegalStateException("cannot create digest", e);
+        }
     }
-  }
+
+    @Override
+    public String digestParams(RestInvocation restInvocation) {
+        try {
+
+            String urlMethod = CryptopiaDigest.privateUrl + restInvocation.getHttpMethod();
+            String nonce = String.valueOf(System.currentTimeMillis());
+
+            String reqSignature =
+                    this.apiKey
+                            + "POST"
+                            + URLEncoder.encode(urlMethod, StandardCharsets.UTF_8.toString()).toLowerCase()
+                            + nonce
+                            + getMD5_B64(restInvocation.getRequestBody());
+
+            String AUTH = "amx "
+                    + this.apiKey
+                    + ":"
+                    + sha256_B64(secretKey, reqSignature)
+                    + ":"
+                    + nonce;
+            return AUTH;
+        } catch (Exception e) {
+            throw new IllegalStateException("Faile to sign request", e);
+        }
+    }
+
+    public static String privateUrl = "https://www.cryptopia.co.nz/Api/";
+
+    public static class HttpCryptopia {
+        public final String response;
+
+        public HttpCryptopia(String method, String jSonPostParam, String apiKey, String secretKey) throws Exception {
+            String urlMethod = privateUrl + method;
+            String nonce = String.valueOf(System.currentTimeMillis());
+
+            String reqSignature =
+                    apiKey
+                            + "POST"
+                            + URLEncoder.encode(urlMethod, StandardCharsets.UTF_8.toString()).toLowerCase()
+                            + nonce
+                            + CryptopiaDigest.getMD5_B64(jSonPostParam);
+
+            String AUTH = "amx "
+                    + apiKey
+                    + ":"
+                    + sha256_B64(secretKey, reqSignature)
+                    + ":"
+                    + nonce;
+
+            this.response = continueForHttp(urlMethod, jSonPostParam, AUTH);
+
+
+            if (HttpCryptopia.debugMe) System.out.println("API RESPONSE : " + response);
+        }
+
+        static final Boolean debugMe = System.getProperty("XChangeDebug", "false") == "true";
+
+        private String continueForHttp(String urlMethod, String postParam, String AUTH) throws Exception {
+            URLConnection con = new URL(urlMethod).openConnection(); // CREATE POST CONNECTION
+            con.setDoOutput(true);
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) con;
+            httpsConn.setRequestMethod("POST");
+            httpsConn.setInstanceFollowRedirects(true);
+
+            con.setRequestProperty("Authorization", AUTH);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            // WRITE POST PARAMS
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParam);
+            wr.flush();
+            wr.close();
+
+            // READ THE RESPONSE
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        }
+
+
+    }
+
+    static String getMD5_B64(String postParameter) throws Exception {
+        return java.util.Base64.getEncoder().encodeToString(MessageDigest.getInstance("MD5").digest(postParameter.getBytes("UTF-8")));
+    }
+
+    static String sha256_B64(String secretKey, String msg) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKey), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        return java.util.Base64.getEncoder().encodeToString(sha256_HMAC.doFinal(msg.getBytes("UTF-8")));
+    }
 }
--- xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/service/CryptopiaAccountServiceRaw.java
+++ xchange-cryptopia/src/main/java/org/knowm/xchange/cryptopia/service/CryptopiaAccountServiceRaw.java
@@ -1,5 +1,7 @@
 package org.knowm.xchange.cryptopia.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -7,9 +9,11 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.knowm.xchange.cryptopia.Cryptopia;
 import org.knowm.xchange.cryptopia.CryptopiaAdapters;
+import org.knowm.xchange.cryptopia.CryptopiaDigest;
 import org.knowm.xchange.cryptopia.CryptopiaExchange;
 import org.knowm.xchange.cryptopia.dto.CryptopiaBaseResponse;
 import org.knowm.xchange.currency.Currency;
@@ -19,100 +23,117 @@ import org.knowm.xchange.exceptions.ExchangeException;
 
 public class CryptopiaAccountServiceRaw extends CryptopiaBaseService {
 
-  public CryptopiaAccountServiceRaw(CryptopiaExchange exchange) {
+    public CryptopiaAccountServiceRaw(CryptopiaExchange exchange) {
 
-    super(exchange);
+        super(exchange);
 
-  }
-
-  public List<Balance> getBalances() throws IOException {
-
-    CryptopiaBaseResponse<List<Map>> response = cryptopia.getBalance(signatureCreator, new HashMap<>());
-    if (!response.isSuccess())
-      throw new ExchangeException("Failed to get balance: " + response.toString());
-
-    List<Balance> balances = new ArrayList<>();
-    for (Map datum : response.getData()) {
-      Currency symbol = Currency.getInstance(datum.get("Symbol").toString());
-      BigDecimal total = new BigDecimal(datum.get("Total").toString());
-      BigDecimal available = new BigDecimal(datum.get("Available").toString());
-      BigDecimal heldForTrades = new BigDecimal(datum.get("HeldForTrades").toString());
-      BigDecimal pendingWithdraw = new BigDecimal(datum.get("PendingWithdraw").toString());
-      BigDecimal unconfirmed = new BigDecimal(datum.get("Unconfirmed").toString());
-      Balance balance = new Balance(symbol, total, available, heldForTrades, BigDecimal.ZERO, BigDecimal.ZERO, pendingWithdraw, unconfirmed);
-
-      balances.add(balance);
     }
 
-    return balances;
-  }
+    public List<Balance> getBalances() throws IOException {
 
-  public String submitWithdraw(Currency currency, BigDecimal amount, String address, String paymentId) throws IOException {
-    CryptopiaBaseResponse<Long> response = cryptopia.submitWithdraw(signatureCreator, new Cryptopia.SubmitWithdrawRequest(currency.getCurrencyCode(), address, paymentId, amount));
-    if (!response.isSuccess())
-      throw new ExchangeException("Failed to withdraw funds: " + response.toString());
+//      @POST
+//  @Path("GetBalance")
+//  @Consumes(MediaType.APPLICATION_JSON)
+//  CryptopiaBaseResponse<List<Map>> getBalance(@HeaderParam("Authorization") ParamsDigest signature, Object o) throws IOException;
+        CryptopiaDigest.HttpCryptopia httpCryptopia = null;
+        try {
+            httpCryptopia = new CryptopiaDigest.HttpCryptopia("GetBalance", "{}", exchange.getExchangeSpecification().getApiKey(), exchange.getExchangeSpecification().getSecretKey());
+            String response = httpCryptopia.response;
+            Map map = new Gson().fromJson(response, Map.class);
 
-    return String.valueOf(response.getData());
-  }
+            List<Balance> balances = new ArrayList<>();
+            List<? extends Map> data = (List<? extends Map>) map.get("Data");
 
-  public String getDepositAddress(Currency currency) throws IOException {
-    CryptopiaBaseResponse<Map> response = cryptopia.getDepositAddress(signatureCreator, new Cryptopia.GetDepositAddressRequest(currency.getCurrencyCode()));
-    if (!response.isSuccess())
-      throw new ExchangeException("Failed to get address: " + response.toString());
+            return data.parallelStream().filter(o -> Double.parseDouble(o.get("Total").toString()) != 0.0D).map(
+                    datum -> {
+                        BigDecimal total = new BigDecimal(datum.get("Total").toString());
+                        Currency symbol = new Currency(datum.get("Symbol").toString());
+                        BigDecimal available = new BigDecimal(datum.get("Available").toString());
+                        BigDecimal heldForTrades = new BigDecimal(datum.get("HeldForTrades").toString());
+                        BigDecimal pendingWithdraw = new BigDecimal(datum.get("PendingWithdraw").toString());
+                        BigDecimal unconfirmed = new BigDecimal(datum.get("Unconfirmed").toString());
+                        return new Balance(symbol, total, available, heldForTrades, BigDecimal.ZERO, BigDecimal.ZERO, pendingWithdraw, unconfirmed);
+                    }).collect(Collectors.toList());
 
-    return response.getData().get("Address").toString();
-  }
-  
-  public List<FundingRecord> getTransactions(String type, Integer count) throws IOException {
-    CryptopiaBaseResponse<List<Map>> response = cryptopia.getTransactions(signatureCreator, new Cryptopia.GetTransactionsRequest(type, count));
-    if (!response.isSuccess())
-      throw new ExchangeException("Failed to get transactions: " + response.toString());
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    List<FundingRecord> results = new ArrayList<>();
-    for (Map map : response.getData()) {
-      Date timeStamp = CryptopiaAdapters.convertTimestamp(map.get("Timestamp").toString());
-      Currency currency = Currency.getInstance(map.get("Currency").toString());
-      FundingRecord.Type fundingType = map.get("Type").toString().equals(CryptopiaAccountService.CryptopiaFundingHistoryParams.Type.Deposit.name())
-          ? FundingRecord.Type.DEPOSIT : FundingRecord.Type.WITHDRAWAL;
+   /*     CryptopiaBaseResponse<List<Map>> response = new CryptopiaBaseResponse<>(false, httpCryptopia.response, new ArrayList<>(), httpCryptopia.response);
+        if (!response.isSuccess())
+            throw new ExchangeException("Failed to get balance: " + response);
+*/
+        return null;
+    }
 
-      FundingRecord.Status status;
-      String rawStatus = map.get("Status").toString();
-      switch (rawStatus) {
-        case "UnConfirmed":
-        case "Pending":
-            status = FundingRecord.Status.PROCESSING;
-            break;
-        case "Confirmed":
-        case "Complete":
-            status = FundingRecord.Status.COMPLETE;
-            break;
-        default:
-            status = FundingRecord.Status.resolveStatus(rawStatus);
-            if (status == null) {
-                status = FundingRecord.Status.FAILED; 
+    public String submitWithdraw(Currency currency, BigDecimal amount, String address, String paymentId) throws IOException {
+        CryptopiaBaseResponse<Long> response = cryptopia.submitWithdraw(signatureCreator, new Cryptopia.SubmitWithdrawRequest(currency.getCurrencyCode(), address, paymentId, amount));
+        if (!response.isSuccess())
+            throw new ExchangeException("Failed to withdraw funds: " + response.toString());
+
+        return String.valueOf(response.getData());
+    }
+
+    public String getDepositAddress(Currency currency) throws IOException {
+        CryptopiaBaseResponse<Map> response = cryptopia.getDepositAddress(signatureCreator, new Cryptopia.GetDepositAddressRequest(currency.getCurrencyCode()));
+        if (!response.isSuccess())
+            throw new ExchangeException("Failed to get address: " + response.toString());
+
+        return response.getData().get("Address").toString();
+    }
+
+    public List<FundingRecord> getTransactions(String type, Integer count) throws IOException {
+        CryptopiaBaseResponse<List<Map>> response = cryptopia.getTransactions(signatureCreator, new Cryptopia.GetTransactionsRequest(type, count));
+        if (!response.isSuccess())
+            throw new ExchangeException("Failed to get transactions: " + response.toString());
+
+        List<FundingRecord> results = new ArrayList<>();
+        for (Map map : response.getData()) {
+            Date timeStamp = CryptopiaAdapters.convertTimestamp(map.get("Timestamp").toString());
+            Currency currency = Currency.getInstance(map.get("Currency").toString());
+            FundingRecord.Type fundingType = map.get("Type").toString().equals(CryptopiaAccountService.CryptopiaFundingHistoryParams.Type.Deposit.name())
+                    ? FundingRecord.Type.DEPOSIT : FundingRecord.Type.WITHDRAWAL;
+
+            FundingRecord.Status status;
+            String rawStatus = map.get("Status").toString();
+            switch (rawStatus) {
+                case "UnConfirmed":
+                case "Pending":
+                    status = FundingRecord.Status.PROCESSING;
+                    break;
+                case "Confirmed":
+                case "Complete":
+                    status = FundingRecord.Status.COMPLETE;
+                    break;
+                default:
+                    status = FundingRecord.Status.resolveStatus(rawStatus);
+                    if (status == null) {
+                        status = FundingRecord.Status.FAILED;
+                    }
+                    break;
             }
-            break;
-      }
 
-      String address = map.get("Address") == null ? null : map.get("Address").toString();
+            String address = map.get("Address") == null ? null : map.get("Address").toString();
 
-      FundingRecord fundingRecord = new FundingRecord(
-          address,
-          timeStamp,
-          currency,
-          new BigDecimal(map.get("Amount").toString()),
-          map.get("Id").toString(),
-          map.get("TxId").toString(),
-          fundingType,
-          status,
-          null,
-          new BigDecimal(map.get("Fee").toString()),
-          null
-      );
+            FundingRecord fundingRecord = new FundingRecord(
+                    address,
+                    timeStamp,
+                    currency,
+                    new BigDecimal(map.get("Amount").toString()),
+                    map.get("Id").toString(),
+                    map.get("TxId").toString(),
+                    fundingType,
+                    status,
+                    null,
+                    new BigDecimal(map.get("Fee").toString()),
+                    null
+            );
 
-      results.add(fundingRecord);
+            results.add(fundingRecord);
+        }
+
+        return results;
     }
-
-    return results;
-  }
 }
