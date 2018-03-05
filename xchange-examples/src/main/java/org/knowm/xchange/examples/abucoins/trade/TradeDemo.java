eaa8ec7a47ac04b4da8a9630ee40e3b006538fcc
bharris <bryant_harris@yahoo.com>
2018-03-05 12:58:16 -0800

    [abucoins] Adding TradeHistory using fills.

--- xchange-examples/src/main/java/org/knowm/xchange/examples/abucoins/trade/TradeDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/abucoins/trade/TradeDemo.java
@@ -2,15 +2,21 @@ package org.knowm.xchange.examples.abucoins.trade;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
 
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.abucoins.AbucoinsAdapters;
+import org.knowm.xchange.abucoins.dto.AbucoinsOrderRequest;
+import org.knowm.xchange.abucoins.dto.trade.AbucoinsOrder;
+import org.knowm.xchange.abucoins.service.AbucoinsTradeServiceRaw;
 import org.knowm.xchange.cexio.dto.trade.CexIOOrder;
 import org.knowm.xchange.cexio.service.CexIOTradeServiceRaw;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.examples.abucoins.AbucoinsDemoUtils;
 import org.knowm.xchange.exceptions.ExchangeException;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
@@ -29,12 +35,11 @@ public class TradeDemo {
     TradeService tradeService = exchange.getTradeService();
 
     generic(tradeService);
-    raw((CexIOTradeServiceRaw) tradeService);
+    raw((AbucoinsTradeServiceRaw) tradeService);
 
   }
 
-  private static void generic(
-      TradeService tradeService) throws NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
+  private static void generic(TradeService tradeService) throws NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException {
 
     printOpenOrders(tradeService);
 
@@ -56,12 +61,15 @@ public class TradeDemo {
     System.out.println("Canceling order id=" + orderId + " returned " + cancelResult);
 
     printOpenOrders(tradeService);
+    
+    UserTrades userTrades = tradeService.getTradeHistory(tradeService.createTradeHistoryParams());
+    System.out.println(userTrades);
   }
 
-  private static void raw(CexIOTradeServiceRaw tradeService) throws IOException {
+  private static void raw(AbucoinsTradeServiceRaw tradeService) throws IOException {
 
-    List<CexIOOrder> openOrders = tradeService.getCexIOOpenOrders(new CurrencyPair("NMC", "BTC"));
-    System.out.println(openOrders);
+    AbucoinsOrder[] openOrders = tradeService.getAbucoinsOrders( new AbucoinsOrderRequest(AbucoinsAdapters.adaptCurrencyPairToProductID(CurrencyPair.BTC_USD)));
+    System.out.println(Arrays.asList(openOrders));
   }
 
   private static void printOpenOrders(TradeService tradeService) throws IOException {
