0da8f68498b0ca8232bf455cecb7f2c7ab546b8a
Your Name <you@example.com>
2018-03-27 06:20:25 +0700

    [bitz] large structure refactor operation #2432

--- xchange-bitz/src/test/java/org/xchange/bitz/dto/marketdata/BitZKlineJSONTest.java
+++ xchange-bitz/src/test/java/org/xchange/bitz/dto/marketdata/BitZKlineJSONTest.java
@@ -17,7 +17,7 @@ public class BitZKlineJSONTest {
   public void testUnmarshal() throws IOException {
 
     // Read in the JSON from the example resources
-    InputStream is = BitZKlineJSONTest.class.getResourceAsStream("/marketdata/example-ticker-data.json");
+    InputStream is = BitZKlineJSONTest.class.getResourceAsStream("/org/xchange/bitz/dto/marketdata/example-ticker-data.json");
 
     // Parse JSON Example Using Jackson
     ObjectMapper mapper = new ObjectMapper();
