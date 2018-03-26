94a0426023c136d5a9942abc7f9f54fe94204080
Your Name <you@example.com>
2018-03-27 06:17:07 +0700

    refscript

--- etc/util/mass-checkin-refactor.sh
+++ etc/util/mass-checkin-refactor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in xchange-core xchange-*; do rsync -ahP --delete-after ../refactor/$i . ; git add $i;git commit -am "[${i/xchange-/}] large structure refactor operation #2432"  ;git tag -f "${i/-/_}_2432" commit ;done
+for i in xchange-core xchange-*; do rsync -ahP --delete-after ../refactor/$i . ; git add $i;git commit -am "[${i/xchange-/}] large structure refactor operation #2432"  ;git tag -f "${i/-/_}_2432" ;done
