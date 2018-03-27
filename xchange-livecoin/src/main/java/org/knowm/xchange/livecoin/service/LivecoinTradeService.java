b14eb91ca1999c3f17dd54d860ab785af9b9fb1b
Your Name <you@example.com>
2018-03-27 08:26:42 +0700

    [livecoin] pom reformatter first time

--- xchange-livecoin/src/main/java/org/knowm/xchange/livecoin/service/LivecoinTradeService.java
+++ xchange-livecoin/src/main/java/org/knowm/xchange/livecoin/service/LivecoinTradeService.java
@@ -3,7 +3,6 @@ package org.knowm.xchange.livecoin.service;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Date;
-
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.dto.trade.LimitOrder;
@@ -61,7 +60,8 @@ public class LivecoinTradeService extends LivecoinTradeServiceRaw implements Tra
       limit = ((TradeHistoryParamLimit) params).getLimit();
     }
 
-    return new UserTrades(tradeHistory(start, end, limit, offset), Trades.TradeSortType.SortByTimestamp);
+    return new UserTrades(
+        tradeHistory(start, end, limit, offset), Trades.TradeSortType.SortByTimestamp);
   }
 
   @Override
