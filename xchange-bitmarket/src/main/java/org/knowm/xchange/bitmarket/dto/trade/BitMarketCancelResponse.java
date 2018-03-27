71035677c0f859edf1b9c3667c224ffc5df8bb57
Your Name <you@example.com>
2018-03-27 08:25:58 +0700

    [bitmarket] pom reformatter first time

--- xchange-bitmarket/src/main/java/org/knowm/xchange/bitmarket/dto/trade/BitMarketCancelResponse.java
+++ xchange-bitmarket/src/main/java/org/knowm/xchange/bitmarket/dto/trade/BitMarketCancelResponse.java
@@ -1,14 +1,11 @@
 package org.knowm.xchange.bitmarket.dto.trade;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.knowm.xchange.bitmarket.dto.BitMarketAPILimit;
 import org.knowm.xchange.bitmarket.dto.BitMarketBaseResponse;
 import org.knowm.xchange.bitmarket.dto.account.BitMarketBalance;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-/**
- * @author kfonal
- */
+/** @author kfonal */
 public class BitMarketCancelResponse extends BitMarketBaseResponse<BitMarketBalance> {
 
   /**
@@ -20,8 +17,12 @@ public class BitMarketCancelResponse extends BitMarketBaseResponse<BitMarketBala
    * @param error
    * @param errorMsg
    */
-  public BitMarketCancelResponse(@JsonProperty("success") boolean success, @JsonProperty("data") BitMarketBalance data,
-      @JsonProperty("limit") BitMarketAPILimit limit, @JsonProperty("error") int error, @JsonProperty("errorMsg") String errorMsg) {
+  public BitMarketCancelResponse(
+      @JsonProperty("success") boolean success,
+      @JsonProperty("data") BitMarketBalance data,
+      @JsonProperty("limit") BitMarketAPILimit limit,
+      @JsonProperty("error") int error,
+      @JsonProperty("errorMsg") String errorMsg) {
 
     super(success, data, limit, error, errorMsg);
   }
