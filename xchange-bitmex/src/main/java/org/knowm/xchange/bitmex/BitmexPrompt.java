9db591152d915901d25e4cb620b16ff7a5dcdb47
Your Name <you@example.com>
2018-03-27 08:25:59 +0700

    [bitmex] pom reformatter first time

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/BitmexPrompt.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/BitmexPrompt.java
@@ -1,16 +1,16 @@
 package org.knowm.xchange.bitmex;
 
-/**
- * Delivery dates for future date currencies
- */
+/** Delivery dates for future date currencies */
 public enum BitmexPrompt {
-  PERPETUAL("perpetual"), WEEKLY("weekly"), MONTHLY("monthly"), QUARTERLY("quarterly"), BIQUARTERLY("biquarterly");
+  PERPETUAL("perpetual"),
+  WEEKLY("weekly"),
+  MONTHLY("monthly"),
+  QUARTERLY("quarterly"),
+  BIQUARTERLY("biquarterly");
 
   private final String name;
 
-  /**
-   * Private constructor so it cannot be instantiated
-   */
+  /** Private constructor so it cannot be instantiated */
   BitmexPrompt(String name) {
 
     this.name = name;
@@ -24,12 +24,12 @@ public enum BitmexPrompt {
       }
     }
 
-    throw new IllegalArgumentException(String.format("There is no value with name '%s' in Enum %s", name, enumeration.getName()));
+    throw new IllegalArgumentException(
+        String.format("There is no value with name '%s' in Enum %s", name, enumeration.getName()));
   }
 
   public String getName() {
 
     return name;
   }
-
 }
