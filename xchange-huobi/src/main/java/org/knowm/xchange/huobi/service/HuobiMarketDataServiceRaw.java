0653cc9e5512aa9d85712689255e2dc7a7b7f9a6
Your Name <you@example.com>
2018-03-27 08:26:32 +0700

    [huobi] pom reformatter first time

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiMarketDataServiceRaw.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiMarketDataServiceRaw.java
@@ -1,5 +1,6 @@
 package org.knowm.xchange.huobi.service;
 
+import java.io.IOException;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.huobi.HuobiUtils;
@@ -8,23 +9,20 @@ import org.knowm.xchange.huobi.dto.marketdata.HuobiTicker;
 import org.knowm.xchange.huobi.dto.marketdata.results.HuobiAssetPairsResult;
 import org.knowm.xchange.huobi.dto.marketdata.results.HuobiTickerResult;
 
-import java.io.IOException;
-
 public class HuobiMarketDataServiceRaw extends HuobiBaseService {
 
-    public HuobiMarketDataServiceRaw(Exchange exchange) {
-        super(exchange);
-    }
+  public HuobiMarketDataServiceRaw(Exchange exchange) {
+    super(exchange);
+  }
 
-    public HuobiTicker getHuobiTicker(CurrencyPair currencyPair) throws IOException {
-        String huobiCurrencyPair = HuobiUtils.createHuobiCurrencyPair(currencyPair);
-        HuobiTickerResult tickerResult = huobi.getTicker(huobiCurrencyPair);
-        return checkResult(tickerResult);
-    }
+  public HuobiTicker getHuobiTicker(CurrencyPair currencyPair) throws IOException {
+    String huobiCurrencyPair = HuobiUtils.createHuobiCurrencyPair(currencyPair);
+    HuobiTickerResult tickerResult = huobi.getTicker(huobiCurrencyPair);
+    return checkResult(tickerResult);
+  }
 
-    public HuobiAssetPair[] getHuobiAssetPairs() throws IOException {
-        HuobiAssetPairsResult assetPairsResult = huobi.getAssetPairs();
-        return checkResult(assetPairsResult);
-    }
-
+  public HuobiAssetPair[] getHuobiAssetPairs() throws IOException {
+    HuobiAssetPairsResult assetPairsResult = huobi.getAssetPairs();
+    return checkResult(assetPairsResult);
+  }
 }
