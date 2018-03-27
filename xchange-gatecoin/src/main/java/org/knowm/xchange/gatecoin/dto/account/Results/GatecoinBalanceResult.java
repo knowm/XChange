96a864534c5142dab6522bfd8d053cf72a8e1280
Your Name <you@example.com>
2018-03-27 08:26:27 +0700

    [gatecoin] pom reformatter first time

--- xchange-gatecoin/src/main/java/org/knowm/xchange/gatecoin/dto/account/Results/GatecoinBalanceResult.java
+++ xchange-gatecoin/src/main/java/org/knowm/xchange/gatecoin/dto/account/Results/GatecoinBalanceResult.java
@@ -1,18 +1,17 @@
 package org.knowm.xchange.gatecoin.dto.account.Results;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.knowm.xchange.gatecoin.dto.GatecoinResult;
 import org.knowm.xchange.gatecoin.dto.account.GatecoinBalance;
 import org.knowm.xchange.gatecoin.dto.marketdata.ResponseStatus;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-/**
- * @author sumedha
- */
+/** @author sumedha */
 public class GatecoinBalanceResult extends GatecoinResult {
   private final GatecoinBalance[] balances;
 
-  public GatecoinBalanceResult(@JsonProperty("balances") GatecoinBalance[] balances, @JsonProperty("responseStatus") ResponseStatus responseStatus) {
+  public GatecoinBalanceResult(
+      @JsonProperty("balances") GatecoinBalance[] balances,
+      @JsonProperty("responseStatus") ResponseStatus responseStatus) {
     super(responseStatus);
     this.balances = balances;
   }
