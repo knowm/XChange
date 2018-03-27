257fc38458aed3fe19f73343eaab738e979a77c1
Your Name <you@example.com>
2018-03-27 08:26:32 +0700

    [huobi] pom reformatter first time

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/HuobiAdapters.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/HuobiAdapters.java
@@ -1,5 +1,9 @@
 package org.knowm.xchange.huobi;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
@@ -21,151 +25,155 @@ import org.knowm.xchange.huobi.dto.marketdata.HuobiAssetPair;
 import org.knowm.xchange.huobi.dto.marketdata.HuobiTicker;
 import org.knowm.xchange.huobi.dto.trade.HuobiOrder;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class HuobiAdapters {
 
-    public static Ticker adaptTicker(HuobiTicker huobiTicker, CurrencyPair currencyPair) {
-        Ticker.Builder builder = new Ticker.Builder();
-        builder.open(huobiTicker.getOpen());
-        builder.ask(huobiTicker.getAsk().getPrice());
-        builder.bid(huobiTicker.getBid().getPrice());
-        builder.last(huobiTicker.getClose());
-        builder.high(huobiTicker.getHigh());
-        builder.low(huobiTicker.getLow());
-        builder.volume(huobiTicker.getVol());
-        builder.timestamp(huobiTicker.getTs());
-        builder.currencyPair(currencyPair);
-        return builder.build();
+  public static Ticker adaptTicker(HuobiTicker huobiTicker, CurrencyPair currencyPair) {
+    Ticker.Builder builder = new Ticker.Builder();
+    builder.open(huobiTicker.getOpen());
+    builder.ask(huobiTicker.getAsk().getPrice());
+    builder.bid(huobiTicker.getBid().getPrice());
+    builder.last(huobiTicker.getClose());
+    builder.high(huobiTicker.getHigh());
+    builder.low(huobiTicker.getLow());
+    builder.volume(huobiTicker.getVol());
+    builder.timestamp(huobiTicker.getTs());
+    builder.currencyPair(currencyPair);
+    return builder.build();
+  }
+
+  static ExchangeMetaData adaptToExchangeMetaData(
+      HuobiAssetPair[] assetPairs, HuobiAsset[] assets) {
+    HuobiUtils.setHuobiAssets(assets);
+    HuobiUtils.setHuobiAssetPairs(assetPairs);
+
+    Map<CurrencyPair, CurrencyPairMetaData> pairs = new HashMap<>();
+    for (HuobiAssetPair pair : assetPairs) {
+      pairs.put(adaptCurrencyPair(pair.getKey()), adaptPair(pair));
     }
 
-    static ExchangeMetaData adaptToExchangeMetaData(HuobiAssetPair[] assetPairs, HuobiAsset[] assets) {
-        HuobiUtils.setHuobiAssets(assets);
-        HuobiUtils.setHuobiAssetPairs(assetPairs);
-
-        Map<CurrencyPair, CurrencyPairMetaData> pairs = new HashMap<>();
-        for (HuobiAssetPair pair : assetPairs) {
-            pairs.put(adaptCurrencyPair(pair.getKey()), adaptPair(pair));
-        }
-
-        Map<Currency, CurrencyMetaData> currencies = new HashMap<>();
-        for (HuobiAsset asset : assets) {
-            Currency currency = adaptCurrency(asset.getAsset());
-            currencies.put(currency, new CurrencyMetaData(0, null));
-        }
-
-        return new ExchangeMetaData(pairs, currencies, null, null, false);
+    Map<Currency, CurrencyMetaData> currencies = new HashMap<>();
+    for (HuobiAsset asset : assets) {
+      Currency currency = adaptCurrency(asset.getAsset());
+      currencies.put(currency, new CurrencyMetaData(0, null));
     }
 
-    private static CurrencyPair adaptCurrencyPair(String currencyPair) {
-        return HuobiUtils.translateHuobiCurrencyPair(currencyPair);
-    }
+    return new ExchangeMetaData(pairs, currencies, null, null, false);
+  }
 
-    private static CurrencyPairMetaData adaptPair(HuobiAssetPair pair) {
-        return new CurrencyPairMetaData(null, null, null,
-                new Integer(pair.getPricePrecision()));
-    }
+  private static CurrencyPair adaptCurrencyPair(String currencyPair) {
+    return HuobiUtils.translateHuobiCurrencyPair(currencyPair);
+  }
 
-    private static Currency adaptCurrency(String currency) {
-        return HuobiUtils.translateHuobiCurrencyCode(currency);
-    }
+  private static CurrencyPairMetaData adaptPair(HuobiAssetPair pair) {
+    return new CurrencyPairMetaData(null, null, null, new Integer(pair.getPricePrecision()));
+  }
 
-    public static Wallet adaptWallet(Map<String, HuobiBalanceSum> huobiWallet) {
-        List<Balance> balances = new ArrayList<>(huobiWallet.size());
-        for (Map.Entry<String, HuobiBalanceSum> record : huobiWallet.entrySet()) {
-            Currency currency = adaptCurrency(record.getKey());
-            Balance balance = new Balance(currency, record.getValue().getTotal(), record.getValue().getAvailable(),
-                    record.getValue().getFrozen());
-            balances.add(balance);
-        }
-        return new Wallet(balances);
-    }
+  private static Currency adaptCurrency(String currency) {
+    return HuobiUtils.translateHuobiCurrencyCode(currency);
+  }
 
-    public static Map<String, HuobiBalanceSum> adaptBalance(HuobiBalanceRecord[] huobiBalance) {
-        Map<String, HuobiBalanceSum> map = new HashMap<>();
-        for (HuobiBalanceRecord record : huobiBalance) {
-            HuobiBalanceSum sum = map.get(record.getCurrency());
-            if (sum == null) {
-                sum = new HuobiBalanceSum();
-                map.put(record.getCurrency(), sum);
-            }
-            if (record.getType().equals("trade")) {
-                sum.setAvailable(record.getBalance());
-            } else if (record.getType().equals("frozen")) {
-                sum.setFrozen(record.getBalance());
-            }
-        }
-        return map;
+  public static Wallet adaptWallet(Map<String, HuobiBalanceSum> huobiWallet) {
+    List<Balance> balances = new ArrayList<>(huobiWallet.size());
+    for (Map.Entry<String, HuobiBalanceSum> record : huobiWallet.entrySet()) {
+      Currency currency = adaptCurrency(record.getKey());
+      Balance balance =
+          new Balance(
+              currency,
+              record.getValue().getTotal(),
+              record.getValue().getAvailable(),
+              record.getValue().getFrozen());
+      balances.add(balance);
     }
+    return new Wallet(balances);
+  }
 
-    public static OpenOrders adaptOpenOrders(HuobiOrder[] openOrders) {
-        List<LimitOrder> limitOrders = new ArrayList<>();
-        for(HuobiOrder openOrder : openOrders) {
-            if (openOrder.isLimit()) {
-                limitOrders.add((LimitOrder) adaptOrder(openOrder));
-            }
-        }
-        return new OpenOrders(limitOrders);
+  public static Map<String, HuobiBalanceSum> adaptBalance(HuobiBalanceRecord[] huobiBalance) {
+    Map<String, HuobiBalanceSum> map = new HashMap<>();
+    for (HuobiBalanceRecord record : huobiBalance) {
+      HuobiBalanceSum sum = map.get(record.getCurrency());
+      if (sum == null) {
+        sum = new HuobiBalanceSum();
+        map.put(record.getCurrency(), sum);
+      }
+      if (record.getType().equals("trade")) {
+        sum.setAvailable(record.getBalance());
+      } else if (record.getType().equals("frozen")) {
+        sum.setFrozen(record.getBalance());
+      }
     }
+    return map;
+  }
 
-    private static Order adaptOrder(HuobiOrder openOrder) {
-        Order order = null;
-        OrderType orderType = adaptOrderType(openOrder.getType());
-        CurrencyPair currencyPair = adaptCurrencyPair(openOrder.getSymbol());
-        if (openOrder.isMarket()) {
-            order = new MarketOrder(orderType, openOrder.getAmount(), currencyPair);
-        }
-        if (openOrder.isLimit()) {
-            order = new LimitOrder(orderType, openOrder.getAmount(), currencyPair, String.valueOf(openOrder.getId()),
-                    openOrder.getCreatedAt(), openOrder.getPrice());
-        }
-        if (order != null) {
-            order.setOrderStatus(adaptOrderStatus(openOrder.getState()));
-        }
-        return order;
+  public static OpenOrders adaptOpenOrders(HuobiOrder[] openOrders) {
+    List<LimitOrder> limitOrders = new ArrayList<>();
+    for (HuobiOrder openOrder : openOrders) {
+      if (openOrder.isLimit()) {
+        limitOrders.add((LimitOrder) adaptOrder(openOrder));
+      }
     }
+    return new OpenOrders(limitOrders);
+  }
 
-    private static OrderStatus adaptOrderStatus(String huobiStatus) {
-        OrderStatus result = OrderStatus.UNKNOWN;
-        switch (huobiStatus) {
-            case "pre-submitted":
-                result = OrderStatus.PENDING_NEW;
-                break;
-            case "submitting":
-                result = OrderStatus.PENDING_NEW;
-                break;
-            case "submitted":
-                result = OrderStatus.NEW;
-                break;
-            case "partial-filled":
-                result = OrderStatus.PARTIALLY_FILLED;
-                break;
-            case "partial-canceled":
-                result = OrderStatus.PARTIALLY_CANCELED;
-                break;
-            case "filled":
-                result = OrderStatus.FILLED;
-                break;
-            case "canceled":
-                result = OrderStatus.CANCELED;
-                break;
-        }
-        return result;
+  private static Order adaptOrder(HuobiOrder openOrder) {
+    Order order = null;
+    OrderType orderType = adaptOrderType(openOrder.getType());
+    CurrencyPair currencyPair = adaptCurrencyPair(openOrder.getSymbol());
+    if (openOrder.isMarket()) {
+      order = new MarketOrder(orderType, openOrder.getAmount(), currencyPair);
     }
-
-    private static OrderType adaptOrderType(String orderType) {
-        return orderType.substring(1, 3).equals("buy") ? OrderType.BID : OrderType.ASK;
+    if (openOrder.isLimit()) {
+      order =
+          new LimitOrder(
+              orderType,
+              openOrder.getAmount(),
+              currencyPair,
+              String.valueOf(openOrder.getId()),
+              openOrder.getCreatedAt(),
+              openOrder.getPrice());
     }
-
-    public static List<Order> adaptOrders(List<HuobiOrder> huobiOrders) {
-        List<Order> orders = new ArrayList<>();
-        for (HuobiOrder order : huobiOrders) {
-            orders.add(adaptOrder(order));
-        }
-        return orders;
+    if (order != null) {
+      order.setOrderStatus(adaptOrderStatus(openOrder.getState()));
     }
+    return order;
+  }
 
+  private static OrderStatus adaptOrderStatus(String huobiStatus) {
+    OrderStatus result = OrderStatus.UNKNOWN;
+    switch (huobiStatus) {
+      case "pre-submitted":
+        result = OrderStatus.PENDING_NEW;
+        break;
+      case "submitting":
+        result = OrderStatus.PENDING_NEW;
+        break;
+      case "submitted":
+        result = OrderStatus.NEW;
+        break;
+      case "partial-filled":
+        result = OrderStatus.PARTIALLY_FILLED;
+        break;
+      case "partial-canceled":
+        result = OrderStatus.PARTIALLY_CANCELED;
+        break;
+      case "filled":
+        result = OrderStatus.FILLED;
+        break;
+      case "canceled":
+        result = OrderStatus.CANCELED;
+        break;
+    }
+    return result;
+  }
+
+  private static OrderType adaptOrderType(String orderType) {
+    return orderType.substring(1, 3).equals("buy") ? OrderType.BID : OrderType.ASK;
+  }
+
+  public static List<Order> adaptOrders(List<HuobiOrder> huobiOrders) {
+    List<Order> orders = new ArrayList<>();
+    for (HuobiOrder order : huobiOrders) {
+      orders.add(adaptOrder(order));
+    }
+    return orders;
+  }
 }
--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/dto/account/HuobiBalanceSum.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/dto/account/HuobiBalanceSum.java
@@ -4,27 +4,26 @@ import java.math.BigDecimal;
 
 public class HuobiBalanceSum {
 
-    private BigDecimal available;
-    private BigDecimal frozen;
+  private BigDecimal available;
+  private BigDecimal frozen;
 
-    public BigDecimal getAvailable() {
-        return available;
-    }
+  public BigDecimal getAvailable() {
+    return available;
+  }
 
-    public BigDecimal getFrozen() {
-        return frozen;
-    }
+  public BigDecimal getFrozen() {
+    return frozen;
+  }
 
-    public BigDecimal getTotal() {
-        return available.add(frozen);
-    }
+  public BigDecimal getTotal() {
+    return available.add(frozen);
+  }
 
-    public void setAvailable(BigDecimal available) {
-        this.available = available;
-    }
+  public void setAvailable(BigDecimal available) {
+    this.available = available;
+  }
 
-    public void setFrozen(BigDecimal frozen) {
-        this.frozen = frozen;
-    }
-
+  public void setFrozen(BigDecimal frozen) {
+    this.frozen = frozen;
+  }
 }
