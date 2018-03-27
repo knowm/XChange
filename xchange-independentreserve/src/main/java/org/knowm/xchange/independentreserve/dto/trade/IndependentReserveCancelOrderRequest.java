4c2290399de1461f9dd99d023f051fee28f8f594
Your Name <you@example.com>
2018-03-27 08:26:33 +0700

    [independentreserve] pom reformatter first time

--- xchange-independentreserve/src/main/java/org/knowm/xchange/independentreserve/dto/trade/IndependentReserveCancelOrderRequest.java
+++ xchange-independentreserve/src/main/java/org/knowm/xchange/independentreserve/dto/trade/IndependentReserveCancelOrderRequest.java
@@ -2,9 +2,7 @@ package org.knowm.xchange.independentreserve.dto.trade;
 
 import org.knowm.xchange.independentreserve.dto.auth.AuthAggregate;
 
-/**
- * Author: Kamil Zbikowski Date: 4/15/15
- */
+/** Author: Kamil Zbikowski Date: 4/15/15 */
 public class IndependentReserveCancelOrderRequest extends AuthAggregate {
 
   public IndependentReserveCancelOrderRequest(String apiKey, Long nonce, String orderGuid) {
