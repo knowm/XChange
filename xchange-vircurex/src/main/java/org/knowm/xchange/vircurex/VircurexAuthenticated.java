f20566c3916a3d5191fafcfed0cee4a4e748ece2
Your Name <you@example.com>
2018-03-27 08:26:56 +0700

    [vircurex] pom reformatter first time

--- xchange-vircurex/src/main/java/org/knowm/xchange/vircurex/VircurexAuthenticated.java
+++ xchange-vircurex/src/main/java/org/knowm/xchange/vircurex/VircurexAuthenticated.java
@@ -5,7 +5,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
 import org.knowm.xchange.vircurex.dto.account.VircurexAccountInfoReturn;
 import org.knowm.xchange.vircurex.dto.trade.VircurexOpenOrdersReturn;
 import org.knowm.xchange.vircurex.dto.trade.VircurexPlaceOrderReturn;
@@ -16,22 +15,40 @@ public interface VircurexAuthenticated extends Vircurex {
 
   @GET
   @Path("get_balances.json")
-  VircurexAccountInfoReturn getInfo(@QueryParam("account") String aUserName, @QueryParam("id") long nonce, @QueryParam("token") String aToken,
+  VircurexAccountInfoReturn getInfo(
+      @QueryParam("account") String aUserName,
+      @QueryParam("id") long nonce,
+      @QueryParam("token") String aToken,
       @QueryParam("timestamp") String aTimestamp);
 
   @Path("create_order.json")
   @GET
-  VircurexPlaceOrderReturn trade(@QueryParam("account") String aUserName, @QueryParam("id") long nonce, @QueryParam("token") String aToken,
-      @QueryParam("timestamp") String aTimestamp, @QueryParam("ordertype") String anOrderType, @QueryParam("amount") String anAmount,
-      @QueryParam("currency1") String aCurrency, @QueryParam("unitprice") String aPrice, @QueryParam("currency2") String aTransactionCurrency);
+  VircurexPlaceOrderReturn trade(
+      @QueryParam("account") String aUserName,
+      @QueryParam("id") long nonce,
+      @QueryParam("token") String aToken,
+      @QueryParam("timestamp") String aTimestamp,
+      @QueryParam("ordertype") String anOrderType,
+      @QueryParam("amount") String anAmount,
+      @QueryParam("currency1") String aCurrency,
+      @QueryParam("unitprice") String aPrice,
+      @QueryParam("currency2") String aTransactionCurrency);
 
   @Path("release_order.json")
   @GET
-  VircurexPlaceOrderReturn release(@QueryParam("account") String apiKey, @QueryParam("id") long nonce, @QueryParam("token") String token,
-      @QueryParam("timestamp") String timestamp, @QueryParam("orderid") String orderId);
+  VircurexPlaceOrderReturn release(
+      @QueryParam("account") String apiKey,
+      @QueryParam("id") long nonce,
+      @QueryParam("token") String token,
+      @QueryParam("timestamp") String timestamp,
+      @QueryParam("orderid") String orderId);
 
   @Path("read_orders.json")
   @GET
-  VircurexOpenOrdersReturn getOpenOrders(@QueryParam("account") String apiKey, @QueryParam("id") long nonce, @QueryParam("token") String token,
-      @QueryParam("timestamp") String timestamp, @QueryParam("otype") int releaseStatus);
+  VircurexOpenOrdersReturn getOpenOrders(
+      @QueryParam("account") String apiKey,
+      @QueryParam("id") long nonce,
+      @QueryParam("token") String token,
+      @QueryParam("timestamp") String timestamp,
+      @QueryParam("otype") int releaseStatus);
 }
