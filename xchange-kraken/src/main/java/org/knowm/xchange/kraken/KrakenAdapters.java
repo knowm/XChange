1ba9179c2d5f3841dcb7a2cd50e402fb870f2a21
caleb <caleb.e.allen@gmail.com>
2018-03-27 17:14:49 -0600

    implement getTickers for Kraken

--- xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenAdapters.java
+++ xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenAdapters.java
@@ -136,6 +136,15 @@ public class KrakenAdapters {
     return builder.build();
   }
 
+  public static List<Ticker> adaptTickers(Map<String, KrakenTicker> krackenTickers) {
+    List<Ticker> tickers = new ArrayList<>();
+    for (Map.Entry<String, KrakenTicker> ticker : krackenTickers.entrySet()) {
+      CurrencyPair pair = KrakenUtils.translateKrakenCurrencyPair(ticker.getKey());
+      tickers.add(adaptTicker(ticker.getValue(), pair));
+    }
+    return tickers;
+  }
+
   public static Trades adaptTrades(List<KrakenPublicTrade> krakenTrades, CurrencyPair currencyPair, long last) {
 
     List<Trade> trades = new ArrayList<>();
