251f9a7015f04027cac4068868bc76e92115a847
Pierre Leandre <pierre.leandre@sgcib.com>
2018-04-03 21:36:48 +0800

    Fix build (#155)
    Fix build.
    Travis build error: https://travis-ci.org/bitrich-info/xchange-stream
    
    [ERROR] /home/travis/build/bitrich-info/xchange-stream/xchange-hitbtc/src/main/java/info/bitrich/xchangestream/hitbtc/HitbtcStreamingExchange.java:[13,8] info.bitrich.xchangestream.hitbtc.HitbtcStreamingExchange is not abstract and does not override abstract method useCompressedMessages(boolean) in info.bitrich.xchangestream.core.StreamingExchange

--- xchange-hitbtc/src/main/java/info/bitrich/xchangestream/hitbtc/HitbtcStreamingExchange.java
+++ xchange-hitbtc/src/main/java/info/bitrich/xchangestream/hitbtc/HitbtcStreamingExchange.java
@@ -53,4 +53,7 @@ public class HitbtcStreamingExchange extends HitbtcExchange implements Streaming
     public StreamingMarketDataService getStreamingMarketDataService() {
         return streamingMarketDataService;
     }
+    
+    @Override
+    public void useCompressedMessages(boolean compressedMessages) { streamingService.useCompressedMessages(compressedMessages); }
 }
