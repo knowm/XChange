f1ed8e15d260141bd5d14b049c0e3387e777d35b
amjad1210 <amjad121092@gmail.com>
2018-04-07 14:42:25 +0100

    Nullpointer fix on kucoin funding history

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/service/KucoinAccountService.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/service/KucoinAccountService.java
@@ -84,8 +84,9 @@ public class KucoinAccountService extends KucoinAccountServiceRaw implements Acc
     TradeHistoryParamCurrency curParams = (TradeHistoryParamCurrency) params;
 
     Type type = null;
-    if (params instanceof HistoryParamsFundingType) {
-      type = ((HistoryParamsFundingType) params).getType();
+    HistoryParamsFundingType fundingType = (HistoryParamsFundingType) params;
+    if (fundingType.getType() != null) {
+      type = fundingType.getType();
     }
     // Paging params are 0-based, Kucoin account balances pages are 1-based
     KucoinSimpleResponse<KucoinWalletRecords> response =
@@ -93,7 +94,7 @@ public class KucoinAccountService extends KucoinAccountServiceRaw implements Acc
             curParams.getCurrency(),
             type,
             pagingParams.getPageLength(),
-            pagingParams.getPageNumber() + 1);
+            pagingParams.getPageNumber());
     return KucoinAdapters.adaptFundingHistory(response.getData().getRecords());
   }
 }
