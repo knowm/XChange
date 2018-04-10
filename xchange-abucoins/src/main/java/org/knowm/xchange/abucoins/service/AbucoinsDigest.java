4406657949bcf8a33c7ef04c40981a536e474d69
npomfret <nick-github@snowmonkey.co.uk>
2018-04-10 09:23:53 +0100

    [abucoins] moved the oddly placed time method into the base service (no functional change)

--- xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsDigest.java
+++ xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/service/AbucoinsDigest.java
@@ -1,33 +1,20 @@
 package org.knowm.xchange.abucoins.service;
 
-import org.knowm.xchange.abucoins.Abucoins;
-import org.knowm.xchange.abucoins.dto.AbucoinsServerTime;
 import org.knowm.xchange.service.BaseParamsDigest;
 import si.mazi.rescu.RestInvocation;
 
 import javax.crypto.Mac;
-import java.io.IOException;
 import java.util.Base64;
 
 /** @author bryant_harris */
 public class AbucoinsDigest extends BaseParamsDigest {
-  private final long timeDiffFromServer;
 
-  private AbucoinsDigest(Abucoins abucoins, String secretKeyBase64) throws IllegalArgumentException {
+  private AbucoinsDigest(String secretKeyBase64) throws IllegalArgumentException {
     super(secretKeyBase64 == null ? null : Base64.getDecoder().decode(secretKeyBase64), HMAC_SHA_256);
-
-    try {
-      AbucoinsServerTime serverTime = abucoins.getTime();
-
-      long ourTime = System.currentTimeMillis() / 1000L;
-      timeDiffFromServer = serverTime.getEpoch() - ourTime;
-    } catch (IOException e) {
-      throw new RuntimeException("Unable to determine server time");
-    }
   }
 
-  public static AbucoinsDigest createInstance(Abucoins abucoins, String secretKeyBase64) {
-    return secretKeyBase64 == null ? null : new AbucoinsDigest(abucoins, secretKeyBase64);
+  public static AbucoinsDigest createInstance(String secretKeyBase64) {
+    return secretKeyBase64 == null ? null : new AbucoinsDigest(secretKeyBase64);
   }
 
   @Override
@@ -43,9 +30,5 @@ public class AbucoinsDigest extends BaseParamsDigest {
     Mac shaMac = getMac();
     final byte[] macData = shaMac.doFinal(queryArgs.getBytes());
     return Base64.getEncoder().encodeToString(macData);
-  }
-
-  public String timestamp() {
-    return String.valueOf((System.currentTimeMillis() / 1000) + timeDiffFromServer);
   }
 }
