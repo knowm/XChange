3cd4742b1ab17747bc1c677a526f315053047745
cacofish <cacofish@hotmail.com>
2018-04-11 23:59:15 -0700

    Add Poloniex deposit key generation
    Add poloniex deposit address generation logic. When deposit address does not exist, generate a new one.
    Also format the code.

--- xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/service/PoloniexAccountServiceRaw.java
+++ xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/service/PoloniexAccountServiceRaw.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+
 import javax.annotation.Nullable;
+
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.Balance;
@@ -17,6 +19,7 @@ import org.knowm.xchange.poloniex.dto.account.PoloniexBalance;
 import org.knowm.xchange.poloniex.dto.account.PoloniexLoan;
 import org.knowm.xchange.poloniex.dto.account.PoloniexWallet;
 import org.knowm.xchange.poloniex.dto.trade.PoloniexDepositsWithdrawalsResponse;
+import org.knowm.xchange.poloniex.dto.trade.PoloniexGenerateNewAddressResponse;
 import org.knowm.xchange.utils.DateUtils;
 
 /** @author Zach Holmes */
@@ -34,9 +37,8 @@ public class PoloniexAccountServiceRaw extends PoloniexBaseService {
 
   public List<Balance> getExchangeWallet() throws IOException {
     try {
-      HashMap<String, PoloniexBalance> response =
-          poloniexAuthenticated.returnCompleteBalances(
-              apiKey, signatureCreator, exchange.getNonceFactory(), null);
+      HashMap<String, PoloniexBalance> response = poloniexAuthenticated.returnCompleteBalances(apiKey, signatureCreator, exchange.getNonceFactory(),
+          null);
       return PoloniexAdapters.adaptPoloniexBalances(response);
     } catch (PoloniexException e) {
       throw new ExchangeException(e.getError(), e);
@@ -46,9 +48,8 @@ public class PoloniexAccountServiceRaw extends PoloniexBaseService {
   public List<Balance> getWallets() throws IOException {
     try {
       // using account="all" for margin + lending balances
-      HashMap<String, PoloniexBalance> response =
-          poloniexAuthenticated.returnCompleteBalances(
-              apiKey, signatureCreator, exchange.getNonceFactory(), "all");
+      HashMap<String, PoloniexBalance> response = poloniexAuthenticated.returnCompleteBalances(apiKey, signatureCreator, exchange.getNonceFactory(),
+          "all");
       return PoloniexAdapters.adaptPoloniexBalances(response);
     } catch (PoloniexException e) {
       throw new ExchangeException(e.getError(), e);
@@ -57,9 +58,7 @@ public class PoloniexAccountServiceRaw extends PoloniexBaseService {
 
   public LoanInfo getLoanInfo() throws IOException {
     try {
-      HashMap<String, PoloniexLoan[]> response =
-          poloniexAuthenticated.returnActiveLoans(
-              apiKey, signatureCreator, exchange.getNonceFactory());
+      HashMap<String, PoloniexLoan[]> response = poloniexAuthenticated.returnActiveLoans(apiKey, signatureCreator, exchange.getNonceFactory());
       return PoloniexAdapters.adaptPoloniexLoans(response);
     } catch (PoloniexException e) {
       throw new ExchangeException(e.getError(), e);
@@ -68,9 +67,7 @@ public class PoloniexAccountServiceRaw extends PoloniexBaseService {
 
   public String getDepositAddress(String currency) throws IOException {
 
-    HashMap<String, String> response =
-        poloniexAuthenticated.returnDepositAddresses(
-            apiKey, signatureCreator, exchange.getNonceFactory());
+    HashMap<String, String> response = poloniexAuthenticated.returnDepositAddresses(apiKey, signatureCreator, exchange.getNonceFactory());
 
     if (response.containsKey("error")) {
       throw new ExchangeException(response.get("error"));
@@ -78,48 +75,29 @@ public class PoloniexAccountServiceRaw extends PoloniexBaseService {
     if (response.containsKey(currency)) {
       return response.get(currency);
     } else {
-      throw new ExchangeException("Poloniex did not return a deposit address for " + currency);
+      PoloniexGenerateNewAddressResponse newAddressResponse = poloniexAuthenticated.generateNewAddress(apiKey, signatureCreator,
+          exchange.getNonceFactory(), currency);
+      if (newAddressResponse.success()) {
+        return newAddressResponse.getAddress();
+      } else {
+        throw new ExchangeException("Failed to get Poloniex deposit address for " + currency);
+      }
     }
   }
 
   /** @param paymentId For XMR withdrawals, you may optionally specify "paymentId". */
-  public String withdraw(
-      Currency currency, BigDecimal amount, String address, @Nullable String paymentId)
-      throws IOException {
+  public String withdraw(Currency currency, BigDecimal amount, String address, @Nullable String paymentId) throws IOException {
     return poloniexAuthenticated
-        .withdraw(
-            apiKey,
-            signatureCreator,
-            exchange.getNonceFactory(),
-            currency.getCurrencyCode(),
-            amount,
-            address,
-            paymentId)
-        .getResponse();
+        .withdraw(apiKey, signatureCreator, exchange.getNonceFactory(), currency.getCurrencyCode(), amount, address, paymentId).getResponse();
   }
 
-  public PoloniexDepositsWithdrawalsResponse returnDepositsWithdrawals(Date start, Date end)
-      throws IOException {
-    return poloniexAuthenticated.returnDepositsWithdrawals(
-        apiKey,
-        signatureCreator,
-        exchange.getNonceFactory(),
-        DateUtils.toUnixTimeNullSafe(start),
+  public PoloniexDepositsWithdrawalsResponse returnDepositsWithdrawals(Date start, Date end) throws IOException {
+    return poloniexAuthenticated.returnDepositsWithdrawals(apiKey, signatureCreator, exchange.getNonceFactory(), DateUtils.toUnixTimeNullSafe(start),
         DateUtils.toUnixTimeNullSafe(end));
   }
 
-  public String transfer(
-      Currency currency, BigDecimal amount, PoloniexWallet fromWallet, PoloniexWallet toWallet)
-      throws IOException {
-    return poloniexAuthenticated
-        .transferBalance(
-            apiKey,
-            signatureCreator,
-            exchange.getNonceFactory(),
-            currency.getCurrencyCode(),
-            amount,
-            fromWallet.name().toLowerCase(),
-            toWallet.name().toLowerCase())
-        .getMessage();
+  public String transfer(Currency currency, BigDecimal amount, PoloniexWallet fromWallet, PoloniexWallet toWallet) throws IOException {
+    return poloniexAuthenticated.transferBalance(apiKey, signatureCreator, exchange.getNonceFactory(), currency.getCurrencyCode(), amount,
+        fromWallet.name().toLowerCase(), toWallet.name().toLowerCase()).getMessage();
   }
 }
