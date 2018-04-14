0c9a5102de44f3b46a2ac9a8a2dc6ab8647c01a4
interwater <donguk.lee@Vitalhint.com>
2018-04-14 20:36:14 +0900

    [coinone] market data tested

--- xchange-coinone/src/main/java/org/knowm/xchange/coinone/CoinoneAdapters.java
+++ xchange-coinone/src/main/java/org/knowm/xchange/coinone/CoinoneAdapters.java
@@ -2,10 +2,12 @@ package org.knowm.xchange.coinone;
 
 import java.math.BigDecimal;
 import java.util.*;
+
 import org.knowm.xchange.coinone.dto.CoinoneException;
 import org.knowm.xchange.coinone.dto.account.CoinoneBalancesResponse;
 import org.knowm.xchange.coinone.dto.marketdata.CoinoneOrderBook;
 import org.knowm.xchange.coinone.dto.marketdata.CoinoneOrderBookData;
+import org.knowm.xchange.coinone.dto.marketdata.CoinoneTicker;
 import org.knowm.xchange.coinone.dto.trade.CoinoneOrderInfo;
 import org.knowm.xchange.coinone.dto.trade.CoinoneOrderInfoResponse;
 import org.knowm.xchange.currency.Currency;
@@ -16,120 +18,137 @@ import org.knowm.xchange.dto.Order.OrderType;
 import org.knowm.xchange.dto.account.Balance;
 import org.knowm.xchange.dto.account.Wallet;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.utils.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class CoinoneAdapters {
 
-  public static final Logger log = LoggerFactory.getLogger(CoinoneAdapters.class);
+    public static final Logger log = LoggerFactory.getLogger(CoinoneAdapters.class);
 
-  private CoinoneAdapters() {}
-
-  public static OrderBook adaptOrderBook(
-      CoinoneOrderBook coinoneOrderBook, CurrencyPair currencyPair) {
-
-    if (!"0".equals(coinoneOrderBook.getErrorCode())) {
-      throw new CoinoneException(coinoneOrderBook.getResult());
+    private CoinoneAdapters() {
     }
 
-    List<LimitOrder> asks =
-        adaptMarketOrderToLimitOrder(coinoneOrderBook.getAsks(), OrderType.ASK, currencyPair);
-    List<LimitOrder> bids =
-        adaptMarketOrderToLimitOrder(coinoneOrderBook.getBids(), OrderType.BID, currencyPair);
+    public static OrderBook adaptOrderBook(
+            CoinoneOrderBook coinoneOrderBook, CurrencyPair currencyPair) {
 
-    return new OrderBook(new Date(coinoneOrderBook.getTimestamp()), asks, bids);
-  }
+        if (!"0".equals(coinoneOrderBook.getErrorCode())) {
+            throw new CoinoneException(coinoneOrderBook.getResult());
+        }
 
-  private static List<LimitOrder> adaptMarketOrderToLimitOrder(
-      CoinoneOrderBookData[] coinoneOrders, OrderType orderType, CurrencyPair currencyPair) {
+        List<LimitOrder> asks =
+                adaptMarketOrderToLimitOrder(coinoneOrderBook.getAsks(), OrderType.ASK, currencyPair);
+        List<LimitOrder> bids =
+                adaptMarketOrderToLimitOrder(coinoneOrderBook.getBids(), OrderType.BID, currencyPair);
 
-    List<LimitOrder> orders = new ArrayList<>(coinoneOrders.length);
-
-    for (int i = 0; i < coinoneOrders.length; i++) {
-      CoinoneOrderBookData coinoneOrder = coinoneOrders[i];
-      BigDecimal price = coinoneOrder.getPrice();
-      BigDecimal amount = coinoneOrder.getQty();
-      LimitOrder limitOrder = new LimitOrder(orderType, amount, currencyPair, null, null, price);
-      orders.add(limitOrder);
+        return new OrderBook(new Date(coinoneOrderBook.getTimestamp()), asks, bids);
     }
 
-    return orders;
-  }
+    private static List<LimitOrder> adaptMarketOrderToLimitOrder(
+            CoinoneOrderBookData[] coinoneOrders, OrderType orderType, CurrencyPair currencyPair) {
 
-  public static Order adaptOrderInfo(CoinoneOrderInfoResponse coinoneOrderInfoResponse) {
-    ArrayList<Order> orders = new ArrayList<Order>();
-    OrderStatus status = OrderStatus.NEW;
-    if (coinoneOrderInfoResponse.getStatus().equals("live")) status = OrderStatus.NEW;
-    else if (coinoneOrderInfoResponse.getStatus().equals("filled")) status = OrderStatus.FILLED;
-    else if (coinoneOrderInfoResponse.getStatus().equals("partially_filled"))
-      status = OrderStatus.PARTIALLY_FILLED;
-    else status = OrderStatus.CANCELED;
-    CoinoneOrderInfo orderInfo = coinoneOrderInfoResponse.getInfo();
-    OrderType type = orderInfo.getType().equals("ask") ? OrderType.ASK : OrderType.BID;
-    BigDecimal originalAmount = orderInfo.getQty();
-    CurrencyPair currencyPair =
-        new CurrencyPair(new Currency(orderInfo.getCurrency().toUpperCase()), Currency.KRW);
-    String orderId = orderInfo.getOrderId();
-    BigDecimal cumulativeAmount = orderInfo.getQty().subtract(orderInfo.getRemainQty());
-    BigDecimal price = orderInfo.getPrice();
-    Calendar cal = Calendar.getInstance();
-    cal.setTimeInMillis(orderInfo.getTimestamp());
-    LimitOrder order =
-        new LimitOrder(
-            type,
-            originalAmount,
-            currencyPair,
-            orderId,
-            cal.getTime(),
-            price,
-            price,
-            cumulativeAmount,
-            orderInfo.getFee(),
-            status);
-    return order;
-  }
+        List<LimitOrder> orders = new ArrayList<>(coinoneOrders.length);
 
-  public static Wallet adaptWallet(CoinoneBalancesResponse coninoneResponse) {
-    if (!"0".equals(coninoneResponse.getErrorCode())) {
-      throw new CoinoneException(coninoneResponse.getResult());
+        for (int i = 0; i < coinoneOrders.length; i++) {
+            CoinoneOrderBookData coinoneOrder = coinoneOrders[i];
+            BigDecimal price = coinoneOrder.getPrice();
+            BigDecimal amount = coinoneOrder.getQty();
+            LimitOrder limitOrder = new LimitOrder(orderType, amount, currencyPair, null, null, price);
+            orders.add(limitOrder);
+        }
+
+        return orders;
     }
-    List<Balance> balances = new ArrayList<>();
-    balances.add(
-        new Balance(
-            Currency.getInstance("KRW"),
-            new BigDecimal(coninoneResponse.getKrw().getBalance()),
-            new BigDecimal(coninoneResponse.getKrw().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("BCH"),
-            new BigDecimal(coninoneResponse.getBch().getBalance()),
-            new BigDecimal(coninoneResponse.getBch().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("BTC"),
-            new BigDecimal(coninoneResponse.getBtc().getBalance()),
-            new BigDecimal(coninoneResponse.getBtc().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("ETC"),
-            new BigDecimal(coninoneResponse.getEtc().getBalance()),
-            new BigDecimal(coninoneResponse.getEtc().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("ETH"),
-            new BigDecimal(coninoneResponse.getEth().getBalance()),
-            new BigDecimal(coninoneResponse.getEth().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("QTUM"),
-            new BigDecimal(coninoneResponse.getQtum().getBalance()),
-            new BigDecimal(coninoneResponse.getQtum().getAvail())));
-    balances.add(
-        new Balance(
-            Currency.getInstance("XRP"),
-            new BigDecimal(coninoneResponse.getXrp().getBalance()),
-            new BigDecimal(coninoneResponse.getXrp().getAvail())));
-    return new Wallet(balances);
-  }
+
+    public static Order adaptOrderInfo(CoinoneOrderInfoResponse coinoneOrderInfoResponse) {
+        ArrayList<Order> orders = new ArrayList<Order>();
+        OrderStatus status = OrderStatus.NEW;
+        if (coinoneOrderInfoResponse.getStatus().equals("live")) status = OrderStatus.NEW;
+        else if (coinoneOrderInfoResponse.getStatus().equals("filled")) status = OrderStatus.FILLED;
+        else if (coinoneOrderInfoResponse.getStatus().equals("partially_filled"))
+            status = OrderStatus.PARTIALLY_FILLED;
+        else status = OrderStatus.CANCELED;
+        CoinoneOrderInfo orderInfo = coinoneOrderInfoResponse.getInfo();
+        OrderType type = orderInfo.getType().equals("ask") ? OrderType.ASK : OrderType.BID;
+        BigDecimal originalAmount = orderInfo.getQty();
+        CurrencyPair currencyPair =
+                new CurrencyPair(new Currency(orderInfo.getCurrency().toUpperCase()), Currency.KRW);
+        String orderId = orderInfo.getOrderId();
+        BigDecimal cumulativeAmount = orderInfo.getQty().subtract(orderInfo.getRemainQty());
+        BigDecimal price = orderInfo.getPrice();
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(orderInfo.getTimestamp());
+        LimitOrder order =
+                new LimitOrder(
+                        type,
+                        originalAmount,
+                        currencyPair,
+                        orderId,
+                        cal.getTime(),
+                        price,
+                        price,
+                        cumulativeAmount,
+                        orderInfo.getFee(),
+                        status);
+        return order;
+    }
+
+    public static Wallet adaptWallet(CoinoneBalancesResponse coninoneResponse) {
+        if (!"0".equals(coninoneResponse.getErrorCode())) {
+            throw new CoinoneException(coninoneResponse.getResult());
+        }
+        List<Balance> balances = new ArrayList<>();
+        balances.add(
+                new Balance(
+                        Currency.getInstance("KRW"),
+                        new BigDecimal(coninoneResponse.getKrw().getBalance()),
+                        new BigDecimal(coninoneResponse.getKrw().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("BCH"),
+                        new BigDecimal(coninoneResponse.getBch().getBalance()),
+                        new BigDecimal(coninoneResponse.getBch().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("BTC"),
+                        new BigDecimal(coninoneResponse.getBtc().getBalance()),
+                        new BigDecimal(coninoneResponse.getBtc().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("ETC"),
+                        new BigDecimal(coninoneResponse.getEtc().getBalance()),
+                        new BigDecimal(coninoneResponse.getEtc().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("ETH"),
+                        new BigDecimal(coninoneResponse.getEth().getBalance()),
+                        new BigDecimal(coninoneResponse.getEth().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("QTUM"),
+                        new BigDecimal(coninoneResponse.getQtum().getBalance()),
+                        new BigDecimal(coninoneResponse.getQtum().getAvail())));
+        balances.add(
+                new Balance(
+                        Currency.getInstance("XRP"),
+                        new BigDecimal(coninoneResponse.getXrp().getBalance()),
+                        new BigDecimal(coninoneResponse.getXrp().getAvail())));
+        return new Wallet(balances);
+    }
+
+    public static Ticker adaptTicker(CoinoneTicker ticker) {
+        CurrencyPair currencyPair = new CurrencyPair(Currency.getInstance(ticker.getCurrency()), Currency.KRW);
+        final Date date = DateUtils.fromMillisUtc(Long.valueOf(ticker.getTimestamp()) * 1000);
+        return new Ticker.Builder()
+                .currencyPair(currencyPair)
+                .high(ticker.getHigh())
+                .low(ticker.getLow())
+                .last(ticker.getLast())
+                .volume(ticker.getVolume())
+                .open(ticker.getFirst())
+                .timestamp(date)
+                .build();
+    }
 }
