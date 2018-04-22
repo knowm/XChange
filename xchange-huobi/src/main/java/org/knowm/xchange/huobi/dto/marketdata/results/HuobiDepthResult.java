4728a4b9d28edbe102eb916a133dd76a4dca751f
cacofish <cacofish@hotmail.com>
2018-04-22 01:24:32 -0700

    Add huobi getorderbook api
    Add huobi getorderbook api

---
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/dto/marketdata/results/HuobiDepthResult.java
@@ -0,0 +1,32 @@
+package org.knowm.xchange.huobi.dto.marketdata.results;
+
+import java.util.Date;
+
+import org.knowm.xchange.huobi.dto.HuobiResult;
+import org.knowm.xchange.huobi.dto.marketdata.HuobiDepth;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HuobiDepthResult extends HuobiResult<HuobiDepth> {
+
+  private final Date ts;
+  private final String ch;
+
+  @JsonCreator
+  public HuobiDepthResult(@JsonProperty("status") String status, @JsonProperty("ts") Date ts, @JsonProperty("tick") HuobiDepth tick,
+      @JsonProperty("ch") String ch, @JsonProperty("err-code") String errCode, @JsonProperty("err-msg") String errMsg) {
+    super(status, errCode, errMsg, tick);
+    this.ts = ts;
+    this.ch = ch;
+    getResult().setTs(ts);
+  }
+
+  public Date getTs() {
+    return ts;
+  }
+
+  public String getCh() {
+    return ch;
+  }
+}
