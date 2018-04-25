d5812e135620deed63b22895c8f377a26a1d5373
cacofish <cacofish@hotmail.com>
2018-04-24 20:29:35 -0700

    Add huobi getdepositaddress, withdraw api
    Add huobi getdepositaddress, withdraw api

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiAccountServiceRaw.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiAccountServiceRaw.java
@@ -1,12 +1,18 @@
 package org.knowm.xchange.huobi.service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.huobi.HuobiUtils;
 import org.knowm.xchange.huobi.dto.account.HuobiAccount;
 import org.knowm.xchange.huobi.dto.account.HuobiBalance;
+import org.knowm.xchange.huobi.dto.account.HuobiCreateWithdrawRequest;
+import org.knowm.xchange.huobi.dto.account.HuobiDepositAddressWithTag;
 import org.knowm.xchange.huobi.dto.account.results.HuobiAccountResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiBalanceResult;
+import org.knowm.xchange.huobi.dto.account.results.HuobiCreateWithdrawResult;
+import org.knowm.xchange.huobi.dto.account.results.HuobiDepositAddressResult;
+import org.knowm.xchange.huobi.dto.account.results.HuobiDepositAddressWithTagResult;
 
 public class HuobiAccountServiceRaw extends HuobiBaseService {
 
@@ -35,5 +41,50 @@ public class HuobiAccountServiceRaw extends HuobiBaseService {
             HuobiUtils.createUTCDate(exchange.getNonceFactory()),
             signatureCreator);
     return checkResult(huobiAccountResult);
+  }
+
+  public String getDepositAddress(String currency) throws IOException {
+    HuobiDepositAddressResult depositAddressResult =
+        huobi.getDepositAddress(
+            currency.toLowerCase(),
+            exchange.getExchangeSpecification().getApiKey(),
+            HuobiDigest.HMAC_SHA_256,
+            2,
+            HuobiUtils.createUTCDate(exchange.getNonceFactory()),
+            signatureCreator);
+    return checkResult(depositAddressResult);
+  }
+
+  public HuobiDepositAddressWithTag getDepositAddressWithTag(String currency) throws IOException {
+    HuobiDepositAddressWithTagResult depositAddressWithTagResult =
+        huobi.getDepositAddressWithTag(
+            currency.toLowerCase(),
+            exchange.getExchangeSpecification().getApiKey(),
+            HuobiDigest.HMAC_SHA_256,
+            2,
+            HuobiUtils.createUTCDate(exchange.getNonceFactory()),
+            signatureCreator);
+    return checkResult(depositAddressWithTagResult);
+  }
+
+  public long createWithdraw(
+      String currency, BigDecimal amount, BigDecimal fee, String address, String addressTag)
+      throws IOException {
+    HuobiCreateWithdrawRequest createWithdrawRequest =
+        new HuobiCreateWithdrawRequest(
+            address,
+            amount.toPlainString(),
+            currency.toLowerCase(),
+            fee == null ? null : fee.toPlainString(),
+            addressTag);
+    HuobiCreateWithdrawResult createWithdrawResult =
+        huobi.createWithdraw(
+            createWithdrawRequest,
+            exchange.getExchangeSpecification().getApiKey(),
+            HuobiDigest.HMAC_SHA_256,
+            2,
+            HuobiUtils.createUTCDate(exchange.getNonceFactory()),
+            signatureCreator);
+    return checkResult(createWithdrawResult);
   }
 }
