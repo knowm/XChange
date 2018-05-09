1b484a14655f2edecfed723fbfc22ee849533918
npomfret <nick-github@snowmonkey.co.uk>
2018-05-09 12:45:31 +0100

    [hitbtc] introduced concept of clientOrderId (instead of hijacking the orderId)

--- xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcTradeService.java
+++ xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcTradeService.java
@@ -1,9 +1,5 @@
 package org.knowm.xchange.hitbtc.v2.service;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
@@ -24,6 +20,11 @@ import org.knowm.xchange.service.trade.params.TradeHistoryParamPaging;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
 import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class HitbtcTradeService extends HitbtcTradeServiceRaw implements TradeService {
 
   public HitbtcTradeService(Exchange exchange) {
@@ -43,24 +44,20 @@ public class HitbtcTradeService extends HitbtcTradeServiceRaw implements TradeSe
 
   @Override
   public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
-    return placeMarketOrderRaw(marketOrder).clientOrderId;
+    return placeMarketOrderRaw(marketOrder).id;
   }
 
   @Override
   public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
-    return placeLimitOrderRaw(limitOrder).clientOrderId;
-  }
-
-  @Override
-  public boolean cancelOrder(String orderId) throws IOException {
-    HitbtcOrder cancelOrderRaw = cancelOrderRaw(orderId);
-    return "canceled".equals(cancelOrderRaw.status);
+    return placeLimitOrderRaw(limitOrder).id;
   }
 
   @Override
   public boolean cancelOrder(CancelOrderParams orderParams) throws IOException {
     if (orderParams instanceof CancelOrderByIdParams) {
-      return cancelOrder(((CancelOrderByIdParams) orderParams).getOrderId());
+      String clientOrderId = ((CancelOrderByIdParams) orderParams).getOrderId();
+      HitbtcOrder cancelOrderRaw = cancelOrderRaw(clientOrderId);
+      return "canceled".equals(cancelOrderRaw.status);
     } else {
       return false;
     }
@@ -89,7 +86,7 @@ public class HitbtcTradeService extends HitbtcTradeServiceRaw implements TradeSe
     }
 
     List<HitbtcOwnTrade> tradeHistoryRaw = getTradeHistoryRaw(symbol, limit, offset);
-    return HitbtcAdapters.adaptTradeHistory(tradeHistoryRaw, exchange.getExchangeMetaData());
+    return HitbtcAdapters.adaptTradeHistory(tradeHistoryRaw);
   }
 
   @Override
@@ -110,7 +107,7 @@ public class HitbtcTradeService extends HitbtcTradeServiceRaw implements TradeSe
 
     Collection<Order> orders = new ArrayList<>();
     for (String orderId : orderIds) {
-      HitbtcOrder rawOrder = getHitbtcOrder("BTCUSD", orderId);
+      HitbtcOrder rawOrder = getHitbtcOrder("BTCUSD", orderId);//why is the currency pair hardcoded?
 
       if (rawOrder != null) orders.add(HitbtcAdapters.adaptOrder(rawOrder));
     }
