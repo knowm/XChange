b902d86ded7709d4cf06826809a4dbf8597c5d2c
陈晔 <chenye94@qq.com>
2018-05-20 11:36:30 +0800

    end

--- xchange-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingExchange.java
+++ xchange-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingExchange.java
@@ -4,6 +4,7 @@ import info.bitrich.xchangestream.core.ProductSubscription;
 import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import io.reactivex.Completable;
+import io.reactivex.Observable;
 import org.knowm.xchange.binance.BinanceExchange;
 import org.knowm.xchange.currency.CurrencyPair;
 
@@ -54,6 +55,11 @@ public class BinanceStreamingExchange extends BinanceExchange implements Streami
     @Override
     public boolean isAlive() {
         return streamingService!= null && streamingService.isSocketOpen();
+    }
+
+    @Override
+    public Observable<Throwable> reconnectFailure() {
+        return streamingService.subscribeReconnectFailure();
     }
 
     @Override
