7692878228c6e8acdeeeaba27ec1faa01fd15529
Dennis Ruhe <dennis@amazingsystems.nl>
2018-05-21 16:24:35 +0200

    [bl3p] More work on the trade service

--- xchange-bl3p/src/main/java/org/knowm/xchange/bl3p/Bl3pUtils.java
+++ xchange-bl3p/src/main/java/org/knowm/xchange/bl3p/Bl3pUtils.java
@@ -14,16 +14,28 @@ public class Bl3pUtils {
     public static final BigDecimal fromSatoshi(BigDecimal bd) {
         return bd.divide(SATOSHI);
     }
+    public static final long toSatoshi(BigDecimal bd) { return bd.multiply(SATOSHI).longValue(); }
 
     public static final BigDecimal fromEuroshi(BigDecimal bd) {
         return bd.divide(EUROSHI);
     }
+    public static final long toEuroshi(BigDecimal bd) { return bd.multiply(EUROSHI).longValue(); }
 
     public static String toPairString(CurrencyPair currencyPair) {
         return currencyPair.base.getCurrencyCode() + currencyPair.counter.getCurrencyCode();
     }
 
-    public static Order.OrderType fromBl3pStatus(String bl3pStatus) {
-        return bl3pStatus.equals("bid") ? Order.OrderType.BID : Order.OrderType.ASK;
+    public static Order.OrderType fromBl3pOrderType(String bl3pType) {
+        return bl3pType.equals("bid") ? Order.OrderType.BID : Order.OrderType.ASK;
+    }
+
+    public static String toBl3pOrderType(Order.OrderType orderType) {
+        switch (orderType) {
+            case BID: return "bid";
+            case ASK: return "ask";
+            case EXIT_ASK: return "exit_ask";
+            case EXIT_BID: return "exit_bid";
+            default: return null;
+        }
     }
 }
--- xchange-bl3p/src/test/java/org/knowm/xchange/bl3p/service/Bl3pTradeServiceTest.java
+++ xchange-bl3p/src/test/java/org/knowm/xchange/bl3p/service/Bl3pTradeServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.bl3p.Bl3pExchange;
+import org.knowm.xchange.bl3p.service.params.CancelOrderByIdAndCurrencyPairParams;
+import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.trade.OpenOrders;
 import org.knowm.xchange.service.trade.TradeService;
 
@@ -19,6 +21,41 @@ public class Bl3pTradeServiceTest {
         OpenOrders openOrders = tradeService.getOpenOrders();
 
         System.out.println(openOrders);
+    }
+
+    @Test
+    public void createMarketOrder() throws IOException {
+        /**
+        MarketOrder order = new MarketOrder.Builder(Order.OrderType.BID, CurrencyPair.BTC_EUR)
+                .originalAmount(new BigDecimal("0.0001"))
+                .build();
+
+        String orderId = tradeService.placeMarketOrder(order);
+
+        System.out.println(orderId);
+        */
+    }
+
+    @Test
+    public void createLimitOrder() throws IOException {
+        /*
+        LimitOrder order = new LimitOrder.Builder(Order.OrderType.BID, CurrencyPair.BTC_EUR)
+                .originalAmount(new BigDecimal("0.001"))
+                .limitPrice(new BigDecimal("5000"))
+                .build();
+
+        String orderId = tradeService.placeLimitOrder(order);
+
+        System.out.println(orderId);
+        */
+    }
+
+    @Test
+    public void cancelOrder() throws IOException {
+        CancelOrderByIdAndCurrencyPairParams params = new CancelOrderByIdAndCurrencyPairParams(CurrencyPair.BTC_EUR, "30058063");
+        boolean result = tradeService.cancelOrder(params);
+
+        System.out.println(result);
     }
 
 }
