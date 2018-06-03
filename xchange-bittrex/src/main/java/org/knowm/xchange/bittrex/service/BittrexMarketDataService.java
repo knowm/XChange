5f644054a01d8e427cdbe83aa3cf6d7286ecaa68
Cyril <nguyen.cyril@gmail.com>
2018-06-03 16:54:25 +0200

    Added getTickers

--- xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/service/BittrexMarketDataService.java
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/service/BittrexMarketDataService.java
@@ -1,7 +1,10 @@
 package org.knowm.xchange.bittrex.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bittrex.BittrexAdapters;
 import org.knowm.xchange.bittrex.BittrexUtils;
@@ -13,12 +16,15 @@ import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.dto.trade.LimitOrder;
 import org.knowm.xchange.service.marketdata.MarketDataService;
+import org.knowm.xchange.service.marketdata.params.CurrencyPairsParam;
+import org.knowm.xchange.service.marketdata.params.Params;
+import org.knowm.xchange.utils.jackson.CurrencyPairDeserializer;
 
 /**
  * Implementation of the market data service for Bittrex
  *
  * <ul>
- *   <li>Provides access to various market data values
+ * <li>Provides access to various market data values
  * </ul>
  */
 public class BittrexMarketDataService extends BittrexMarketDataServiceRaw
@@ -41,7 +47,24 @@ public class BittrexMarketDataService extends BittrexMarketDataServiceRaw
         getBittrexMarketSummary(BittrexUtils.toPairString(currencyPair)), currencyPair);
   }
 
-  /** @param args If an integer is provided, then it used as depth of order book */
+  @Override
+  public List<Ticker> getTickers(Params params) throws IOException {
+    List<CurrencyPair> currencyPairs =
+        (params instanceof CurrencyPairsParam)
+            ? new ArrayList<>(((CurrencyPairsParam) params).getCurrencyPairs())
+            : new ArrayList<>();
+    return getBittrexMarketSummaries()
+        .stream()
+        .map(bittrexMarketSummary ->
+                 BittrexAdapters.adaptTicker(bittrexMarketSummary,
+                                             CurrencyPairDeserializer.getCurrencyPairFromString(bittrexMarketSummary.getMarketName())))
+        .filter(ticker -> currencyPairs.size() == 0 || currencyPairs.contains(ticker.getCurrencyPair()))
+        .collect(Collectors.toList());
+  }
+
+  /**
+   * @param args If an integer is provided, then it used as depth of order book
+   */
   @Override
   public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
 
