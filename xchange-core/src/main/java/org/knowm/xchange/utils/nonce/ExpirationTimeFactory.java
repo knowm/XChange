79a77f34beb1a8160e3d70ff070086f3aef0fc21
陈晔 <chenye94@qq.com>
2018-06-11 12:27:16 +0800

    add fcoin

--- xchange-core/src/main/java/org/knowm/xchange/utils/nonce/ExpirationTimeFactory.java
+++ xchange-core/src/main/java/org/knowm/xchange/utils/nonce/ExpirationTimeFactory.java
@@ -5,20 +5,19 @@ import si.mazi.rescu.SynchronizedValueFactory;
 // Generator for unix timestamp of signature expiration
 public class ExpirationTimeFactory implements SynchronizedValueFactory<Long> {
 
-    // Valid time in seconds. Generated timestamp would be current time plus this value
-    private long validTime;
+  // Valid time in seconds. Generated timestamp would be current time plus this value
+  private long validTime;
 
-    public ExpirationTimeFactory() {
-        validTime = 30;
-    }
+  public ExpirationTimeFactory() {
+    validTime = 30;
+  }
 
-    public ExpirationTimeFactory(long validTime) {
-        this.validTime = validTime;
-    }
+  public ExpirationTimeFactory(long validTime) {
+    this.validTime = validTime;
+  }
 
-    @Override
-    public Long createValue() {
-        return System.currentTimeMillis() / 1000 + validTime;
-    }
-
+  @Override
+  public Long createValue() {
+    return System.currentTimeMillis() / 1000 + validTime;
+  }
 }
