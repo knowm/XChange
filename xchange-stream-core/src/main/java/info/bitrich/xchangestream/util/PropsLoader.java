cf8d9dee0c89835b613e3aeb78bf85e4ca58dfa0
Foat Akhmadeev <foat.akhmadeev@gmail.com>
2018-06-12 23:52:32 +0300

    keys from system props; some renaming

--- xchange-stream-core/src/main/java/info/bitrich/xchangestream/util/PropsLoader.java
+++ xchange-stream-core/src/main/java/info/bitrich/xchangestream/util/PropsLoader.java
@@ -15,11 +15,23 @@ import java.util.Properties;
 public class PropsLoader {
     private static final Logger LOG = LoggerFactory.getLogger(PropsLoader.class);
 
-    public static LocalExchangeConfig loadKeys(String fileName, String originName) throws IOException {
+    public static LocalExchangeConfig loadKeys(String fileName, String originName, String prefix) throws IOException {
+        String apiKeyName = prefix + ".api.key";
+        String secretKeyName = prefix + ".secret.key";
+
+        String apiKey = System.getProperty(apiKeyName);
+        String secretKey = System.getProperty(secretKeyName);
+
+        if (apiKey != null && secretKey != null) {
+            return new LocalExchangeConfig(apiKey, secretKey);
+        } else {
+            LOG.info("Not found keys in system props, loading from a file {}...", fileName);
+        }
+
         try (FileInputStream input = new FileInputStream(fileName)) {
             Properties properties = new Properties();
             properties.load(input);
-            return new LocalExchangeConfig(properties.getProperty("api.key"), properties.getProperty("secret.key"));
+            return new LocalExchangeConfig(properties.getProperty(apiKeyName), properties.getProperty(secretKeyName));
         } catch (FileNotFoundException e) {
             LOG.error("Please create {} file from {}", fileName, originName);
             throw e;
