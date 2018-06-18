77db1b036009852228b538c0b2b43861ca486089
Nikita Belenkiy <jkolobok@gmail.com>
2018-06-18 15:19:15 -0500

    Fixed project version in pom.xml's

--- xchange-core/src/main/java/org/knowm/xchange/utils/ObjectMapperHelper.java
+++ xchange-core/src/main/java/org/knowm/xchange/utils/ObjectMapperHelper.java
@@ -10,50 +10,53 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ObjectMapperHelper {
 
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final Logger logger = LoggerFactory.getLogger(ObjectMapperHelper.class);
+  private static final ObjectMapper objectMapperWithIndentation = initWithIndentation();
+
+  private static final ObjectMapper objectMapperWithoutIndentation = initWithoutIndentation();
 
   private ObjectMapperHelper() {}
 
   public static <T> T readValue(URL src, Class<T> valueType) throws IOException {
-
-    InputStream inputStream = null;
-    try {
-      inputStream = src.openStream();
+    try (InputStream inputStream = src.openStream()) {
       Reader reader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
-      return objectMapper.readValue(reader, valueType);
-    } finally {
-      if (inputStream != null) {
-        inputStream.close();
-      }
+      return objectMapperWithoutIndentation.readValue(reader, valueType);
     }
   }
 
   public static <T> T readValue(String value, Class<T> valueType) throws IOException {
-    return objectMapper.readValue(value, valueType);
+    return objectMapperWithoutIndentation.readValue(value, valueType);
   }
 
   public static <T> String toJSON(T valueType) {
-    return toJSON(valueType, SerializationFeature.INDENT_OUTPUT);
+    return toJSON(objectMapperWithIndentation, valueType);
   }
 
   public static <T> String toCompactJSON(T valueType) {
-    return toJSON(valueType);
+    return toJSON(objectMapperWithoutIndentation, valueType);
   }
 
-  private static <T> String toJSON(T valueType, SerializationFeature... features) {
-    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    for (SerializationFeature feature : features) {
-      objectMapper.enable(feature);
-    }
-    String json = "Problem serializing " + valueType.getClass();
+  private static <T> String toJSON(ObjectMapper objectMapper, T valueType) {
     try {
-      json = objectMapper.writeValueAsString(valueType);
+      return objectMapper.writeValueAsString(valueType);
     } catch (JsonProcessingException e) {
       e.printStackTrace();
+      return "Problem serializing " + valueType.getClass();
     }
-    return json;
+  }
+
+  private static ObjectMapper initWithIndentation() {
+    return new ObjectMapper()
+        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        .enable(SerializationFeature.INDENT_OUTPUT);
+  }
+
+  private static ObjectMapper initWithoutIndentation() {
+    return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
   }
 }
