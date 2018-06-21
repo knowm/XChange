e3bae78b94f8452eebd08e0cba66712f3ebd86f7
陈晔 <chenye94@qq.com>
2018-06-21 10:59:09 +0800

    add fcoin

--- xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoinExchange.java
+++ xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoinExchange.java
@@ -7,36 +7,36 @@ import si.mazi.rescu.SynchronizedValueFactory;
 
 public class FCoinExchange extends BaseExchange {
 
-    @Override
-    public void applySpecification(ExchangeSpecification exchangeSpecification) {
+  @Override
+  public void applySpecification(ExchangeSpecification exchangeSpecification) {
 
-        super.applySpecification(exchangeSpecification);
-    }
+    super.applySpecification(exchangeSpecification);
+  }
 
-    @Override
-    protected void initServices() {
-        this.tradeService = new FCoinTradeService(this);
-    }
+  @Override
+  protected void initServices() {
+    this.tradeService = new FCoinTradeService(this);
+  }
 
-    @Override
-    public ExchangeSpecification getDefaultExchangeSpecification() {
+  @Override
+  public ExchangeSpecification getDefaultExchangeSpecification() {
 
-        ExchangeSpecification exchangeSpecification =
-                new ExchangeSpecification(this.getClass().getCanonicalName());
-        exchangeSpecification.setSslUri("https://api.fcoin.com");
-        exchangeSpecification.setHost("api.fcoin.com");
-        exchangeSpecification.setExchangeName("FCoin");
-        exchangeSpecification.setMetaDataJsonFileOverride(null);
-        exchangeSpecification.setExchangeDescription(
-                "FCoin is a globally oriented crypto-currency trading platform.");
+    ExchangeSpecification exchangeSpecification =
+        new ExchangeSpecification(this.getClass().getCanonicalName());
+    exchangeSpecification.setSslUri("https://api.fcoin.com");
+    exchangeSpecification.setHost("api.fcoin.com");
+    exchangeSpecification.setExchangeName("FCoin");
+    exchangeSpecification.setMetaDataJsonFileOverride(null);
+    exchangeSpecification.setExchangeDescription(
+        "FCoin is a globally oriented crypto-currency trading platform.");
 
-        return exchangeSpecification;
-    }
+    return exchangeSpecification;
+  }
 
-    @Override
-    public SynchronizedValueFactory<Long> getNonceFactory() {
+  @Override
+  public SynchronizedValueFactory<Long> getNonceFactory() {
 
-        // This exchange doesn't use a nonce for authentication
-        return null;
-    }
+    // This exchange doesn't use a nonce for authentication
+    return null;
+  }
 }
