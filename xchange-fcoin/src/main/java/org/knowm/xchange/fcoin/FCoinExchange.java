75d47ce4793a0f5b3a62a46b8827264727ec4781
陈晔 <chenye94@qq.com>
2018-06-21 10:48:04 +0800

    add fcoin place limit order

--- xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoinExchange.java
+++ xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoinExchange.java
@@ -2,37 +2,41 @@ package org.knowm.xchange.fcoin;
 
 import org.knowm.xchange.BaseExchange;
 import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.fcoin.service.FCoinTradeService;
 import si.mazi.rescu.SynchronizedValueFactory;
 
 public class FCoinExchange extends BaseExchange {
 
-  @Override
-  public void applySpecification(ExchangeSpecification exchangeSpecification) {
+    @Override
+    public void applySpecification(ExchangeSpecification exchangeSpecification) {
 
-    super.applySpecification(exchangeSpecification);
-  }
+        super.applySpecification(exchangeSpecification);
+    }
 
-  @Override
-  protected void initServices() {}
+    @Override
+    protected void initServices() {
+        this.tradeService = new FCoinTradeService(this);
+    }
 
-  @Override
-  public ExchangeSpecification getDefaultExchangeSpecification() {
+    @Override
+    public ExchangeSpecification getDefaultExchangeSpecification() {
 
-    ExchangeSpecification exchangeSpecification =
-        new ExchangeSpecification(this.getClass().getCanonicalName());
-    exchangeSpecification.setSslUri("https://api.fcoin.com/v2");
-    exchangeSpecification.setHost("api.fcoin.com");
-    exchangeSpecification.setExchangeName("FCoin");
-    exchangeSpecification.setExchangeDescription(
-        "FCoin is a globally oriented crypto-currency trading platform.");
+        ExchangeSpecification exchangeSpecification =
+                new ExchangeSpecification(this.getClass().getCanonicalName());
+        exchangeSpecification.setSslUri("https://api.fcoin.com");
+        exchangeSpecification.setHost("api.fcoin.com");
+        exchangeSpecification.setExchangeName("FCoin");
+        exchangeSpecification.setMetaDataJsonFileOverride(null);
+        exchangeSpecification.setExchangeDescription(
+                "FCoin is a globally oriented crypto-currency trading platform.");
 
-    return exchangeSpecification;
-  }
+        return exchangeSpecification;
+    }
 
-  @Override
-  public SynchronizedValueFactory<Long> getNonceFactory() {
+    @Override
+    public SynchronizedValueFactory<Long> getNonceFactory() {
 
-    // This exchange doesn't use a nonce for authentication
-    return null;
-  }
+        // This exchange doesn't use a nonce for authentication
+        return null;
+    }
 }
