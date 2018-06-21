31413666cf5bcef534092494daff411472bd3f00
陈晔 <chenye94@qq.com>
2018-06-21 10:48:04 +0800

    add fcoin place limit order

--- xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/dto/marketdata/FCoinDepth.java
+++ xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/dto/marketdata/FCoinDepth.java
@@ -1,41 +1,59 @@
 package org.knowm.xchange.fcoin.dto.marketdata;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Date;
 
 public class FCoinDepth {
-    private final BigDecimal[][] asks;
-    private final BigDecimal[][] bids;
-    private final Date ts;
-    private final Long seq;
+  private final BigDecimal[][] asks;
+  private final BigDecimal[][] bids;
+  private final Date ts;
+  private final Long seq;
 
-    public FCoinDepth(@JsonProperty("asks") BigDecimal[][] asks, @JsonProperty("bids") BigDecimal[][] bids, @JsonProperty(required = false,value = "ts") Date ts, @JsonProperty(required = false,value = "seq") Long seq) {
-        this.asks = asks;
-        this.bids = bids;
-        this.ts = ts;
-        this.seq = seq;
-    }
+  public FCoinDepth(
+      @JsonProperty("asks") BigDecimal[] asks,
+      @JsonProperty("bids") BigDecimal[] bids,
+      @JsonProperty(required = false, value = "ts") Long ts,
+      @JsonProperty(required = false, value = "seq") Long seq) {
 
-    public BigDecimal[][] getAsks() {
-        return this.asks;
+    this.asks = new BigDecimal[asks.length / 2][];
+    for (int i = 0; i < asks.length / 2; i++) {
+      this.asks[i] = new BigDecimal[2];
+      this.asks[i][0] = asks[i * 2];
+      this.asks[i][1] = asks[i * 2 + 1];
     }
+    this.bids = new BigDecimal[bids.length / 2][];
+    for (int i = 0; i < asks.length / 2; i++) {
+      this.bids[i] = new BigDecimal[2];
+      this.bids[i][0] = bids[i * 2];
+      this.bids[i][1] = bids[i * 2 + 1];
+    }
+    this.ts = new Date(ts);
+    this.seq = seq;
+  }
 
-    public BigDecimal[][] getBids() {
-        return this.bids;
-    }
+  public BigDecimal[][] getAsks() {
+    return this.asks;
+  }
 
-    public Date getTs() {
-        return ts;
-    }
+  public BigDecimal[][] getBids() {
+    return this.bids;
+  }
 
-    public Long getSeq() {
-        return seq;
-    }
+  public Date getTs() {
+    return ts;
+  }
 
-    public String toString() {
-        return "FcoinDepth [asks=" + Arrays.toString(this.asks) + ", bids=" + Arrays.toString(this.bids) + "]";
-    }
+  public Long getSeq() {
+    return seq;
+  }
+
+  public String toString() {
+    return "FcoinDepth [asks="
+        + Arrays.toString(this.asks)
+        + ", bids="
+        + Arrays.toString(this.bids)
+        + "]";
+  }
 }
