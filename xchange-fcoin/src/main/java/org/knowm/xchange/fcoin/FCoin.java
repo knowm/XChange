fad81f0c5c2f48debea90f773d4c0b7bc8de39fa
陈晔 <chenye94@qq.com>
2018-06-21 10:48:04 +0800

    add fcoin place limit order

--- xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoin.java
+++ xchange-fcoin/src/main/java/org/knowm/xchange/fcoin/FCoin.java
@@ -1,20 +1,24 @@
 package org.knowm.xchange.fcoin;
 
-import java.math.BigDecimal;
-import javax.annotation.Nullable;
-import javax.ws.rs.*;
+import org.knowm.xchange.fcoin.dto.trade.FCoinOrder;
+import org.knowm.xchange.fcoin.dto.trade.FCoinOrderResult;
+import si.mazi.rescu.ParamsDigest;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.math.BigDecimal;
+
+@Path("/v2")
+@Produces(MediaType.APPLICATION_JSON)
 public interface FCoin {
 
-  @POST
-  @Path("order")
-  String placeOrder(
-      @FormParam("symbol") String symbol,
-      @Nullable @FormParam("side") String side,
-      @FormParam("orderQty") int orderQuantity,
-      @FormParam("price") BigDecimal price,
-      @Nullable @FormParam("stopPx") BigDecimal stopPrice,
-      @Nullable @FormParam("ordType") String orderType,
-      @Nullable @FormParam("clOrdID") String clOrdID,
-      @Nullable @FormParam("execInst") String executionInstructions);
+    @POST
+    @Path("orders")
+    @Consumes(MediaType.APPLICATION_JSON)
+    FCoinOrderResult placeOrder(
+            @HeaderParam("FC-ACCESS-KEY") String apiKey,
+            @HeaderParam("FC-ACCESS-TIMESTAMP") Long timestamp,
+            @HeaderParam("FC-ACCESS-SIGNATURE") ParamsDigest paramsDigest,
+            FCoinOrder order);
+
 }
