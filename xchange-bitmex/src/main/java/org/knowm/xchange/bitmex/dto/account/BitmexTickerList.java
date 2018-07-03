a5653787338d73ae317999d3058e7ad7d24b828f
Nikita Belenkiy <jkolobok@gmail.com>
2018-07-03 16:09:45 -0500

    Added ability to track bitmex rate limit

---
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/dto/account/BitmexTickerList.java
@@ -0,0 +1,6 @@
+package org.knowm.xchange.bitmex.dto.account;
+
+import org.knowm.xchange.bitmex.HttpResponseAwareList;
+
+/** @author Nikita Belenkiy on 03/07/2018. */
+public class BitmexTickerList extends HttpResponseAwareList<BitmexTicker> {}
