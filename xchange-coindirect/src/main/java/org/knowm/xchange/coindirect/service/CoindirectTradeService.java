b3db1acae0514217b2198c202d95268d82936b14
Donald Jackson <donald@ddj.co.za>
2018-07-12 17:04:45 +0200

    [coindirect] trade service release

--- xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectTradeService.java
+++ xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectTradeService.java
@@ -1,78 +1,190 @@
 package org.knowm.xchange.coindirect.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.coindirect.CoindirectAdapters;
 import org.knowm.xchange.coindirect.dto.trade.CoindirectOrder;
-import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.coindirect.dto.trade.CoindirectOrderRequest;
+import org.knowm.xchange.coindirect.service.params.CoindirectTradeHistoryParams;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.marketdata.Trades;
-import org.knowm.xchange.dto.trade.LimitOrder;
-import org.knowm.xchange.dto.trade.OpenOrders;
-import org.knowm.xchange.dto.trade.UserTrade;
-import org.knowm.xchange.dto.trade.UserTrades;
+import org.knowm.xchange.dto.trade.*;
+import org.knowm.xchange.exceptions.ExchangeException;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.CancelOrderByIdParams;
+import org.knowm.xchange.service.trade.params.CancelOrderParams;
 import org.knowm.xchange.service.trade.params.TradeHistoryParams;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.knowm.xchange.service.trade.params.TradeHistoryParamsIdSpan;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParamCurrencyPair;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
+import org.knowm.xchange.service.trade.params.orders.OrderQueryParams;
 
 public class CoindirectTradeService extends CoindirectTradeServiceRaw implements TradeService {
-    /**
-     * Constructor
-     *
-     * @param exchange
-     */
-    public CoindirectTradeService(Exchange exchange) {
-        super(exchange);
+  /**
+   * Constructor
+   *
+   * @param exchange
+   */
+  public CoindirectTradeService(Exchange exchange) {
+    super(exchange);
+  }
+
+  private List<CoindirectOrder> getOpenOrders(String symbol) throws IOException {
+    return listExchangeOrders(symbol, false, 0, 1000);
+  }
+
+  private OpenOrders createOpenOrders(List<CoindirectOrder> coindirectOrders) {
+    List<LimitOrder> limitOrders = new ArrayList<>();
+    List<Order> otherOrders = new ArrayList<>();
+    coindirectOrders.forEach(
+        coindirectOrder -> {
+          Order order = CoindirectAdapters.adaptOrder(coindirectOrder);
+          if (order instanceof LimitOrder) {
+            limitOrders.add((LimitOrder) order);
+          } else {
+            otherOrders.add(order);
+          }
+        });
+    return new OpenOrders(limitOrders, otherOrders);
+  }
+
+  @Override
+  public OpenOrders getOpenOrders() throws IOException {
+    List<CoindirectOrder> coindirectOrders = getOpenOrders((String) null);
+    return createOpenOrders(coindirectOrders);
+  }
+
+  @Override
+  public OpenOrders getOpenOrders(OpenOrdersParams params) throws IOException {
+    String symbol = null;
+    if (params instanceof OpenOrdersParamCurrencyPair) {
+      OpenOrdersParamCurrencyPair pairParams = (OpenOrdersParamCurrencyPair) params;
+      symbol = CoindirectAdapters.toSymbol(pairParams.getCurrencyPair());
+    }
+    return createOpenOrders(getOpenOrders(symbol));
+  }
+
+  @Override
+  public UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
+
+    long fromOffset = 0;
+    long max = 10;
+    if (params instanceof TradeHistoryParamsIdSpan) {
+      TradeHistoryParamsIdSpan idParams = (TradeHistoryParamsIdSpan) params;
+
+      try {
+        fromOffset = Long.valueOf(idParams.getStartId());
+      } catch (Throwable ignored) {
+      }
     }
 
-    @Override
-    public OpenOrders getOpenOrders() throws IOException {
-        List<CoindirectOrder> coindirectOrders = listExchangeOrders(null, false, 0, 1000);
-        List<LimitOrder> limitOrders = new ArrayList<>();
-        List<Order> otherOrders = new ArrayList<>();
-        coindirectOrders.forEach(
-                coindirectOrder -> {
-                    Order order = CoindirectAdapters.adaptOrder(coindirectOrder);
-                    if (order instanceof LimitOrder) {
-                        limitOrders.add((LimitOrder) order);
-                    } else {
-                        otherOrders.add(order);
-                    }
-                });
-        return new OpenOrders(limitOrders, otherOrders);
+    List<CoindirectOrder> coindirectOrders = listExchangeOrders(null, true, fromOffset, max);
+
+    List<UserTrade> trades =
+        coindirectOrders
+            .stream()
+            .map(
+                t -> {
+                  if (t.executedAmount == null || t.executedAmount.signum() == 0) {
+                    return null;
+                  }
+                  return new UserTrade(
+                      CoindirectAdapters.convert(t.side),
+                      t.executedAmount,
+                      CoindirectAdapters.toCurrencyPair(t.symbol),
+                      t.executedPrice,
+                      t.dateCreated,
+                      t.uuid,
+                      t.uuid,
+                      t.executedFees,
+                      CoindirectAdapters.toCurrencyPair(t.symbol).counter);
+                })
+            .filter(t -> t != null)
+            .collect(Collectors.toList());
+
+    return new UserTrades(trades, (fromOffset + max), Trades.TradeSortType.SortByTimestamp);
+  }
+
+  @Override
+  public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
+    CoindirectOrderRequest coindirectOrderRequest =
+        new CoindirectOrderRequest(
+            CoindirectAdapters.toSymbol(marketOrder.getCurrencyPair()),
+            marketOrder.getOriginalAmount(),
+            null,
+            CoindirectOrder.Type.MARKET,
+            CoindirectAdapters.convert(marketOrder.getType()));
+
+    CoindirectOrder coindirectOrder = placeExchangeOrder(coindirectOrderRequest);
+
+    return coindirectOrder.uuid;
+  }
+
+  @Override
+  public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
+    CoindirectOrderRequest coindirectOrderRequest =
+        new CoindirectOrderRequest(
+            CoindirectAdapters.toSymbol(limitOrder.getCurrencyPair()),
+            limitOrder.getOriginalAmount(),
+            limitOrder.getLimitPrice(),
+            CoindirectOrder.Type.LIMIT,
+            CoindirectAdapters.convert(limitOrder.getType()));
+
+    CoindirectOrder coindirectOrder = placeExchangeOrder(coindirectOrderRequest);
+
+    return coindirectOrder.uuid;
+  }
+
+  @Override
+  public String placeStopOrder(StopOrder stopOrder) throws IOException {
+    throw new NotAvailableFromExchangeException();
+  }
+
+  @Override
+  public boolean cancelOrder(String orderId) throws IOException {
+    CoindirectOrder coindirectOrder = cancelExchangeOrder(orderId);
+    switch (coindirectOrder.status) {
+      case CANCELLED:
+      case PENDING_CANCEL:
+        return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean cancelOrder(CancelOrderParams orderParams) throws IOException {
+    if (orderParams instanceof CancelOrderByIdParams) {
+      return cancelOrder(((CancelOrderByIdParams) orderParams).getOrderId());
     }
 
-    @Override
-    public UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
-        List<CoindirectOrder> coindirectOrders = listExchangeOrders(null, true, 0, 1000);
+    throw new ExchangeException("You need to provide the the order id to cancel an order.");
+  }
 
-        List<UserTrade> trades =
-                coindirectOrders
-                        .stream()
-                        .map(
-                                t -> {
-                                        if(t.executedAmount == null || t.executedAmount.signum() == 0) {
-                                            return null;
-                                        }
-                                        return new UserTrade(
-                                                CoindirectAdapters.convert(t.side),
-                                                t.executedAmount,
-                                                CoindirectAdapters.toCurrencyPair(t.symbol),
-                                                t.executedPrice,
-                                                t.dateCreated,
-                                                t.uuid,
-                                                t.uuid,
-                                                BigDecimal.ZERO,
-                                                CoindirectAdapters.toCurrencyPair(t.symbol).counter
-                                        );
-                                })
-                        .filter(t -> t != null)
-                        .collect(Collectors.toList());
+  @Override
+  public TradeHistoryParams createTradeHistoryParams() {
+    return new CoindirectTradeHistoryParams();
+  }
 
-        return new UserTrades(trades, Trades.TradeSortType.SortByTimestamp);
+  @Override
+  public Collection<Order> getOrder(String... orderIds) throws IOException {
+    List<Order> orderList = new ArrayList<>();
+    for (String orderId : orderIds) {
+      orderList.add(CoindirectAdapters.adaptOrder(getExchangeOrder(orderId)));
     }
+    return orderList;
+  }
+
+  @Override
+  public Collection<Order> getOrder(OrderQueryParams... orderQueryParams) throws IOException {
+    List<Order> orderList = new ArrayList<>();
+    for (OrderQueryParams orderParams : orderQueryParams) {
+      orderList.add(CoindirectAdapters.adaptOrder(getExchangeOrder(orderParams.getOrderId())));
+    }
+    return orderList;
+  }
 }
--- xchange-examples/src/main/java/org/knowm/xchange/examples/coindirect/account/CoindirectAccountDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/coindirect/account/CoindirectAccountDemo.java
@@ -1,44 +1,36 @@
 package org.knowm.xchange.examples.coindirect.account;
 
+import java.io.IOException;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.coindirect.CoindirectExchange;
 import org.knowm.xchange.coindirect.service.CoindirectAccountService;
-import org.knowm.xchange.coindirect.service.CoindirectMarketDataService;
-import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.examples.coindirect.CoindirectDemoUtils;
 import org.knowm.xchange.service.account.AccountService;
-import org.knowm.xchange.service.marketdata.MarketDataService;
-
-import java.io.IOException;
 
 public class CoindirectAccountDemo {
-    public static void main(String[] args) throws IOException {
-        Exchange exchange = CoindirectDemoUtils.createExchange();
+  public static void main(String[] args) throws IOException {
+    Exchange exchange = CoindirectDemoUtils.createExchange();
 
+    /* create a data service from the exchange */
+    AccountService accountService = exchange.getAccountService();
 
-        /* create a data service from the exchange */
-        AccountService accountService = exchange.getAccountService();
+    generic(exchange, accountService);
+    raw((CoindirectExchange) exchange, (CoindirectAccountService) accountService);
+  }
 
-        generic(exchange, accountService);
-        raw((CoindirectExchange) exchange, (CoindirectAccountService) accountService);
-    }
+  private static void generic(Exchange exchange, AccountService accountService) throws IOException {
 
-    private static void generic(Exchange exchange, AccountService accountService) throws IOException {
+    AccountInfo accountInfo = accountService.getAccountInfo();
+    System.out.println("Account Info: " + accountInfo);
 
-        AccountInfo accountInfo = accountService.getAccountInfo();
-        System.out.println("Account Info: " + accountInfo);
+    //        String depositAddress = accountService.requestDepositAddress(Currency.BTC);
+    //        System.out.println("Deposit Address: " + depositAddress);
 
-//        String depositAddress = accountService.requestDepositAddress(Currency.BTC);
-//        System.out.println("Deposit Address: " + depositAddress);
+    // String transactionHash = accountService.withdrawFunds(new BigDecimal(".01"), "XXX");
+    // System.out.println("Bitcoin blockchain transaction hash: " + transactionHash);
+  }
 
-        // String transactionHash = accountService.withdrawFunds(new BigDecimal(".01"), "XXX");
-        // System.out.println("Bitcoin blockchain transaction hash: " + transactionHash);
-    }
-
-    public static void raw(CoindirectExchange exchange, CoindirectAccountService accountService) throws IOException {
-
-
-
-    }
+  public static void raw(CoindirectExchange exchange, CoindirectAccountService accountService)
+      throws IOException {}
 }
