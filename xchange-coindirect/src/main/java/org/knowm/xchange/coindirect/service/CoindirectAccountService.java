2b933ce8a0fbd12fde513c62a9fbf1dddc6aeb0f
Donald Jackson <donald@ddj.co.za>
2018-07-12 17:04:45 +0200

    [coindirect] trade service release

--- xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectAccountService.java
+++ xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectAccountService.java
@@ -1,5 +1,9 @@
 package org.knowm.xchange.coindirect.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.coindirect.CoindirectAdapters;
 import org.knowm.xchange.coindirect.dto.account.CoindirectWallet;
@@ -8,37 +12,39 @@ import org.knowm.xchange.dto.account.Balance;
 import org.knowm.xchange.dto.account.Wallet;
 import org.knowm.xchange.service.account.AccountService;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+public class CoindirectAccountService extends CoindirectAccountServiceRaw
+    implements AccountService {
+  /**
+   * Constructor
+   *
+   * @param exchange
+   */
+  public CoindirectAccountService(Exchange exchange) {
+    super(exchange);
+  }
 
-public class CoindirectAccountService extends CoindirectAccountServiceRaw implements AccountService {
-    /**
-     * Constructor
-     *
-     * @param exchange
-     */
-    public CoindirectAccountService(Exchange exchange) {
-        super(exchange);
+  @Override
+  public AccountInfo getAccountInfo() throws IOException {
+    List<CoindirectWallet> coindirectWallets = listCoindirectWallets(1000);
+
+    Wallet wallet;
+    Balance balance;
+
+    List<Wallet> wallets = new ArrayList<>();
+
+    for (CoindirectWallet coindirectWallet : coindirectWallets) {
+      balance =
+          new Balance(
+              CoindirectAdapters.toCurrency(coindirectWallet.currency.code),
+              coindirectWallet.balance);
+      wallet =
+          new Wallet(
+              String.valueOf(coindirectWallet.id),
+              coindirectWallet.description,
+              Arrays.asList(balance));
+      wallets.add(wallet);
     }
 
-    @Override
-    public AccountInfo getAccountInfo() throws IOException {
-        List<CoindirectWallet> coindirectWallets = listCoindirectWallets(1000);
-
-        Wallet wallet;
-        Balance balance;
-
-        List<Wallet> wallets = new ArrayList<>();
-
-        for(CoindirectWallet coindirectWallet : coindirectWallets) {
-            balance = new Balance(CoindirectAdapters.toCurrency(coindirectWallet.currency.code), coindirectWallet.balance);
-            wallet = new Wallet(String.valueOf(coindirectWallet.id), coindirectWallet.description, Arrays.asList(balance));
-            wallets.add(wallet);
-        }
-
-        return new AccountInfo(wallets);
-    }
+    return new AccountInfo(wallets);
+  }
 }
--- xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectBaseService.java
+++ xchange-coindirect/src/main/java/org/knowm/xchange/coindirect/service/CoindirectBaseService.java
@@ -6,35 +6,32 @@ import org.knowm.xchange.service.BaseExchangeService;
 import org.knowm.xchange.service.BaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import si.mazi.rescu.Interceptor;
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.RestProxyFactory;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-
 public class CoindirectBaseService extends BaseExchangeService implements BaseService {
-    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+  protected final Logger LOG = LoggerFactory.getLogger(getClass());
 
-    protected final String apiKey;
-    protected final CoindirectAuthenticated coindirect;
-    protected final ParamsDigest signatureCreator;
-    /**
-     * Constructor
-     *
-     * @param exchange
-     */
-    protected CoindirectBaseService(Exchange exchange) {
-        super(exchange);
+  protected final String apiKey;
+  protected final CoindirectAuthenticated coindirect;
+  protected final ParamsDigest signatureCreator;
+  /**
+   * Constructor
+   *
+   * @param exchange
+   */
+  protected CoindirectBaseService(Exchange exchange) {
+    super(exchange);
 
-        this.coindirect =
-                RestProxyFactory.createProxy(
-                        CoindirectAuthenticated.class,
-                        exchange.getExchangeSpecification().getSslUri(),
-                        getClientConfig());
-        this.apiKey = exchange.getExchangeSpecification().getApiKey();
-        this.signatureCreator = CoindirectHawkDigest.createInstance(exchange.getExchangeSpecification().getApiKey(), exchange.getExchangeSpecification().getSecretKey());
-    }
-
-
+    this.coindirect =
+        RestProxyFactory.createProxy(
+            CoindirectAuthenticated.class,
+            exchange.getExchangeSpecification().getSslUri(),
+            getClientConfig());
+    this.apiKey = exchange.getExchangeSpecification().getApiKey();
+    this.signatureCreator =
+        CoindirectHawkDigest.createInstance(
+            exchange.getExchangeSpecification().getApiKey(),
+            exchange.getExchangeSpecification().getSecretKey());
+  }
 }
