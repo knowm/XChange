5a7d771f486af4505bfca740d48adb891f042cbf
Panchen <Panchen@users.noreply.github.com>
2018-07-23 16:39:04 +0100

    [BitFinex.V2] Add Public Trades Endpoint
    - Add generic v2 getTrades and raw getBitfinexPublicTrades methods
    - Add new raw v2 getBitfinexPublicFundingTrades method
    - Resolve BitfinexExchange conflict post PR #2714

--- xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v2/service/BitfinexMarketDataServiceRaw.java
+++ xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v2/service/BitfinexMarketDataServiceRaw.java
@@ -1,13 +1,18 @@
 package org.knowm.xchange.bitfinex.v2.service;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bitfinex.common.dto.BitfinexException;
 import org.knowm.xchange.bitfinex.v2.BitfinexAdapters;
+import org.knowm.xchange.bitfinex.v2.dto.marketdata.BitfinexPublicFundingTrade;
+import org.knowm.xchange.bitfinex.v2.dto.marketdata.BitfinexPublicTrade;
 import org.knowm.xchange.bitfinex.v2.dto.marketdata.BitfinexTicker;
+import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
+import si.mazi.rescu.HttpStatusIOException;
 
 public class BitfinexMarketDataServiceRaw extends BitfinexBaseService {
   /**
@@ -33,5 +38,21 @@ public class BitfinexMarketDataServiceRaw extends BitfinexBaseService {
     } else {
       return ticker[0];
     }
+  }
+
+  public BitfinexPublicTrade[] getBitfinexPublicTrades(CurrencyPair currencyPair, int limitTrades, long startTimestamp, long endTimestamp, int sort) throws IOException {
+    try {
+      return bitfinex.getPublicTrades("t" + currencyPair.base.toString() + currencyPair.counter.toString(), limitTrades, startTimestamp, endTimestamp, sort);
+    } catch ( HttpStatusIOException e) {
+        throw handleException(new BitfinexException(e.getHttpBody()));
+    } 
+  }
+
+  public BitfinexPublicFundingTrade[] getBitfinexPublicFundingTrades(Currency currency, int limitTrades, long startTimestamp, long endTimestamp, int sort) throws IOException {
+    try {
+      return bitfinex.getPublicFundingTrades("f" + currency.toString(), limitTrades, startTimestamp, endTimestamp, sort);
+    } catch ( HttpStatusIOException e) {
+        throw handleException(new BitfinexException(e.getHttpBody()));
+    } 
   }
 }
