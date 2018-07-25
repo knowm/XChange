0515c6b8f65940bd20f806b8bfd28c45b16f1b4f
trix <trixhome@mail.com>
2018-07-25 22:26:30 +0200

    [Bity] - Add Tickers holder - Add some tests

--- xchange-bity/src/main/java/org/knowm/xchange/bity/dto/BityResponse.java
+++ xchange-bity/src/main/java/org/knowm/xchange/bity/dto/BityResponse.java
@@ -1,8 +1,10 @@
 package org.knowm.xchange.bity.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BityResponse<T> {
 
   @JsonProperty("objects")
