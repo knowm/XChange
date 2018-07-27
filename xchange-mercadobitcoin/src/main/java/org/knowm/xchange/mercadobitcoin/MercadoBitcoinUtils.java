b0922c8bc417b513623117332d52ccb5f165a9c4
Leone Lisboa Magevski <leone1983@gmail.com>
2018-07-27 05:22:00 -0300

    Update the data API to v3 and parameterize currencies in service calls

--- xchange-mercadobitcoin/src/main/java/org/knowm/xchange/mercadobitcoin/MercadoBitcoinUtils.java
+++ xchange-mercadobitcoin/src/main/java/org/knowm/xchange/mercadobitcoin/MercadoBitcoinUtils.java
@@ -1,5 +1,7 @@
 package org.knowm.xchange.mercadobitcoin;
 
+import java.util.Arrays;
+import java.util.List;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.trade.LimitOrder;
@@ -7,6 +9,12 @@ import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 
 /** @author Felipe Micaroni Lalli */
 public final class MercadoBitcoinUtils {
+
+  public static final List<CurrencyPair> availablePairs =
+      Arrays.asList(
+          CurrencyPair.BTC_BRL,
+          new CurrencyPair(Currency.LTC, Currency.BRL),
+          new CurrencyPair(Currency.BCH, Currency.BRL));
 
   private MercadoBitcoinUtils() {}
 
@@ -30,5 +38,12 @@ public final class MercadoBitcoinUtils {
   public static String makeMercadoBitcoinOrderId(LimitOrder limitOrder) {
 
     return makeMercadoBitcoinOrderId(limitOrder.getCurrencyPair(), limitOrder.getId());
+  }
+
+  public static void verifyCurrencyPairAvailability(CurrencyPair currencyPair) {
+
+    if (!availablePairs.contains(currencyPair)) {
+      throw new NotAvailableFromExchangeException();
+    }
   }
 }
