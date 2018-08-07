8f24548dc06f22df00c219fed83e436bb97518e4
Niels <niels@draaisma.email>
2018-08-07 12:24:38 +1000

    [independentreserve] Added accountService.getFundingHistory support

--- xchange-independentreserve/src/main/java/org/knowm/xchange/independentreserve/service/IndependentReserveAccountService.java
+++ xchange-independentreserve/src/main/java/org/knowm/xchange/independentreserve/service/IndependentReserveAccountService.java
@@ -2,15 +2,17 @@ package org.knowm.xchange.independentreserve.service;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.*;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
-import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+import org.knowm.xchange.dto.account.Balance;
+import org.knowm.xchange.dto.account.FundingRecord;
+import org.knowm.xchange.dto.account.Wallet;
 import org.knowm.xchange.independentreserve.IndependentReserveAdapters;
+import org.knowm.xchange.independentreserve.dto.trade.IndependentReserveTransaction;
 import org.knowm.xchange.service.account.AccountService;
-import org.knowm.xchange.service.trade.params.DefaultWithdrawFundsParams;
-import org.knowm.xchange.service.trade.params.TradeHistoryParams;
-import org.knowm.xchange.service.trade.params.WithdrawFundsParams;
+import org.knowm.xchange.service.trade.params.*;
 
 /** Author: Kamil Zbikowski Date: 4/10/15 */
 public class IndependentReserveAccountService extends IndependentReserveAccountServiceRaw
@@ -29,7 +31,7 @@ public class IndependentReserveAccountService extends IndependentReserveAccountS
   public AccountInfo getAccountInfo() throws IOException {
     return new AccountInfo(
         exchange.getExchangeSpecification().getUserName(),
-        IndependentReserveAdapters.adaptWallet(getIndependentReserveBalance()));
+        IndependentReserveAdapters.adaptWallets(getIndependentReserveBalance()));
   }
 
   @Override
@@ -51,6 +53,104 @@ public class IndependentReserveAccountService extends IndependentReserveAccountS
 
   @Override
   public TradeHistoryParams createFundingHistoryParams() {
-    throw new NotAvailableFromExchangeException();
+    return new IndependentReserveTradeHistoryParams();
+  }
+
+  @Override
+  public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws IOException {
+    if (params instanceof IndependentReserveTradeHistoryParams) {
+      IndependentReserveTradeHistoryParams historyParams =
+          (IndependentReserveTradeHistoryParams) params;
+      AccountInfo accountInfo = this.getAccountInfo();
+
+      Set<Currency> currencies = new HashSet<>();
+      if (historyParams.getCurrency() == null) {
+        for (Wallet wallet : accountInfo.getWallets().values()) {
+          currencies.addAll(wallet.getBalances().keySet());
+        }
+      } else {
+        currencies.add(historyParams.getCurrency());
+      }
+      List<FundingRecord> fundingRecords = new ArrayList<>();
+
+      for (Wallet wallet : accountInfo.getWallets().values()) {
+        for (Map.Entry<Currency, Balance> e : wallet.getBalances().entrySet()) {
+          if (currencies.contains(e.getKey())) {
+            fundingRecords.addAll(
+                IndependentReserveAdapters.adaptTransaction(
+                    super.getTransactions(
+                        wallet.getId(),
+                        historyParams.startTime,
+                        historyParams.endTime,
+                        historyParams.transactionTypes,
+                        historyParams.getPageNumber(),
+                        historyParams.getPageLength())));
+          }
+        }
+      }
+      return fundingRecords;
+    } else {
+      throw new IllegalArgumentException(
+          "Invalid TradeHistoryParams used as argument of getFundingHistory");
+    }
+  }
+
+  public static class IndependentReserveTradeHistoryParams extends DefaultTradeHistoryParamPaging
+      implements TradeHistoryParams, TradeHistoryParamCurrency, TradeHistoryParamsTimeSpan {
+
+    private Currency currency;
+    private Date startTime;
+    private Date endTime;
+    private IndependentReserveTransaction.Type[] transactionTypes;
+
+    public IndependentReserveTradeHistoryParams() {
+      transactionTypes =
+          new IndependentReserveTransaction.Type[] {
+            IndependentReserveTransaction.Type.Brokerage,
+            IndependentReserveTransaction.Type.Deposit,
+            IndependentReserveTransaction.Type.Withdrawal,
+            IndependentReserveTransaction.Type.Trade
+          };
+      setPageLength(50);
+      setPageNumber(1);
+    }
+
+    @Override
+    public Currency getCurrency() {
+      return currency;
+    }
+
+    @Override
+    public void setCurrency(Currency currency) {
+      this.currency = currency;
+    }
+
+    @Override
+    public Date getStartTime() {
+      return startTime;
+    }
+
+    @Override
+    public void setStartTime(Date startTime) {
+      this.startTime = startTime;
+    }
+
+    @Override
+    public Date getEndTime() {
+      return endTime;
+    }
+
+    @Override
+    public void setEndTime(Date endTime) {
+      this.endTime = endTime;
+    }
+
+    public IndependentReserveTransaction.Type[] getTransactionTypes() {
+      return transactionTypes;
+    }
+
+    public void setTransactionTypes(IndependentReserveTransaction.Type[] transactionTypes) {
+      this.transactionTypes = transactionTypes;
+    }
   }
 }
