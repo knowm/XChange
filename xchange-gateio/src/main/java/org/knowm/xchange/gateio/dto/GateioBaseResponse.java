5452d5e0572f2f140bb9cea077ab7912e452a5c2
André Reiter <mail@areiter.net>
2018-08-08 01:49:33 +0200

    Added support for funding history

--- xchange-gateio/src/main/java/org/knowm/xchange/gateio/dto/GateioBaseResponse.java
+++ xchange-gateio/src/main/java/org/knowm/xchange/gateio/dto/GateioBaseResponse.java
@@ -1,14 +1,11 @@
 package org.knowm.xchange.gateio.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class GateioBaseResponse {
 
   private final boolean result;
   private final String message;
 
-  protected GateioBaseResponse(
-      @JsonProperty("result") final boolean result, @JsonProperty("msg") final String message) {
+  protected GateioBaseResponse(final boolean result, final String message) {
 
     this.result = result;
     this.message = message;
