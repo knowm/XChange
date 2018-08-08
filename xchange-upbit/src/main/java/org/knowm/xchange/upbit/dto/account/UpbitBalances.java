abaef2ef4bfd8f424d30c17fb035895781e24e59
water0126 <water820126@gmail.com>
2018-08-08 18:24:25 +0900

    [Upbit] add getAccountInfo

---
+++ xchange-upbit/src/main/java/org/knowm/xchange/upbit/dto/account/UpbitBalances.java
@@ -0,0 +1,26 @@
+package org.knowm.xchange.upbit.dto.account;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.knowm.xchange.upbit.service.UpbitArrayOrMessageDeserializer;
+
+/** @author interwater */
+@JsonDeserialize(using = UpbitBalances.UpbitBalancesDeserializer.class)
+public class UpbitBalances {
+  private final UpbitBalance[] balances;
+
+  public UpbitBalance[] getBalances() {
+    return balances;
+  }
+
+  public UpbitBalances(@JsonProperty() UpbitBalance[] balances) {
+    this.balances = balances;
+  }
+
+  static class UpbitBalancesDeserializer
+      extends UpbitArrayOrMessageDeserializer<UpbitBalance, UpbitBalances> {
+    public UpbitBalancesDeserializer() {
+      super(UpbitBalance.class, UpbitBalances.class);
+    }
+  }
+}
