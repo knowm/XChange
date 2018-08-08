db85142f18fcbddbbfb001b009fa18f0db0261f5
André Reiter <mail@areiter.net>
2018-08-08 02:37:48 +0200

    added support for withdrawal execution

--- xchange-gateio/src/main/java/org/knowm/xchange/gateio/dto/GateioBaseResponse.java
+++ xchange-gateio/src/main/java/org/knowm/xchange/gateio/dto/GateioBaseResponse.java
@@ -1,11 +1,14 @@
 package org.knowm.xchange.gateio.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GateioBaseResponse {
 
   private final boolean result;
   private final String message;
 
-  protected GateioBaseResponse(final boolean result, final String message) {
+  protected GateioBaseResponse(
+      @JsonProperty("result") final boolean result, @JsonProperty("msg") final String message) {
 
     this.result = result;
     this.message = message;
