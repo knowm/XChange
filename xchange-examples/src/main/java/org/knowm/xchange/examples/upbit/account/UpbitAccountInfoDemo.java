f18cc1ead4134379a0465f08eef03304b467432f
water0126 <water820126@gmail.com>
2018-08-14 14:44:00 +0900

    [Upbit, qouine] change location java-jwt  to parent pom.xml and java-jwt version up

--- xchange-examples/src/main/java/org/knowm/xchange/examples/upbit/account/UpbitAccountInfoDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/upbit/account/UpbitAccountInfoDemo.java
@@ -1,16 +1,14 @@
 package org.knowm.xchange.examples.upbit.account;
 
+import java.io.IOException;
+import java.util.Arrays;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.AccountInfo;
 import org.knowm.xchange.examples.upbit.UpbitDemoUtils;
 import org.knowm.xchange.service.account.AccountService;
 import org.knowm.xchange.upbit.dto.account.UpbitBalance;
-import org.knowm.xchange.upbit.dto.account.UpbitBalances;
 import org.knowm.xchange.upbit.service.UpbitAccountServiceRaw;
-
-import java.io.IOException;
-import java.util.Arrays;
 
 public class UpbitAccountInfoDemo {
   public static void main(String[] args) throws IOException {
@@ -31,8 +29,14 @@ public class UpbitAccountInfoDemo {
 
   private static void raw(UpbitAccountServiceRaw accountService) throws IOException {
     UpbitBalance[] upbitBalance = accountService.getWallet().getBalances();
-    Arrays.stream(upbitBalance).forEach(balance -> {
-      System.out.println("UPBIT Currency : " + balance.getCurrency() + " balance :" + balance.getBalance());
-    });
+    Arrays.stream(upbitBalance)
+        .forEach(
+            balance -> {
+              System.out.println(
+                  "UPBIT Currency : "
+                      + balance.getCurrency()
+                      + " balance :"
+                      + balance.getBalance());
+            });
   }
 }
