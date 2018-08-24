b0c098de6247bb5696152e051f8296873132d681
Theofanis Pantelides <theofanis.pantelides@gmail.com>
2018-08-24 16:53:20 -0400

    [Bittrex] BittrexTradeId fix

--- xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/dto/trade/BittrexTradeId.java
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/dto/trade/BittrexTradeId.java
@@ -1,52 +1,17 @@
 package org.knowm.xchange.bittrex.dto.trade;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.Generated;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@Generated("org.jsonschema2pojo")
-@JsonPropertyOrder({"uuid"})
 public class BittrexTradeId {
 
-  @JsonProperty("uuid")
-  private String uuid;
+  private final String uuid;
 
-  @JsonIgnore private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-  @JsonProperty("uuid")
-  public String getUuid() {
-
-    return uuid;
-  }
-
-  @JsonProperty("uuid")
-  public void setUuid(String uuid) {
-
+  public BittrexTradeId(@JsonProperty( "uuid" ) String uuid) {
     this.uuid = uuid;
-  }
-
-  @JsonAnyGetter
-  public Map<String, Object> getAdditionalProperties() {
-
-    return this.additionalProperties;
-  }
-
-  @JsonAnySetter
-  public void setAdditionalProperty(String name, Object value) {
-
-    this.additionalProperties.put(name, value);
   }
 
   @Override
   public String toString() {
-
-    return "BittrexTradeId [uuid=" + uuid + ", additionalProperties=" + additionalProperties + "]";
+    return "BittrexTradeId [uuid=" + uuid + "]";
   }
 }
