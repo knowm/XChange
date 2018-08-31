d5da4b9c0ecb03ab7bde2413a0f8412167c686f6
Ian W <Ian@xps13>
2018-08-31 16:29:42 -0500

    Format and remove some printStackTrace calls

--- xchange-gdax/src/main/java/org/knowm/xchange/gdax/dto/GDAXTrades.java
+++ xchange-gdax/src/main/java/org/knowm/xchange/gdax/dto/GDAXTrades.java
@@ -3,9 +3,7 @@ package org.knowm.xchange.gdax.dto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.knowm.xchange.gdax.dto.marketdata.GDAXTrade;
-
 import si.mazi.rescu.HttpResponseAware;
 
 public class GDAXTrades extends ArrayList<GDAXTrade> implements HttpResponseAware {
@@ -13,34 +11,33 @@ public class GDAXTrades extends ArrayList<GDAXTrade> implements HttpResponseAwar
   private Map<String, List<String>> headers;
   private Long earliestTradeId;
   private Long latestTradeId;
-  
-  
-  public GDAXTrades() {
-	super();
-	earliestTradeId = null;
-	latestTradeId = null;
-  }
-  
-  public boolean addAll(GDAXTrades gdaxTrades) {
-	  if (earliestTradeId == null) {
-		  earliestTradeId = gdaxTrades.getEarliestTradeId();
-	  } else if (gdaxTrades.getEarliestTradeId() != null) {
-		  earliestTradeId = Math.min( earliestTradeId, gdaxTrades.getEarliestTradeId() );
-	  }
 
-	  if (latestTradeId == null) {
-		  latestTradeId = gdaxTrades.getLatestTradeId();
-	  } else if (gdaxTrades.getLatestTradeId() != null) {
-		  latestTradeId = Math.max( latestTradeId, gdaxTrades.getLatestTradeId() );
-	  }	  
-	  
-	  return super.addAll(gdaxTrades);	  
+  public GDAXTrades() {
+    super();
+    earliestTradeId = null;
+    latestTradeId = null;
+  }
+
+  public boolean addAll(GDAXTrades gdaxTrades) {
+    if (earliestTradeId == null) {
+      earliestTradeId = gdaxTrades.getEarliestTradeId();
+    } else if (gdaxTrades.getEarliestTradeId() != null) {
+      earliestTradeId = Math.min(earliestTradeId, gdaxTrades.getEarliestTradeId());
+    }
+
+    if (latestTradeId == null) {
+      latestTradeId = gdaxTrades.getLatestTradeId();
+    } else if (gdaxTrades.getLatestTradeId() != null) {
+      latestTradeId = Math.max(latestTradeId, gdaxTrades.getLatestTradeId());
+    }
+
+    return super.addAll(gdaxTrades);
   }
 
   @Override
   public void setResponseHeaders(Map<String, List<String>> headers) {
     this.headers = headers;
-    
+
     earliestTradeId = getHeaderAsLong("cb-after");
     latestTradeId = getHeaderAsLong("cb-before");
   }
@@ -51,23 +48,22 @@ public class GDAXTrades extends ArrayList<GDAXTrade> implements HttpResponseAwar
   }
 
   public Long getHeaderAsLong(String key) {
-	Long header = null;
-	  
-	try {
-      header = Long.valueOf( getResponseHeaders().get(key).get(0) );
-	} catch (NullPointerException | NumberFormatException e) {
-	  // nop 
-	}
-    
-    return header;    
+    Long header = null;
+
+    try {
+      header = Long.valueOf(getResponseHeaders().get(key).get(0));
+    } catch (NullPointerException | NumberFormatException e) {
+      // nop
+    }
+
+    return header;
   }
 
-	public Long getEarliestTradeId() {
-		return earliestTradeId;
-	}
-	
-	public Long getLatestTradeId() {
-		return latestTradeId;
-	}
+  public Long getEarliestTradeId() {
+    return earliestTradeId;
+  }
 
+  public Long getLatestTradeId() {
+    return latestTradeId;
+  }
 }
