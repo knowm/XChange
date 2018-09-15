69f44132fd0646fe61e303c1a18c0eabdc3be0d9
Sascha Zieger <zieger@karlmax-berlin.com>
2018-09-15 10:45:59 +0200

    added Builder for BitmexExecutionInstructions

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/dto/trade/BitmexExecutionInstruction.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/dto/trade/BitmexExecutionInstruction.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public enum BitmexExecutionInstruction {
-  PARTICIPATE_DO_NOT_INITIATE_FOO("ParticipateDoNotInitiate"),
+  PARTICIPATE_DO_NOT_INITIATE("ParticipateDoNotInitiate"),
   ALL_OR_NONE("AllOrNone"),
   MARK_PRICE("MarkPrice"),
   INDEX_PRICE("IndexPrice"),
@@ -25,16 +25,84 @@ public enum BitmexExecutionInstruction {
       return apiParameter;
   }
 
-  // TODO Builder
-  public static List<BitmexExecutionInstruction> fromParameter(
-      final boolean postOnly, final boolean reduceOnly) {
-    final ArrayList<BitmexExecutionInstruction> result = new ArrayList<>();
-    if (postOnly) {
-      result.add(PARTICIPATE_DO_NOT_INITIATE_FOO);
+  public static class Builder {
+
+    private boolean postOnly;
+    private boolean allOrNone;
+    private boolean markPrice;
+    private boolean indexPrice;
+    private boolean lastPrice;
+    private boolean close;
+    private boolean reduceOnly;
+    private boolean fixed;
+
+    public List<BitmexExecutionInstruction> build() {
+      final ArrayList<BitmexExecutionInstruction> instructions = new ArrayList<>();
+      if (postOnly) {
+        instructions.add(PARTICIPATE_DO_NOT_INITIATE);
+      }
+      if (allOrNone) {
+        instructions.add(ALL_OR_NONE);
+      }
+      if (markPrice) {
+        instructions.add(MARK_PRICE);
+      }
+      if (indexPrice) {
+        instructions.add(INDEX_PRICE);
+      }
+      if (lastPrice) {
+        instructions.add(LAST_PRICE);
+      }
+      if (close) {
+        instructions.add(CLOSE);
+      }
+      if (reduceOnly) {
+        instructions.add(REDUCE_ONLY);
+      }
+      if (fixed) {
+        instructions.add(FIXED);
+      }
+      return instructions;
     }
-    if (reduceOnly) {
-      result.add(REDUCE_ONLY);
+
+    public Builder setPostOnly(boolean postOnly) {
+      this.postOnly = postOnly;
+      return this;
     }
-    return result;
+
+    public Builder setAllOrNone(boolean allOrNone) {
+      this.allOrNone = allOrNone;
+      return this;
+    }
+
+    public Builder setMarkPrice(boolean markPrice) {
+      this.markPrice = markPrice;
+      return this;
+    }
+
+    public Builder setIndexPrice(boolean indexPrice) {
+      this.indexPrice = indexPrice;
+      return this;
+    }
+
+    public Builder setLastPrice(boolean lastPrice) {
+      this.lastPrice = lastPrice;
+      return this;
+    }
+
+    public Builder setClose(boolean close) {
+      this.close = close;
+      return this;
+    }
+
+    public Builder setReduceOnly(boolean reduceOnly) {
+      this.reduceOnly = reduceOnly;
+      return this;
+    }
+
+    public Builder setFixed(boolean fixed) {
+      this.fixed = fixed;
+      return this;
+    }
   }
 }
