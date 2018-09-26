8d54f354a92b504dbe50deacebea4c69fef2b054
Kevin Gates <kevinobamatheus@gmail.com>
2018-09-26 10:46:22 +0800

    update

--- xchange-getbtc/src/test/java/org/knowm/xchange/test/getbtc/GetbtcConfig.java
+++ xchange-getbtc/src/test/java/org/knowm/xchange/test/getbtc/GetbtcConfig.java
@@ -7,22 +7,22 @@ import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.getbtc.GetbtcExchange;
 
 public class GetbtcConfig {
-  public static Exchange getExchange() throws IOException {      
-	    Exchange getbtc = ExchangeFactory.INSTANCE.createExchange(GetbtcExchange.class.getName());
-	    
-	    ExchangeSpecification exchangeSpecification = getbtc.getExchangeSpecification();
-	    exchangeSpecification.setApiKey(apiKey());
-	    exchangeSpecification.setSecretKey(secretKey());
-	    getbtc.applySpecification(exchangeSpecification);
+  public static Exchange getExchange() throws IOException {
+    Exchange getbtc = ExchangeFactory.INSTANCE.createExchange(GetbtcExchange.class.getName());
+
+    ExchangeSpecification exchangeSpecification = getbtc.getExchangeSpecification();
+    exchangeSpecification.setApiKey(apiKey());
+    exchangeSpecification.setSecretKey(secretKey());
+    getbtc.applySpecification(exchangeSpecification);
 
     return getbtc;
   }
-    
+
   private static String apiKey() {
-	return "4LFNrkVQW7iPgp0x";
+    return "4LFNrkVQW7iPgp0x";
   }
-  
+
   private static String secretKey() {
-	return "WKsXO98bICDxwYpHjiN3T7tedZ14gAuM";
+    return "WKsXO98bICDxwYpHjiN3T7tedZ14gAuM";
   }
 }
