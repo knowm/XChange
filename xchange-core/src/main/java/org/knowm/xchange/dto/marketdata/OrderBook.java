0f70be562be7afd02f0f50d8bd283e86e5cae54c
EzBar <ezerba@gmail.com>
2018-09-30 01:19:42 +0300

    [Core] Remove OrderBook hashCode and equals
    LimitOrder is immutable, however OrderBook is mutable (it has Update method etc.) so we can't use by-equality.
    For example, after adding an orderbook to a set we may change it so it will be similar to an already existing orderbook, breaking the HashSet...

--- xchange-core/src/main/java/org/knowm/xchange/dto/marketdata/OrderBook.java
+++ xchange-core/src/main/java/org/knowm/xchange/dto/marketdata/OrderBook.java
@@ -195,54 +195,6 @@ public class OrderBook implements Serializable {
     }
   }
 
-  @Override
-  public int hashCode() {
-
-    int hash = 17;
-    hash = 31 * hash + (this.timeStamp != null ? this.timeStamp.hashCode() : 0);
-    for (LimitOrder order : this.bids) {
-      hash = 31 * hash + order.hashCode();
-    }
-    for (LimitOrder order : this.asks) {
-      hash = 31 * hash + order.hashCode();
-    }
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    final OrderBook other = (OrderBook) obj;
-    if (this.timeStamp == null
-        ? other.timeStamp != null
-        : !this.timeStamp.equals(other.timeStamp)) {
-      return false;
-    }
-    if (this.bids.size() != other.bids.size()) {
-      return false;
-    }
-    for (int index = 0; index < this.bids.size(); index++) {
-      if (!this.bids.get(index).equals(other.bids.get(index))) {
-        return false;
-      }
-    }
-    if (this.asks.size() != other.asks.size()) {
-      return false;
-    }
-    for (int index = 0; index < this.asks.size(); index++) {
-      if (!this.asks.get(index).equals(other.asks.get(index))) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   /**
    * Identical to {@link #equals(Object) equals} method except that this ignores different
    * timestamps. In other words, this version of equals returns true if the order internal to the
