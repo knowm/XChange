f529c403533e8ac4bdbe4a2c6a829ed9e9184f59
Cyril <nguyen.cyril@gmail.com>
2018-10-09 11:23:49 +0200

    Throwing exceptions on too long urls while trying to fetch order books and tickers

--- xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
+++ xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
@@ -1,8 +1,10 @@
 package org.knowm.xchange.yobit.service;
 
 import java.io.IOException;
+
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.exceptions.ExchangeException;
 import org.knowm.xchange.yobit.YoBit;
 import org.knowm.xchange.yobit.YoBitAdapters;
 import org.knowm.xchange.yobit.dto.marketdata.YoBitInfo;
@@ -12,6 +14,9 @@ import org.knowm.xchange.yobit.dto.marketdata.YoBitTrades;
 
 public class YoBitMarketDataServiceRaw extends YoBitBaseService<YoBit> {
 
+  // Not specified in the API doc but real
+  private static final int MAX_PAIR_LIST_SIZE = 512;
+
   protected YoBitMarketDataServiceRaw(Exchange exchange) {
     super(YoBit.class, exchange);
   }
@@ -20,15 +25,22 @@ public class YoBitMarketDataServiceRaw extends YoBitBaseService<YoBit> {
     return service.getProducts();
   }
 
-  // TODO error message handling http 414 (url too long)
   public YoBitTickersReturn getYoBitTickers(Iterable<CurrencyPair> currencyPairs)
       throws IOException {
-    return service.getTickers(YoBitAdapters.adaptCcyPairsToUrlFormat(currencyPairs));
+    return service.getTickers(getPairListAsString(currencyPairs));
   }
 
   public YoBitOrderBooksReturn getOrderBooks(Iterable<CurrencyPair> currencyPairs, Integer limit)
       throws IOException {
-    return service.getOrderBooks(YoBitAdapters.adaptCcyPairsToUrlFormat(currencyPairs), limit);
+    return service.getOrderBooks(getPairListAsString(currencyPairs), limit);
+  }
+
+  private String getPairListAsString(Iterable<CurrencyPair> currencyPairs) {
+    String markets = YoBitAdapters.adaptCcyPairsToUrlFormat(currencyPairs);
+    if (markets.length() > MAX_PAIR_LIST_SIZE) {
+      throw new ExchangeException("URL too long: YoBit allows a maximum of" + MAX_PAIR_LIST_SIZE + " characters for total pair lists size. Provided string is " + markets.length() + " characters long.");
+    }
+    return markets;
   }
 
   public YoBitTrades getPublicTrades(Iterable<CurrencyPair> currencyPairs) throws IOException {
