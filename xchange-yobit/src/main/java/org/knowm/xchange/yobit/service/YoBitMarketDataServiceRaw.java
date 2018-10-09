1409ceaac200b92ae19cda108d3538f406b2bec6
Cyril <nguyen.cyril@gmail.com>
2018-10-09 11:26:11 +0200

    message fix

--- xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
+++ xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
@@ -38,7 +38,7 @@ public class YoBitMarketDataServiceRaw extends YoBitBaseService<YoBit> {
   private String getPairListAsString(Iterable<CurrencyPair> currencyPairs) {
     String markets = YoBitAdapters.adaptCcyPairsToUrlFormat(currencyPairs);
     if (markets.length() > MAX_PAIR_LIST_SIZE) {
-      throw new ExchangeException("URL too long: YoBit allows a maximum of" + MAX_PAIR_LIST_SIZE + " characters for total pair lists size. Provided string is " + markets.length() + " characters long.");
+      throw new ExchangeException("URL too long: YoBit allows a maximum of " + MAX_PAIR_LIST_SIZE + " characters for total pair lists size. Provided string is " + markets.length() + " characters long.");
     }
     return markets;
   }
