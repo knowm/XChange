ba35ffe8cf749654fe5c3a5b5ab42758aa024682
Cyril <nguyen.cyril@gmail.com>
2018-10-09 11:28:45 +0200

    cleaning

--- xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
+++ xchange-yobit/src/main/java/org/knowm/xchange/yobit/service/YoBitMarketDataServiceRaw.java
@@ -35,15 +35,15 @@ public class YoBitMarketDataServiceRaw extends YoBitBaseService<YoBit> {
     return service.getOrderBooks(getPairListAsString(currencyPairs), limit);
   }
 
+  public YoBitTrades getPublicTrades(Iterable<CurrencyPair> currencyPairs) throws IOException {
+    return service.getTrades(getPairListAsString(currencyPairs));
+  }
+
   private String getPairListAsString(Iterable<CurrencyPair> currencyPairs) {
     String markets = YoBitAdapters.adaptCcyPairsToUrlFormat(currencyPairs);
     if (markets.length() > MAX_PAIR_LIST_SIZE) {
       throw new ExchangeException("URL too long: YoBit allows a maximum of " + MAX_PAIR_LIST_SIZE + " characters for total pair lists size. Provided string is " + markets.length() + " characters long.");
     }
     return markets;
-  }
-
-  public YoBitTrades getPublicTrades(Iterable<CurrencyPair> currencyPairs) throws IOException {
-    return service.getTrades(getPairListAsString(currencyPairs));
   }
 }
