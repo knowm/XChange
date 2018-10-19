aa5100288fe41cf1eb8dbe27716d6b5b9bf6203a
Cyril <nguyen.cyril@gmail.com>
2018-10-19 19:13:13 +0200

    [core] Improved the trade ID comparator

--- xchange-core/src/main/java/org/knowm/xchange/dto/marketdata/Trades.java
+++ xchange-core/src/main/java/org/knowm/xchange/dto/marketdata/Trades.java
@@ -3,12 +3,15 @@ package org.knowm.xchange.dto.marketdata;
 import static org.knowm.xchange.dto.marketdata.Trades.TradeSortType.SortByID;
 
 import java.io.Serializable;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-/** DTO representing a collection of trades */
+/**
+ * DTO representing a collection of trades
+ */
 public class Trades implements Serializable {
 
   private static final TradeIDComparator TRADE_ID_COMPARATOR = new TradeIDComparator();
@@ -59,7 +62,7 @@ public class Trades implements Serializable {
    * @param lastID Last Unique ID
    * @param tradeSortType Trade sort type
    * @param nextPageCursor a marker that lets you receive the next page of trades using
-   *     TradeHistoryParamNextPageCursor
+   * TradeHistoryParamNextPageCursor
    */
   public Trades(
       List<Trade> trades, long lastID, TradeSortType tradeSortType, String nextPageCursor) {
@@ -82,13 +85,17 @@ public class Trades implements Serializable {
     }
   }
 
-  /** @return A list of trades ordered by id */
+  /**
+   * @return A list of trades ordered by id
+   */
   public List<Trade> getTrades() {
 
     return trades;
   }
 
-  /** @return a Unique ID for the fetched trades */
+  /**
+   * @return a Unique ID for the fetched trades
+   */
   public long getlastID() {
 
     return lastID;
@@ -134,8 +141,21 @@ public class Trades implements Serializable {
 
     @Override
     public int compare(Trade trade1, Trade trade2) {
-
-      return trade1.getId().compareTo(trade2.getId());
+      BigInteger id1;
+      BigInteger id2;
+      try {
+        id1 = new BigInteger(trade1.getId());
+        id2 = new BigInteger(trade2.getId());
+        return id1.compareTo(id2);
+      } catch (NumberFormatException e1) {
+        try {
+          id1 = new BigInteger(trade1.getId(), 16);
+          id2 = new BigInteger(trade2.getId(), 16);
+          return id1.compareTo(id2);
+        } catch (NumberFormatException e2) {
+          return trade1.getId().compareTo(trade2.getId());
+        }
+      }
     }
   }
 }
