352548f30c82a2ce5ce42d7eba6da7303537a95b
water0126 <water820126@gmail.com>
2018-10-25 11:36:26 +0900

    [Upbit] Added limit order request, cancel order, order info

--- xchange-upbit/src/main/java/org/knowm/xchange/upbit/dto/UpbitException.java
+++ xchange-upbit/src/main/java/org/knowm/xchange/upbit/dto/UpbitException.java
@@ -2,15 +2,16 @@ package org.knowm.xchange.upbit.dto;
 
 @SuppressWarnings("serial")
 public class UpbitException extends RuntimeException {
-  private String message;
 
-  public UpbitException(String message) {
-    super();
-    this.message = message;
-  }
+    private String message;
 
-  @Override
-  public String toString() {
-    return this.message;
-  }
+    public UpbitException(String message) {
+        super();
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
 }
---
+++ xchange-upbit/src/main/java/org/knowm/xchange/upbit/service/UpbitTradeService.java
@@ -0,0 +1,92 @@
+package org.knowm.xchange.upbit.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
+import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.dto.trade.StopOrder;
+import org.knowm.xchange.dto.trade.UserTrades;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.CancelOrderParams;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
+import org.knowm.xchange.upbit.UpbitAdapters;
+import org.knowm.xchange.upbit.dto.trade.UpbitOrderResponse;
+
+public class UpbitTradeService extends UpbitTradeServiceRaw implements TradeService {
+
+    /**
+     *
+     * @param exchange
+     */
+    public UpbitTradeService(Exchange exchange) {
+        super(exchange);
+    }
+
+    @Override
+    public OpenOrders getOpenOrders() throws IOException {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public OpenOrders getOpenOrders(OpenOrdersParams params) throws IOException {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
+        throw new NotAvailableFromExchangeException();
+    }
+
+    @Override
+    public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
+        return super.limitOrder(limitOrder).getUuid();
+    }
+
+    @Override
+    public String placeStopOrder(StopOrder stopOrder) throws IOException {
+        throw new NotAvailableFromExchangeException();
+    }
+
+    @Override
+    public boolean cancelOrder(String orderId) throws IOException {
+        return super.cancelOrderRaw(orderId) == null ? false : true;
+
+    }
+
+    @Override
+    public boolean cancelOrder(CancelOrderParams orderParams) throws IOException {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public TradeHistoryParams createTradeHistoryParams() {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public OpenOrdersParams createOpenOrdersParams() {
+        throw new NotYetImplementedForExchangeException();
+    }
+
+    @Override
+    public Collection<Order> getOrder(String... orderIds) throws IOException {
+        ArrayList<Order> rtn = new ArrayList<>();
+        for (String orderId : orderIds) {
+            UpbitOrderResponse res = super.getOrderRaw(orderId);
+            rtn.add(UpbitAdapters.adaptOrderInfo(res));
+        }
+        return rtn;
+    }
+}
