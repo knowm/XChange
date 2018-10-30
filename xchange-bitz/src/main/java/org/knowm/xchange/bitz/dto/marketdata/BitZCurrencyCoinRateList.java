7d53d3274eb50f234dafae1238dfb71fd042cca5
Tangjun He <belerweb@gmail.com>
2018-10-30 16:13:05 +0800

    add more bit-z support

--- xchange-bitz/src/main/java/org/knowm/xchange/bitz/dto/marketdata/BitZCurrencyCoinRateList.java
+++ xchange-bitz/src/main/java/org/knowm/xchange/bitz/dto/marketdata/BitZCurrencyCoinRateList.java
@@ -1,18 +1,20 @@
 package org.knowm.xchange.bitz.dto.marketdata;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import org.knowm.xchange.currency.Currency;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class BitZCurrencyCoinRateList {
-  private final Map<String, BitZCurrencyCoinRate> currencyCoinRateMap;
+  private final Map<Currency, Map<Currency, BigDecimal>> currencyCoinRateMap;
 
   @JsonCreator
-  public BitZCurrencyCoinRateList(Map<String, BitZCurrencyCoinRate> currencyCoinRateMap) {
+  public BitZCurrencyCoinRateList(Map<Currency, Map<Currency, BigDecimal>> currencyCoinRateMap) {
     this.currencyCoinRateMap = currencyCoinRateMap;
   }
 
-  public Map<String, BitZCurrencyCoinRate> getCurrencyCoinRateMap() {
+  public Map<Currency, Map<Currency, BigDecimal>> getCurrencyCoinRateMap() {
     return currencyCoinRateMap;
   }
 }
--- xchange-bitz/src/main/java/org/knowm/xchange/bitz/dto/marketdata/result/BitZCurrencyCoinRateResult.java
+++ xchange-bitz/src/main/java/org/knowm/xchange/bitz/dto/marketdata/result/BitZCurrencyCoinRateResult.java
@@ -2,13 +2,16 @@ package org.knowm.xchange.bitz.dto.marketdata.result;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.knowm.xchange.bitz.dto.BitZResult;
-import org.knowm.xchange.bitz.dto.marketdata.BitZCurrencyCoinRateList;
+import org.knowm.xchange.currency.Currency;
 
-public class BitZCurrencyCoinRateResult extends BitZResult<BitZCurrencyCoinRateList> {
+import java.math.BigDecimal;
+import java.util.Map;
+
+public class BitZCurrencyCoinRateResult extends BitZResult<Map<Currency, Map<Currency, BigDecimal>>> {
   public BitZCurrencyCoinRateResult(
       @JsonProperty("status") int status,
       @JsonProperty("msg") String message,
-      @JsonProperty("data") BitZCurrencyCoinRateList data,
+      @JsonProperty("data") Map<Currency, Map<Currency, BigDecimal>> data,
       @JsonProperty("time") String time,
       @JsonProperty("microtime") String microtime,
       @JsonProperty("source") String source) {
