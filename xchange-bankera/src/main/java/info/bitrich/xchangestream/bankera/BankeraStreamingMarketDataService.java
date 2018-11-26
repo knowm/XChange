f85e0f4ff3a27d4dacfc0ef92832cbb93eb047b7
Mindaugas Rolius <m.rolius@spectrofinance.lt>
2018-11-26 10:04:40 +0200

    Initial commit

---
+++ xchange-bankera/src/main/java/info/bitrich/xchangestream/bankera/BankeraStreamingMarketDataService.java
@@ -0,0 +1,6 @@
+package info.bitrich.xchangestream.bankera;
+
+import info.bitrich.xchangestream.core.StreamingMarketDataService;
+
+public class BankeraStreamingMarketDataService implements StreamingMarketDataService {
+}
