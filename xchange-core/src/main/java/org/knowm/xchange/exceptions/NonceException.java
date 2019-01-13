f7be47d9cea80033c8e7af813aea4325833a7886
Adam Walczak <adam@walczak.it>
2019-01-14 00:14:30 +0100

    [bitfinex] better error handling

--- xchange-core/src/main/java/org/knowm/xchange/exceptions/NonceException.java
+++ xchange-core/src/main/java/org/knowm/xchange/exceptions/NonceException.java
@@ -2,6 +2,14 @@ package org.knowm.xchange.exceptions;
 
 public class NonceException extends ExchangeException {
 
+  public NonceException(Throwable cause) {
+    super(cause);
+  }
+
+  public NonceException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
   public NonceException(String message) {
     super(message);
   }
