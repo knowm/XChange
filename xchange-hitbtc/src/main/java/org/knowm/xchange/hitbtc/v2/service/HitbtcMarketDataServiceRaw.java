cc1c4c4d15498e2eb21980ea15f11cd859445206
Gilbert Kimutai <gilbertkimutai@gmail.com>
2019-01-26 07:53:16 +0300

    [hitbtc] Added till and from date filter, sort and offset filters on ohlc candles

--- xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcMarketDataServiceRaw.java
+++ xchange-hitbtc/src/main/java/org/knowm/xchange/hitbtc/v2/service/HitbtcMarketDataServiceRaw.java
@@ -1,6 +1,7 @@
 package org.knowm.xchange.hitbtc.v2.service;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -39,11 +40,11 @@ public class HitbtcMarketDataServiceRaw extends HitbtcBaseService {
   public Map<String, HitbtcTicker> getHitbtcTickers() throws IOException {
 
     return hitbtc
-        .getHitbtcTickers()
-        .stream()
-        .collect(
-            Collectors.toMap(
-                hitbtcTicker -> hitbtcTicker.getSymbol(), hitbtcTicker -> hitbtcTicker));
+            .getHitbtcTickers()
+            .stream()
+            .collect(
+                    Collectors.toMap(
+                            hitbtcTicker -> hitbtcTicker.getSymbol(), hitbtcTicker -> hitbtcTicker));
   }
 
   public HitbtcTicker getHitbtcTicker(CurrencyPair currencyPair) throws IOException {
@@ -57,7 +58,7 @@ public class HitbtcMarketDataServiceRaw extends HitbtcBaseService {
   }
 
   public HitbtcOrderBook getHitbtcOrderBook(CurrencyPair currencyPair, Integer limit)
-      throws IOException {
+          throws IOException {
 
     return hitbtc.getOrderBook(HitbtcAdapters.adaptCurrencyPair(currencyPair), limit);
   }
@@ -65,48 +66,66 @@ public class HitbtcMarketDataServiceRaw extends HitbtcBaseService {
   public List<HitbtcTrade> getHitbtcTrades(CurrencyPair currencyPair) throws IOException {
 
     return getHitbtcTrades(
-        currencyPair,
-        100,
-        HitbtcTrade.HitbtcTradesSortField.SORT_BY_TRADE_ID,
-        HitbtcSort.SORT_ASCENDING,
-        0,
-        100,
-        0);
+            currencyPair,
+            100,
+            HitbtcTrade.HitbtcTradesSortField.SORT_BY_TRADE_ID,
+            HitbtcSort.SORT_ASCENDING,
+            0,
+            100,
+            0);
   }
 
   // TODO add extra params in API
   public List<HitbtcTrade> getHitbtcTrades(
-      CurrencyPair currencyPair,
-      long from,
-      HitbtcTrade.HitbtcTradesSortField sortBy,
-      HitbtcSort sortDirection,
-      long startIndex,
-      long maxResults,
-      long offset)
-      throws IOException {
+          CurrencyPair currencyPair,
+          long from,
+          HitbtcTrade.HitbtcTradesSortField sortBy,
+          HitbtcSort sortDirection,
+          long startIndex,
+          long maxResults,
+          long offset)
+          throws IOException {
 
     return hitbtc.getTrades(HitbtcAdapters.adaptCurrencyPair(currencyPair), maxResults, offset);
   }
 
   public List<HitbtcTrade> getHitbtcTrades(
-      CurrencyPair currencyPair,
-      long from,
-      HitbtcTrade.HitbtcTradesSortField sortBy,
-      HitbtcSort sortDirection,
-      long maxResults)
-      throws IOException {
+          CurrencyPair currencyPair,
+          long from,
+          HitbtcTrade.HitbtcTradesSortField sortBy,
+          HitbtcSort sortDirection,
+          long maxResults)
+          throws IOException {
 
     return hitbtc.getTrades(
-        HitbtcAdapters.adaptCurrencyPair(currencyPair),
-        sortDirection.toString(),
-        sortBy.toString(),
-        String.valueOf(from),
-        maxResults);
+            HitbtcAdapters.adaptCurrencyPair(currencyPair),
+            sortDirection.toString(),
+            sortBy.toString(),
+            String.valueOf(from),
+            maxResults);
   }
 
   public List<HitbtcCandle> getHitbtcCandles(CurrencyPair currencyPair, int limit, String period)
-      throws IOException {
+          throws IOException {
 
     return hitbtc.getHitbtcOHLC(HitbtcAdapters.adaptCurrencyPair(currencyPair), limit, period);
+  }
+
+  public List<HitbtcCandle> getHitbtcCandles(CurrencyPair currencyPair, int limit, String period, String sort)
+          throws IOException {
+
+    return hitbtc.getHitbtcOHLC(HitbtcAdapters.adaptCurrencyPair(currencyPair), limit, period, sort);
+  }
+
+  public List<HitbtcCandle> getHitbtcCandles(CurrencyPair currencyPair, int limit, String period, Date from, Date till, String sort)
+          throws IOException {
+
+    return hitbtc.getHitbtcOHLC(HitbtcAdapters.adaptCurrencyPair(currencyPair), limit, period, from, till, sort);
+  }
+
+  public List<HitbtcCandle> getHitbtcCandles(CurrencyPair currencyPair, int limit, String period, int offset, String sort)
+          throws IOException {
+
+    return hitbtc.getHitbtcOHLC(HitbtcAdapters.adaptCurrencyPair(currencyPair), limit, period, offset, sort);
   }
 }
