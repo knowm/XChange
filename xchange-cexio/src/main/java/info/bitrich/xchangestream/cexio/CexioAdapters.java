51ba370f071d7615ba31ee695c58ee08e640a793
BenTrapani <ben.trapani1995@gmail.com>
2019-01-27 18:14:25 -0800

    Revert "Reformat using mvn com.coveo:fmt-maven-plugin:format"
    This reverts commit 62fbab212e11e324d095536e19140b2494db11db.

--- xchange-cexio/src/main/java/info/bitrich/xchangestream/cexio/CexioAdapters.java
+++ xchange-cexio/src/main/java/info/bitrich/xchangestream/cexio/CexioAdapters.java
@@ -3,131 +3,128 @@ package info.bitrich.xchangestream.cexio;
 import info.bitrich.xchangestream.cexio.dto.CexioWebSocketOrder;
 import info.bitrich.xchangestream.cexio.dto.CexioWebSocketOrderBookSubscribeResponse;
 import info.bitrich.xchangestream.cexio.dto.CexioWebSocketPair;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.Order.OrderType;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.trade.LimitOrder;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 public class CexioAdapters {
 
-  private static final int PRECISION = 3;
-  private static final BigDecimal SATOSHI_SCALE = new BigDecimal("100000000");
+    private static final int PRECISION = 3;
+    private static final BigDecimal SATOSHI_SCALE = new BigDecimal("100000000");
 
-  static Order adaptOrder(CexioWebSocketOrder order) {
-    if (order.getType() != null) {
-      return new CexioOrder(
-          adaptOrderType(order.getType()),
-          adaptCurrencyPair(order.getPair()),
-          adaptAmount(order.getRemains()),
-          order.getId(),
-          order.getTime(),
-          order.getPrice(),
-          order.getFee(),
-          getOrderStatus(order));
-    } else {
-      return new CexioOrder(
-          adaptCurrencyPair(order.getPair()),
-          order.getId(),
-          getOrderStatus(order),
-          adaptAmount(order.getRemains()));
-    }
-  }
-
-  private static CurrencyPair adaptCurrencyPair(CexioWebSocketPair pair) {
-    return new CurrencyPair(pair.getSymbol1(), pair.getSymbol2());
-  }
-
-  private static Order.OrderType adaptOrderType(String type) {
-    if (type == null) {
-      return null;
+    static Order adaptOrder(CexioWebSocketOrder order) {
+        if (order.getType() != null) {
+            return new CexioOrder(adaptOrderType(order.getType()),
+                              adaptCurrencyPair(order.getPair()),
+                              adaptAmount(order.getRemains()),
+                              order.getId(),
+                              order.getTime(),
+                              order.getPrice(),
+                              order.getFee(),
+                              getOrderStatus(order));
+        } else {
+            return new CexioOrder(adaptCurrencyPair(order.getPair()),
+                                  order.getId(),
+                                  getOrderStatus(order),
+                                  adaptAmount(order.getRemains()));
+        }
     }
 
-    switch (type) {
-      case "buy":
-        return Order.OrderType.BID;
-      case "sell":
-        return Order.OrderType.ASK;
-      default:
-        return null;
-    }
-  }
-
-  private static BigDecimal adaptAmount(BigDecimal amount) {
-    if (amount == null) {
-      return null;
+    private static CurrencyPair adaptCurrencyPair(CexioWebSocketPair pair) {
+        return new CurrencyPair(pair.getSymbol1(), pair.getSymbol2());
     }
 
-    return amount.divide(SATOSHI_SCALE, PRECISION, RoundingMode.DOWN);
-  }
+    private static Order.OrderType adaptOrderType(String type) {
+        if (type == null) {
+            return null;
+        }
 
-  private static Order.OrderStatus getOrderStatus(CexioWebSocketOrder order) {
-    Order.OrderStatus status;
-    if (order.isCancel()) {
-      status = Order.OrderStatus.CANCELED;
-    } else if (order.getRemains().compareTo(BigDecimal.ZERO) == 0) {
-      status = Order.OrderStatus.FILLED;
-    } else if (order.getType() != null) {
-      status = Order.OrderStatus.NEW;
-    } else {
-      status = Order.OrderStatus.PARTIALLY_FILLED;
+        switch (type) {
+            case "buy":
+                return Order.OrderType.BID;
+            case "sell":
+                return Order.OrderType.ASK;
+            default:
+                return null;
+        }
     }
-    return status;
-  }
 
-  private static List<LimitOrder> updateOrInsertQuantityForPrice(
-      List<LimitOrder> existingOrders,
-      List<List<BigDecimal>> updatedQuantitiesPerPrice,
-      OrderType orderType,
-      Date timestamp,
-      CurrencyPair currencyPair,
-      String id) {
-    Collections.sort(existingOrders);
-    for (List<BigDecimal> updatedQuantityPrice : updatedQuantitiesPerPrice) {
-      if (updatedQuantityPrice.size() != 2) {
-        throw new IllegalArgumentException(
-            "Expected price quantity list to contain exactly two big decimals");
-      }
-      BigDecimal price = updatedQuantityPrice.get(0);
-      BigDecimal quantity = updatedQuantityPrice.get(1);
-      LimitOrder orderForUpdatedQuantityPrice =
-          new LimitOrder(orderType, quantity, currencyPair, id, timestamp, price);
-      int insertIndex = Collections.binarySearch(existingOrders, orderForUpdatedQuantityPrice);
-      if (insertIndex == existingOrders.size()) {
-        existingOrders.add(orderForUpdatedQuantityPrice);
-      } else if (insertIndex < 0) {
-        existingOrders.add(0, orderForUpdatedQuantityPrice);
-      } else if (existingOrders.get(insertIndex).compareTo(orderForUpdatedQuantityPrice) == 0) {
-        existingOrders.set(insertIndex, orderForUpdatedQuantityPrice);
-      } else {
-        existingOrders.add(insertIndex, orderForUpdatedQuantityPrice);
-      }
+    private static BigDecimal adaptAmount(BigDecimal amount) {
+        if (amount == null) {
+            return null;
+        }
+
+        return amount.divide(SATOSHI_SCALE, PRECISION, RoundingMode.DOWN);
     }
-    return existingOrders;
-  }
 
-  protected static CurrencyPair adaptCurrencyPairString(String currencyPairString) {
-    String[] splitOnColon = currencyPairString.split(":");
-    return new CurrencyPair(splitOnColon[0], splitOnColon[1]);
-  }
-
-  protected static OrderBook adaptOrderBookIncremental(
-      OrderBook prevOrderBook, CexioWebSocketOrderBookSubscribeResponse update) {
-    CurrencyPair currencyPair = adaptCurrencyPairString(update.pair);
-    List<LimitOrder> asks = prevOrderBook.getAsks();
-    List<LimitOrder> bids = prevOrderBook.getBids();
-    asks =
-        updateOrInsertQuantityForPrice(
-            asks, update.asks, OrderType.ASK, update.timestamp, currencyPair, update.id.toString());
-    bids =
-        updateOrInsertQuantityForPrice(
-            bids, update.bids, OrderType.BID, update.timestamp, currencyPair, update.id.toString());
-
-    return new OrderBook(update.timestamp, asks, bids, true);
-  }
+    private static Order.OrderStatus getOrderStatus(CexioWebSocketOrder order) {
+        Order.OrderStatus status;
+        if (order.isCancel()) {
+            status = Order.OrderStatus.CANCELED;
+        } else if (order.getRemains().compareTo(BigDecimal.ZERO) == 0) {
+            status = Order.OrderStatus.FILLED;
+        } else if (order.getType() != null) {
+            status = Order.OrderStatus.NEW;
+        } else {
+            status = Order.OrderStatus.PARTIALLY_FILLED;
+        }
+        return status;
+    }
+    
+    private static List<LimitOrder> updateOrInsertQuantityForPrice(List<LimitOrder> existingOrders, 
+    		List<List<BigDecimal>> updatedQuantitiesPerPrice,
+    		OrderType orderType,
+    		Date timestamp,
+    		CurrencyPair currencyPair,
+    		String id) {
+    	Collections.sort(existingOrders);
+    	for (List<BigDecimal> updatedQuantityPrice : updatedQuantitiesPerPrice) {
+    		if (updatedQuantityPrice.size() != 2) {
+    			throw new IllegalArgumentException("Expected price quantity list to contain exactly two big decimals");
+    		}
+    		BigDecimal price = updatedQuantityPrice.get(0);
+    		BigDecimal quantity = updatedQuantityPrice.get(1);
+    		LimitOrder orderForUpdatedQuantityPrice = new LimitOrder(orderType, quantity, currencyPair, id, timestamp, price);
+    		int insertIndex = Collections.binarySearch(existingOrders, orderForUpdatedQuantityPrice);
+    		if (insertIndex == existingOrders.size()) {
+    			existingOrders.add(orderForUpdatedQuantityPrice);
+    		}
+    		else if (insertIndex < 0)
+    		{
+    			existingOrders.add(0, orderForUpdatedQuantityPrice);
+    		}
+    		else if (existingOrders.get(insertIndex).compareTo(orderForUpdatedQuantityPrice) == 0) 
+    		{
+    			existingOrders.set(insertIndex, orderForUpdatedQuantityPrice);
+    		} 
+    		else 
+    		{
+    			existingOrders.add(insertIndex, orderForUpdatedQuantityPrice);
+    		}
+    	}
+    	return existingOrders;
+    }
+    
+    protected static CurrencyPair adaptCurrencyPairString(String currencyPairString) {
+    	String[] splitOnColon = currencyPairString.split(":");
+    	return new CurrencyPair(splitOnColon[0], splitOnColon[1]);
+    }
+    
+    protected static OrderBook adaptOrderBookIncremental(OrderBook prevOrderBook, CexioWebSocketOrderBookSubscribeResponse update) {
+    	CurrencyPair currencyPair = adaptCurrencyPairString(update.pair);
+    	List<LimitOrder> asks = prevOrderBook.getAsks();
+    	List<LimitOrder> bids = prevOrderBook.getBids();
+    	asks = updateOrInsertQuantityForPrice(asks, update.asks, OrderType.ASK, update.timestamp, currencyPair, update.id.toString());
+    	bids = updateOrInsertQuantityForPrice(bids, update.bids, OrderType.BID, update.timestamp, currencyPair, update.id.toString());
+    	
+    	return new OrderBook(update.timestamp, asks, bids, true);
+    }
 }
