4082d30b3bcccb499c6c590d982bc5963cc2d9ff
Jakub Kalicki <jakub.kalicki@tacton.com>
2019-01-29 17:43:02 +0100

    [Abucoins] Remove whole module because exchange is no longer operating

--- xchange-abucoins/src/main/java/org/knowm/xchange/abucoins/dto/AbucoinsSingleIdRequest.java
+++
@@ -1,9 +0,0 @@
-package org.knowm.xchange.abucoins.dto;
-
-public class AbucoinsSingleIdRequest extends AbucoinsRequest {
-  public final String id;
-
-  public AbucoinsSingleIdRequest(String id) {
-    this.id = id;
-  }
-}
