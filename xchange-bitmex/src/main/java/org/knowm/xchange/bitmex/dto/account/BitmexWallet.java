ce3c7dfc357533deb344bb92a76c64a99ec6e6d7
Jan-Terje Sørensen <jtbsorensen@gmail.com>
2019-01-30 13:27:51 +0100

    Added some toString methods

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/dto/account/BitmexWallet.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/dto/account/BitmexWallet.java
@@ -220,4 +220,67 @@ public final class BitmexWallet extends AbstractHttpResponseAware {
   public Map<String, Object> getAdditionalProperties() {
     return additionalProperties;
   }
+
+  @Override
+  public String toString() {
+    return "BitmexWallet{"
+        + "account="
+        + account
+        + ", currency='"
+        + currency
+        + '\''
+        + ", prevDeposited="
+        + prevDeposited
+        + ", prevWithdrawn="
+        + prevWithdrawn
+        + ", prevTransferIn="
+        + prevTransferIn
+        + ", prevTransferOut="
+        + prevTransferOut
+        + ", prevAmount="
+        + prevAmount
+        + ", prevTimestamp='"
+        + prevTimestamp
+        + '\''
+        + ", deltaDeposited="
+        + deltaDeposited
+        + ", deltaWithdrawn="
+        + deltaWithdrawn
+        + ", deltaTransferIn="
+        + deltaTransferIn
+        + ", deltaTransferOut="
+        + deltaTransferOut
+        + ", deltaAmount="
+        + deltaAmount
+        + ", deposited="
+        + deposited
+        + ", withdrawn="
+        + withdrawn
+        + ", transferIn="
+        + transferIn
+        + ", transferOut="
+        + transferOut
+        + ", amount="
+        + amount
+        + ", pendingCredit="
+        + pendingCredit
+        + ", pendingDebit="
+        + pendingDebit
+        + ", confirmedDebit="
+        + confirmedDebit
+        + ", timestamp='"
+        + timestamp
+        + '\''
+        + ", addr='"
+        + addr
+        + '\''
+        + ", script='"
+        + script
+        + '\''
+        + ", withdrawalLock="
+        + withdrawalLock
+        + ", additionalProperties="
+        + additionalProperties
+        + '}';
+  }
 }
