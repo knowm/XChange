4b03c189f7368a35b12fee5bacc1450194c1211b
VKozlov <voloda1992@gmail.com>
2019-02-07 10:58:58 +0300

    INV-1626

--- xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAdapters.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/BinanceAdapters.java
@@ -94,6 +94,14 @@ public class BinanceAdapters {
     int pairLength = symbol.length();
     if (symbol.endsWith("USDT")) {
       return new CurrencyPair(symbol.substring(0, pairLength - 4), "USDT");
+    } else if (symbol.endsWith("TUSD")) {
+      return new CurrencyPair(symbol.substring(0, pairLength - 4), "TUSD");
+    } else if (symbol.endsWith("USDS")) {
+      return new CurrencyPair(symbol.substring(0, pairLength - 4), "USDS");
+    } else if (symbol.endsWith("ALTS")) {
+      return new CurrencyPair(symbol.substring(0, pairLength - 4), "ALTS");
+    } else if (symbol.endsWith("USDC")) {
+      return new CurrencyPair(symbol.substring(0, pairLength - 4), "USDC");
     } else {
       return new CurrencyPair(
           symbol.substring(0, pairLength - 3), symbol.substring(pairLength - 3));
