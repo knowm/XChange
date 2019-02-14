6f5514c5e87e92af970af463e04fc606e2c5148b
Jakub Kalicki <jakub.kalicki@tacton.com>
2019-02-14 01:25:39 +0100

    [CoinMarketCap] Implement ticker adapter

---
+++ xchange-coinmarketcap/src/main/java/org/knowm/xchange/coinmarketcap/deprecated/v2/CoinMarketCapAdapters.java
@@ -0,0 +1,30 @@
+package org.knowm.xchange.coinmarketcap.deprecated.v2;
+
+
+import org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.CoinMarketCapTicker;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Ticker;
+
+import java.math.BigDecimal;
+
+public class CoinMarketCapAdapters {
+
+    public static Ticker adaptTicker(CoinMarketCapTicker ticker, CurrencyPair currencyPair) {
+        BigDecimal price = ticker.getQuote().get(currencyPair.counter.getCurrencyCode()).getPrice();
+        BigDecimal volume24h = ticker.getQuote().get(currencyPair.counter.getCurrencyCode()).getVolume24h();
+
+        return new Ticker.Builder()
+                .currencyPair(currencyPair)
+                .timestamp(ticker.getLastUpdated())
+                .open(price)
+                .last(price)
+                .bid(price)
+                .ask(price)
+                .high(price)
+                .low(price)
+                .vwap(price)
+                .volume(volume24h)
+                .build();
+    }
+
+}
