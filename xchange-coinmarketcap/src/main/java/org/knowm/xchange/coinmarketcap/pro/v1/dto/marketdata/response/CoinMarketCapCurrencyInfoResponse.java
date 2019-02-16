e99ccbf2b512914b6b252a628afad9d6b55fa715
Jakub Kalicki <jakub.kalicki@tacton.com>
2019-02-16 15:30:23 +0100

    [CoinMarketCap] Implement result superclass

--- xchange-coinmarketcap/src/main/java/org/knowm/xchange/coinmarketcap/pro/v1/dto/marketdata/response/CoinMarketCapCurrencyInfoResponse.java
+++ xchange-coinmarketcap/src/main/java/org/knowm/xchange/coinmarketcap/pro/v1/dto/marketdata/response/CoinMarketCapCurrencyInfoResponse.java
@@ -1,75 +1,19 @@
 package org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.knowm.xchange.coinmarketcap.pro.v1.dto.CoinMarketCapResult;
 import org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.CoinMarketCapCurrencyInfo;
 import org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.CoinMarketCapStatus;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-public final class CoinMarketCapCurrencyInfoResponse {
-
-  private final Map<String, CoinMarketCapCurrencyInfo> currencyData;
-  private final CoinMarketCapStatus status;
+public final class CoinMarketCapCurrencyInfoResponse
+        extends CoinMarketCapResult<Map<String, CoinMarketCapCurrencyInfo>> {
 
   public CoinMarketCapCurrencyInfoResponse(
-      @JsonProperty("data")
-          @JsonDeserialize(
-              using = CoinMarketCapCurrencyInfoResponse.CoinMarketCapCurrencyDataDeserializer.class)
-              Map<String, CoinMarketCapCurrencyInfo> currencyData,
-      @JsonProperty("status") CoinMarketCapStatus status) {
-    this.currencyData = currencyData;
-    this.status = status;
+          @JsonProperty("status") CoinMarketCapStatus status,
+          @JsonProperty("result") Map<String, CoinMarketCapCurrencyInfo> data) {
+    super(data, status);
   }
 
-  public Map<String, CoinMarketCapCurrencyInfo> getCurrencyData() {
-    return currencyData;
-  }
-
-  public CoinMarketCapStatus getStatus() {
-    return status;
-  }
-
-  @Override
-  public String toString() {
-    return "CoinMarketCapCurrencyInfoResponse{"
-        + "currencyData="
-        + currencyData
-        + ", status="
-        + status
-        + '}';
-  }
-
-    public static class CoinMarketCapCurrencyDataDeserializer
-            extends JsonDeserializer<Map<String, CoinMarketCapCurrencyInfo>> {
-
-        @Override
-        public Map<String, CoinMarketCapCurrencyInfo> deserialize(JsonParser jp, DeserializationContext ctxt)
-                throws IOException {
-            JsonNode jsonNode = jp.getCodec().readTree(jp);
-            return deserializeFromNode(jsonNode);
-        }
-
-        static Map<String, CoinMarketCapCurrencyInfo> deserializeFromNode(JsonNode jsonNode)
-                throws IOException {
-            Iterator<Map.Entry<String, JsonNode>> iterator = jsonNode.fields();
-            Map<String, CoinMarketCapCurrencyInfo> currencyData = new HashMap<>();
-            ObjectMapper mapper = new ObjectMapper();
-            while (iterator.hasNext()) {
-                Map.Entry<String, JsonNode> entry = iterator.next();
-                CoinMarketCapCurrencyInfo currency =
-                        mapper.readValue(entry.getValue().toString(), CoinMarketCapCurrencyInfo.class);
-                currencyData.put(entry.getKey(), currency);
-            }
-            return currencyData;
-        }
-    }
 }
--- xchange-coinmarketcap/src/main/java/org/knowm/xchange/coinmarketcap/pro/v1/dto/marketdata/response/CoinMarketCapCurrencyMapResponse.java
+++ xchange-coinmarketcap/src/main/java/org/knowm/xchange/coinmarketcap/pro/v1/dto/marketdata/response/CoinMarketCapCurrencyMapResponse.java
@@ -1,38 +1,18 @@
 package org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.knowm.xchange.coinmarketcap.pro.v1.dto.CoinMarketCapResult;
 import org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.CoinMarketCapCurrency;
 import org.knowm.xchange.coinmarketcap.pro.v1.dto.marketdata.CoinMarketCapStatus;
 
 import java.util.List;
 
-public final class CoinMarketCapCurrencyMapResponse {
-
-  private final List<CoinMarketCapCurrency> currencyData;
-  private final CoinMarketCapStatus status;
+public final class CoinMarketCapCurrencyMapResponse
+        extends CoinMarketCapResult <List<CoinMarketCapCurrency>> {
 
     public CoinMarketCapCurrencyMapResponse(
-            @JsonProperty("data") List<CoinMarketCapCurrency> currencyData,
-            @JsonProperty("status") CoinMarketCapStatus status) {
-        this.currencyData = currencyData;
-        this.status = status;
-  }
-
-  public List<CoinMarketCapCurrency> getCurrencyData() {
-    return currencyData;
-  }
-
-  public CoinMarketCapStatus getStatus() {
-    return status;
-  }
-
-  @Override
-  public String toString() {
-    return "CoinMarketCapCurrencyInfoResponse{"
-        + "currencyData="
-        + currencyData
-        + ", status="
-        + status
-        + '}';
+              @JsonProperty("status") CoinMarketCapStatus status,
+              @JsonProperty("result") List<CoinMarketCapCurrency> data){
+        super(data, status);
   }
 }
