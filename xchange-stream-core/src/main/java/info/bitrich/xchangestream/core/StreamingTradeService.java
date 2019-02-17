1f65a1a050416649128f1deb0fa07c9ed71bc98e
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-17 18:41:26 +0000

    Add some health warnings regarding the current weaknesses in implementation.

--- xchange-stream-core/src/main/java/info/bitrich/xchangestream/core/StreamingTradeService.java
+++ xchange-stream-core/src/main/java/info/bitrich/xchangestream/core/StreamingTradeService.java
@@ -12,6 +12,15 @@ public interface StreamingTradeService {
     /**
      * Get the changes of order state for the logged-in user.
      *
+     * <p><strong>Warning:</strong> there are currently no guarantees that messages will
+     * arrive in order, that messages will not be skipped, or that any initial state
+     * message will be sent on connection. Most exchanges have a recommended approach
+     * for managing this, involving timestamps, sequence numbers and a separate REST
+     * API for re-sync when inconsistencies appear. The intention is for this to be
+     * managed automatically by this method, but this doesn't currently happen. See
+     * https://github.com/bitrich-info/xchange-stream/issues/274 for progress towards
+     * this.</p>
+     *
      * <p>Emits {@link info.bitrich.xchangestream.service.exception.NotConnectedException} When
      * not connected to the WebSocket API <strong>and</strong> authenticated.</p>
      *
@@ -24,6 +33,14 @@ public interface StreamingTradeService {
 
     /**
      * Gets authenticated trades for the logged-in user.
+     *
+     * <p><strong>Warning:</strong> there are currently no guarantees that messages
+     * will not be skipped. Most exchanges have a recommended approach
+     * for managing this, involving timestamps, sequence numbers and a separate REST
+     * API for re-sync when inconsistencies appear. The intention is for this to be
+     * managed automatically by this method, but this doesn't currently happen. See
+     * https://github.com/bitrich-info/xchange-stream/issues/274 for progress towards
+     * this.</p>
      *
      * <p>Emits {@link info.bitrich.xchangestream.service.exception.NotConnectedException} When
      * not connected to the WebSocket API <strong>and</strong> authenticated.</p>
