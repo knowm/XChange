732aa38113824cf29f1ed2466cba9f64208dcb7c
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-18 11:26:58 +0000

    Remove 1.0 Kucoin implementation entirely since it has been disabled. 2.0 is still not complete, but it's more functional than the one one!

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinExchange.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinExchange.java
@@ -1,51 +1,89 @@
 package org.knowm.xchange.kucoin;
 
 import java.io.IOException;
+
 import org.knowm.xchange.BaseExchange;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.exceptions.ExchangeException;
-import org.knowm.xchange.kucoin.service.KucoinAccountService;
-import org.knowm.xchange.kucoin.service.KucoinMarketDataService;
-import org.knowm.xchange.kucoin.service.KucoinTradeService;
-import org.knowm.xchange.utils.nonce.CurrentTimeNonceFactory;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+import org.knowm.xchange.service.account.AccountService;
+import org.knowm.xchange.service.trade.TradeService;
+
 import si.mazi.rescu.SynchronizedValueFactory;
 
 public class KucoinExchange extends BaseExchange implements Exchange {
 
-  private SynchronizedValueFactory<Long> nonceFactory = new CurrentTimeNonceFactory();
+  /**
+   * Use with {@link ExchangeSpecification#getExchangeSpecificParametersItem(String)}
+   * to specify that connection should be made to the Kucoin sandbox instead of the
+   * live API.
+   */
+  public static final String PARAM_SANDBOX = "Use_Sandbox";
+
+  static final String SANDBOX_HOST = "openapi-sandbox.kucoin.com";
+  static final String SANDBOX_URI = "https://" + SANDBOX_HOST;
+
+  static final String LIVE_HOST = "openapi-v2.kucoin.com";
+  static final String LIVE_URI = "https://" + LIVE_HOST;
+
+  private void concludeHostParams(ExchangeSpecification exchangeSpecification) {
+    if (exchangeSpecification.getExchangeSpecificParameters() != null) {
+      if (exchangeSpecification.getExchangeSpecificParametersItem(PARAM_SANDBOX).equals(true)) {
+        exchangeSpecification.setSslUri(KucoinExchange.SANDBOX_URI);
+        exchangeSpecification.setHost(KucoinExchange.SANDBOX_HOST);
+      }
+    }
+  }
+
+  @Override
+  public void applySpecification(ExchangeSpecification exchangeSpecification) {
+    super.applySpecification(exchangeSpecification);
+    concludeHostParams(exchangeSpecification);
+  }
 
   @Override
   protected void initServices() {
-
+    concludeHostParams(exchangeSpecification);
     this.marketDataService = new KucoinMarketDataService(this);
-    this.accountService = new KucoinAccountService(this);
-    this.tradeService = new KucoinTradeService(this);
   }
 
   @Override
   public ExchangeSpecification getDefaultExchangeSpecification() {
-
-    ExchangeSpecification exchangeSpecification =
-        new ExchangeSpecification(this.getClass().getCanonicalName());
-    exchangeSpecification.setSslUri("https://api.kucoin.com/");
-    exchangeSpecification.setHost("kucoin.com");
+    ExchangeSpecification exchangeSpecification = new ExchangeSpecification(this.getClass().getCanonicalName());
+    exchangeSpecification.setSslUri(LIVE_URI);
+    exchangeSpecification.setHost(LIVE_HOST);
     exchangeSpecification.setPort(80);
     exchangeSpecification.setExchangeName("Kucoin");
     exchangeSpecification.setExchangeDescription("Kucoin is a bitcoin and altcoin exchange.");
-
     return exchangeSpecification;
   }
 
   @Override
   public SynchronizedValueFactory<Long> getNonceFactory() {
-
-    return nonceFactory;
+    throw new UnsupportedOperationException("Kucoin does not use a nonce factory.");
   }
 
   @Override
   public void remoteInit() throws IOException, ExchangeException {
+    this.exchangeMetaData = KucoinAdapters.adaptMetadata(
+      this.exchangeMetaData,
+      getMarketDataService().getKucoinSymbols()
+    );
+  }
 
-    exchangeMetaData = ((KucoinMarketDataService) marketDataService).getMetadata();
+  @Override
+  public KucoinMarketDataService getMarketDataService() {
+    return (KucoinMarketDataService) super.getMarketDataService();
+  }
+
+  @Override
+  public TradeService getTradeService() {
+    throw new NotYetImplementedForExchangeException("Trade service not yet implemented. Monitor https://github.com/knowm/XChange/issues/2914 for progress.");
+  }
+
+  @Override
+  public AccountService getAccountService() {
+    throw new NotYetImplementedForExchangeException("Account service not yet implemented. Monitor https://github.com/knowm/XChange/issues/2914 for progress.");
   }
 }
