6372bd58a11b3f1e9cfb0178a7d380c7acc9195a
Andre Reiter <a.reiter@web.de>
2019-02-19 09:09:38 +0100

    use DateUtils for date parsing

--- xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/Util.java
+++ xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/Util.java
@@ -1,17 +1,13 @@
 package org.knowm.xchange.cryptofacilities;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.knowm.xchange.utils.DateUtils;
 
 public class Util {
 
-  private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
-
-  public static Date parseDate(String str) throws ParseException {
+  public static Date parseDate(String str) {
     try {
-      // need to create a new instance of SFD, since its not thread safe
-      return str == null ? null : new SimpleDateFormat(DATE_FORMAT).parse(str);
+      return str == null ? null : DateUtils.fromISODateString(str);
     } catch (Exception e) {
       throw new RuntimeException("Could not parse date using '" + str + "'.", e);
     }
