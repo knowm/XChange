5c203113974068c2cd300619af292461433c178f
BenTrapani <ben.trapani1995@gmail.com>
2019-02-20 21:54:05 -0800

    Format cexio per style guidelines

--- xchange-cexio/src/main/java/info/bitrich/xchangestream/cexio/dto/CexioWebSocketTransactionMessage.java
+++ xchange-cexio/src/main/java/info/bitrich/xchangestream/cexio/dto/CexioWebSocketTransactionMessage.java
@@ -4,29 +4,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CexioWebSocketTransactionMessage {
 
-    private final String e;
-    private final CexioWebSocketTransaction data;
+  private final String e;
+  private final CexioWebSocketTransaction data;
 
-    public CexioWebSocketTransactionMessage(@JsonProperty("e") String e,
-                                            @JsonProperty("data") CexioWebSocketTransaction data) {
-        this.e = e;
-        this.data = data;
-    }
+  public CexioWebSocketTransactionMessage(
+      @JsonProperty("e") String e, @JsonProperty("data") CexioWebSocketTransaction data) {
+    this.e = e;
+    this.data = data;
+  }
 
-    public String getE() {
-        return e;
-    }
+  public String getE() {
+    return e;
+  }
 
-    public CexioWebSocketTransaction getData() {
-        return data;
-    }
+  public CexioWebSocketTransaction getData() {
+    return data;
+  }
 
-    @Override
-    public String toString() {
-        final StringBuffer buffer = new StringBuffer("{");
-        buffer.append("e='").append(e).append('\'');
-        buffer.append(", data=").append(data);
-        buffer.append('}');
-        return buffer.toString();
-    }
+  @Override
+  public String toString() {
+    final StringBuffer buffer = new StringBuffer("{");
+    buffer.append("e='").append(e).append('\'');
+    buffer.append(", data=").append(data);
+    buffer.append('}');
+    return buffer.toString();
+  }
 }
