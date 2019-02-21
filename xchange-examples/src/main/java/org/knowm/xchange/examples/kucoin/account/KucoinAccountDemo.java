1ce1c769b180c10464534fb054d3c54525d58c20
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-21 01:45:10 +0000

    Add account balances

--- xchange-examples/src/main/java/org/knowm/xchange/examples/kucoin/account/KucoinAccountDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/kucoin/account/KucoinAccountDemo.java
@@ -1,38 +1,48 @@
 package org.knowm.xchange.examples.kucoin.account;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.dto.account.AccountInfo;
+import org.knowm.xchange.examples.kucoin.KucoinExamplesUtils;
+import org.knowm.xchange.kucoin.KucoinAccountServiceRaw;
+import org.knowm.xchange.service.account.AccountService;
+
+import com.kucoin.sdk.rest.response.AccountBalancesResponse;
+
 public class KucoinAccountDemo {
 
-  // Currently not supported. See https://github.com/knowm/XChange/issues/2914
-//  public static void main(String[] args) throws IOException {
-//
-//    Exchange exchange = KucoinExamplesUtils.getExchange();
-//
-//    AccountService accountService = exchange.getAccountService();
-//
-//    generic(accountService);
-//    raw((KucoinAccountServiceRaw) accountService);
-//  }
-//
-//  private static void generic(AccountService accountService) throws IOException {
-//
-//    System.out.println("----------GENERIC---------");
-//
-//    Map<Currency, Balance> balances = accountService.getAccountInfo().getWallet().getBalances();
-//    System.out.println(balances);
-//
+  public static void main(String[] args) throws IOException {
+
+    Exchange exchange = KucoinExamplesUtils.getExchange();
+
+    AccountService accountService = exchange.getAccountService();
+
+    generic(accountService);
+    raw((KucoinAccountServiceRaw) accountService);
+  }
+
+  private static void generic(AccountService accountService) throws IOException {
+
+    System.out.println("----------GENERIC---------");
+
+    AccountInfo accountInfo = accountService.getAccountInfo();
+    System.out.println(accountInfo);
+
+    // Not yet implemented
 //    System.out.println(accountService.requestDepositAddress(Currency.BTC));
-//  }
-//
-//  private static void raw(KucoinAccountServiceRaw accountService) throws IOException {
-//
-//    System.out.println("------------RAW-----------");
-//
-//    KucoinSimpleResponse<KucoinCoinBalances> responseBalances =
-//        accountService.getKucoinBalances(20, 1);
-//    System.out.println(responseBalances.getData());
-//
-//    KucoinResponse<KucoinDepositAddress> responseAddress =
-//        accountService.getKucoinDepositAddress(Currency.BTC);
-//    System.out.println(responseAddress.getData().getAddress());
-//  }
+  }
+
+  private static void raw(KucoinAccountServiceRaw accountService) throws IOException {
+
+    System.out.println("------------RAW-----------");
+
+    List<AccountBalancesResponse> responseBalances = accountService.getKucoinAccounts();
+    System.out.println(responseBalances);
+
+    // Not yet implemented
+//    ????? responseAddress = accountService.getKucoinDepositAddress(Currency.BTC);
+//    System.out.println(responseAddress.?????);
+  }
 }
--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinAccountService.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinAccountService.java
@@ -1,7 +1,38 @@
 package org.knowm.xchange.kucoin;
 
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.knowm.xchange.dto.account.AccountInfo;
+import org.knowm.xchange.dto.account.Wallet;
 import org.knowm.xchange.service.account.AccountService;
 
-public class KucoinAccountService implements AccountService {
+import com.kucoin.sdk.rest.response.AccountBalancesResponse;
 
+public class KucoinAccountService extends KucoinAccountServiceRaw
+                                  implements AccountService {
+
+  protected KucoinAccountService(KucoinExchange exchange) {
+    super(exchange);
+  }
+
+  @Override
+  public AccountInfo getAccountInfo() throws IOException {
+    List<AccountBalancesResponse> accounts = getKucoinAccounts();
+    return new AccountInfo(
+      accounts.stream()
+        .map(AccountBalancesResponse::getType)
+        .distinct()
+        .map(type -> new Wallet(
+            type,
+            accounts.stream()
+              .filter(a -> a.getType().equals(type))
+              .map(KucoinAdapters::adaptBalance)
+              .collect(toList())
+        ))
+        .collect(toList())
+    );
+  }
 }
