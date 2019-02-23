e0827ae5283e01b869166a6b6435e3a8c817c556
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-23 14:52:53 +0000

    Update to use version of Kucoin SDK that ensures propagation of IOException. This is necessary to be able to catch and gracefully handle socket errors such as timeouts.

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinMarketDataServiceRaw.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinMarketDataServiceRaw.java
@@ -2,6 +2,7 @@ package org.knowm.xchange.kucoin;
 
 import static org.knowm.xchange.kucoin.KucoinExceptionClassifier.classifyingExceptions;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.knowm.xchange.currency.CurrencyPair;
@@ -18,34 +19,34 @@ public class KucoinMarketDataServiceRaw extends KucoinBaseService {
     super(exchange);
   }
 
-  public TickerResponse getKucoinTicker(CurrencyPair pair) {
+  public TickerResponse getKucoinTicker(CurrencyPair pair) throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.symbolAPI().getTicker(KucoinAdapters.adaptCurrencyPair(pair)));
   }
 
-  public SymbolTickResponse getKucoin24hrStats(CurrencyPair pair) {
+  public SymbolTickResponse getKucoin24hrStats(CurrencyPair pair) throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.symbolAPI().get24hrStats(KucoinAdapters.adaptCurrencyPair(pair)));
   }
 
-  public List<SymbolResponse> getKucoinSymbols() {
+  public List<SymbolResponse> getKucoinSymbols() throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.symbolAPI().getSymbols());
   }
 
-  public OrderBookResponse getKucoinOrderBookPartial(CurrencyPair pair) {
+  public OrderBookResponse getKucoinOrderBookPartial(CurrencyPair pair) throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.orderBookAPI().getPartOrderBookAggregated(
         KucoinAdapters.adaptCurrencyPair(pair)));
   }
 
-  public OrderBookResponse getKucoinOrderBookFull(CurrencyPair pair) {
+  public OrderBookResponse getKucoinOrderBookFull(CurrencyPair pair) throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.orderBookAPI().getFullOrderBookAggregated(
         KucoinAdapters.adaptCurrencyPair(pair)));
   }
 
-  public List<TradeHistoryResponse> getKucoinTrades(CurrencyPair pair) {
+  public List<TradeHistoryResponse> getKucoinTrades(CurrencyPair pair) throws IOException {
     return classifyingExceptions(() ->
       kucoinRestClient.historyAPI().getTradeHistories(
         KucoinAdapters.adaptCurrencyPair(pair)));
