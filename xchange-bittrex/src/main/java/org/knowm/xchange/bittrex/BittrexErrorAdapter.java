3dc4e0f3d8082a00b6e7ce95b310e83baba77096
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-02-25 15:26:18 +0000

    Catch rate limit exceptions and rethrow as RateLimitExceededException

--- xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/BittrexErrorAdapter.java
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/BittrexErrorAdapter.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.knowm.xchange.bittrex.dto.BittrexException;
 import org.knowm.xchange.exceptions.CurrencyPairNotValidException;
 import org.knowm.xchange.exceptions.ExchangeException;
+import org.knowm.xchange.exceptions.RateLimitExceededException;
 
 /** @author walec51 */
 public class BittrexErrorAdapter {
@@ -12,6 +13,9 @@ public class BittrexErrorAdapter {
     String message = e.getMessage();
     if (StringUtils.isEmpty(message)) {
       return new ExchangeException("Operation failed without any error message");
+    }
+    if (message.contains("was throttled")) {
+      return new RateLimitExceededException(e.getMessage(), e);
     }
     switch (message) {
       case "INVALID_MARKET":
