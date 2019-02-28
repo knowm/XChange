cba51b8f1da65bb5645395056ec778e8829f02c9
Jakub Kalicki <jakub.kalicki@tacton.com>
2019-03-01 00:08:13 +0100

    [Bitmex] Use exchange specific currency pairs to provide consistency

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/BitmexAdapters.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/BitmexAdapters.java
@@ -47,14 +47,16 @@ public class BitmexAdapters {
         bidsOrdersContainer.getLimitOrders());
   }
 
-  public static BitmexDepth adaptDepth(BitmexPublicOrderList orders, CurrencyPair currencyPair) {
+  public static BitmexDepth adaptDepth(BitmexPublicOrderList orders) {
 
     BitmexDepth bitmexDepth = new BitmexDepth(new ArrayList<>(), new ArrayList<>());
 
     for (BitmexPublicOrder bitmexOrder : orders) {
-      if (bitmexOrder.getSide().equals(BitmexSide.BUY)) bitmexDepth.getBids().add(bitmexOrder);
-      else if (bitmexOrder.getSide().equals(BitmexSide.SELL))
+      if (bitmexOrder.getSide().equals(BitmexSide.BUY)) {
+        bitmexDepth.getBids().add(bitmexOrder);
+      } else if (bitmexOrder.getSide().equals(BitmexSide.SELL)) {
         bitmexDepth.getAsks().add(bitmexOrder);
+      }
     }
 
     return bitmexDepth;
