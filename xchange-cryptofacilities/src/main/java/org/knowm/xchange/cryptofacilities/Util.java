e682361d4727b7568c5acec5df96983e2ab8543a
Andre Reiter <a.reiter@web.de>
2019-03-01 09:21:10 +0100

    added support for additional parameter 'lastFillTime'

--- xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/Util.java
+++ xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/Util.java
@@ -1,6 +1,8 @@
 package org.knowm.xchange.cryptofacilities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 import org.knowm.xchange.utils.DateUtils;
 
 public class Util {
@@ -11,5 +13,14 @@ public class Util {
     } catch (Exception e) {
       throw new RuntimeException("Could not parse date using '" + str + "'.", e);
     }
+  }
+
+  public static String format(Date date) {
+    if (date == null) {
+      return null;
+    }
+    SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+    f.setTimeZone(TimeZone.getTimeZone("UTC"));
+    return f.format(date);
   }
 }
