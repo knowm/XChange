46292e66aebeae93dd450bcd42b0a5284cfb0e8f
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-03-16 20:15:57 +0100

    Move new hook up the modular hierarchy where XChange is visible.

--- service-core/src/main/java/info/bitrich/xchangestream/service/ConnectableService.java
+++ service-core/src/main/java/info/bitrich/xchangestream/service/ConnectableService.java
@@ -19,29 +19,8 @@ public abstract class ConnectableService {
      * ));
      * }
      * </pre>
-     *
-     * @see #BEFORE_API_CALL_HANDLER for a similar hook for stream-initiated API calls.
      */
     public static final String BEFORE_CONNECTION_HANDLER = "Before_Connection_Event_Handler";
-
-    /**
-     * Exchange-specific parameter used for providing a {@link Runnable} action to be run
-     * prior to any API calls initiated during the course of maintaining a streamed connection.
-     *
-     * <p>While some exchanges allow bidirectional streamed communication, such that a socket
-     * can be opened and then authenticated, others perform authentication by means of a
-     * separate API call which can count towards API rate limits.  In addition, many exchanges
-     * require API calls to obtain initial snapshots for streamed data such as order books
-     * or account updates. XChange requires that the developer manage rate limits themselves,
-     * but this is not possible when xchange-stream has to initiate these API calls
-     * automatically. This parameter provides a means for the developer to get a callback
-     * prior to these API calls, principally to apply such rate limiting globally.  However,
-     * in principle there are wider potential uses.</p>
-     *
-     * @see #BEFORE_CONNECTION_HANDLER which provides the same sort of hook for socket
-     * reconnections. This also includes example usage.
-     */
-    public static final String BEFORE_API_CALL_HANDLER = "Before_API_Call_Event_Handler";
 
     /**
      * {@link Runnable} handler is called before opening new socket connection.
