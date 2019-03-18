2c18cab68d2d6fb15c8d4113fe85e75520847dae
Graham Crockford <badgerwithagun@users.noreply.github.com>
2019-03-18 10:28:12 +0000

    Coveo format

--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/BinanceAccountInformation.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/BinanceAccountInformation.java
@@ -1,9 +1,8 @@
 package org.knowm.xchange.binance.dto.account;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public final class BinanceAccountInformation {
 
