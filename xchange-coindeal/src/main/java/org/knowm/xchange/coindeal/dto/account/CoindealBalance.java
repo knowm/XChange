41a3a2a5619427942dd77d9e12c37cb2f41a2803
makarid <makarid@gmail.com>
2019-03-29 15:52:26 +0200

    coindeal new methods

--- xchange-coindeal/src/main/java/org/knowm/xchange/coindeal/dto/account/CoindealBalance.java
+++ xchange-coindeal/src/main/java/org/knowm/xchange/coindeal/dto/account/CoindealBalance.java
@@ -2,21 +2,23 @@ package org.knowm.xchange.coindeal.dto.account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigDecimal;
+
 public class CoindealBalance {
 
     @JsonProperty("currency")
     private final String currency;
 
     @JsonProperty("available")
-    private final String available;
+    private final BigDecimal available;
 
     @JsonProperty("reserved")
-    private final String reserved;
+    private final BigDecimal reserved;
 
     public CoindealBalance(
             @JsonProperty("currency") String currency,
-            @JsonProperty("available") String available,
-            @JsonProperty("reserved") String reserved) {
+            @JsonProperty("available") BigDecimal available,
+            @JsonProperty("reserved") BigDecimal reserved) {
         this.currency = currency;
         this.available = available;
         this.reserved = reserved;
@@ -26,11 +28,11 @@ public class CoindealBalance {
         return currency;
     }
 
-    public String getAvailable() {
+    public BigDecimal getAvailable() {
         return available;
     }
 
-    public String getReserved() {
+    public BigDecimal getReserved() {
         return reserved;
     }
 
