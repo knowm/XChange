a27c6a5ed0a8f74cf0b9952da65697c40496392e
makarid <makarid@gmail.com>
2019-03-30 18:06:51 +0200

    adding globitex exchange

--- xchange-coinex/src/main/java/org/knowm/xchange/coinex/dto/CoinexResponse.java
+++ xchange-coinex/src/main/java/org/knowm/xchange/coinex/dto/CoinexResponse.java
@@ -2,32 +2,30 @@ package org.knowm.xchange.coinex.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 public class CoinexResponse<T> {
 
-    private final int code;
-    private final T data;
-    private final String message;
+  private final int code;
+  private final T data;
+  private final String message;
 
-    public CoinexResponse(
-            @JsonProperty("code") int code,
-            @JsonProperty("data") T data,
-            @JsonProperty("message") String message) {
-        this.code = code;
-        this.data = data;
-        this.message = message;
-    }
+  public CoinexResponse(
+      @JsonProperty("code") int code,
+      @JsonProperty("data") T data,
+      @JsonProperty("message") String message) {
+    this.code = code;
+    this.data = data;
+    this.message = message;
+  }
 
-    public int getCode() {
-        return code;
-    }
+  public int getCode() {
+    return code;
+  }
 
-    public T getData() {
-        return data;
-    }
+  public T getData() {
+    return data;
+  }
 
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 }
