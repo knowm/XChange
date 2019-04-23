c94ce4ff66b5ade5915e37f72d7c9ad2bceab251
Jakub Kalicki <jakub.kalicki@protonmail.com>
2019-04-23 15:01:57 +0200

    [Deribit] Fix currency dto + test

---
+++ xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/dto/marketdata/WithdrawalPriority.java
@@ -0,0 +1,20 @@
+package org.knowm.xchange.deribit.v2.dto.marketdata;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class WithdrawalPriority {
+
+    @JsonProperty("value") public float value;
+    @JsonProperty("name") public String name;
+
+    public float getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
