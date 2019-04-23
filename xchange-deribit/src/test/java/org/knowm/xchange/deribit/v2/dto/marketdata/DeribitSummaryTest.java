aabb1dee8d61d632a6f23f517eabba7e3eef4134
Jakub Kalicki <jakub.kalicki@protonmail.com>
2019-04-23 15:47:24 +0200

    [Deribit] Fix summary dto + test

--- xchange-deribit/src/test/java/org/knowm/xchange/deribit/v2/dto/marketdata/DeribitSummaryTest.java
+++ xchange-deribit/src/test/java/org/knowm/xchange/deribit/v2/dto/marketdata/DeribitSummaryTest.java
@@ -25,23 +25,21 @@ public class DeribitSummaryTest {
         // then
         assertThat(summary).isNotNull();
 
-        assertThat(summary.getCurrentFunding()).isEqualTo(new BigDecimal("-0.00034500919497000614"));
-        assertThat(summary.getFunding8h()).isEqualTo(new BigDecimal("-0.00016358808971835147"));
-        assertThat(summary.getInstrumentName()).isEqualTo("BTC-PERPETUAL");
-        assertThat(summary.getOpenInterest()).isEqualTo(new BigDecimal("13673303.5"));
-        assertThat(summary.getOpenInterestAmount()).isEqualTo(new BigDecimal("136733035"));
-        assertThat(summary.getHigh()).isEqualTo(new BigDecimal("4099.75"));
-        assertThat(summary.getLow()).isEqualTo(new BigDecimal("4056"));
-        assertThat(summary.getVolume()).isEqualTo(new BigDecimal("1388007"));
-        assertThat(summary.getVolumeUsd()).isEqualTo(new BigDecimal("13880070"));
-        assertThat(summary.getVolumeBtc()).isEqualTo(new BigDecimal("3402.11339229"));
-        assertThat(summary.getLast()).isEqualTo(new BigDecimal("4091"));
-        assertThat(summary.getBidPrice()).isEqualTo(new BigDecimal("4091"));
-        assertThat(summary.getAskPrice()).isEqualTo(new BigDecimal("4091.25"));
-        assertThat(summary.getMidPrice()).isEqualTo(new BigDecimal("4091.13"));
-        assertThat(summary.getEstDelPrice()).isEqualTo(new BigDecimal("4094.63"));
-        assertThat(summary.getMarkPrice()).isEqualTo(new BigDecimal("4091.17"));
-        assertThat(summary.getCreated()).isEqualTo("2019-03-31 13:41:37 GMT");
+        assertThat(summary.getVolume()).isEqualTo(new BigDecimal("0.55"));
+        assertThat(summary.getUnderlyingPrice()).isEqualTo(new BigDecimal("121.38"));
+        assertThat(summary.getUnderlyingIndex()).isEqualTo("index_price");
+        assertThat(summary.getQuoteCurrency()).isEqualTo("USD");
+        assertThat(summary.getOpenInterest()).isEqualTo("0.55");
+        assertThat(summary.getMidPrice()).isEqualTo(new BigDecimal("0.2444"));
+        assertThat(summary.getMarkPrice()).isEqualTo(new BigDecimal("0.179112"));
+        assertThat(summary.getLow()).isEqualTo(new BigDecimal("0.34"));
+        assertThat(summary.getLast()).isEqualTo(new BigDecimal("0.34"));
+        assertThat(summary.getInterestRate()).isEqualTo(new BigDecimal("0.207"));
+        assertThat(summary.getInstrumentName()).isEqualTo("ETH-22FEB19-140-P");
+        assertThat(summary.getHigh()).isEqualTo(new BigDecimal("0.34"));
+        assertThat(summary.getCreationTimestamp()).isEqualTo(1550227952163L);
+        assertThat(summary.getBidPrice()).isEqualTo(new BigDecimal("0.1488"));
+        assertThat(summary.getBaseCurrency()).isEqualTo("ETH");
+        assertThat(summary.getAskPrice()).isEqualTo(new BigDecimal("0.34"));
     }
-
 }
