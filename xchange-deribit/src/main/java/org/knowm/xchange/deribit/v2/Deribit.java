5f0b2c3ce9efaf069e9c85ea420f0f24c5d05e77
Jakub Kalicki <jakub.kalicki@protonmail.com>
2019-05-02 21:09:31 +0200

    [Deribit] Remove unnecessary response classes

--- xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/Deribit.java
+++ xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/Deribit.java
@@ -1,7 +1,8 @@
 package org.knowm.xchange.deribit.v2;
 
 import org.knowm.xchange.deribit.v2.dto.DeribitException;
-import org.knowm.xchange.deribit.v2.dto.marketdata.response.*;
+import org.knowm.xchange.deribit.v2.dto.DeribitResponse;
+import org.knowm.xchange.deribit.v2.dto.marketdata.*;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.List;
 
 @Path("/api/v2/public")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,36 +18,36 @@ public interface Deribit {
 
   @GET
   @Path("get_instruments")
-  DeribitInstrumentsResponse getInstruments(@QueryParam("currency") String currency)
+  DeribitResponse<List<DeribitInstrument>> getInstruments(@QueryParam("currency") String currency)
       throws DeribitException, IOException;
 
   @GET
   @Path("get_currencies")
-  DeribitCurrenciesResponse getCurrencies() throws DeribitException, IOException;
+  DeribitResponse<List<DeribitCurrency>> getCurrencies() throws DeribitException, IOException;
 
   @GET
   @Path("get_order_book")
-  DeribitOrderBookResponse getOrderBook(@QueryParam("instrument_name") String instrumentName)
+  DeribitResponse<DeribitOrderBook> getOrderBook(@QueryParam("instrument_name") String instrumentName)
       throws DeribitException, IOException;
 
   @GET
   @Path("get_order_book")
-  DeribitOrderBookResponse getOrderBook(
+  DeribitResponse<DeribitOrderBook> getOrderBook(
       @QueryParam("instrument_name") String instrumentName, @QueryParam("depth") int depth)
       throws DeribitException, IOException;
 
   @GET
   @Path("get_last_trades_by_instrument")
-  DeribitTradesResponse getLastTrades(@QueryParam("instrument_name") String instrumentName)
+  DeribitResponse<DeribitTrades> getLastTrades(@QueryParam("instrument_name") String instrumentName)
       throws DeribitException, IOException;
 
   @GET
   @Path("get_book_summary_by_instrument")
-  DeribitSummaryResponse getSummary(@QueryParam("instrument_name") String instrumentName)
+  DeribitResponse<List<DeribitSummary>> getSummary(@QueryParam("instrument_name") String instrumentName)
       throws DeribitException, IOException;
 
   @GET
   @Path("ticker")
-  DeribitTickerResponse getTicker(@QueryParam("instrument_name") String instrumentName)
+  DeribitResponse<DeribitTicker> getTicker(@QueryParam("instrument_name") String instrumentName)
       throws DeribitException, IOException;
 }
