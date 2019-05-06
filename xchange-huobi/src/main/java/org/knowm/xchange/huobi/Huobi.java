444e2a1d24a9c1405128c7b76ecd1d005766b030
cacofish <cacofish@hotmail.com>
2019-05-06 02:30:39 -0700

    Fix withdraw fee range issue
    Fix read response issue in huobi withdreaw fee range api.
    Argument #3 of constructor [constructor for org.knowm.xchange.huobi.dto.HuobiResult, annotations: {interface com.fasterxml.jackson.annotation.JsonCreator=@com.fasterxml.jackson.annotation.JsonCreator(mode=DEFAULT)}] has no property name annotation; must have name when multiple-parameter constructor annotated as Creator

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/Huobi.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/Huobi.java
@@ -9,15 +9,14 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import org.knowm.xchange.huobi.dto.HuobiResult;
 import org.knowm.xchange.huobi.dto.account.HuobiCreateWithdrawRequest;
-import org.knowm.xchange.huobi.dto.account.HuobiWithdrawFeeRange;
 import org.knowm.xchange.huobi.dto.account.results.HuobiAccountResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiBalanceResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiCreateWithdrawResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiDepositAddressResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiDepositAddressWithTagResult;
 import org.knowm.xchange.huobi.dto.account.results.HuobiFundingHistoryResult;
+import org.knowm.xchange.huobi.dto.account.results.HuobiWithdrawFeeRangeResult;
 import org.knowm.xchange.huobi.dto.marketdata.results.HuobiAssetPairsResult;
 import org.knowm.xchange.huobi.dto.marketdata.results.HuobiAssetsResult;
 import org.knowm.xchange.huobi.dto.marketdata.results.HuobiDepthResult;
@@ -107,7 +106,7 @@ public interface Huobi {
   @GET
   @Path("v1/dw/withdraw-virtual/fee-range")
   @Consumes(MediaType.APPLICATION_JSON)
-  HuobiResult<HuobiWithdrawFeeRange> getWithdrawFeeRange(
+  HuobiWithdrawFeeRangeResult getWithdrawFeeRange(
       @QueryParam("currency") String currency,
       @QueryParam("AccessKeyId") String apiKey,
       @QueryParam("SignatureMethod") String signatureMethod,
