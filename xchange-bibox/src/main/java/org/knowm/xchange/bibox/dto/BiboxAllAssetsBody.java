8a660b15282eb05c887ac1a15e60cd7a1a8d2839
cyril <nguyen.cyril@gmail.com>
2019-05-25 11:25:59 +0200

    fixed get balances

---
+++ xchange-bibox/src/main/java/org/knowm/xchange/bibox/dto/BiboxAllAssetsBody.java
@@ -0,0 +1,10 @@
+package org.knowm.xchange.bibox.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BiboxAllAssetsBody {
+
+  @JsonProperty public final int select = 1;
+
+  public BiboxAllAssetsBody() {}
+}
