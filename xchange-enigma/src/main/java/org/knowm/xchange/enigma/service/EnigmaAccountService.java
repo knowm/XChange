9323da9044c8b747aae6623637c6a3d56a1cedbc
vitalii <vitaliy.melnik@introlab-systems.com>
2019-08-19 14:44:12 +0300

    [ENIGMA] Add stubs for missed API endpoints

--- xchange-enigma/src/main/java/org/knowm/xchange/enigma/service/EnigmaAccountService.java
+++ xchange-enigma/src/main/java/org/knowm/xchange/enigma/service/EnigmaAccountService.java
@@ -1,10 +1,25 @@
 package org.knowm.xchange.enigma.service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.account.AccountInfo;
+import org.knowm.xchange.dto.account.AddressWithTag;
+import org.knowm.xchange.dto.account.Fee;
+import org.knowm.xchange.dto.account.FundingRecord;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
 import org.knowm.xchange.enigma.EnigmaAdapters;
 import org.knowm.xchange.service.account.AccountService;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.WithdrawFundsParams;
+import si.mazi.rescu.ClientConfig;
 
 public class EnigmaAccountService extends EnigmaAccountServiceRaw implements AccountService {
 
@@ -18,4 +33,45 @@ public class EnigmaAccountService extends EnigmaAccountServiceRaw implements Acc
     return EnigmaAdapters.adaptAccountInfo(
         getBalance(), this.exchange.getExchangeSpecification().getUserName());
   }
+
+  @Override
+  public String withdrawFunds(Currency currency, BigDecimal amount, String address) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String withdrawFunds(Currency currency, BigDecimal amount, AddressWithTag address) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String withdrawFunds(WithdrawFundsParams params) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String requestDepositAddress(Currency currency, String... args) throws IOException {
+    return null;
+  }
+
+  @Override
+  public AddressWithTag requestDepositAddressData(Currency currency, String... args) throws IOException {
+    return null;
+  }
+
+  @Override
+  public TradeHistoryParams createFundingHistoryParams() {
+    return null;
+  }
+
+  @Override
+  public List<FundingRecord> getFundingHistory(TradeHistoryParams params) throws IOException {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public Map<CurrencyPair, Fee> getDynamicTradingFees() throws IOException {
+    return null;
+  }
+
 }
--- xchange-enigma/src/main/java/org/knowm/xchange/enigma/service/EnigmaTradeService.java
+++ xchange-enigma/src/main/java/org/knowm/xchange/enigma/service/EnigmaTradeService.java
@@ -1,10 +1,18 @@
 package org.knowm.xchange.enigma.service;
 
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.trade.*;
 import org.knowm.xchange.enigma.model.EnigmaException;
 import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.CancelOrderParams;
+import org.knowm.xchange.service.trade.params.TradeHistoryParams;
+import org.knowm.xchange.service.trade.params.orders.OpenOrdersParams;
+import org.knowm.xchange.service.trade.params.orders.OrderQueryParams;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 
 public class EnigmaTradeService extends EnigmaTradeServiceRaw implements TradeService {
 
@@ -15,5 +23,65 @@ public class EnigmaTradeService extends EnigmaTradeServiceRaw implements TradeSe
   @Override
   public boolean cancelOrder(String orderId) throws IOException, EnigmaException {
     return cancelOrder(Integer.parseInt(orderId)).isResult();
+  }
+
+  @Override
+  public OpenOrders getOpenOrders() throws IOException {
+    return null;
+  }
+
+  @Override
+  public OpenOrders getOpenOrders(OpenOrdersParams params) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String placeLimitOrder(LimitOrder limitOrder) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String placeStopOrder(StopOrder stopOrder) throws IOException {
+    return null;
+  }
+
+  @Override
+  public String changeOrder(LimitOrder limitOrder) throws IOException {
+    return null;
+  }
+
+  @Override
+  public boolean cancelOrder(CancelOrderParams orderParams) throws IOException {
+    return false;
+  }
+
+  @Override
+  public UserTrades getTradeHistory(TradeHistoryParams params) throws IOException {
+    return null;
+  }
+
+  @Override
+  public TradeHistoryParams createTradeHistoryParams() {
+    return null;
+  }
+
+  @Override
+  public OpenOrdersParams createOpenOrdersParams() {
+    return null;
+  }
+
+  @Override
+  public Collection<Order> getOrder(String... orderIds) throws IOException {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public Collection<Order> getOrder(OrderQueryParams... orderQueryParams) throws IOException {
+    return Collections.emptyList();
   }
 }
