e7e0681f0d5bc5232c2c2bfaef9624a29ed22aa3
Adam Walczak <adam@walczak.it>
2019-09-04 18:46:36 +0200

    [core] BigDecimalUtils

--- xchange-core/src/main/java/org/knowm/xchange/utils/Assert.java
+++ xchange-core/src/main/java/org/knowm/xchange/utils/Assert.java
@@ -64,4 +64,8 @@ public abstract class Assert {
       throw new IllegalArgumentException(message);
     }
   }
+
+  static void ass(boolean b) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 }
