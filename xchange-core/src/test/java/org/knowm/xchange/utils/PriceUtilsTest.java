46780bbe918f2122418b53db4f14ac143722e494
makarid <makarid@gmail.com>
2019-09-04 18:54:11 +0300

    Adding test for the PriceUtil.tickRound method

---
+++ xchange-core/src/test/java/org/knowm/xchange/utils/PriceUtilsTest.java
@@ -0,0 +1,16 @@
+package org.knowm.xchange.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class PriceUtilsTest {
+
+    @Test
+    public void tickSizeConvertionFormulaTest(){
+        BigDecimal priceAfterRounding = PriceUtils.tickRounding(new BigDecimal(3502.31111111),0.25);
+        System.out.println(priceAfterRounding);
+        Assert.assertEquals(priceAfterRounding,BigDecimal.valueOf(3502.25));
+    }
+}
