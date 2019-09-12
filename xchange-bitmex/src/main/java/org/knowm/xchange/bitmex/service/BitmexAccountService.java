9b41ffb4402d94dec61c9c44b0e21652a126a61d
makarid <makarid@gmail.com>
2019-09-12 09:53:04 +0300

    Delete unused import

--- xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/service/BitmexAccountService.java
+++ xchange-bitmex/src/main/java/org/knowm/xchange/bitmex/service/BitmexAccountService.java
@@ -11,7 +11,6 @@ import org.knowm.xchange.bitmex.BitmexAdapters;
 import org.knowm.xchange.bitmex.BitmexExchange;
 import org.knowm.xchange.bitmex.dto.account.BitmexAccount;
 import org.knowm.xchange.bitmex.dto.account.BitmexMarginAccount;
-import org.knowm.xchange.bitmex.dto.account.BitmexWallet;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.dto.account.*;
 import org.knowm.xchange.exceptions.ExchangeException;
