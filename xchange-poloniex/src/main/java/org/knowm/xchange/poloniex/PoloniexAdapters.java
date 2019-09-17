3493ad8f8079c295e5e2e81d788e4421189f912a
Jan Handrich <jan.handrich@firstcoin.de>
2019-09-17 09:33:08 +0200

    [poloniex] Breaking down Adapter method for better readability

--- xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/PoloniexAdapters.java
+++ xchange-poloniex/src/main/java/org/knowm/xchange/poloniex/PoloniexAdapters.java
@@ -286,77 +286,85 @@ public class PoloniexAdapters {
 
     final ArrayList<FundingRecord> fundingRecords = new ArrayList<>();
     for (PoloniexAdjustment a : poloFundings.getAdjustments()) {
-      FundingRecord.Type type = DEPOSIT;
-      // There seems to be a spelling error in the returning reason. In case that ever gets
-      // corrected, this will still pick it up.
-      if (a.getReason().toLowerCase().endsWith("aidrop")
-          || a.getReason().toLowerCase().endsWith("airdrop")) {
-        type = Type.AIRDROP;
-      }
-      // There could be other forms of adjustements, but it seems to be some kind of deposit.
-
-      fundingRecords.add(
-          new FundingRecord(
-              null,
-              a.getTimestamp(),
-              Currency.getInstance(a.getCurrency()),
-              a.getAmount(),
-              null,
-              null,
-              type,
-              FundingRecord.Status.resolveStatus(a.getStatus()),
-              null,
-              null,
-              a.getCategory()
-                  + ":"
-                  + a.getReason()
-                  + "\n"
-                  + a.getAdjustmentTitle()
-                  + "\n"
-                  + a.getAdjustmentDesc()
-                  + "\n"
-                  + a.getAdjustmentHelp()));
+      fundingRecords.add(adaptAdjustment(a));
     }
-
     for (PoloniexDeposit d : poloFundings.getDeposits()) {
-      fundingRecords.add(
-          new FundingRecord(
-              d.getAddress(),
-              d.getTimestamp(),
-              Currency.getInstance(d.getCurrency()),
-              d.getAmount(),
-              String.valueOf(d.getDepositNumber()),
-              d.getTxid(),
-              DEPOSIT,
-              FundingRecord.Status.resolveStatus(d.getStatus()),
-              null,
-              null,
-              d.getStatus()));
+      fundingRecords.add(adaptDeposit(d));
     }
     for (PoloniexWithdrawal w : poloFundings.getWithdrawals()) {
-      final String[] statusParts = w.getStatus().split(": *");
-      final String statusStr = statusParts[0];
-      final FundingRecord.Status status = FundingRecord.Status.resolveStatus(statusStr);
-      final String externalId = statusParts.length == 1 ? null : statusParts[1];
-
-      // Poloniex returns the fee as an absolute value, that behaviour differs from UserTrades
-      final BigDecimal feeAmount = w.getFee();
-
-      fundingRecords.add(
-          new FundingRecord(
-              w.getAddress(),
-              w.getTimestamp(),
-              Currency.getInstance(w.getCurrency()),
-              w.getAmount(),
-              String.valueOf(w.getWithdrawalNumber()),
-              externalId,
-              WITHDRAWAL,
-              status,
-              null,
-              feeAmount,
-              w.getStatus()));
+      fundingRecords.add(adaptWithdrawal(w));
     }
     return fundingRecords;
+  }
+
+  private static FundingRecord adaptAdjustment(PoloniexAdjustment a) {
+    FundingRecord.Type type = DEPOSIT;
+    // There seems to be a spelling error in the returning reason. In case that ever gets
+    // corrected, this will still pick it up.
+    if (a.getReason().toLowerCase().endsWith("aidrop")
+        || a.getReason().toLowerCase().endsWith("airdrop")) {
+      type = Type.AIRDROP;
+    }
+    // There could be other forms of adjustements, but it seems to be some kind of deposit.
+
+    return new FundingRecord(
+        null,
+        a.getTimestamp(),
+        Currency.getInstance(a.getCurrency()),
+        a.getAmount(),
+        null,
+        null,
+        type,
+        FundingRecord.Status.resolveStatus(a.getStatus()),
+        null,
+        null,
+        a.getCategory()
+            + ":"
+            + a.getReason()
+            + "\n"
+            + a.getAdjustmentTitle()
+            + "\n"
+            + a.getAdjustmentDesc()
+            + "\n"
+            + a.getAdjustmentHelp());
+  }
+
+  private static FundingRecord adaptDeposit(final PoloniexDeposit d) {
+    return new FundingRecord(
+        d.getAddress(),
+        d.getTimestamp(),
+        Currency.getInstance(d.getCurrency()),
+        d.getAmount(),
+        String.valueOf(d.getDepositNumber()),
+        d.getTxid(),
+        DEPOSIT,
+        FundingRecord.Status.resolveStatus(d.getStatus()),
+        null,
+        null,
+        d.getStatus());
+  }
+
+  private static FundingRecord adaptWithdrawal(final PoloniexWithdrawal w) {
+    final String[] statusParts = w.getStatus().split(": *");
+    final String statusStr = statusParts[0];
+    final FundingRecord.Status status = FundingRecord.Status.resolveStatus(statusStr);
+    final String externalId = statusParts.length == 1 ? null : statusParts[1];
+
+    // Poloniex returns the fee as an absolute value, that behaviour differs from UserTrades
+    final BigDecimal feeAmount = w.getFee();
+
+    return new FundingRecord(
+        w.getAddress(),
+        w.getTimestamp(),
+        Currency.getInstance(w.getCurrency()),
+        w.getAmount(),
+        String.valueOf(w.getWithdrawalNumber()),
+        externalId,
+        WITHDRAWAL,
+        status,
+        null,
+        feeAmount,
+        w.getStatus());
   }
 
   public static LimitOrder adaptUserTradesToOrderStatus(
