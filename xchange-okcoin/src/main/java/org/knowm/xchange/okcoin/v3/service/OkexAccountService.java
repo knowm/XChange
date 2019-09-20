5972e93355595eca1fe1eb8d8adc6898f832cf07
makarid <makarid@gmail.com>
2019-09-20 13:13:23 +0300

    Clean install annd running tests

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/service/OkexAccountService.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/service/OkexAccountService.java
@@ -54,14 +54,14 @@ public class OkexAccountService extends OkexAccountServiceRaw implements Account
     Collection<Balance> swapBalances =
         swapAccounts.stream().map(OkexAdaptersV3::convert).collect(Collectors.toList());*/
     return new AccountInfo(
-            Wallet.Builder.from(fundingBalances)
-                    .id("Funding")
-                    .features(Stream.of(Wallet.WalletFeature.FUNDING).collect(Collectors.toSet()))
-                    .build(),
-            Wallet.Builder.from(tradingBalances)
-                    .id("Trading")
-                    .features(Stream.of(Wallet.WalletFeature.TRADING).collect(Collectors.toSet()))
-                    .build());
+        Wallet.Builder.from(fundingBalances)
+            .id("Funding")
+            .features(Stream.of(Wallet.WalletFeature.FUNDING).collect(Collectors.toSet()))
+            .build(),
+        Wallet.Builder.from(tradingBalances)
+            .id("Trading")
+            .features(Stream.of(Wallet.WalletFeature.TRADING).collect(Collectors.toSet()))
+            .build());
   }
 
   @Override
