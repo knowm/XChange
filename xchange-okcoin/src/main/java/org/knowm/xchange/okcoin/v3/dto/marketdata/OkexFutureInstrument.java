798a8b8649a462c43e59b4fc95802d4ddaa627a0
Andre Reiter <a.reiter@web.de>
2019-11-01 11:45:43 +0100

    added new fields for future instruments, additionally use SnakeCaseStrategy for json naming

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/marketdata/OkexFutureInstrument.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/marketdata/OkexFutureInstrument.java
@@ -1,30 +1,28 @@
 package org.knowm.xchange.okcoin.v3.dto.marketdata;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
+@JsonNaming(SnakeCaseStrategy.class)
 public class OkexFutureInstrument {
 
   /** Contract ID, e.g. "BTC-USD-190621" */
-  @JsonProperty("instrument_id")
   private String instrumentId;
 
   /** Trade currency, such as BTC in BTC-USD-190621 */
-  @JsonProperty("underlying_index")
   private String underlyingIndex;
 
   /** Quote currency，such as USD in BTC-USD-190621 */
-  @JsonProperty("quote_currency")
   private String quoteCurrency;
 
   /** Order price accuracy, ie "0.01" */
-  @JsonProperty("tick_size")
   private BigDecimal tickSize;
 
   /** Contract value (USD), ie "100" */
-  @JsonProperty("contract_val")
   private BigDecimal contractVal;
 
   /** Listing date, ie "2019-06-07" */
@@ -34,9 +32,20 @@ public class OkexFutureInstrument {
   private String delivery;
 
   /** Order quantity accuracy, ie "1" */
-  @JsonProperty("trade_increment")
   private BigDecimal tradeIncrement;
 
   /** this_week, next_week, quarter */
   private String alias;
+
+  /** Underlying index，eg：BTC-USD */
+  private String underlying;
+  /** Transaction currency，eg:BTC in BTC-USD,BTC in BTC-USDT */
+  private String baseCurrency;
+  /** Settlement currency，eg:BTC */
+  private String settlementCurrency;
+  /** (true or false) ,inverse contract or not */
+  @JsonProperty("is_inverse")
+  private boolean inverse;
+  /** Contract denomination currency eg: USD，BTC，LTC，ETC , XRP, EOS */
+  private String contractValCurrency;
 }
