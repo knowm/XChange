86117fd30feb76e5f6a5d5dbb4c73e80e5f00bc0
Nikita Belenkiy <nikita@dev366.com>
2019-11-04 15:28:19 +0300

    #133 added default switch case

--- xchange-poloniex2/src/main/java/info/bitrich/xchangestream/poloniex2/dto/PoloniexWebSocketEventsTransaction.java
+++ xchange-poloniex2/src/main/java/info/bitrich/xchangestream/poloniex2/dto/PoloniexWebSocketEventsTransaction.java
@@ -82,6 +82,7 @@ public class PoloniexWebSocketEventsTransaction {
                     events.add(insertEvent);
                     break;
                 }
+                default: //Ignore
             }
         }
 
