3e6c25447b181afed3e3d57cbc0c69ffc30b285a
Mathijs Vogelzang <mathijs@gmail.com>
2019-11-11 14:04:08 +0100

    Fix handling of unknown 5-letter currencies in Kraken

--- xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenUtils.java
+++ xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenUtils.java
@@ -53,22 +53,13 @@ public class KrakenUtils {
     CurrencyPair pair = assetPairMap.get(currencyPairIn);
     if (pair == null) {
       // kraken can give short pairs back from open orders ?
-      if (currencyPairIn.length() == 6) {
-        Currency base = Currency.getInstance(currencyPairIn.substring(0, 3));
+      if (currencyPairIn.length() >= 6 && currencyPairIn.length() <= 8) {
+        int firstCurrencyLength = currencyPairIn.length() - 3;
+        Currency base = Currency.getInstance(currencyPairIn.substring(0, firstCurrencyLength));
         if (base.getCommonlyUsedCurrency() != null) {
           base = base.getCommonlyUsedCurrency();
         }
-        Currency counter = Currency.getInstance(currencyPairIn.substring(3, 6));
-        if (counter.getCommonlyUsedCurrency() != null) {
-          counter = counter.getCommonlyUsedCurrency();
-        }
-        pair = new CurrencyPair(base, counter);
-      } else if (currencyPairIn.length() == 7) {
-        Currency base = Currency.getInstance(currencyPairIn.substring(0, 4));
-        if (base.getCommonlyUsedCurrency() != null) {
-          base = base.getCommonlyUsedCurrency();
-        }
-        Currency counter = Currency.getInstance(currencyPairIn.substring(4, 7));
+        Currency counter = Currency.getInstance(currencyPairIn.substring(firstCurrencyLength, currencyPairIn.length()));
         if (counter.getCommonlyUsedCurrency() != null) {
           counter = counter.getCommonlyUsedCurrency();
         }
