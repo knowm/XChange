989dc42a702694bb034232a3ee37d96badcc5cb1
Alexey Kopylov <tonn.tamerlan@gmail.com>
2019-12-18 23:58:59 +0200

    use lombok ToString

--- xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v2/dto/marketdata/BitfinexTicker.java
+++ xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v2/dto/marketdata/BitfinexTicker.java
@@ -5,11 +5,13 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @JsonFormat(shape = JsonFormat.Shape.ARRAY)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
 public class BitfinexTicker {
 
   private String symbol;
@@ -29,43 +31,4 @@ public class BitfinexTicker {
   private Object placeHolder0;
   private Object placeHolder1;
   private BigDecimal frrAmountAvailable;
-
-  @Override
-  public String toString() {
-    return "BitfinexTicker{"
-        + "symbol='"
-        + symbol
-        + '\''
-        + ", frr="
-        + frr
-        + ", bid="
-        + bid
-        + ", bidPeriod="
-        + bidPeriod
-        + ", bidSize="
-        + bidSize
-        + ", askPeriod="
-        + askPeriod
-        + ", askSize="
-        + askSize
-        + ", dailyChange="
-        + dailyChange
-        + ", dailyChangePerc="
-        + dailyChangePerc
-        + ", lastPrice="
-        + lastPrice
-        + ", volume="
-        + volume
-        + ", high="
-        + high
-        + ", low="
-        + low
-        + ", placeHolder0="
-        + placeHolder0
-        + ", placeHolder1="
-        + placeHolder1
-        + ", frrAmountAvailable="
-        + frrAmountAvailable
-        + '}';
-  }
 }
