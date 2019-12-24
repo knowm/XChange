a1c0ad98005358758770febffc0d884788296ad4
Bartosz Milczarek <bartosz@milczarek.it>
2019-12-24 09:19:35 +0100

    Removed GDAX references (replaced with CoinbasePro)

--- xchange-examples/src/main/java/org/knowm/xchange/examples/coinbasepro/CoinbaseProHistoricalCandlesDemo.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/coinbasepro/CoinbaseProHistoricalCandlesDemo.java
@@ -1,7 +1,5 @@
 package org.knowm.xchange.examples.coinbasepro;
 
-import java.io.IOException;
-import java.util.Arrays;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.coinbasepro.CoinbaseProExchange;
@@ -9,11 +7,14 @@ import org.knowm.xchange.coinbasepro.dto.marketdata.CoinbaseProCandle;
 import org.knowm.xchange.coinbasepro.service.CoinbaseProMarketDataService;
 import org.knowm.xchange.currency.CurrencyPair;
 
-public class CoinbaseproHistoricalCandlesDemo {
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CoinbaseProHistoricalCandlesDemo {
 
   public static void main(String[] args) throws IOException {
-    Exchange gdax = ExchangeFactory.INSTANCE.createExchange(CoinbaseProExchange.class.getName());
-    CoinbaseProMarketDataService mds = (CoinbaseProMarketDataService) gdax.getMarketDataService();
+    Exchange coinbasePro = ExchangeFactory.INSTANCE.createExchange(CoinbaseProExchange.class.getName());
+    CoinbaseProMarketDataService mds = (CoinbaseProMarketDataService) coinbasePro.getMarketDataService();
     CoinbaseProCandle[] candles =
         mds.getCoinbaseProHistoricalCandles(
             CurrencyPair.BTC_USD, "2018-02-01T00:00:00Z", "2018-02-01T00:10:00Z", "60");
