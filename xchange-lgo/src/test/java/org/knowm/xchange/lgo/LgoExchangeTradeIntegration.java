735c7dc752cecad94333dab130598b683fdcf1e4
Jean-Baptiste Dusseaut <157962+BodySplash@users.noreply.github.com>
2019-12-30 15:02:47 +0100

    feature: encrypting orders is now an exchange specific parameter.

--- xchange-lgo/src/test/java/org/knowm/xchange/lgo/LgoExchangeTradeIntegration.java
+++ xchange-lgo/src/test/java/org/knowm/xchange/lgo/LgoExchangeTradeIntegration.java
@@ -1,26 +1,30 @@
 package org.knowm.xchange.lgo;
 
-import org.apache.commons.io.IOUtils;
-import org.junit.*;
-import org.knowm.xchange.*;
-import org.knowm.xchange.currency.CurrencyPair;
-import org.knowm.xchange.dto.Order.OrderType;
-import org.knowm.xchange.dto.trade.*;
-import org.knowm.xchange.lgo.service.LgoTradeService;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-
-import static org.assertj.core.api.Assertions.*;
+import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order.OrderType;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
+import org.knowm.xchange.dto.trade.UserTrades;
+import org.knowm.xchange.lgo.service.LgoTradeService;
 
 @Ignore
 public class LgoExchangeTradeIntegration {
 
   @Test
   public void fetchLastTrades() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
+    LgoExchange lgoExchange = exchangeWithCredentials(false);
     LgoTradeService tradeService = lgoExchange.getTradeService();
     UserTrades tradeHistory = tradeService.getTradeHistory(tradeService.createTradeHistoryParams());
     assertThat(tradeHistory.getUserTrades()).isNotEmpty();
@@ -29,46 +33,7 @@ public class LgoExchangeTradeIntegration {
 
   @Test
   public void placeLimitOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
-    LgoTradeService tradeService = lgoExchange.getTradeService();
-
-    String orderId =
-        tradeService.placeEncryptedLimitOrder(
-            new LimitOrder(
-                OrderType.ASK,
-                new BigDecimal("2"),
-                CurrencyPair.BTC_USD,
-                null,
-                new Date(),
-                new BigDecimal("6000")));
-
-    System.out.println(orderId);
-  }
-
-  @Test
-  public void placeMarketOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
-    LgoTradeService tradeService = lgoExchange.getTradeService();
-
-    String orderId =
-        tradeService.placeEncryptedMarketOrder(
-            new MarketOrder(
-                OrderType.ASK, new BigDecimal("2"), CurrencyPair.BTC_USD, null, new Date()));
-
-    System.out.println(orderId);
-  }
-
-  @Test
-  public void cancelOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
-    LgoTradeService tradeService = lgoExchange.getTradeService();
-
-    tradeService.placeEncryptedCancelOrder("156941460160700001");
-  }
-
-  @Test
-  public void placeUnencryptedLimitOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
+    LgoExchange lgoExchange = exchangeWithCredentials(false);
     LgoTradeService tradeService = lgoExchange.getTradeService();
 
     String orderId =
@@ -85,33 +50,73 @@ public class LgoExchangeTradeIntegration {
   }
 
   @Test
-  public void placeUnencryptedMarketOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
+  public void placeMarketOrder() throws IOException {
+    LgoExchange lgoExchange = exchangeWithCredentials(false);
     LgoTradeService tradeService = lgoExchange.getTradeService();
 
     String orderId =
         tradeService.placeMarketOrder(
             new MarketOrder(
-                OrderType.ASK, new BigDecimal("200"), CurrencyPair.BTC_USD, null, new Date()));
+                OrderType.ASK, new BigDecimal("2"), CurrencyPair.BTC_USD, null, new Date()));
 
     System.out.println(orderId);
   }
 
   @Test
-  public void placeUnencryptedCancelOrder() throws IOException {
-    LgoExchange lgoExchange = exchangeWithCredentials();
+  public void cancelOrder() throws IOException {
+    LgoExchange lgoExchange = exchangeWithCredentials(false);
     LgoTradeService tradeService = lgoExchange.getTradeService();
 
-    tradeService.cancelOrder("156940341166500001");
+    tradeService.cancelOrder("156941460160700001");
+  }
+
+  @Test
+  public void placeEncryptedLimitOrder() throws IOException {
+    LgoExchange lgoExchange = exchangeWithCredentials(true);
+    LgoTradeService tradeService = lgoExchange.getTradeService();
+
+    String orderId =
+        tradeService.placeLimitOrder(
+            new LimitOrder(
+                OrderType.ASK,
+                new BigDecimal("2"),
+                CurrencyPair.BTC_USD,
+                null,
+                new Date(),
+                new BigDecimal("6000")));
+
+    System.out.println(orderId);
+  }
+
+  @Test
+  public void placeEncryptedMarketOrder() throws IOException {
+    LgoExchange lgoExchange = exchangeWithCredentials(true);
+    LgoTradeService tradeService = lgoExchange.getTradeService();
+
+    String orderId =
+        tradeService.placeMarketOrder(
+            new MarketOrder(
+                OrderType.ASK, new BigDecimal("2"), CurrencyPair.BTC_USD, null, new Date()));
+
+    System.out.println(orderId);
+  }
+
+  @Test
+  public void placeEncryptedCancelOrder() throws IOException {
+    LgoExchange lgoExchange = exchangeWithCredentials(true);
+    LgoTradeService tradeService = lgoExchange.getTradeService();
+
+    tradeService.cancelOrder("157771427343700001");
   }
 
   // api key and secret key are expected to be in test resources under
   // integration directory
   // this directory is added to .gitignore to avoid committing a real usable key
-  protected LgoExchange exchangeWithCredentials() throws IOException {
+  protected LgoExchange exchangeWithCredentials(boolean shouldEncryptOrders) throws IOException {
     ExchangeSpecification spec = LgoEnv.sandbox();
     spec.setSecretKey(readResource("/integration/private_key.pem"));
     spec.setApiKey(readResource("/integration/api_key.txt"));
+    spec.setExchangeSpecificParametersItem(LgoEnv.SHOULD_ENCRYPT_ORDERS, shouldEncryptOrders);
 
     return (LgoExchange) ExchangeFactory.INSTANCE.createExchange(spec);
   }
