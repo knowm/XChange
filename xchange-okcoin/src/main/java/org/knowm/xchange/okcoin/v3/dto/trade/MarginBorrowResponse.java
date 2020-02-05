8b06852bf104ea285b8169a1c7a68b4079d84dfc
MaximFeofilov <MaximFeofilov@gmail.com>
2020-02-05 14:29:20 +0300

    [OkCoin] Add  margin api support

---
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/trade/MarginBorrowResponse.java
@@ -0,0 +1,18 @@
+package org.knowm.xchange.okcoin.v3.dto.trade;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MarginBorrowResponse extends OkexResponse {
+
+    @JsonProperty("borrow_id")
+    private String borrowId;
+
+    @JsonProperty("client_oid")
+    private String clientOid;
+}
