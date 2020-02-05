b3862500c5f86889f12eba7224cf2725e99a959b
MaximFeofilov <MaximFeofilov@gmail.com>
2020-02-05 14:29:20 +0300

    [OkCoin] Add  margin api support

---
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/trade/MarginRepaymentRequest.java
@@ -0,0 +1,24 @@
+package org.knowm.xchange.okcoin.v3.dto.trade;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MarginRepaymentRequest {
+
+    @JsonProperty("borrow_id")
+    private String borrowId;
+
+    @JsonProperty("instrument_id")
+    private String instrument_id;
+
+    @JsonProperty("currency")
+    private String currency;
+
+    @JsonProperty("amount")
+    private String amount;
+}
---
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/trade/MarginRepaymentResponse.java
@@ -0,0 +1,18 @@
+package org.knowm.xchange.okcoin.v3.dto.trade;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MarginRepaymentResponse extends OkexResponse {
+
+    @JsonProperty("repayment_id")
+    private String repaymentId;
+
+    @JsonProperty("client_oid")
+    private String clientOid;
+}
