dd75e3fe0a72f7aec74e29ae7a51479d849fa102
MaximFeofilov <MaximFeofilov@gmail.com>
2020-02-06 11:35:18 +0300

    [OkCoin] Add get account info to account raw service

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/service/OkexAccountServiceRaw.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/service/OkexAccountServiceRaw.java
@@ -4,13 +4,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import org.knowm.xchange.okcoin.OkexExchangeV3;
-import org.knowm.xchange.okcoin.v3.dto.account.FuturesPosition;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexDepositRecord;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexFundingAccountRecord;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexSpotAccountRecord;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexWithdrawalRecord;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexWithdrawalRequest;
-import org.knowm.xchange.okcoin.v3.dto.account.OkexWithdrawalResponse;
+import org.knowm.xchange.okcoin.v3.dto.account.*;
 import org.knowm.xchange.okcoin.v3.dto.trade.FundsTransferRequest;
 import org.knowm.xchange.okcoin.v3.dto.trade.FundsTransferResponse;
 import org.knowm.xchange.okcoin.v3.dto.trade.FuturesAccountsByCurrencyResponse;
@@ -32,6 +26,14 @@ public class OkexAccountServiceRaw extends OkexBaseService {
 
   public List<OkexSpotAccountRecord> spotTradingAccount() throws IOException {
     return okex.spotTradingAccount(apikey, digest, timestamp(), passphrase);
+  }
+
+  public MarginAccountResponse marginAccounts() throws IOException {
+    return okex.marginAccounts(apikey, digest, timestamp(), passphrase);
+  }
+
+  public List<MarginAccountSettingsRecord> marginAccountsSettings() throws IOException {
+    return okex.marginAccountsSettings(apikey, digest, timestamp(), passphrase);
   }
 
   public FundsTransferResponse fundsTransfer(
