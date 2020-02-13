dee9570ab42197b0796c86bd9f530f4055242c67
Matej Spiller Muys <matej.spiller@gmail.com>
2020-02-13 14:23:24 +0100

    support for bittrex v3 api

---
+++ xchange-bittrex/src/main/java/org/knowm/xchange/bittrex/BittrexAuthenticatedV3.java
@@ -0,0 +1,26 @@
+package org.knowm.xchange.bittrex;
+
+import org.knowm.xchange.bittrex.dto.account.BittrexAccountVolume;
+import si.mazi.rescu.ParamsDigest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+@Path("v3")
+@Produces(MediaType.APPLICATION_JSON)
+public interface BittrexAuthenticatedV3 {
+
+  @GET
+  @Path("account/volume")
+  BittrexAccountVolume getAccountVolume(
+          @HeaderParam("Api-Key") String apiKey,
+          @HeaderParam("Api-Timestamp") Long timestamp,
+          @HeaderParam("Api-Content-Hash") ParamsDigest hash,
+          @HeaderParam("Api-Signature") ParamsDigest signature
+  ) throws IOException;
+
+}
