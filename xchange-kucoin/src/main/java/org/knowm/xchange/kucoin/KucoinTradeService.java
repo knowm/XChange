c7cac77cd43d7c22e22b82c13c8d0136202f592e
Josua Troesch <josuat@users.noreply.github.com>
2020-02-27 11:29:27 +0800

    [KuCoin] Check & adjust time span before trade history API query

--- xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinTradeService.java
+++ xchange-kucoin/src/main/java/org/knowm/xchange/kucoin/KucoinTradeService.java
@@ -1,12 +1,10 @@
 package org.knowm.xchange.kucoin;
 
-import static java.util.stream.Collectors.toCollection;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
-
 import java.io.IOException;
+import java.time.Instant;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,7 +12,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.Order.IOrderFlags;
 import org.knowm.xchange.dto.marketdata.Trades.TradeSortType;
@@ -37,7 +34,8 @@ public class KucoinTradeService extends KucoinTradeServiceRaw implements TradeSe
   private static final long cutoffHistOrdersMillis =
       Date.from(java.time.LocalDate.of(2019, 2, 18).atStartOfDay(ZoneId.of("UTC+8")).toInstant())
           .getTime();
-  private static final long oneWeekMillis = 7 * 24 * 60 * 60 * 1000;
+  // Although API doc says 7 days max timespan, KuCoin actually allows (almost) 8 days :)
+  private static final long oneWeekMillis = (8 * 24 * 60 * 60 * 1000) - 1000;
 
   KucoinTradeService(KucoinExchange exchange) {
     super(exchange);
@@ -62,42 +60,70 @@ public class KucoinTradeService extends KucoinTradeServiceRaw implements TradeSe
   @Override
   public UserTrades getTradeHistory(TradeHistoryParams genericParams) throws IOException {
     String symbol = null;
-    if (genericParams != null) {
-      // TODO Currency pair is actually optional on KuCoin API, i.e. it should be possible to
-      // proceed with genericParams = null
-      Preconditions.checkArgument(
-          genericParams instanceof TradeHistoryParamCurrencyPair,
-          "Only currency pair parameters are currently supported.");
-      TradeHistoryParamCurrencyPair params = (TradeHistoryParamCurrencyPair) genericParams;
-      symbol = KucoinAdapters.adaptCurrencyPair(params.getCurrencyPair());
-    }
-
-    List<UserTrade> userTrades = new ArrayList<>();
     Long startTime = null;
     Long endTime = null;
-    if (genericParams instanceof TradeHistoryParamsTimeSpan) {
-      if (((TradeHistoryParamsTimeSpan) genericParams).getStartTime() != null) {
-        startTime = ((TradeHistoryParamsTimeSpan) genericParams).getStartTime().getTime();
-      }
-      if (((TradeHistoryParamsTimeSpan) genericParams).getEndTime() != null) {
-        endTime = ((TradeHistoryParamsTimeSpan) genericParams).getEndTime().getTime();
-      }
-      /*
-        KuCoin restricts time spans to 7 days on new fills API but not hist-orders. I.e. you could request a whole year of
-        trades before 2019-2-18 but only 7 days of trades after that date.
-        It would be nice to check and enforce this rule here but that could end up being confusing for users.
-      */
-      if (startTime == null && endTime == null) {
-        startTime = new Date().getTime() - oneWeekMillis;
-        logger.warn(
-            "No start or end time for trade history request specified, defaulting to last 7 days!");
+
+    if (genericParams != null) {
+      TradeHistoryParamCurrencyPair params = (TradeHistoryParamCurrencyPair) genericParams;
+      symbol = KucoinAdapters.adaptCurrencyPair(params.getCurrencyPair());
+
+      if (genericParams instanceof TradeHistoryParamsTimeSpan) {
+        if (((TradeHistoryParamsTimeSpan) genericParams).getStartTime() != null) {
+          startTime = ((TradeHistoryParamsTimeSpan) genericParams).getStartTime().getTime();
+        }
+        if (((TradeHistoryParamsTimeSpan) genericParams).getEndTime() != null) {
+          endTime = ((TradeHistoryParamsTimeSpan) genericParams).getEndTime().getTime();
+        }
+        /*
+          KuCoin restricts time spans to 7 days (as per API docs) on new fills API but not hist-orders. I.e. you could request
+           a whole year of trades before 2019-2-18 but only 7 days of trades after that date.
+        */
+        if (startTime != null) {
+          if (endTime == null) {
+            if (startTime < cutoffHistOrdersMillis) {
+              endTime = Math.max(cutoffHistOrdersMillis - 1, startTime + oneWeekMillis);
+            } else {
+              endTime = startTime + oneWeekMillis;
+            }
+            logger.warn(
+                "End time not specified, adjusted to the following time span {} - {}",
+                Date.from(Instant.ofEpochMilli(startTime)),
+                Date.from(Instant.ofEpochMilli(endTime)));
+          } else if (endTime >= cutoffHistOrdersMillis && endTime - startTime > oneWeekMillis) {
+            endTime = startTime + oneWeekMillis;
+            logger.warn(
+                "End time more than one week from start time, adjusted to the following time span {} - {}",
+                Date.from(Instant.ofEpochMilli(startTime)),
+                Date.from(Instant.ofEpochMilli(endTime)));
+          }
+        }
+
+        if (endTime != null && startTime == null) {
+          if (endTime > cutoffHistOrdersMillis) {
+            startTime = endTime - oneWeekMillis;
+            logger.warn(
+                "Start time not specified, adjusted to the following time span {} - {}",
+                Date.from(Instant.ofEpochMilli(startTime)),
+                Date.from(Instant.ofEpochMilli(endTime)));
+          }
+        }
+        if (startTime == null && endTime == null) {
+          endTime = new Date().getTime();
+          startTime = endTime - oneWeekMillis;
+          logger.warn(
+              "No start or end time for trade history request specified, adjusted to the following time span {} - {}",
+              Date.from(Instant.ofEpochMilli(startTime)),
+              Date.from(Instant.ofEpochMilli(endTime)));
+        }
       }
     }
+
     // TODO getKucoinFills(...).getItems will just get the current items and silently ignore any
     // other pages if present!
     Long startTimeSecs = startTime != null ? startTime / 1000 : null;
     Long endTimeSecs = endTime != null ? endTime / 1000 : null;
 
+    List<UserTrade> userTrades;
     if (startTime != null && startTime > cutoffHistOrdersMillis) {
       userTrades =
           getKucoinFills(symbol, 1, TRADE_HISTORIES_TO_FETCH, startTime, endTime).getItems()
@@ -113,14 +139,14 @@ public class KucoinTradeService extends KucoinTradeServiceRaw implements TradeSe
     } else {
       userTrades =
           Stream.concat(
-              getKucoinHistOrders(
-                  symbol, 1, TRADE_HISTORIES_TO_FETCH, startTimeSecs, endTimeSecs)
-                  .getItems().stream()
-                  .map(KucoinAdapters::adaptHistOrder),
-              getKucoinFills(symbol, 1, TRADE_HISTORIES_TO_FETCH, startTime, endTime).getItems()
-                  .stream()
-                  .map(KucoinAdapters::adaptUserTrade))
-              .collect(toCollection(ArrayList::new));
+                  getKucoinHistOrders(
+                          symbol, 1, TRADE_HISTORIES_TO_FETCH, startTimeSecs, endTimeSecs)
+                      .getItems().stream()
+                      .map(KucoinAdapters::adaptHistOrder),
+                  getKucoinFills(symbol, 1, TRADE_HISTORIES_TO_FETCH, startTime, endTime).getItems()
+                      .stream()
+                      .map(KucoinAdapters::adaptUserTrade))
+              .collect(Collectors.toList());
     }
 
     return new UserTrades(userTrades, TradeSortType.SortByTimestamp);
@@ -172,7 +198,7 @@ public class KucoinTradeService extends KucoinTradeServiceRaw implements TradeSe
     Builder<Order> hiddenOrders = ImmutableList.builder();
     orders.stream()
         .map(KucoinAdapters::adaptOrder)
-        .filter(o -> params == null ? true : params.accept(o))
+        .filter(o -> params == null || params.accept(o))
         .forEach(
             o -> {
               if (o instanceof LimitOrder) {
@@ -184,9 +210,7 @@ public class KucoinTradeService extends KucoinTradeServiceRaw implements TradeSe
     return new OpenOrders(openOrders.build(), hiddenOrders.build());
   }
 
-  /**
-   * TODO same as Binance. Should be merged into generic API
-   */
+  /** TODO same as Binance. Should be merged into generic API */
   public interface KucoinOrderFlags extends IOrderFlags {
     String getClientId();
   }
