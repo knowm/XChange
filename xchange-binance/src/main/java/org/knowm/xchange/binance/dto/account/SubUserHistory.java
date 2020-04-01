0ed8e514687cf12aadf40fde0d9bccff5cf5238e
trix <trixhome@gmail.com>
2020-04-02 00:57:51 +0200

    [Binance] PR Review #3463 Add Sub Account transfert history

--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/SubUserHistory.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/SubUserHistory.java
@@ -1,7 +1,9 @@
 package org.knowm.xchange.binance.dto.account;
 
 import java.math.BigDecimal;
+import lombok.Data;
 
+@Data
 public final class SubUserHistory {
 
   private String counterParty;
@@ -10,52 +12,4 @@ public final class SubUserHistory {
   private String asset;
   private BigDecimal qty;
   private long time;
-
-  public String getCounterParty() {
-    return counterParty;
-  }
-
-  public void setCounterParty(String counterParty) {
-    this.counterParty = counterParty;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public Integer getType() {
-    return type;
-  }
-
-  public void setType(Integer type) {
-    this.type = type;
-  }
-
-  public String getAsset() {
-    return asset;
-  }
-
-  public void setAsset(String asset) {
-    this.asset = asset;
-  }
-
-  public BigDecimal getQty() {
-    return qty;
-  }
-
-  public void setQty(BigDecimal qty) {
-    this.qty = qty;
-  }
-
-  public long getTime() {
-    return time;
-  }
-
-  public void setTime(long time) {
-    this.time = time;
-  }
 }
--- xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/TransfertHistoryResponse.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/dto/account/TransfertHistoryResponse.java
@@ -36,45 +36,5 @@ public final class TransfertHistoryResponse
     private String asset;
     private BigDecimal qty;
     private long time;
-
-    public String getFrom() {
-      return from;
-    }
-
-    public void setFrom(String from) {
-      this.from = from;
-    }
-
-    public String getTo() {
-      return to;
-    }
-
-    public void setTo(String to) {
-      this.to = to;
-    }
-
-    public String getAsset() {
-      return asset;
-    }
-
-    public void setAsset(String asset) {
-      this.asset = asset;
-    }
-
-    public BigDecimal getQty() {
-      return qty;
-    }
-
-    public void setQty(BigDecimal qty) {
-      this.qty = qty;
-    }
-
-    public long getTime() {
-      return time;
-    }
-
-    public void setTime(long time) {
-      this.time = time;
-    }
   }
 }
