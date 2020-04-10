bc6ebc1b2f3c4c5a4560224d407cbc29b8b78626
Adam Walczak <adam.walczak@ewejsciowki.pl>
2020-04-10 13:24:59 +0200

    formatting fix

--- xchange-gemini/src/main/java/org/knowm/xchange/gemini/v1/dto/trade/GeminiLimitOrder.java
+++ xchange-gemini/src/main/java/org/knowm/xchange/gemini/v1/dto/trade/GeminiLimitOrder.java
@@ -2,7 +2,6 @@ package org.knowm.xchange.gemini.v1.dto.trade;
 
 import java.math.BigDecimal;
 import java.util.Date;
-
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.trade.LimitOrder;
 
