20cc7099d5ccd7b1d31c85837a868c72a6785e77
Keshav Sirohi <46991789+KeshavSirohi@users.noreply.github.com>
2020-05-25 13:10:26 +0530

    Order Book implementation
    Order Book implementation changes as per review.

--- xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/marketdata/OkexOrderBook.java
+++ xchange-okcoin/src/main/java/org/knowm/xchange/okcoin/v3/dto/marketdata/OkexOrderBook.java
@@ -1,51 +1,20 @@
 package org.knowm.xchange.okcoin.v3.dto.marketdata;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.math.BigDecimal;
 import java.util.Arrays;
+import lombok.Data;
 
+@Data
 public class OkexOrderBook {
 
-  private final String timestamp;
-  private final OkexOrderBookEntry[] bids;
-  private final OkexOrderBookEntry[] asks;
+  @JsonProperty("timestamp")
+  private String timestamp;
 
-  public OkexOrderBook(
-      @JsonProperty("timestamp") String timestamp,
-      @JsonProperty("bids") Object[][] bids,
-      @JsonProperty("asks") Object[][] asks) {
+  @JsonProperty("bids")
+  private OkexOrderBookEntry[] bids;
 
-    this.timestamp = timestamp;
-
-    if (bids != null && bids.length > 0) {
-      this.bids = new OkexOrderBookEntry[bids.length];
-      for (int i = 0; i < bids.length; i++) {
-        this.bids[i] = convertToBookEntry(bids[i]);
-      }
-    } else {
-      this.bids = null;
-    }
-
-    if (asks != null && asks.length > 0) {
-      this.asks = new OkexOrderBookEntry[asks.length];
-      for (int i = 0; i < asks.length; i++) {
-        this.asks[i] = convertToBookEntry(asks[i]);
-      }
-    } else {
-      this.asks = null;
-    }
-  }
-
-  private static OkexOrderBookEntry convertToBookEntry(Object[] dataObject) {
-
-    if (dataObject != null && dataObject.length == 3) {
-      BigDecimal price = new BigDecimal((String) dataObject[0]);
-      BigDecimal volume = new BigDecimal((String) dataObject[1]);
-      String numberOfOrders = (String) dataObject[2];
-      return new OkexOrderBookEntry(price, volume, numberOfOrders);
-    }
-    return null;
-  }
+  @JsonProperty("asks")
+  private OkexOrderBookEntry[] asks;
 
   public String getTimestamp() {
 
