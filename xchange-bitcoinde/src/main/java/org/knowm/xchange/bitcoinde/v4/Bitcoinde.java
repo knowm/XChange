ce91a66ba16951bfda69d5afa7bc6599f68ba7b5
Jan Handrich <jan.handrich@firstcoin.de>
2020-06-28 20:02:53 +0200

    [bitcoinde] Support for API V4 initial commit

---
+++ xchange-bitcoinde/src/main/java/org/knowm/xchange/bitcoinde/v4/Bitcoinde.java
@@ -0,0 +1,11 @@
+package org.knowm.xchange.bitcoinde.v4;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("v4")
+@Produces(MediaType.APPLICATION_JSON)
+public interface Bitcoinde {
+    
+}
---
+++ xchange-bitcoinde/src/main/java/org/knowm/xchange/bitcoinde/v4/BitcoindeAdapters.java
@@ -0,0 +1,9 @@
+package org.knowm.xchange.bitcoinde.v4;
+
+public final class BitcoindeAdapters {
+
+  /** Private constructor. */
+  private BitcoindeAdapters() {}
+
+  
+}
