a4c4c52621d212f85d05f9e6b690d84867fa92fb
cymp <nguyen.cyril@gmail.com>
2020-07-01 18:04:45 +0200

    review

--- xchange-bittrexV3/src/main/java/org/knowm/xchange/bittrex/BittrexUtils.java
+++ xchange-bittrexV3/src/main/java/org/knowm/xchange/bittrex/BittrexUtils.java
@@ -2,12 +2,10 @@ package org.knowm.xchange.bittrex;
 
 import org.knowm.xchange.currency.CurrencyPair;
 
-/** A central place for shared Bittrex properties */
+/** A central place for shared Bittrex utility operations */
 public final class BittrexUtils {
-  protected static final String MARKET_NAME_SEPARATOR = "-";
 
-  /** Utility class */
-  private BittrexUtils() {}
+  public static final String MARKET_NAME_SEPARATOR = "-";
 
   public static String toPairString(CurrencyPair currencyPair) {
     if (currencyPair == null) return null;
@@ -19,7 +17,13 @@ public final class BittrexUtils {
   public static CurrencyPair toCurrencyPair(String pairString) {
     if (pairString == null) return null;
     String[] pairStringSplit = pairString.split(MARKET_NAME_SEPARATOR);
-    if (pairStringSplit.length < 2) return null;
+    if (pairStringSplit.length != 2) return null;
     return new CurrencyPair(pairStringSplit[0], pairStringSplit[1]);
   }
+
+  /** Utility class */
+  private BittrexUtils() {
+    throw new AssertionError();
+  }
+
 }
