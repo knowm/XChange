96489ae63594841855d78f5b7c037e90afba02d3
Spyros Stylianou <spyrosstylianou@Spyross-MacBook-Pro.local>
2020-07-19 10:39:31 +0100

    [BitMex Stream]: Remove nonce from BitMex stream as its now deprecated

--- xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingService.java
+++ xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingService.java
@@ -178,7 +178,6 @@ public class BitmexStreamingService extends JsonNettyStreamingService {
     String stringToDigest = "GET/realtime" + expires;
     String signature = bitmexDigester.digestString(stringToDigest);
 
-    customHeaders.add("api-nonce", expires);
     customHeaders.add("api-key", apiKey);
     customHeaders.add("api-signature", signature);
     return customHeaders;
