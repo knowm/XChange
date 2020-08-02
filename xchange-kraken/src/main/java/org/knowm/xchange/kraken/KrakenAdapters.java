d90382c9f15a6dcc736ca8ef3ae5a28ea9f53329
Matej Spiller Muys <matej.spiller@gmail.com>
2020-08-02 12:39:16 +0200

    support for minimum order volume

--- xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenAdapters.java
+++ xchange-kraken/src/main/java/org/knowm/xchange/kraken/KrakenAdapters.java
@@ -413,14 +413,14 @@ public class KrakenAdapters {
     if (OriginalMeta != null) {
       return new CurrencyPairMetaData(
           krakenPair.getFees().get(0).getPercentFee().divide(new BigDecimal(100)),
-          krakenPair.getMinimumOrderVolume() != null ? krakenPair.getMinimumOrderVolume() : OriginalMeta.getMinimumAmount(),
+          krakenPair.getOrderMin() != null ? krakenPair.getOrderMin() : OriginalMeta.getMinimumAmount(),
           OriginalMeta.getMaximumAmount(),
           krakenPair.getPairScale(),
           adaptFeeTiers(krakenPair.getFees_maker(), krakenPair.getFees()));
     } else {
       return new CurrencyPairMetaData(
           krakenPair.getFees().get(0).getPercentFee().divide(new BigDecimal(100)),
-          krakenPair.getMinimumOrderVolume(),
+          krakenPair.getOrderMin(),
           null,
           krakenPair.getPairScale(),
           adaptFeeTiers(krakenPair.getFees_maker(), krakenPair.getFees()));
