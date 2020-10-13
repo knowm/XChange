96d7260690749f0940820fa4f2e458ef229209c1
daz <plm@dazito.com>
2020-10-13 22:35:37 +0200

    Fix NPE, maxDepth default to zero instead of null when args is empty

--- xchange-stream-gemini/src/main/java/info/bitrich/xchangestream/gemini/GeminiStreamingMarketDataService.java
+++ xchange-stream-gemini/src/main/java/info/bitrich/xchangestream/gemini/GeminiStreamingMarketDataService.java
@@ -4,6 +4,7 @@ import static org.knowm.xchange.gemini.v1.GeminiAdapters.adaptTrades;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.MoreObjects;
 import info.bitrich.xchangestream.core.StreamingMarketDataService;
 import info.bitrich.xchangestream.gemini.dto.GeminiLimitOrder;
 import info.bitrich.xchangestream.gemini.dto.GeminiOrderbook;
@@ -57,7 +58,7 @@ public class GeminiStreamingMarketDataService implements StreamingMarketDataServ
   @Override
   public Observable<OrderBook> getOrderBook(CurrencyPair currencyPair, Object... args) {
 
-    int maxDepth = args.length > 0 ? (int) args[0] : 0;
+    int maxDepth = (int) MoreObjects.firstNonNull(args.length > 0 ? args[0] : null, 0);
 
     Observable<GeminiOrderbook> subscribedOrderbookSnapshot =
         service
