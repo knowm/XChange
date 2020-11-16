83f8bbad28406933b2953322dfaeec95d0cc008e
Aaron Jang <jljc@naver.com>
2020-11-17 00:45:46 +0900

    [Upbit] Implements getTickers and metadata

--- xchange-upbit/src/main/java/org/knowm/xchange/upbit/service/UpbitMarketDataService.java
+++ xchange-upbit/src/main/java/org/knowm/xchange/upbit/service/UpbitMarketDataService.java
@@ -1,15 +1,19 @@
 package org.knowm.xchange.upbit.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trades;
+import org.knowm.xchange.dto.meta.ExchangeMetaData;
 import org.knowm.xchange.exceptions.ExchangeException;
 import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
 import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
 import org.knowm.xchange.service.marketdata.MarketDataService;
+import org.knowm.xchange.service.marketdata.params.Params;
 import org.knowm.xchange.upbit.UpbitAdapters;
 
 /** @author interwater */
@@ -23,6 +27,13 @@ public class UpbitMarketDataService extends UpbitMarketDataServiceRaw implements
   public UpbitMarketDataService(Exchange exchange) {
 
     super(exchange);
+  }
+
+  @Override
+  public List<Ticker> getTickers(Params params) throws IOException {
+    final List<CurrencyPair> currencyPairs =
+        new ArrayList<>(exchange.getExchangeMetaData().getCurrencyPairs().keySet());
+    return UpbitAdapters.adaptTickers(super.getTickers(currencyPairs));
   }
 
   @Override
@@ -42,5 +53,9 @@ public class UpbitMarketDataService extends UpbitMarketDataServiceRaw implements
       throws ExchangeException, NotAvailableFromExchangeException,
           NotYetImplementedForExchangeException, IOException {
     return UpbitAdapters.adaptTrades(super.getTrades(currencyPair), currencyPair);
+  }
+
+  public ExchangeMetaData getMetaData() throws IOException {
+    return UpbitAdapters.adaptMetadata(getMarketAll());
   }
 }
