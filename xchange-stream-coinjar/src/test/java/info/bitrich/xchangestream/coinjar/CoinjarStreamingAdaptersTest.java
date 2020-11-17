6dda04c176242b9323003cbe7712b8ea4bdc89be
Niels Draaisma <niels@draaisma.email>
2020-11-18 10:41:04 +1100

    Fixed coinjar streaming adapter for four character markets

--- xchange-stream-coinjar/src/test/java/info/bitrich/xchangestream/coinjar/CoinjarStreamingAdaptersTest.java
+++ xchange-stream-coinjar/src/test/java/info/bitrich/xchangestream/coinjar/CoinjarStreamingAdaptersTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import org.junit.Test;
+import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.trade.UserTrade;
@@ -18,6 +19,13 @@ public class CoinjarStreamingAdaptersTest {
     String topic = "book:BTCAUD";
     assertThat(CoinjarStreamingAdapters.adaptTopicToCurrencyPair(topic))
         .isEqualTo(CurrencyPair.BTC_AUD);
+  }
+
+  @Test
+  public void testAdaptTopicToCurrencyPairFourChars() {
+    String topic = "book:USDC-AUD";
+    assertThat(CoinjarStreamingAdapters.adaptTopicToCurrencyPair(topic))
+        .isEqualTo(new CurrencyPair(Currency.USDC, Currency.AUD));
   }
 
   @Test
