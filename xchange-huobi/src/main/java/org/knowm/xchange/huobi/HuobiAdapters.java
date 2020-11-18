f2f1c2ca47ef9717cc39f73ae7b00580d5fbf101
Khye <lowkhyeean@gmail.com>
2020-11-18 15:36:21 +1100

    fixed divide by zero

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/HuobiAdapters.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/HuobiAdapters.java
@@ -189,6 +189,14 @@ public class HuobiAdapters {
     Order order = null;
     OrderType orderType = adaptOrderType(openOrder.getType());
     CurrencyPair currencyPair = adaptCurrencyPair(openOrder.getSymbol());
+    BigDecimal openOrderAvgPrice;
+    if (openOrder.getFieldAmount().compareTo(BigDecimal.ZERO) == 0) {
+      openOrderAvgPrice = BigDecimal.ZERO;
+    } else {
+      openOrderAvgPrice = openOrder
+          .getFieldCashAmount()
+          .divide(openOrder.getFieldAmount(), 8, BigDecimal.ROUND_DOWN);
+    }
     if (openOrder.isMarket()) {
       order =
           new MarketOrder(
@@ -197,9 +205,7 @@ public class HuobiAdapters {
               currencyPair,
               String.valueOf(openOrder.getId()),
               openOrder.getCreatedAt(),
-              openOrder
-                  .getFieldCashAmount()
-                  .divide(openOrder.getFieldAmount(), 8, BigDecimal.ROUND_DOWN),
+              openOrderAvgPrice,
               openOrder.getFieldAmount(),
               openOrder.getFieldFees(),
               adaptOrderStatus(openOrder.getState()),
@@ -214,9 +220,7 @@ public class HuobiAdapters {
               String.valueOf(openOrder.getId()),
               openOrder.getCreatedAt(),
               openOrder.getPrice(),
-              openOrder
-                  .getFieldCashAmount()
-                  .divide(openOrder.getFieldAmount(), 8, BigDecimal.ROUND_DOWN),
+              openOrderAvgPrice,
               openOrder.getFieldAmount(),
               openOrder.getFieldFees(),
               adaptOrderStatus(openOrder.getState()),
@@ -232,9 +236,7 @@ public class HuobiAdapters {
               openOrder.getCreatedAt(),
               openOrder.getStopPrice(),
               openOrder.getPrice(),
-              openOrder
-                  .getFieldCashAmount()
-                  .divide(openOrder.getFieldAmount(), 8, BigDecimal.ROUND_DOWN),
+              openOrderAvgPrice,
               openOrder.getFieldAmount(),
               openOrder.getFieldFees(),
               adaptOrderStatus(openOrder.getState()),
@@ -242,14 +244,7 @@ public class HuobiAdapters {
               openOrder.getOperator().equals("lte") ? Intention.STOP_LOSS : Intention.TAKE_PROFIT);
     }
 
-    if (openOrder.getFieldAmount().compareTo(BigDecimal.ZERO) == 0) {
-      order.setAveragePrice(BigDecimal.ZERO);
-    } else {
-      order.setAveragePrice(
-          openOrder
-              .getFieldCashAmount()
-              .divide(openOrder.getFieldAmount(), 8, BigDecimal.ROUND_DOWN));
-    }
+    order.setAveragePrice(openOrderAvgPrice);
     return order;
   }
 
