03301849366cd3e26a53bd33304580e429f346cf
java4tech <9290629+java4tech@users.noreply.github.com>
2020-12-03 22:17:22 +1100

    Update BTCMarketsWebSocketSubscribeMessage.java
    Reverted back messageType to subscribe, addSubscription is part of subscription message types and need some work to support

--- xchange-stream-btcmarkets/src/main/java/info/bitrich/xchangestream/btcmarkets/dto/BTCMarketsWebSocketSubscribeMessage.java
+++ xchange-stream-btcmarkets/src/main/java/info/bitrich/xchangestream/btcmarkets/dto/BTCMarketsWebSocketSubscribeMessage.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class BTCMarketsWebSocketSubscribeMessage {
 
   @JsonProperty("messageType")
-  public final String messageType = "addSubscription";
+  public final String messageType = "subscribe";
 
   @JsonProperty("marketIds")
   public final List<String> marketIds;
