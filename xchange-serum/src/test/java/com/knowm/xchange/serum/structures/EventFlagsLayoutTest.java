288eeac3dff9d2453b60705811239906ad7819cc
Erick <erick.arce10@gmail.com>
2021-01-05 12:58:25 -0500

    adding some additional classes for deserializing data

--- xchange-serum/src/test/java/com/knowm/xchange/serum/structures/EventFlagsLayoutTest.java
+++ xchange-serum/src/test/java/com/knowm/xchange/serum/structures/EventFlagsLayoutTest.java
@@ -2,12 +2,13 @@ package com.knowm.xchange.serum.structures;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.knowm.xchange.utils.DigestUtils;
 
 public class EventFlagsLayoutTest {
 
     @Test
     public void test() {
-        byte[] bts1 = fromHexString("0500000000000000");
+        byte[] bts1 = DigestUtils.hexToBytes("0500000000000000");
         EventFlagsLayout flagsLayout1 = new EventFlagsLayout();
         flagsLayout1.bytes = bts1;
         EventFlagsLayout.EventFlags flg1 = flagsLayout1.decode();
@@ -16,7 +17,7 @@ public class EventFlagsLayoutTest {
         Assert.assertTrue(flg1.bid);
         Assert.assertFalse(flg1.maker);
 
-        byte[] bts = fromHexString("0600000000000000");
+        byte[] bts = DigestUtils.hexToBytes("0600000000000000");
         EventFlagsLayout flagsLayout = new EventFlagsLayout();
         flagsLayout.bytes = bts;
         EventFlagsLayout.EventFlags flg = flagsLayout.decode();
@@ -24,19 +25,5 @@ public class EventFlagsLayoutTest {
         Assert.assertTrue(flg.out);
         Assert.assertTrue(flg.bid);
         Assert.assertFalse(flg.maker);
-    }
-
-    private static byte[] fromHexString(final String encoded) {
-        if ((encoded.length() % 2) != 0)
-            throw new IllegalArgumentException("Input string must contain an even number of characters");
-
-        final byte[] result = new byte[encoded.length()/2];
-        final char[] enc = encoded.toCharArray();
-        for (int i = 0; i < enc.length; i += 2) {
-            StringBuilder curr = new StringBuilder(2);
-            curr.append(enc[i]).append(enc[i + 1]);
-            result[i/2] = (byte) Integer.parseInt(curr.toString(), 16);
-        }
-        return result;
     }
 }
