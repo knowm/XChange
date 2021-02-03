d048a51d1fd0ace327ff24152aab8bfaaf406458
gaamox@tutanota.com <gaamox@tutanota.com>
2021-02-03 10:40:39 -0600

    Fix Bleutrade tests

--- xchange-bleutrade/src/test/java/org/knowm/xchange/bleutrade/service/marketdata/TickerFetchIntegration.java
+++ xchange-bleutrade/src/test/java/org/knowm/xchange/bleutrade/service/marketdata/TickerFetchIntegration.java
@@ -6,10 +6,16 @@ import org.junit.Test;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.bleutrade.BleutradeExchange;
+import org.knowm.xchange.bleutrade.dto.marketdata.BleutradeTicker;
+import org.knowm.xchange.bleutrade.service.BleutradeMarketDataService;
+import org.knowm.xchange.bleutrade.service.BleutradeMarketDataServiceRaw;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 import org.knowm.xchange.utils.CertHelper;
+
+import java.util.Iterator;
+import java.util.List;
 
 /** @author timmolter */
 public class TickerFetchIntegration {
@@ -22,10 +28,11 @@ public class TickerFetchIntegration {
     Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BleutradeExchange.class);
     exchange.remoteInit();
     MarketDataService marketDataService = exchange.getMarketDataService();
-    CurrencyPair market =
-        exchange.getExchangeMetaData().getCurrencyPairs().keySet().iterator().next();
-    Ticker ticker = marketDataService.getTicker(market);
+
+    //TODO: getTicker() call with a single currency pair returns a result json object, not a result json array
+    //{"success":true,"message":"","result":{"Ask":"8014.39600000","Average":"7717.79135089","BaseCurrency":"CBRL","BaseVolume":"1291.33706711","Bid":"7944.64770000","High":"7979.64770000","IsActive":"true","Last":"7979.64770000","Low":"7347.52880000","MarketCurrency":"ETH","MarketName":"ETH_CBRL","PrevDay":"7347.52880000","TimeStamp":"2021-02-03 16:03:51","Volume":"0.16731951"}}
+    List<BleutradeTicker> tickers = ((BleutradeMarketDataService) marketDataService).getBleutradeTickers();
     //    System.out.println(ticker.toString());
-    assertThat(ticker).isNotNull();
+    assertThat(tickers).isNotNull();
   }
 }
