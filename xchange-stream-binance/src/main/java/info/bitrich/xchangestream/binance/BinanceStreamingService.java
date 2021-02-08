d3ba079602ae6e4530495c980d5d63a7a2e52ba4
Paolo Rendano <info@paolorendano.it>
2021-02-08 19:37:38 +0100

    #3907 Quick fix for binance stream due to invalid websocket exception

--- xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingService.java
+++ xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingService.java
@@ -1,6 +1,7 @@
 package info.bitrich.xchangestream.binance;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import info.bitrich.xchangestream.binance.netty.WebSocketClientCompressionAllowClientNoContextHandler;
 import info.bitrich.xchangestream.core.ProductSubscription;
 import info.bitrich.xchangestream.service.netty.JsonNettyStreamingService;
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
