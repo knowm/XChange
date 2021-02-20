89ef760944910019286ed879ac44f7a470d30f8f
George Soultos <soultos@protonmail.ch>
2021-02-20 15:14:42 +0200

    [Coinbase PRO] Implement suggested feedback.

--- xchange-coinbasepro/src/main/java/org/knowm/xchange/coinbasepro/dto/marketdata/CoinbaseProStats.java
+++ xchange-coinbasepro/src/main/java/org/knowm/xchange/coinbasepro/dto/marketdata/CoinbaseProStats.java
@@ -1,13 +1,10 @@
 package org.knowm.xchange.coinbasepro.dto.marketdata;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.knowm.xchange.currency.CurrencyPair;
-
 import java.math.BigDecimal;
 import java.util.Map;
 
 public final class CoinbaseProStats {
-  private CurrencyPair currencyPair;
   private final BigDecimal open;
   private final BigDecimal high;
   private final BigDecimal low;
@@ -15,19 +12,11 @@ public final class CoinbaseProStats {
   private final BigDecimal last;
 
   public CoinbaseProStats(@JsonProperty("stats_24hour") Map<String, BigDecimal> stats24Hour) {
-    this.open = stats24Hour.get("open");
+    this.open = stats24Hour.get("opApen");
     this.high = stats24Hour.get("high");
     this.low = stats24Hour.get("low");
     this.volume = stats24Hour.get("volume");
     this.last = stats24Hour.get("last");
-  }
-
-  public void setCurrencyPair(CurrencyPair currencyPair) {
-    this.currencyPair = currencyPair;
-  }
-
-  public CurrencyPair getCurrencyPair() {
-    return currencyPair;
   }
 
   public BigDecimal getOpen() {
