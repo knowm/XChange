3ee2ddf18eaf6fa41c6e40700324d2424b45e2bb
Niels Draaisma <niels@draaisma.email>
2021-02-26 13:43:15 +1100

    Refactored additional KrakenStreamingMarketDataService methods

--- xchange-stream-kraken/src/test/java/info/bitrich/xchangestream/kraken/KrakenTradesExample.java
+++ xchange-stream-kraken/src/test/java/info/bitrich/xchangestream/kraken/KrakenTradesExample.java
@@ -3,16 +3,16 @@ package info.bitrich.xchangestream.kraken;
 import info.bitrich.xchangestream.core.StreamingExchange;
 import info.bitrich.xchangestream.core.StreamingExchangeFactory;
 import io.reactivex.disposables.Disposable;
-import java.util.concurrent.TimeUnit;
 import org.knowm.xchange.ExchangeSpecification;
-import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KrakenTickerExample {
+import java.util.concurrent.TimeUnit;
 
-  private static final Logger LOG = LoggerFactory.getLogger(KrakenTickerExample.class);
+public class KrakenTradesExample {
+
+  private static final Logger LOG = LoggerFactory.getLogger(KrakenTradesExample.class);
 
   public static void main(String[] args) throws InterruptedException {
 
@@ -23,10 +23,10 @@ public class KrakenTickerExample {
         StreamingExchangeFactory.INSTANCE.createExchange(exchangeSpecification);
     krakenExchange.connect().blockingAwait();
 
-    Disposable tickerDis =
+    Disposable tradesDisp =
         krakenExchange
             .getStreamingMarketDataService()
-            .getTicker(CurrencyPair.BTC_USD)
+            .getTrades(CurrencyPair.BTC_USD)
             .subscribe(
                 s -> {
                   LOG.info("Received {}", s);
@@ -37,7 +37,7 @@ public class KrakenTickerExample {
 
     TimeUnit.SECONDS.sleep(45);
 
-    tickerDis.dispose();
+    tradesDisp.dispose();
 
     krakenExchange.disconnect().subscribe(() -> LOG.info("Disconnected"));
   }
