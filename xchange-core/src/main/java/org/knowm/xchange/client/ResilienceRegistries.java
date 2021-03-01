585d05a2772f3cffa8113d05ae8c4dd548a8b6e9
Erick Arce <earce@users.noreply.github.com>
2021-03-01 04:06:51 -0500

    Fixing spelling and small library improvements (#3956)

--- xchange-core/src/main/java/org/knowm/xchange/client/ResilienceRegistries.java
+++ xchange-core/src/main/java/org/knowm/xchange/client/ResilienceRegistries.java
@@ -3,16 +3,20 @@ package org.knowm.xchange.client;
 import com.google.common.annotations.Beta;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
-import io.github.resilience4j.retry.IntervalFunction;
+import io.github.resilience4j.core.IntervalFunction;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.time.Duration;
+import java.util.function.Function;
+
+import io.vavr.control.Either;
 import org.knowm.xchange.exceptions.ExchangeUnavailableException;
 import org.knowm.xchange.exceptions.InternalServerException;
 import org.knowm.xchange.exceptions.OperationTimeoutException;
+import si.mazi.rescu.HttpStatusExceptionSupport;
 
 @Beta
 public class ResilienceRegistries {
@@ -28,17 +32,17 @@ public class ResilienceRegistries {
               OperationTimeoutException.class)
           .build();
 
-  public static final String NON_IDEMPOTENTE_CALLS_RETRY_CONFIG_NAME = "nonIdempotenteCallsBase";
+  public static final String NON_IDEMPOTENT_CALLS_RETRY_CONFIG_NAME = "nonIdempotentCallsBase";
 
   /**
-   * Suggested for calls that are not idemotente like placing order or withrawing funds
+   * Suggested for calls that are not idempotent like placing order or withdrawing funds
    *
-   * <p>Well designed exchange APIs will have mechanisms that make even placing orders idemotent.
-   * Most however cannot handle retrys on this type of calls and if you do one after a socket read
+   * <p>Well designed exchange APIs will have mechanisms that make even placing orders idempotent.
+   * Most however cannot handle retries on this type of calls and if you do one after a socket read
    * timeout for eq. then this may result in placing two identical orders instead of one. For such
-   * exchanged this retry configuration is recomended.
+   * exchanged this retry configuration is recommended.
    */
-  public static final RetryConfig DEFAULT_NON_IDEMPOTENTE_CALLS_RETRY_CONFIG =
+  public static final RetryConfig DEFAULT_NON_IDEMPOTENT_CALLS_RETRY_CONFIG =
       RetryConfig.from(DEFAULT_RETRY_CONFIG)
           .retryExceptions(
               UnknownHostException.class, SocketException.class, ExchangeUnavailableException.class)
@@ -51,25 +55,32 @@ public class ResilienceRegistries {
           .limitForPeriod(1200)
           .build();
 
+  /**
+   * Function which can be used by resilience registry to check if 429 was returned
+   */
+  public static Function<Either<? extends Throwable, ?>, Boolean> IS_429 = e ->
+          e instanceof HttpStatusExceptionSupport &&
+                  ((HttpStatusExceptionSupport) e).getHttpStatusCode() == 429;
+
   private final RetryRegistry retryRegistry;
 
   private final RateLimiterRegistry rateLimiterRegistry;
 
   public ResilienceRegistries() {
-    this(DEFAULT_RETRY_CONFIG, DEFAULT_NON_IDEMPOTENTE_CALLS_RETRY_CONFIG);
+    this(DEFAULT_RETRY_CONFIG, DEFAULT_NON_IDEMPOTENT_CALLS_RETRY_CONFIG);
   }
 
   public ResilienceRegistries(
-      RetryConfig globalRetryConfig, RetryConfig nonIdempotenteCallsRetryConfig) {
-    this(globalRetryConfig, nonIdempotenteCallsRetryConfig, DEFAULT_GLOBAL_RATE_LIMITER_CONFIG);
+      RetryConfig globalRetryConfig, RetryConfig nonIdempotentCallsRetryConfig) {
+    this(globalRetryConfig, nonIdempotentCallsRetryConfig, DEFAULT_GLOBAL_RATE_LIMITER_CONFIG);
   }
 
   public ResilienceRegistries(
       RetryConfig globalRetryConfig,
-      RetryConfig nonIdempotenteCallsRetryConfig,
+      RetryConfig nonIdempotentCallsRetryConfig,
       RateLimiterConfig globalRateLimiterConfig) {
     this(
-        retryRegistryOf(globalRetryConfig, nonIdempotenteCallsRetryConfig),
+        retryRegistryOf(globalRetryConfig, nonIdempotentCallsRetryConfig),
         RateLimiterRegistry.of(globalRateLimiterConfig));
   }
 
@@ -88,10 +99,10 @@ public class ResilienceRegistries {
   }
 
   private static RetryRegistry retryRegistryOf(
-      RetryConfig globalRetryConfig, RetryConfig nonIdempotenteCallsRetryConfig) {
+      RetryConfig globalRetryConfig, RetryConfig nonIdempotentCallsRetryConfig) {
     RetryRegistry registry = RetryRegistry.of(globalRetryConfig);
     registry.addConfiguration(
-        NON_IDEMPOTENTE_CALLS_RETRY_CONFIG_NAME, nonIdempotenteCallsRetryConfig);
+            NON_IDEMPOTENT_CALLS_RETRY_CONFIG_NAME, nonIdempotentCallsRetryConfig);
     return registry;
   }
 }
