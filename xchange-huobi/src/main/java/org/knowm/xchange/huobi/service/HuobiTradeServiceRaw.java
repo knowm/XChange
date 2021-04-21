76ffd52245bb08e534dde68512375cca261c9fbb
makarid <makarid@gmail.com>
2021-04-21 15:32:44 +0300

    Adding post only feature on limit order

--- xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiTradeServiceRaw.java
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/service/HuobiTradeServiceRaw.java
@@ -133,6 +133,7 @@ public class HuobiTradeServiceRaw extends HuobiBaseService {
     }
     if (limitOrder.hasFlag(HuobiTradeService.FOK)) type = type + "-fok";
     if (limitOrder.hasFlag(HuobiTradeService.IOC)) type = type + "-ioc";
+    if (limitOrder.hasFlag(HuobiTradeService.POST_ONLY)) type = type + "-maker";
 
     HuobiOrderResult result =
         huobi.placeLimitOrder(
