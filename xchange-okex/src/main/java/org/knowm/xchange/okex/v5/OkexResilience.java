b93be659f26beee665c605cd2b000ece11f0c59f
gaamox@tutanota.com <gaamox@tutanota.com>
2021-06-09 12:26:03 -0500

    Rework Rate limits

--- xchange-okex/src/main/java/org/knowm/xchange/okex/v5/OkexResilience.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/v5/OkexResilience.java
@@ -10,35 +10,36 @@ import static javax.ws.rs.core.Response.Status.TOO_MANY_REQUESTS;
 
 /** Author: Max Gao (gaamox@tutanota.com) Created: 08-06-2021 */
 public class OkexResilience {
-
-  public static final String PUBLIC_REST_ENDPOINT_RATE_LIMITER = "publicEndpointLimit";
-
-  public static final String PRIVATE_REST_ENDPOINT_RATE_LIMITER = "privateEndpointLimit";
-
   public static ResilienceRegistries createRegistries() {
     final ResilienceRegistries registries = new ResilienceRegistries();
 
-    registries
-        .rateLimiters()
-        .rateLimiter(
-            PUBLIC_REST_ENDPOINT_RATE_LIMITER,
-            RateLimiterConfig.from(registries.rateLimiters().getDefaultConfig())
-                .limitRefreshPeriod(Duration.ofSeconds(1))
-                .limitForPeriod(3)
-                .drainPermissionsOnResult(
-                    e -> ResilienceUtils.matchesHttpCode(e, TOO_MANY_REQUESTS))
-                .build());
+    Okex.publicPathRateLimits.forEach(
+        (path, limit) -> {
+          registries
+              .rateLimiters()
+              .rateLimiter(
+                  path,
+                  RateLimiterConfig.from(registries.rateLimiters().getDefaultConfig())
+                      .limitRefreshPeriod(Duration.ofSeconds(limit.get(1)))
+                      .limitForPeriod(limit.get(0))
+                      .drainPermissionsOnResult(
+                          e -> ResilienceUtils.matchesHttpCode(e, TOO_MANY_REQUESTS))
+                      .build());
+        });
 
-    registries
-        .rateLimiters()
-        .rateLimiter(
-            PRIVATE_REST_ENDPOINT_RATE_LIMITER,
-            RateLimiterConfig.from(registries.rateLimiters().getDefaultConfig())
-                .limitRefreshPeriod(Duration.ofSeconds(1))
-                .limitForPeriod(5)
-                .drainPermissionsOnResult(
-                    e -> ResilienceUtils.matchesHttpCode(e, TOO_MANY_REQUESTS))
-                .build());
+    OkexAuthenticated.privatePathRateLimits.forEach(
+        (path, limit) -> {
+          registries
+              .rateLimiters()
+              .rateLimiter(
+                  path,
+                  RateLimiterConfig.from(registries.rateLimiters().getDefaultConfig())
+                      .limitRefreshPeriod(Duration.ofSeconds(limit.get(1)))
+                      .limitForPeriod(limit.get(0))
+                      .drainPermissionsOnResult(
+                          e -> ResilienceUtils.matchesHttpCode(e, TOO_MANY_REQUESTS))
+                      .build());
+        });
 
     return registries;
   }
--- xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexMarketDataServiceRaw.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexMarketDataServiceRaw.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
+import static org.knowm.xchange.okex.v5.Okex.instrumentsPath;
+import static org.knowm.xchange.okex.v5.OkexAuthenticated.currenciesPath;
+
 /** Author: Max Gao (gaamox@tutanota.com) Created: 08-06-2021 */
 public class OkexMarketDataServiceRaw extends OkexBaseService {
   public OkexMarketDataServiceRaw(
@@ -23,22 +26,30 @@ public class OkexMarketDataServiceRaw extends OkexBaseService {
       String instrumentType, String underlying, String instrumentId)
       throws OkexException, IOException {
     try {
-      return okex.getInstruments(instrumentType, underlying, instrumentId);
+      return decorateApiCall(() -> okex.getInstruments(instrumentType, underlying, instrumentId))
+          .withRateLimiter(rateLimiter(instrumentsPath))
+          .call();
     } catch (OkexException e) {
-      throw new OkexException(e.getMessage());
+      throw handleError(e);
     }
   }
 
   public OkexResponse<List<OkexCurrency>> getOkexCurrencies() throws OkexException, IOException {
     try {
-      return okexAuthenticated.getCurrencies(
-          exchange.getExchangeSpecification().getApiKey(),
-          signatureCreator,
-          DateUtils.toUTCISODateString(new Date()),
-          (String)
-              exchange.getExchangeSpecification().getExchangeSpecificParametersItem("passphrase"));
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.getCurrencies(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase")))
+          .withRateLimiter(rateLimiter(currenciesPath))
+          .call();
     } catch (OkexException e) {
-      throw new OkexException(e.getMessage());
+      throw handleError(e);
     }
   }
 }
