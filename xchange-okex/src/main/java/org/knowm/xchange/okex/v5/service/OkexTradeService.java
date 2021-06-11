b22b8c13b5f0d4f2efd28ab96724ed82703e58b2
gaamox@tutanota.com <gaamox@tutanota.com>
2021-06-11 00:10:22 -0500

    Adding some trading functionality

--- xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexTradeService.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexTradeService.java
@@ -1,12 +1,98 @@
 package org.knowm.xchange.okex.v5.service;
 
 import org.knowm.xchange.client.ResilienceRegistries;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.OpenOrders;
+import org.knowm.xchange.exceptions.FundsExceededException;
+import org.knowm.xchange.okex.v5.OkexAdapters;
 import org.knowm.xchange.okex.v5.OkexExchange;
+import org.knowm.xchange.okex.v5.dto.trade.OkexCancelOrderRequest;
+import org.knowm.xchange.okex.v5.dto.trade.OkexTradeParams;
 import org.knowm.xchange.service.trade.TradeService;
+import org.knowm.xchange.service.trade.params.CancelOrderParams;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /** Author: Max Gao (gaamox@tutanota.com) Created: 08-06-2021 */
 public class OkexTradeService extends OkexTradeServiceRaw implements TradeService {
   public OkexTradeService(OkexExchange exchange, ResilienceRegistries resilienceRegistries) {
     super(exchange, resilienceRegistries);
+  }
+
+  @Override
+  public OpenOrders getOpenOrders() throws IOException {
+    return OkexAdapters.adaptOpenOrders(
+        getOkexPendingOrder(null, null, null, null, null, null, null, null).getData());
+  }
+
+  @Override
+  public String placeLimitOrder(LimitOrder limitOrder) throws IOException, FundsExceededException {
+    return placeOkexOrder(OkexAdapters.adaptOrder(limitOrder)).getData().get(0).getOrderId();
+  }
+
+  public List<String> placeLimitOrder(List<LimitOrder> limitOrders)
+      throws IOException, FundsExceededException {
+    return placeOkexOrder(
+            limitOrders.stream()
+                .map(order -> OkexAdapters.adaptOrder(order))
+                .collect(Collectors.toList()))
+        .getData()
+        .stream()
+        .map(result -> result.getOrderId())
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public String changeOrder(LimitOrder limitOrder) throws IOException, FundsExceededException {
+    return amendOkexOrder(OkexAdapters.adaptAmendOrder(limitOrder)).getData().get(0).getOrderId();
+  }
+
+  public List<String> changeOrder(List<LimitOrder> limitOrders)
+      throws IOException, FundsExceededException {
+    return amendOkexOrder(
+            limitOrders.stream()
+                .map(order -> OkexAdapters.adaptAmendOrder(order))
+                .collect(Collectors.toList()))
+        .getData()
+        .stream()
+        .map(result -> result.getOrderId())
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public boolean cancelOrder(CancelOrderParams params) throws IOException {
+    return "0"
+        .equals(
+            cancelOkexOrder(
+                    OkexCancelOrderRequest.builder()
+                        .orderId(((OkexTradeParams.OkexCancelOrderParams) params).getOrderId())
+                        .instrumentId(
+                            OkexAdapters.adaptCurrencyPairId(
+                                ((OkexTradeParams.OkexCancelOrderParams) params).getCurrencyPair()))
+                        .build())
+                .getData()
+                .get(0)
+                .getCode());
+  }
+
+  public List<Boolean> cancelOrder(List<CancelOrderParams> params) throws IOException {
+    return cancelOkexOrder(
+            params.stream()
+                .map(
+                    param ->
+                        OkexCancelOrderRequest.builder()
+                            .orderId(((OkexTradeParams.OkexCancelOrderParams) param).getOrderId())
+                            .instrumentId(
+                                OkexAdapters.adaptCurrencyPairId(
+                                    ((OkexTradeParams.OkexCancelOrderParams) param)
+                                        .getCurrencyPair()))
+                            .build())
+                .collect(Collectors.toList()))
+        .getData()
+        .stream()
+        .map(result -> "0".equals(result.getCode()))
+        .collect(Collectors.toList());
   }
 }
--- xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexTradeServiceRaw.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/v5/service/OkexTradeServiceRaw.java
@@ -2,10 +2,191 @@ package org.knowm.xchange.okex.v5.service;
 
 import org.knowm.xchange.client.ResilienceRegistries;
 import org.knowm.xchange.okex.v5.OkexExchange;
+import org.knowm.xchange.okex.v5.dto.OkexException;
+import org.knowm.xchange.okex.v5.dto.OkexResponse;
+import org.knowm.xchange.okex.v5.dto.trade.OkexAmendOrderRequest;
+import org.knowm.xchange.okex.v5.dto.trade.OkexCancelOrderRequest;
+import org.knowm.xchange.okex.v5.dto.trade.OkexOrderRequest;
+import org.knowm.xchange.okex.v5.dto.trade.OkexOrderResponse;
+import org.knowm.xchange.okex.v5.dto.trade.OkexPendingOrder;
+import org.knowm.xchange.utils.DateUtils;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static org.knowm.xchange.okex.v5.OkexAuthenticated.*;
 
 /** Author: Max Gao (gaamox@tutanota.com) Created: 08-06-2021 */
 public class OkexTradeServiceRaw extends OkexBaseService {
   public OkexTradeServiceRaw(OkexExchange exchange, ResilienceRegistries resilienceRegistries) {
     super(exchange, resilienceRegistries);
+  }
+
+  public OkexResponse<List<OkexPendingOrder>> getOkexPendingOrder(
+      String instrumentType,
+      String underlying,
+      String instrumentId,
+      String orderType,
+      String state,
+      String after,
+      String before,
+      String limit)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.getPendingOrders(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      instrumentType,
+                      underlying,
+                      instrumentId,
+                      orderType,
+                      state,
+                      after,
+                      before,
+                      limit))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-place-order */
+  public OkexResponse<List<OkexOrderResponse>> placeOkexOrder(OkexOrderRequest order)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.placeOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      order))
+          .withRateLimiter(rateLimiter(placeOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-place-multiple-orders */
+  public OkexResponse<List<OkexOrderResponse>> placeOkexOrder(List<OkexOrderRequest> orders)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.placeBatchOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      orders))
+          .withRateLimiter(rateLimiter(placeBatchOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-cancel-order */
+  public OkexResponse<List<OkexOrderResponse>> cancelOkexOrder(OkexCancelOrderRequest order)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.cancelOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      order))
+          .withRateLimiter(rateLimiter(cancelOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-cancel-multiple-orders */
+  public OkexResponse<List<OkexOrderResponse>> cancelOkexOrder(List<OkexCancelOrderRequest> orders)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.cancelBatchOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      orders))
+          .withRateLimiter(rateLimiter(cancelBatchOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-amend-order */
+  public OkexResponse<List<OkexOrderResponse>> amendOkexOrder(OkexAmendOrderRequest order)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.amendOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      order))
+          .withRateLimiter(rateLimiter(amendOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
+  }
+
+  /** https://www.okex.com/docs-v5/en/#rest-api-trade-amend-multiple-orders */
+  public OkexResponse<List<OkexOrderResponse>> amendOkexOrder(List<OkexAmendOrderRequest> orders)
+      throws IOException {
+    try {
+      return decorateApiCall(
+              () ->
+                  okexAuthenticated.amendBatchOrder(
+                      exchange.getExchangeSpecification().getApiKey(),
+                      signatureCreator,
+                      DateUtils.toUTCISODateString(new Date()),
+                      (String)
+                          exchange
+                              .getExchangeSpecification()
+                              .getExchangeSpecificParametersItem("passphrase"),
+                      orders))
+          .withRateLimiter(rateLimiter(amendBatchOrderPath))
+          .call();
+    } catch (OkexException e) {
+      throw handleError(e);
+    }
   }
 }
