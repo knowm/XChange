053686ddc8d1b68b36901491e40bb4f84a9fb7fd
Vasyl Parasyna <krabdog@gmail.com>
2021-07-14 16:51:02 +0300

    [Deribit] DeribitTradeService implementation

--- xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/DeribitAuthenticated.java
+++ xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/DeribitAuthenticated.java
@@ -30,10 +30,11 @@ import si.mazi.rescu.ParamsDigest;
 public interface DeribitAuthenticated {
 
   /**
-   * Retrieves user account summary. https://docs.deribit.com/v2/#private-get_account_summary
+   * Retrieves user account summary.
    *
    * @param currency required, The currency symbol, BTC or ETH
    * @param extended Include additional fields
+   * @see <a href="https://docs.deribit.com/#private-get_account_summary">docs.deribit.com</a>
    */
   @GET
   @Path("get_account_summary")
@@ -44,33 +45,40 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-buy
+   * Places a buy order for an instrument.
    *
    * @param instrumentName required, Instrument name
-   * @param amount required, It represents the requested order size. For perpetual and futures the
-   *     amount is in USD units, for options it is amount of corresponding cryptocurrency contracts,
-   *     e.g., BTC or ETH
-   * @param type optional, The order type, default: "limit"
-   * @param label optional, user defined label for the order (maximum 32 characters)
-   * @param price optional, The order price in base currency (Only for limit and stop_limit orders)
-   *     When adding order with advanced=usd, the field price should be the option price value in
-   *     USD. When adding order with advanced=implv, the field price should be a value of implied
-   *     volatility in percentages. For example, price=100, means implied volatility of 100%
-   * @param timeInForce optional, Specifies how long the order remains in effect. Default
-   *     "good_til_cancelled" "good_til_cancelled" - unfilled order remains in order book until
-   *     cancelled "fill_or_kill" - execute a transaction immediately and completely or not at all
-   *     "immediate_or_cancel" - execute a transaction immediately, and any portion of the order
-   *     that cannot be immediately filled is cancelled
-   * @param maxShow optional, Maximum amount within an order to be shown to other customers, 0 for
-   *     invisible order
-   * @param postOnly optional, If true, the order is considered post-only. If the new price would
-   *     cause the order to be filled immediately (as taker), the price will be changed to be just
-   *     below the bid. Only valid in combination with time_in_force="good_til_cancelled"
-   * @param reduceOnly optional, If true, the order is considered reduce-only which is intended to
-   *     only reduce a current position
-   * @param stopPrice optional, Stop price, required for stop limit orders (Only for stop orders)
-   * @param trigger optional, Defines trigger type, required for "stop_limit" order type
-   * @param advanced optional, Advanced option order type. (Only for options)
+   * @param amount         required, It represents the requested order size. For perpetual and futures the
+   *                       amount is in USD units, for options it is amount of corresponding cryptocurrency contracts,
+   *                       e.g., BTC or ETH
+   * @param type           optional, The order type, default: "limit"
+   * @param label          optional, user defined label for the order (maximum 32 characters)
+   * @param price          optional, The order price in base currency (Only for limit and stop_limit orders)
+   *                       When adding order with advanced=usd, the field price should be the option price value in
+   *                       USD. When adding order with advanced=implv, the field price should be a value of implied
+   *                       volatility in percentages. For example, price=100, means implied volatility of 100%
+   * @param timeInForce    optional, Specifies how long the order remains in effect. Default
+   *                       "good_til_cancelled" "good_til_cancelled" - unfilled order remains in order book until
+   *                       cancelled "fill_or_kill" - execute a transaction immediately and completely or not at all
+   *                       "immediate_or_cancel" - execute a transaction immediately, and any portion of the order
+   *                       that cannot be immediately filled is cancelled
+   * @param maxShow        optional, Maximum amount within an order to be shown to other customers, 0 for
+   *                       invisible order
+   * @param postOnly       optional, If true, the order is considered post-only. If the new price would
+   *                       cause the order to be filled immediately (as taker), the price will be changed to be just
+   *                       below the bid. Only valid in combination with time_in_force="good_til_cancelled"
+   * @param rejectPostOnly optional, If an order is considered post-only and this field is set to
+   *                       true then the order is put to order book unmodified or request is rejected and order is
+   *                       canceled. Only valid in combination with "post_only" set to true
+   * @param reduceOnly     optional, If true, the order is considered reduce-only which is intended to
+   *                       only reduce a current position
+   * @param triggerPrice   optional, Trigger price, required for trigger orders only (Stop-loss or
+   *                       Take-profit orders)
+   * @param trigger        optional, Defines trigger type, required for "stop_limit", "stop_market",
+   *                       "take_limit" or "take_market" order types
+   * @param advanced       optional, Advanced option order type. (Only for options)
+   * @param mmp            optional, Order MMP flag, only for order_type 'limit'
+   * @see <a href="https://docs.deribit.com/#private-buy">docs.deribit.com</a>
    */
   @GET
   @Path("buy")
@@ -83,13 +91,20 @@ public interface DeribitAuthenticated {
       @QueryParam("time_in_force") TimeInForce timeInForce,
       @QueryParam("max_show") BigDecimal maxShow,
       @QueryParam("post_only") Boolean postOnly,
+      @QueryParam("reject_post_only") Boolean rejectPostOnly,
       @QueryParam("reduce_only") Boolean reduceOnly,
-      @QueryParam("stop_price") BigDecimal stopPrice,
+      @QueryParam("trigger_price") BigDecimal triggerPrice,
       @QueryParam("trigger") Trigger trigger,
       @QueryParam("advanced") AdvancedOptions advanced,
+      @QueryParam("mmp") Boolean mmp,
       @HeaderParam("Authorization") ParamsDigest auth)
       throws DeribitException, IOException;
 
+  /**
+   * Places a sell order for an instrument.
+   *
+   * @see <a href="https://docs.deribit.com/#private-sell">docs.deribit.com</a>
+   */
   @GET
   @Path("sell")
   DeribitResponse<OrderPlacement> sell(
@@ -101,39 +116,40 @@ public interface DeribitAuthenticated {
       @QueryParam("time_in_force") TimeInForce timeInForce,
       @QueryParam("max_show") BigDecimal maxShow,
       @QueryParam("post_only") Boolean postOnly,
+      @QueryParam("reject_post_only") Boolean rejectPostOnly,
       @QueryParam("reduce_only") Boolean reduceOnly,
-      @QueryParam("stop_price") BigDecimal stopPrice,
+      @QueryParam("trigger_price") BigDecimal triggerPrice,
       @QueryParam("trigger") Trigger trigger,
       @QueryParam("advanced") AdvancedOptions advanced,
+      @QueryParam("mmp") Boolean mmp,
       @HeaderParam("Authorization") ParamsDigest auth)
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-edit Edit an existing order, specified by order id
+   * Changes price, amount and/or other properties of an order.
    *
-   * @param orderID, amount, price
+   * @see <a href="https://docs.deribit.com/#private-edit">docs.deribit.com</a>
    */
   @GET
   @Path("edit")
   DeribitResponse<OrderPlacement> edit(
-      @QueryParam("order_id") String orderID,
+      @QueryParam("order_id") String orderId,
       @QueryParam("amount") BigDecimal amount,
       @QueryParam("price") BigDecimal price,
-      // @QueryParam("post_only") Boolean postOnly,
-      // @QueryParam("reduce_only") Boolean reduceOnly,
-      // @QueryParam("reject_post_only") Boolean reducePostOnly,
-      // @QueryParam("advanced") AdvancedOptions advanced,
-      // @QueryParam("stop_price") BigDecimal stopPrice,
-      // @QueryParam("mp") Boolean mp,
-      // here is a bug! on the website it says mmp, but if you tipe in mmp, you get a
-      // bad request 400.
+      @QueryParam("post_only") Boolean postOnly,
+      @QueryParam("reject_post_only") Boolean rejectPostOnly,
+      @QueryParam("reduce_only") Boolean reduceOnly,
+      @QueryParam("trigger_price") BigDecimal triggerPrice,
+      @QueryParam("advanced") AdvancedOptions advanced,
+      @QueryParam("mmp") Boolean mmp,
       @HeaderParam("Authorization") ParamsDigest auth)
       throws DeribitException, IOException;
 
   /**
-   * Cancel an order, specified by order id
+   * Cancels an order, specified by order id.
    *
    * @param orderId required, The order id
+   * @see <a href="https://docs.deribit.com/#private-cancel">docs.deribit.com</a>
    */
   @GET
   @Path("cancel")
@@ -142,11 +158,44 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-get_open_orders_by_instrument
+   * Cancels orders by label. All user's orders (trigger orders too), with given label are canceled
+   * in all currencies
+   *
+   * @param label user defined label for the order (maximum 64 characters)
+   * @see <a href="https://docs.deribit.com/#private-cancel_by_label">docs.deribit.com</a>
+   */
+  @GET
+  @Path("cancel_by_label")
+  DeribitResponse<Integer> cancelByLabel(
+      @QueryParam("label") String label, @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieves list of user's open orders.
+   *
+   * @param currency required, The currency symbol
+   * @param kind     optional, Instrument kind, if not provided instruments of all kinds are considered.
+   *                 One of: "future", "option"
+   * @param type     optional, Order type, one of (all, limit, stop_all, stop_limit, stop_market),
+   *                 default - all
+   * @see <a href="https://docs.deribit.com/#private-get_open_orders_by_currency">docs.deribit.com</a>
+   */
+  @GET
+  @Path("get_open_orders_by_currency")
+  DeribitResponse<List<Order>> getOpenOrdersByCurrency(
+      @QueryParam("currency") String currency,
+      @QueryParam("kind") Kind kind,
+      @QueryParam("type") String type,
+      @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieves list of user's open orders within given Instrument.
    *
    * @param instrumentName required, Instrument name
-   * @param type optional, Order type, one of (all, limit, stop_all, stop_limit, stop_market),
-   *     default - all
+   * @param type           optional, Order type, one of (all, limit, stop_all, stop_limit, stop_market),
+   *                       default - all
+   * @see <a href="https://docs.deribit.com/#private-get_open_orders_by_instrument">docs.deribit.com</a>
    */
   @GET
   @Path("get_open_orders_by_instrument")
@@ -157,15 +206,53 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-get_user_trades_by_instrument
+   * Retrieves the latest user trades that have occurred for instruments in a specific currency symbol.
+   *
+   * @see <a href="https://docs.deribit.com/#private-get_user_trades_by_currency">docs.deribit.com</a>
+   */
+  @GET
+  @Path("get_user_trades_by_currency")
+  DeribitResponse<UserTrades> getUserTradesByCurrency(
+      @QueryParam("currency") String currency,
+      @QueryParam("kind") Kind kind,
+      @QueryParam("start_id") String startId,
+      @QueryParam("end_id") String endId,
+      @QueryParam("count") Integer count,
+      @QueryParam("include_old") Boolean includeOld,
+      @QueryParam("sorting") String sorting,
+      @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieves the latest user trades that have occurred for instruments in a specific currency
+   * symbol and within given time range.
+   *
+   * @see <a href="https://docs.deribit.com/#private-get_user_trades_by_currency_and_time">docs.deribit.com</a>
+   */
+  @GET
+  @Path("get_user_trades_by_currency")
+  DeribitResponse<UserTrades> getUserTradesByCurrencyAndTime(
+      @QueryParam("currency") String currency,
+      @QueryParam("kind") Kind kind,
+      @QueryParam("start_timestamp") long startTimestamp,
+      @QueryParam("end_timestamp") long endTimestamp,
+      @QueryParam("count") Integer count,
+      @QueryParam("include_old") Boolean includeOld,
+      @QueryParam("sorting") String sorting,
+      @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieves the latest user trades that have occurred for a specific instrument.
    *
    * @param instrumentName required Instrument name
-   * @param startSeq optional The sequence number of the first trade to be returned
-   * @param endSeq optional The sequence number of the last trade to be returned
-   * @param count optional Number of requested items, default - 10
-   * @param includeOld optional Include trades older than 7 days, default - false
-   * @param sorting optional ( asc, desc, default) Direction of results sorting (default value means
-   *     no sorting, results will be returned in order in which they left the database)
+   * @param startSeq       optional The sequence number of the first trade to be returned
+   * @param endSeq         optional The sequence number of the last trade to be returned
+   * @param count          optional Number of requested items, default - 10
+   * @param includeOld     optional Include trades older than 7 days, default - false
+   * @param sorting        optional ( asc, desc, default) Direction of results sorting (default value means
+   *                       no sorting, results will be returned in order in which they left the database)
+   * @see <a href="https://docs.deribit.com/#private-get_user_trades_by_instrument">docs.deribit.com</a>
    */
   @GET
   @Path("get_user_trades_by_instrument")
@@ -180,15 +267,17 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-get_user_trades_by_instrument_and_time
+   * Retrieves the latest user trades that have occurred for a specific instrument and within
+   * given time range.
    *
    * @param instrumentName required Instrument name
    * @param startTimestamp required The earliest timestamp to return result for
-   * @param endTimestamp required The most recent timestamp to return result for
-   * @param count optional Number of requested items, default - 10
-   * @param includeOld optional Include trades older than 7 days, default - false
-   * @param sorting optional ( asc, desc, default) Direction of results sorting (default value means
-   *     no sorting, results will be returned in order in which they left the database)
+   * @param endTimestamp   required The most recent timestamp to return result for
+   * @param count          optional Number of requested items, default - 10
+   * @param includeOld     optional Include trades older than 7 days, default - false
+   * @param sorting        optional ( asc, desc, default) Direction of results sorting (default value means
+   *                       no sorting, results will be returned in order in which they left the database)
+   * @see <a href="https://docs.deribit.com/#private-get_user_trades_by_instrument_and_time">docs.deribit.com</a>
    */
   @GET
   @Path("get_user_trades_by_instrument_and_time")
@@ -203,10 +292,11 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-get_positions
+   * Retrieves user positions.
    *
    * @param currency BTC, ETH
-   * @param kind Kind filter on positions
+   * @param kind     Kind filter on positions
+   * @see <a href="https://docs.deribit.com/#private-get_positions">docs.deribit.com</a>
    */
   @GET
   @Path("get_positions")
@@ -217,12 +307,13 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/v2/#private-get_settlement_history_by_instrument
+   * Retrieves public settlement, delivery and bankruptcy events filtered by instrument name.
    *
    * @param instrumentName required - Instrument name
-   * @param count optional - Number of requested items, default - 20
-   * @param type optional - Settlement type
-   * @param continuation optional - Continuation string for pagination
+   * @param count          optional - Number of requested items, default - 20
+   * @param type           optional - Settlement type
+   * @param continuation   optional - Continuation string for pagination
+   * @see <a href="https://docs.deribit.com/#private-get_settlement_history_by_instrument">docs.deribit.com</a>
    */
   @GET
   @Path("get_settlement_history_by_instrument")
@@ -235,13 +326,31 @@ public interface DeribitAuthenticated {
       throws DeribitException, IOException;
 
   /**
-   * https://docs.deribit.com/#private-get_order_history_by_instrument
+   * Retrieves history of orders that have been partially or fully filled.
    *
-   * @param instrumentName required - Instrument name
-   * @param count optional - Number of requested items, default - 20
-   * @param offset optional - The offset for pagination, default - 0
-   * @param includeOld optional - Include orders older than 2 days, default - false
+   * @see <a href="https://docs.deribit.com/#private-get_order_history_by_currency">docs.deribit.com</a>
+   */
+  @GET
+  @Path("get_order_history_by_instrument")
+  DeribitResponse<List<Order>> getOrderHistoryByCurrency(
+      @QueryParam("currency") String currency,
+      @QueryParam("kind") Kind kind,
+      @QueryParam("count") Integer count,
+      @QueryParam("offset") Integer offset,
+      @QueryParam("include_old") Boolean includeOld,
+      @QueryParam("include_unfilled") Boolean includeUnfilled,
+      @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieves history of orders that have been partially or fully filled.
+   *
+   * @param instrumentName  required - Instrument name
+   * @param count           optional - Number of requested items, default - 20
+   * @param offset          optional - The offset for pagination, default - 0
+   * @param includeOld      optional - Include orders older than 2 days, default - false
    * @param includeUnfilled optional - Include fully unfilled closed orders, default - false
+   * @see <a href="https://docs.deribit.com/#private-get_order_history_by_instrument">docs.deribit.com</a>
    */
   @GET
   @Path("get_order_history_by_instrument")
@@ -252,5 +361,16 @@ public interface DeribitAuthenticated {
       @QueryParam("include_old") Boolean includeOld,
       @QueryParam("include_unfilled") Boolean includeUnfilled,
       @HeaderParam("Authorization") ParamsDigest auth)
+      throws DeribitException, IOException;
+
+  /**
+   * Retrieve the current state of an order
+   *
+   * @see <a href="https://docs.deribit.com/#private-get_order_state">docs.deribit.com</a>
+   */
+  @GET
+  @Path("get_order_state")
+  DeribitResponse<Order> getOrderState(
+      @QueryParam("order_id") String orderId, @HeaderParam("Authorization") ParamsDigest auth)
       throws DeribitException, IOException;
 }
--- xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/service/DeribitAccountService.java
+++ xchange-deribit/src/main/java/org/knowm/xchange/deribit/v2/service/DeribitAccountService.java
@@ -1,11 +1,12 @@
 package org.knowm.xchange.deribit.v2.service;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.deribit.v2.DeribitAdapters;
 import org.knowm.xchange.deribit.v2.DeribitExchange;
 import org.knowm.xchange.deribit.v2.dto.account.AccountSummary;
@@ -15,9 +16,6 @@ import org.knowm.xchange.service.account.AccountService;
 
 public class DeribitAccountService extends DeribitAccountServiceRaw implements AccountService {
 
-  /** currently there are only two (base) currencies supported by deribit */
-  private final Supplier<Stream<String>> currencies = () -> Stream.of("BTC", "ETH");
-
   public DeribitAccountService(DeribitExchange exchange) {
     super(exchange);
   }
@@ -25,8 +23,9 @@ public class DeribitAccountService extends DeribitAccountServiceRaw implements A
   @Override
   public AccountInfo getAccountInfo() {
     List<Balance> balances =
-        currencies
-            .get()
+        currencies()
+            .stream()
+            .map(Currency::getCurrencyCode)
             .parallel()
             .map(
                 c -> {
@@ -39,21 +38,26 @@ public class DeribitAccountService extends DeribitAccountServiceRaw implements A
                 })
             .collect(Collectors.toList());
     Wallet wallet = Wallet.Builder.from(balances).build();
+    return new AccountInfo(null, null, Collections.singleton(wallet), openPositions(), null);
+  }
 
-    List<OpenPosition> openPositions =
-        currencies
-            .get()
-            .parallel()
-            .flatMap(
-                c -> {
-                  try {
-                    return super.getPositions(c, null).stream().map(DeribitAdapters::adapt);
-                  } catch (IOException e) {
-                    throw new ExchangeException(e);
-                  }
-                })
-            .collect(Collectors.toList());    
-    
-    return new AccountInfo(null, null, Collections.singleton(wallet), openPositions, null);
+  List<OpenPosition> openPositions() {
+    return currencies()
+      .stream()
+      .map(Currency::getCurrencyCode)
+      .parallel()
+      .flatMap(
+        c -> {
+          try {
+            return super.getPositions(c, null).stream().map(DeribitAdapters::adapt);
+          } catch (IOException e) {
+            throw new ExchangeException(e);
+          }
+        })
+      .collect(Collectors.toList());
+  }
+  
+  Collection<Currency> currencies() {
+      return exchange.getExchangeMetaData().getCurrencies().keySet();
   }
 }
