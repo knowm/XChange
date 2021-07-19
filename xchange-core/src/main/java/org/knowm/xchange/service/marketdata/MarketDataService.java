07b60487a7afaa9b78ea845449f7e57767baa9e4
Vasyl Parasyna <krabdog@gmail.com>
2021-07-19 16:10:58 +0300

    [Core] Marked MarketDataService CurrencyPair methods deprecated

--- xchange-core/src/main/java/org/knowm/xchange/service/marketdata/MarketDataService.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/marketdata/MarketDataService.java
@@ -37,7 +37,9 @@ public interface MarketDataService extends BaseService {
    * @throws NotYetImplementedForExchangeException - Indication that the exchange supports the
    *     requested function or data, but it has not yet been implemented
    * @throws IOException - Indication that a networking error occurred while fetching JSON data
+   * @deprecated replaced by {@link #getTicker(Instrument, Object...)}
    */
+  @Deprecated
   default Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
     throw new NotYetImplementedForExchangeException("getTicker");
   }
@@ -89,7 +91,9 @@ public interface MarketDataService extends BaseService {
    * @throws NotYetImplementedForExchangeException - Indication that the exchange supports the
    *     requested function or data, but it has not yet been implemented
    * @throws IOException - Indication that a networking error occurred while fetching JSON data
+   * @deprecated replaced by {@link #getOrderBook(Instrument, Object...)}
    */
+  @Deprecated
   default OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
     throw new NotYetImplementedForExchangeException("getOrderBook");
   }
@@ -143,7 +147,9 @@ public interface MarketDataService extends BaseService {
    * @throws NotYetImplementedForExchangeException - Indication that the exchange supports the
    *     requested function or data, but it has not yet been implemented
    * @throws IOException - Indication that a networking error occurred while fetching JSON data
+   * @deprecated replaced by {@link #getTrades(Instrument, Object...)} 
    */
+  @Deprecated
   default Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
     throw new NotYetImplementedForExchangeException("getTrades");
   }
