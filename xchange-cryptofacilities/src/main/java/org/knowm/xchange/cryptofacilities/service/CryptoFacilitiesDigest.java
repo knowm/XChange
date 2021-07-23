388b7c6e3ea9253613afb0b9d4da4e088db5f20e
Nibaru <22958894+nibarulabs@users.noreply.github.com>
2021-07-23 14:43:10 -0700

    Fixed my copy of the CryptoFacilitiesDigest file (thought my pull of develop was recent?)

--- xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/service/CryptoFacilitiesDigest.java
+++ xchange-cryptofacilities/src/main/java/org/knowm/xchange/cryptofacilities/service/CryptoFacilitiesDigest.java
@@ -9,8 +9,6 @@ import java.util.Base64;
 import javax.crypto.Mac;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.QueryParam;
-
-import lombok.SneakyThrows;
 import org.knowm.xchange.service.BaseParamsDigest;
 import si.mazi.rescu.RestInvocation;
 
@@ -38,7 +36,6 @@ public class CryptoFacilitiesDigest extends BaseParamsDigest {
     }
   }
 
-  @SneakyThrows
   @Override
   public String digestParams(RestInvocation restInvocation) {
 
@@ -47,19 +44,25 @@ public class CryptoFacilitiesDigest extends BaseParamsDigest {
       sha256 = MessageDigest.getInstance("SHA-256");
     } catch (NoSuchAlgorithmException e) {
       throw new RuntimeException(
-          "Illegal algorithm for post body digest. Check the implementation.");
+          "Illegal algorithm (SHA-256) for post body digest. Check the implementation.");
     }
-    String decodedQuery =
-        URLDecoder.decode(
-            restInvocation.getParamsMap().get(QueryParam.class).asQueryString(),
-            String.valueOf(StandardCharsets.UTF_8));
-    sha256.update(decodedQuery.getBytes());
+    try {
+      String decodedQuery =
+          URLDecoder.decode(
+              restInvocation.getParamsMap().get(QueryParam.class).asQueryString(),
+              StandardCharsets.UTF_8.name());
+      sha256.update(decodedQuery.getBytes());
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(
+          "Bad encoding found on request query while hashing (SHA256) the POST data.", e);
+    }
     try {
       sha256.update(
           URLDecoder.decode(restInvocation.getRequestBody(), StandardCharsets.UTF_8.name())
               .getBytes());
     } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException("Could not SHA256 the POST data.");
+      throw new RuntimeException(
+          "Bad encoding found on request body while hashing (SHA256) the POST data.", e);
     }
 
     sha256.update(restInvocation.getParamValue(HeaderParam.class, "Nonce").toString().getBytes());
