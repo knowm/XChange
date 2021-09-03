b94b3109f718f50c4d04a02f7470fc9acf972ded
douggie <douggie@melivlleclarke.com>
2021-09-03 16:12:14 +0100

    Support for ticker by instrument

--- xchange-core/src/main/java/org/knowm/xchange/service/marketdata/MarketDataService.java
+++ xchange-core/src/main/java/org/knowm/xchange/service/marketdata/MarketDataService.java
@@ -39,7 +39,24 @@ public interface MarketDataService extends BaseService {
    *     requested function or data, but it has not yet been implemented
    * @throws IOException - Indication that a networking error occurred while fetching JSON data
    */
+  @Deprecated
   default Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
+    throw new NotYetImplementedForExchangeException("getTicker");
+  }
+
+  /**
+   * Get a ticker representing the current exchange rate
+   *
+   * @return The Ticker, null if some sort of error occurred. Implementers should log the error.
+   * @throws ExchangeException - Indication that the exchange reported some kind of error with the
+   *     request or response
+   * @throws NotAvailableFromExchangeException - Indication that the exchange does not support the
+   *     requested function or data
+   * @throws NotYetImplementedForExchangeException - Indication that the exchange supports the
+   *     requested function or data, but it has not yet been implemented
+   * @throws IOException - Indication that a networking error occurred while fetching JSON data
+   */
+  default Ticker getTicker(Instrument instrument, Object... args) throws IOException {
     throw new NotYetImplementedForExchangeException("getTicker");
   }
 
