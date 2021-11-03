3e3b7b3a1e0182836252634bb8eb8ab527fbf0eb
Vasyl Parasyna <krabdog@gmail.com>
2021-11-03 18:20:08 +0200

    Extended Ftx to instrument API, integrated FuturesContract usage

--- xchange-ftx/src/main/java/org/knowm/xchange/ftx/service/FtxMarketDataService.java
+++ xchange-ftx/src/main/java/org/knowm/xchange/ftx/service/FtxMarketDataService.java
@@ -2,11 +2,11 @@ package org.knowm.xchange.ftx.service;
 
 import java.io.IOException;
 import org.knowm.xchange.Exchange;
-import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.dto.marketdata.Trades;
 import org.knowm.xchange.ftx.FtxAdapters;
+import org.knowm.xchange.instrument.Instrument;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
 public class FtxMarketDataService extends FtxMarketDataServiceRaw implements MarketDataService {
@@ -16,25 +16,22 @@ public class FtxMarketDataService extends FtxMarketDataServiceRaw implements Mar
   }
 
   @Override
-  public OrderBook getOrderBook(CurrencyPair currencyPair, Object... args) throws IOException {
+  public OrderBook getOrderBook(Instrument instrument, Object... args) throws IOException {
     return FtxAdapters.adaptOrderBook(
-        getFtxOrderbook(FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair)), currencyPair);
+        getFtxOrderbook(FtxAdapters.adaptInstrumentToFtxMarket(instrument)), instrument);
   }
 
   @Override
-  public Trades getTrades(CurrencyPair currencyPair, Object... args) throws IOException {
+  public Trades getTrades(Instrument instrument, Object... args) throws IOException {
     return FtxAdapters.adaptTrades(
-        getFtxTrades(FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair)).getResult(),
-        currencyPair);
+        getFtxTrades(FtxAdapters.adaptInstrumentToFtxMarket(instrument)).getResult(), instrument);
   }
 
-
   @Override
-  public Ticker getTicker(CurrencyPair currencyPair, Object... args) throws IOException {
-
+  public Ticker getTicker(Instrument instrument, Object... args) throws IOException {
     return FtxAdapters.adaptTicker(
-            getFtxMarket(FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair)),
-            getFtxCandles(FtxAdapters.adaptCurrencyPairToFtxMarket(currencyPair), "60"), //60 seconds
-            currencyPair);
+        getFtxMarket(FtxAdapters.adaptInstrumentToFtxMarket(instrument)),
+        getFtxCandles(FtxAdapters.adaptInstrumentToFtxMarket(instrument), "60"), // 60 seconds
+        instrument);
   }
 }
