782275730351c249ff2beef6735f6e804b0e982e
Maksym Hontar <maksym@springsoftware.io>
2021-12-08 18:46:18 +0200

    fix binance leverage deserialization issue

--- xchange-binance/src/main/java/org/knowm/xchange/binance/futures/dto/account/BinanceFuturesInitialLeverage.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/futures/dto/account/BinanceFuturesInitialLeverage.java
@@ -6,15 +6,12 @@ import java.math.BigDecimal;
 
 public final class BinanceFuturesInitialLeverage {
     public final int leverage;
-    public final BigDecimal maxNotionalValue;
     public final String symbol;
 
     public BinanceFuturesInitialLeverage(
             @JsonProperty("leverage") int leverage,
-            @JsonProperty("maxNotionalValue") BigDecimal maxNotionalValue,
             @JsonProperty("symbol") String symbol) {
         this.leverage = leverage;
-        this.maxNotionalValue = maxNotionalValue;
         this.symbol = symbol;
     }
 }
