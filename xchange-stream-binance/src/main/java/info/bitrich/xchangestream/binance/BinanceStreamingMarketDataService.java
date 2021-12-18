b9ae9cdebd342ecd5ba684d7c53402f49210b84b
Jaroslaw Wisniewski <j.wisniewski@appsisle.com>
2021-12-18 22:15:33 +0100

    #4361 Added missing volatile keywords.

--- xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingMarketDataService.java
+++ xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingMarketDataService.java
@@ -341,11 +341,23 @@ public class BinanceStreamingMarketDataService implements StreamingMarketDataSer
 
     private final CompositeDisposable disposables = new CompositeDisposable();
 
-    private OrderBook book;
-
-    private long bookLastUpdateId;
-
+    /**
+     * Used as a gate, preventing access to the book snapshot, until initialized on background
+     * thread.
+     */
     private volatile boolean bookInitialized;
+
+    /**
+     * Volatile as, it is initialized on background thread, then accessed and updated on observable
+     * thread.
+     */
+    private volatile long bookLastUpdateId;
+
+    /**
+     * Volatile as, it is initialized on background thread, then accessed and updated on observable
+     * thread
+     */
+    private volatile OrderBook book;
 
     private OrderBookSubscription(
         Observable<DepthBinanceWebSocketTransaction> deltasObservable, CurrencyPair currencyPair) {
