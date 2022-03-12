e74d1e1067ef733422987f67080b6bc0deaea98c
Nibaru <22958894+nibarulabs@users.noreply.github.com>
2022-03-11 18:21:08 -0800

    Updated the websocket extension handler since I was seeing errors from coinbase handshake similar to what we saw with binance before.

--- xchange-stream-coinbasepro/src/main/java/info/bitrich/xchangestream/coinbasepro/CoinbaseProStreamingService.java
+++ xchange-stream-coinbasepro/src/main/java/info/bitrich/xchangestream/coinbasepro/CoinbaseProStreamingService.java
@@ -7,10 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import info.bitrich.xchangestream.coinbasepro.dto.CoinbaseProWebSocketSubscriptionMessage;
 import info.bitrich.xchangestream.coinbasepro.dto.CoinbaseProWebSocketTransaction;
 import info.bitrich.xchangestream.core.ProductSubscription;
-import info.bitrich.xchangestream.service.netty.JsonNettyStreamingService;
-import info.bitrich.xchangestream.service.netty.StreamingObjectMapperHelper;
-import info.bitrich.xchangestream.service.netty.WebSocketClientCompressionAllowClientNoContextHandler;
-import info.bitrich.xchangestream.service.netty.WebSocketClientHandler;
+import info.bitrich.xchangestream.service.netty.*;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
@@ -129,7 +126,7 @@ public class CoinbaseProStreamingService extends JsonNettyStreamingService {
 
   @Override
   protected WebSocketClientExtensionHandler getWebSocketClientExtensionHandler() {
-    return WebSocketClientCompressionAllowClientNoContextHandler.INSTANCE;
+    return WebSocketClientCompressionAllowClientNoContextAndServerNoContextHandler.INSTANCE;
   }
 
   @Override
