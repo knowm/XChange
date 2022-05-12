51b67be7692c983d9f3f03e9f650919ac5b5c6d3
Martin Stachon <stachon@gmail.com>
2022-05-12 10:44:15 +0200

    coinmate orders - merge CoinmateOrder and CoinmateOrderHistoryEntry, add trades ands cumulativeAmount fields.

--- xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/dto/trade/CoinmateOrders.java
+++ xchange-coinmate/src/main/java/org/knowm/xchange/coinmate/dto/trade/CoinmateOrders.java
@@ -3,12 +3,12 @@ package org.knowm.xchange.coinmate.dto.trade;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.knowm.xchange.coinmate.dto.CoinmateBaseResponse;
 
-public class CoinmateOrders extends CoinmateBaseResponse<CoinmateOrder> {
+public class CoinmateOrders extends CoinmateBaseResponse<CoinmateOrderHistoryEntry> {
 
   public CoinmateOrders(
       @JsonProperty("error") boolean error,
       @JsonProperty("errorMessage") String errorMessage,
-      @JsonProperty("data") CoinmateOrder data) {
+      @JsonProperty("data") CoinmateOrderHistoryEntry data) {
 
     super(error, errorMessage, data);
   }
