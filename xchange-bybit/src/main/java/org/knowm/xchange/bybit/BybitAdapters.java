2ed60cc607192df3308afb1e55486f52979a0b00
tonykwok1992 <tonykwok1992@gmail.com>
2022-06-24 14:02:23 +0800

    Trade service get and place order

--- xchange-bybit/src/main/java/org/knowm/xchange/bybit/BybitAdapters.java
+++ xchange-bybit/src/main/java/org/knowm/xchange/bybit/BybitAdapters.java
@@ -2,16 +2,21 @@ package org.knowm.xchange.bybit;
 
 import org.knowm.xchange.bybit.dto.account.BybitBalance;
 import org.knowm.xchange.currency.Currency;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order;
 import org.knowm.xchange.dto.account.Balance;
 import org.knowm.xchange.dto.account.Wallet;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BybitAdapters {
 
-    public static Wallet adaptBybitBalances(List<BybitBalance> bybitBalances){
+    public static final List<String> QUOTE_CURRENCIES = Arrays.asList("USDT", "USDC", "BTC", "DAI");
+
+    public static Wallet adaptBybitBalances(List<BybitBalance> bybitBalances) {
         List<Balance> balances = new ArrayList<>(bybitBalances.size());
         for (BybitBalance bybitBalance : bybitBalances) {
             balances.add(new Balance(new Currency(bybitBalance.getCoin()),
@@ -20,6 +25,40 @@ public class BybitAdapters {
             ));
         }
         return Wallet.Builder.from(balances).build();
+    }
+
+    public static String getSideString(Order.OrderType type) {
+        if (type == Order.OrderType.ASK)
+            return "Sell";
+        if (type == Order.OrderType.BID)
+            return "Buy";
+        throw new IllegalArgumentException("invalid order type");
+    }
+
+    public static Order.OrderType getOrderType(String side) {
+        if ("sell".equalsIgnoreCase(side)) {
+            return Order.OrderType.ASK;
+        }
+        if ("buy".equalsIgnoreCase(side)) {
+            return Order.OrderType.BID;
+        }
+        throw new IllegalArgumentException("invalid order type");
+    }
+
+    public static String convertToBybitSymbol(String instrumentName) {
+        return instrumentName.replace("/", "").toUpperCase();
+    }
+
+    public static CurrencyPair guessSymbol(String symbol) {
+        for (String quoteCurrency : QUOTE_CURRENCIES) {
+            if (symbol.endsWith(quoteCurrency)) {
+                int splitIndex = symbol.lastIndexOf(quoteCurrency);
+                return new CurrencyPair(symbol.substring(0, splitIndex), symbol.substring(splitIndex));
+            }
+        }
+        int splitIndex = symbol.length() - 3;
+        return new CurrencyPair(symbol.substring(0, splitIndex), symbol.substring(splitIndex));
+
     }
 
 }
--- xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitTradeService.java
+++ xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitTradeService.java
@@ -1,11 +1,64 @@
 package org.knowm.xchange.bybit.service;
 
 import org.knowm.xchange.bybit.BybitExchange;
+import org.knowm.xchange.bybit.dto.BybitResult;
+import org.knowm.xchange.bybit.dto.trade.BybitOrderDetails;
+import org.knowm.xchange.bybit.dto.trade.BybitOrderRequest;
+import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.trade.LimitOrder;
+import org.knowm.xchange.dto.trade.MarketOrder;
 import org.knowm.xchange.service.trade.TradeService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import static org.knowm.xchange.bybit.BybitAdapters.*;
 
 public class BybitTradeService extends BybitTradeServiceRaw implements TradeService {
 
     public BybitTradeService(BybitExchange exchange) {
         super(exchange);
+    }
+
+    @Override
+    public String placeMarketOrder(MarketOrder marketOrder) throws IOException {
+        BybitResult<BybitOrderRequest> order = placeOrder(
+                convertToBybitSymbol(marketOrder.getInstrument().toString()),
+                marketOrder.getOriginalAmount().longValue(),
+                getSideString(marketOrder.getType()),
+                "MARKET");
+
+        return order.getResult().getOrderId();
+    }
+
+    @Override
+    public Collection<Order> getOrder(String... orderIds) throws IOException {
+        List<Order> results = new ArrayList<>();
+
+        for (String orderId : orderIds) {
+
+            BybitResult<BybitOrderDetails> bybitOrder = getBybitOrder(orderId);
+            BybitOrderDetails bybitOrderResult = bybitOrder.getResult();
+
+            LimitOrder limitOrder = new LimitOrder(
+                    getOrderType(bybitOrderResult.getSide()),
+                    new BigDecimal(bybitOrderResult.getOrigQty()),
+                    new BigDecimal(bybitOrderResult.getExecutedQty()),
+                    guessSymbol(bybitOrderResult.getSymbol()),
+                    bybitOrderResult.getOrderId(),
+                    new Date(Long.parseLong(bybitOrderResult.getTime())),
+                    new BigDecimal(bybitOrderResult.getPrice())) {
+            };
+            BigDecimal averagePrice = new BigDecimal(bybitOrderResult.getAvgPrice());
+            limitOrder.setAveragePrice(averagePrice);
+            limitOrder.setOrderStatus(Order.OrderStatus.valueOf(bybitOrderResult.getStatus()));
+            results.add(limitOrder);
+        }
+
+        return results;
     }
 }
