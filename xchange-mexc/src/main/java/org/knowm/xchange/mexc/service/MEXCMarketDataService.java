f1e21fb4787ac970f9d8f176bf5d5c1484fb950f
tonykwok1992 <tonykwok1992@gmail.com>
2022-06-30 12:55:57 +0800

    Add mexc skeleton

---
+++ xchange-mexc/src/main/java/org/knowm/xchange/mexc/service/MEXCMarketDataService.java
@@ -0,0 +1,10 @@
+package org.knowm.xchange.mexc.service;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.mexc.MEXCExchange;
+import org.knowm.xchange.service.marketdata.MarketDataService;
+
+public class MEXCMarketDataService implements MarketDataService {
+  public MEXCMarketDataService(Exchange exchange) {
+  }
+}
---
+++ xchange-mexc/src/main/java/org/knowm/xchange/mexc/service/MEXCTradeService.java
@@ -0,0 +1,10 @@
+package org.knowm.xchange.mexc.service;
+
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.mexc.MEXCExchange;
+import org.knowm.xchange.service.trade.TradeService;
+
+public class MEXCTradeService implements TradeService {
+  public MEXCTradeService(Exchange exchange) {
+  }
+}
