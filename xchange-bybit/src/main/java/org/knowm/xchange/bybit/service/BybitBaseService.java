798c602f8585636362056d4fa48f4ad2186b85ac
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2022-07-10 19:31:00 +0200

    Add getting of ticker

--- xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitBaseService.java
+++ xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitBaseService.java
@@ -1,6 +1,8 @@
 package org.knowm.xchange.bybit.service;
 
+import java.util.concurrent.TimeUnit;
 import org.knowm.xchange.Exchange;
+import org.knowm.xchange.bybit.Bybit;
 import org.knowm.xchange.bybit.BybitAuthenticated;
 import org.knowm.xchange.client.ExchangeRestProxyBuilder;
 import org.knowm.xchange.service.BaseService;
@@ -8,20 +10,26 @@ import org.knowm.xchange.utils.nonce.CurrentTimeIncrementalNonceFactory;
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.SynchronizedValueFactory;
 
-import java.util.concurrent.TimeUnit;
-
 public class BybitBaseService implements BaseService {
 
-    protected final BybitAuthenticated bybitAuthenticated;
-    protected final ParamsDigest signatureCreator;
-    protected final SynchronizedValueFactory<Long> nonceFactory = new CurrentTimeIncrementalNonceFactory(TimeUnit.MILLISECONDS);
-    protected final String apiKey;
+  protected final BybitAuthenticated bybitAuthenticated;
+  protected final Bybit bybit;
+  protected final ParamsDigest signatureCreator;
+  protected final SynchronizedValueFactory<Long> nonceFactory = new CurrentTimeIncrementalNonceFactory(TimeUnit.MILLISECONDS);
+  protected final String apiKey;
 
-    public BybitBaseService(Exchange exchange) {
-        bybitAuthenticated = ExchangeRestProxyBuilder.forInterface(BybitAuthenticated.class, exchange.getExchangeSpecification()).build();
-        signatureCreator = BybitDigest.createInstance(exchange.getExchangeSpecification().getSecretKey());
-        apiKey = exchange.getExchangeSpecification().getApiKey();
-    }
+  public BybitBaseService(Exchange exchange) {
+    bybit = ExchangeRestProxyBuilder
+        .forInterface(Bybit.class, exchange.getExchangeSpecification())
+        .clientConfigCustomizer(clientConfig -> clientConfig.setJacksonObjectMapperFactory(new BybitJacksonObjectMapperFactory()))
+        .build();
+    bybitAuthenticated = ExchangeRestProxyBuilder
+        .forInterface(BybitAuthenticated.class, exchange.getExchangeSpecification())
+        .clientConfigCustomizer(clientConfig -> clientConfig.setJacksonObjectMapperFactory(new BybitJacksonObjectMapperFactory()))
+        .build();
+    signatureCreator = BybitDigest.createInstance(exchange.getExchangeSpecification().getSecretKey());
+    apiKey = exchange.getExchangeSpecification().getApiKey();
+  }
 
 
 }
--- xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitMarketDataServiceRaw.java
+++ xchange-bybit/src/main/java/org/knowm/xchange/bybit/service/BybitMarketDataServiceRaw.java
@@ -1,9 +1,26 @@
 package org.knowm.xchange.bybit.service;
 
+import static org.knowm.xchange.bybit.BybitAdapters.createBybitExceptionFromResult;
+
+import java.io.IOException;
+import java.util.List;
 import org.knowm.xchange.bybit.BybitExchange;
+import org.knowm.xchange.bybit.dto.BybitResult;
+import org.knowm.xchange.bybit.dto.marketdata.BybitTicker;
 
 public class BybitMarketDataServiceRaw extends BybitBaseService {
-    public BybitMarketDataServiceRaw(BybitExchange exchange) {
-        super(exchange);
+
+  public BybitMarketDataServiceRaw(BybitExchange exchange) {
+    super(exchange);
+  }
+
+  public BybitResult<List<BybitTicker>> getTicker24h(String symbol)
+      throws IOException {
+    BybitResult<List<BybitTicker>> result = bybit.getTicker24h(symbol);
+    if (!result.isSuccess()) {
+      throw createBybitExceptionFromResult(result);
     }
+    return result;
+  }
+
 }
