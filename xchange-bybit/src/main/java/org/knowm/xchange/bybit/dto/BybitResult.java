4358559376e09ea03e481f560671a90b98f5cabc
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2022-07-10 14:55:07 +0200

    Simplify Bybit dto's

--- xchange-bybit/src/main/java/org/knowm/xchange/bybit/dto/BybitResult.java
+++ xchange-bybit/src/main/java/org/knowm/xchange/bybit/dto/BybitResult.java
@@ -1,61 +1,32 @@
 package org.knowm.xchange.bybit.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
+@Builder
+@Jacksonized
+@Value
 public class BybitResult<V> {
 
-    private final int retCode;
-    private final String retMsg;
-    private final String extCode;
-    private final String extInfo;
-    private final V result;
+  @JsonProperty("ret_code")
+  int retCode;
 
-    @JsonCreator
-    public BybitResult(@JsonProperty("ret_code") int retCode,
-                       @JsonProperty("ret_msg") String retMsg,
-                       @JsonProperty("ext_code") String extCode,
-                       @JsonProperty("ext_info") String extInfo,
-                       @JsonProperty("result") V result) {
-        this.retCode = retCode;
-        this.retMsg = retMsg;
-        this.extCode = extCode;
-        this.extInfo = extInfo;
-        this.result = result;
-    }
+  @JsonProperty("ret_msg")
+  String retMsg;
 
-    public int getRetCode() {
-        return retCode;
-    }
+  @JsonProperty("ext_code")
+  String extCode;
 
-    public String getRetMsg() {
-        return retMsg;
-    }
+  @JsonProperty("ext_info")
+  String extInfo;
 
-    public String getExtCode() {
-        return extCode;
-    }
+  @JsonProperty("result")
+  V result;
 
-    public String getExtInfo() {
-        return extInfo;
-    }
+  public boolean isSuccess() {
+    return retCode == 0;
+  }
 
-    public V getResult() {
-        return result;
-    }
-
-    public boolean isSuccess() {
-        return retCode == 0;
-    }
-
-    @Override
-    public String toString() {
-        return "BybitResult{" +
-                "retCode=" + retCode +
-                ", retMsg='" + retMsg + '\'' +
-                ", extCode='" + extCode + '\'' +
-                ", extInfo='" + extInfo + '\'' +
-                ", result=" + result +
-                '}';
-    }
 }
