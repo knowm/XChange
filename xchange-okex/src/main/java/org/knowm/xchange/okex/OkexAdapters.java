9ea197038ee5f55735ce5cf4119130448c1ae13e
makarid <makarid@gmail.com>
2022-12-14 17:08:26 +0200

    [okex] Adding funding rate support

--- xchange-okex/src/main/java/org/knowm/xchange/okex/OkexAdapters.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/OkexAdapters.java
@@ -21,6 +21,7 @@ import org.knowm.xchange.dto.trade.OpenOrders;
 import org.knowm.xchange.dto.trade.UserTrade;
 import org.knowm.xchange.dto.trade.UserTrades;
 import org.knowm.xchange.instrument.Instrument;
+import org.knowm.xchange.okex.dto.OkexInstType;
 import org.knowm.xchange.okex.dto.account.OkexAssetBalance;
 import org.knowm.xchange.okex.dto.account.OkexPosition;
 import org.knowm.xchange.okex.dto.account.OkexTradeFee;
@@ -32,6 +33,7 @@ import org.knowm.xchange.okex.dto.trade.OkexOrderDetails;
 import org.knowm.xchange.okex.dto.trade.OkexOrderRequest;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -50,7 +52,6 @@ public class OkexAdapters {
             new UserTrade.Builder()
                 .originalAmount(new BigDecimal(okexOrderDetails.getAmount()))
                 .instrument(adaptOkexInstrumentId(okexOrderDetails.getInstrumentId()))
-                .currencyPair(new CurrencyPair(okexOrderDetails.getInstrumentId()))
                 .price(new BigDecimal(okexOrderDetails.getAverageFilledPrice()))
                 .type(adaptOkexOrderSideToOrderType(okexOrderDetails.getSide()))
                 .id(okexOrderDetails.getOrderId())
