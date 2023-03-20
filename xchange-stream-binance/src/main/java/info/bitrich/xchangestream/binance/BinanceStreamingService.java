288ba2adfa5550ef3d4d8ecff98708b4f1b47308
rizer1980 <4340180@gmail.com>
2023-03-20 04:09:23 +0300

    [binance] implement channel inactive handler

--- xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingService.java
+++ xchange-stream-binance/src/main/java/info/bitrich/xchangestream/binance/BinanceStreamingService.java
@@ -7,10 +7,10 @@ import info.bitrich.xchangestream.binance.dto.BinanceWebSocketSubscriptionMessag
 import info.bitrich.xchangestream.core.ProductSubscription;
 import info.bitrich.xchangestream.service.netty.JsonNettyStreamingService;
 import info.bitrich.xchangestream.service.netty.WebSocketClientCompressionAllowClientNoContextAndServerNoContextHandler;
+import info.bitrich.xchangestream.service.netty.WebSocketClientHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
 import io.netty.handler.codec.http.websocketx.extensions.WebSocketClientExtensionHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -20,6 +20,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BinanceStreamingService extends JsonNettyStreamingService {
 
@@ -32,6 +34,9 @@ public class BinanceStreamingService extends JsonNettyStreamingService {
   private final KlineSubscription klineSubscription;
 
   private boolean isLiveSubscriptionEnabled = false;
+
+  private WebSocketClientHandler.WebSocketMessageHandler channelInactiveHandler = null;
+
   private final Map<Integer, BinanceWebSocketSubscriptionMessage> liveSubscriptionMessage =
       new ConcurrentHashMap<>();
 
@@ -234,6 +239,41 @@ public class BinanceStreamingService extends JsonNettyStreamingService {
       } catch (Exception e) {
         LOGGER.warn("Failed to unsubscribe channel: {}", channelId, e);
       }
+    }
+  }
+
+  @Override
+  protected WebSocketClientHandler getWebSocketClientHandler(
+          WebSocketClientHandshaker handshake, WebSocketClientHandler.WebSocketMessageHandler handler) {
+    LOGGER.info("Registering BinanceWebSocketClientHandler");
+    return new BinanceWebSocketClientHandler(handshake, handler);
+  }
+
+  public void setChannelInactiveHandler(
+          WebSocketClientHandler.WebSocketMessageHandler channelInactiveHandler) {
+    this.channelInactiveHandler = channelInactiveHandler;
+  }
+
+  /**
+   * Custom client handler in order to execute an external, user-provided handler on channel events.
+   */
+  class BinanceWebSocketClientHandler extends NettyWebSocketClientHandler {
+
+    public BinanceWebSocketClientHandler(
+            WebSocketClientHandshaker handshake, WebSocketMessageHandler handler) {
+      super(handshake, handler);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+      super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+      super.channelInactive(ctx);
+      if (channelInactiveHandler != null)
+        channelInactiveHandler.onMessage("WebSocket Client disconnected!");
     }
   }
 }
