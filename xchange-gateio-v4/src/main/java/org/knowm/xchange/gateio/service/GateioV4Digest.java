5c69cc8c95e2666326f4877050150ecf14ac3642
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2023-06-05 10:24:35 +0200

    [gateio-v4] Code cleanup

--- xchange-gateio-v4/src/main/java/org/knowm/xchange/gateio/service/GateioV4Digest.java
+++ xchange-gateio-v4/src/main/java/org/knowm/xchange/gateio/service/GateioV4Digest.java
@@ -1,6 +1,6 @@
 package org.knowm.xchange.gateio.service;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import javax.crypto.Mac;
 import lombok.SneakyThrows;
@@ -8,61 +8,35 @@ import org.knowm.xchange.service.BaseParamsDigest;
 import org.knowm.xchange.utils.DigestUtils;
 import si.mazi.rescu.RestInvocation;
 
-/**
- * This may be used as the value of a @HeaderParam, @QueryParam or @PathParam to create a digest of
- * the post body (composed of @FormParam's). Don't use as the value of a @FormParam, it will
- * probably cause an infinite loop.
- *
- * <p>This may be used for REST APIs where some parameters' values must be digests of other
- * parameters. An example is the MtGox API v1, where the Rest-Sign header parameter must be a digest
- * of the request body (which is composed of @FormParams).
- */
-public class GateioV4Digest extends BaseParamsDigest {
+public final class GateioV4Digest extends BaseParamsDigest {
 
-  /**
-   * Constructor
-   *
-   * @param secretKeyBase64
-   * @throws IllegalArgumentException if key is invalid (cannot be base-64-decoded or the decoded
-   *     key is invalid).
-   */
   private GateioV4Digest(String secretKeyBase64) {
 
     super(secretKeyBase64, HMAC_SHA_512);
   }
 
   public static GateioV4Digest createInstance(String secretKeyBase64) {
-
     return secretKeyBase64 == null ? null : new GateioV4Digest(secretKeyBase64);
   }
 
   @SneakyThrows
   @Override
   public String digestParams(RestInvocation restInvocation) {
-    try {
-      String postBody = restInvocation.getRequestBody();
-      String method = restInvocation.getHttpMethod();
-      String path = restInvocation.getPath();
-      String query = restInvocation.getQueryString();
+    String method = restInvocation.getHttpMethod();
+    String path = restInvocation.getPath();
 
-      String body = restInvocation.getRequestBody();
-      MessageDigest md = MessageDigest.getInstance("SHA-512");
-      String hexedHashedBody = DigestUtils.bytesToHex(md.digest(body.getBytes()));
+    String query = restInvocation.getQueryString();
+    String body = restInvocation.getRequestBody();
+    MessageDigest md = MessageDigest.getInstance("SHA-512");
+    String hexedHashedBody = DigestUtils.bytesToHex(md.digest(body.getBytes(StandardCharsets.UTF_8)));
 
-      String timestamp = restInvocation.getHttpHeadersFromParams().get("Timestamp");
+    String timestamp = restInvocation.getHttpHeadersFromParams().get("Timestamp");
 
-      String payloadToSign = String.format("%s\n/%s\n%s\n%s\n%s", method, path, query, hexedHashedBody, timestamp);
+    String payloadToSign = String.format("%s\n/%s\n%s\n%s\n%s", method, path, query, hexedHashedBody, timestamp);
 
-      System.out.println(payloadToSign);
+    Mac mac = getMac();
+    mac.update(payloadToSign.getBytes(StandardCharsets.UTF_8));
 
-      Mac mac = getMac();
-      mac.update(payloadToSign.getBytes("UTF-8"));
-      return DigestUtils.bytesToHex(mac.doFinal());
-//      return String.format("%0128x", new BigInteger(1, mac.doFinal()));
-
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException("Illegal encoding, check the code.", e);
-    }
-    // return Base64.encodeBytes(mac.doFinal()).trim();
+    return DigestUtils.bytesToHex(mac.doFinal());
   }
 }
