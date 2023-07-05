bd870774d32f5f195788a44143b787ef9148db0f
Rachid <rachid@weareblox.com>
2023-07-05 12:15:58 +0200

    Make KucoinStreamingMarketDataService available outside package

--- xchange-stream-kucoin/src/main/java/info/bitrich/xchangestream/kucoin/KucoinStreamingMarketDataService.java
+++ xchange-stream-kucoin/src/main/java/info/bitrich/xchangestream/kucoin/KucoinStreamingMarketDataService.java
@@ -23,7 +23,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-class KucoinStreamingMarketDataService implements StreamingMarketDataService {
+public class KucoinStreamingMarketDataService implements StreamingMarketDataService {
 
   private static final Logger logger =
       LoggerFactory.getLogger(KucoinStreamingMarketDataService.class);
