f2a4e44e2cb27911b52c0a395f76012df903518b
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2023-08-09 15:34:45 +0200

    [gateio-streaming] Add test for trades

--- xchange-stream-gateio/src/test/java/info/bitrich/xchangestream/gateio/GateioStreamingMarketDataServiceIntegration.java
+++ xchange-stream-gateio/src/test/java/info/bitrich/xchangestream/gateio/GateioStreamingMarketDataServiceIntegration.java
@@ -4,6 +4,7 @@ import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.Trade;
 
 public class GateioStreamingMarketDataServiceIntegration extends GateioStreamingExchangeIT {
 
@@ -18,6 +19,21 @@ public class GateioStreamingMarketDataServiceIntegration extends GateioStreaming
         .awaitCount(1)
         .assertNoTimeout()
         .assertValueAt(0, orderBook -> !orderBook.getBids().isEmpty() || !orderBook.getAsks().isEmpty())
+        .dispose();
+  }
+
+
+  @Test
+  void trades() {
+    Observable<Trade> observable = exchange
+        .getStreamingMarketDataService()
+        .getTrades(CurrencyPair.BTC_USDT);
+
+    observable.test()
+        .assertSubscribed()
+        .awaitCount(1)
+        .assertNoTimeout()
+        .assertValueAt(0, trade -> trade.getInstrument().equals(CurrencyPair.BTC_USDT))
         .dispose();
   }
 
