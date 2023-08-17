46ed4e15836f9f7a6861c537473a6fc040d86190
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2023-08-17 11:59:46 +0200

    [gateio-streaming] Add enums for side and role

--- xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/dto/response/trade/TradePayload.java
+++ xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/dto/response/trade/TradePayload.java
@@ -2,12 +2,16 @@ package info.bitrich.xchangestream.gateio.dto.response.trade;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.math.BigDecimal;
 import java.time.Instant;
 import lombok.Data;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order.OrderType;
 import org.knowm.xchange.gateio.config.converter.DoubleToInstantConverter;
+import org.knowm.xchange.gateio.config.converter.OrderTypeToStringConverter;
 import org.knowm.xchange.gateio.config.converter.StringToCurrencyPairConverter;
+import org.knowm.xchange.gateio.config.converter.StringToOrderTypeConverter;
 import org.knowm.xchange.gateio.config.converter.TimestampSecondsToInstantConverter;
 
 @Data
@@ -18,14 +22,16 @@ public class TradePayload {
 
   @JsonProperty("create_time")
   @JsonDeserialize(converter = TimestampSecondsToInstantConverter.class)
-  private Instant time;
+  Instant time;
 
   @JsonProperty("create_time_ms")
   @JsonDeserialize(converter = DoubleToInstantConverter.class)
-  private Instant timeMs;
+  Instant timeMs;
 
   @JsonProperty("side")
-  private String side;
+  @JsonDeserialize(converter = StringToOrderTypeConverter.class)
+  @JsonSerialize(converter = OrderTypeToStringConverter.class)
+  OrderType side;
 
   @JsonProperty("currency_pair")
   @JsonDeserialize(converter = StringToCurrencyPairConverter.class)
--- xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/dto/response/usertrade/UserTradePayload.java
+++ xchange-stream-gateio/src/main/java/info/bitrich/xchangestream/gateio/dto/response/usertrade/UserTradePayload.java
@@ -2,14 +2,19 @@ package info.bitrich.xchangestream.gateio.dto.response.usertrade;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.math.BigDecimal;
 import java.time.Instant;
 import lombok.Data;
 import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.Order.OrderType;
 import org.knowm.xchange.gateio.config.converter.DoubleToInstantConverter;
+import org.knowm.xchange.gateio.config.converter.OrderTypeToStringConverter;
 import org.knowm.xchange.gateio.config.converter.StringToCurrencyPairConverter;
+import org.knowm.xchange.gateio.config.converter.StringToOrderTypeConverter;
 import org.knowm.xchange.gateio.config.converter.TimestampSecondsToInstantConverter;
+import org.knowm.xchange.gateio.dto.trade.Role;
 
 @Data
 public class UserTradePayload {
@@ -29,20 +34,22 @@ public class UserTradePayload {
 
   @JsonProperty("create_time")
   @JsonDeserialize(converter = TimestampSecondsToInstantConverter.class)
-  private Instant time;
+  Instant time;
 
   @JsonProperty("create_time_ms")
   @JsonDeserialize(converter = DoubleToInstantConverter.class)
-  private Instant timeMs;
+  Instant timeMs;
 
   @JsonProperty("side")
-  private String side;
+  @JsonDeserialize(converter = StringToOrderTypeConverter.class)
+  @JsonSerialize(converter = OrderTypeToStringConverter.class)
+  OrderType side;
 
   @JsonProperty("amount")
   BigDecimal amount;
 
   @JsonProperty("role")
-  String role;
+  Role role;
 
   @JsonProperty("price")
   BigDecimal price;
