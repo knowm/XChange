6074e4483c74399ec3c7bbee190290c0003073ca
makarid <makarid@gmail.com>
2023-08-21 19:08:19 +0300

    [CoinbasePro] Update test after changing type String to Enum

--- xchange-coinbasepro/src/test/java/org/knowm/xchange/coinbasepro/dto/trade/CoinbaseProFillTest.java
+++ xchange-coinbasepro/src/test/java/org/knowm/xchange/coinbasepro/dto/trade/CoinbaseProFillTest.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import org.junit.Test;
+import org.knowm.xchange.coinbasepro.dto.trade.CoinbaseProFill.Liquidity;
+import org.knowm.xchange.coinbasepro.dto.trade.CoinbaseProFill.Side;
 import si.mazi.rescu.serialization.jackson.DefaultJacksonObjectMapperFactory;
 import si.mazi.rescu.serialization.jackson.JacksonObjectMapperFactory;
 
@@ -31,9 +33,9 @@ public class CoinbaseProFillTest {
     assertThat(fill.getSize()).isEqualTo("0.01000000");
     assertThat(fill.getOrderId()).isEqualTo("b4b3bbb1-e0e3-4532-9413-23123448ce35");
     assertThat(fill.getCreatedAt()).isEqualTo("2017-05-01T07:31:50.243Z");
-    assertThat(fill.getLiquidity()).isEqualTo("T");
+    assertThat(fill.getLiquidity()).isEqualTo(Liquidity.T);
     assertThat(fill.getFee()).isEqualTo("0.0000017745000000");
-    assertThat(fill.isSettled()).isEqualTo(true);
-    assertThat(fill.getSide()).isEqualTo("buy");
+    assertThat(fill.isSettled()).isTrue();
+    assertThat(fill.getSide()).isEqualTo(Side.buy);
   }
 }
