81a40dd41a9f56f99b44c0a27e0a965f98e110a0
midnightcoder <midnightcoders@protonmail.com>
2023-11-02 18:58:05 +0000

    Added binance support for: * add, query, cancel inverse futures orders * add, query, cancel inverse margin orders * add, query, cancel portfolio margin inverse futures orders * add, query, cancel portfolio margin futures orders * add, query, cancel portfolio margin margin orders * Query portfolio margin positions * Query portfolio margin balances

--- xchange-examples/src/main/java/org/knowm/xchange/examples/binance/BinanceDemoUtils.java
+++ xchange-examples/src/main/java/org/knowm/xchange/examples/binance/BinanceDemoUtils.java
@@ -2,6 +2,7 @@ package org.knowm.xchange.examples.binance;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.binance.BinanceExchange;
 
 public class BinanceDemoUtils {
@@ -9,6 +10,14 @@ public class BinanceDemoUtils {
   public static Exchange createExchange() {
 
     Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BinanceExchange.class);
+    ExchangeSpecification binanceSpec = exchange.getDefaultExchangeSpecification();
+
+    binanceSpec.setApiKey("");
+    binanceSpec.setSecretKey("");
+    binanceSpec.setExchangeSpecificParametersItem("Portfolio_Margin_Enabled", true);
+
+    exchange.applySpecification(binanceSpec);
+
     return exchange;
   }
 }
