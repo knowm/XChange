3cb56ffb3232d2f9a942646425e859118fd933a9
rRajivramachandran <rrajiv9802@gmail.com>
2023-11-15 19:12:00 -0600

    Use JSON assert to improve test stability

--- xchange-stream-bitmex/src/test/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTest.java
+++ xchange-stream-bitmex/src/test/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTest.java
@@ -1,24 +1,27 @@
 package info.bitrich.xchangestream.bitmex;
 
 import java.io.IOException;
-import org.junit.Assert;
+
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 /** @author Foat Akhmadeev 13/06/2018 */
 public class BitmexStreamingTest {
   @Test
-  public void shouldGetCorrectSubscribeMessage() throws IOException {
+  public void shouldGetCorrectSubscribeMessage() throws IOException, JSONException {
     BitmexStreamingService service = new BitmexStreamingService("url", "api", "secret");
 
-    Assert.assertEquals(
-        "{\"op\":\"subscribe\",\"args\":[\"name\"]}", service.getSubscribeMessage("name"));
+    JSONAssert.assertEquals(
+        "{\"op\":\"subscribe\",\"args\":[\"name\"]}", service.getSubscribeMessage("name"), JSONCompareMode.NON_EXTENSIBLE);
   }
 
   @Test
-  public void shouldGetCorrectUnsubscribeMessage() throws IOException {
+  public void shouldGetCorrectUnsubscribeMessage() throws IOException, JSONException {
     BitmexStreamingService service = new BitmexStreamingService("url", "api", "secret");
 
-    Assert.assertEquals(
-        "{\"op\":\"unsubscribe\",\"args\":[\"name\"]}", service.getUnsubscribeMessage("name"));
+    JSONAssert.assertEquals(
+        "{\"op\":\"unsubscribe\",\"args\":[\"name\"]}", service.getUnsubscribeMessage("name"), JSONCompareMode.NON_EXTENSIBLE);
   }
 }
