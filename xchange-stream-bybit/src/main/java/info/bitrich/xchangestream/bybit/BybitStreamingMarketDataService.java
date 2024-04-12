5039eb5bed1a56d608650efc0c28e372e8389517
rizer1980 <4340180@gmail.com>
2024-04-12 07:56:59 +0300

    [bybit-stream] initial commit

---
+++ xchange-stream-bybit/src/main/java/info/bitrich/xchangestream/bybit/BybitStreamingMarketDataService.java
@@ -0,0 +1,170 @@
+package info.bitrich.xchangestream.bybit;
+
+import static org.knowm.xchange.bybit.BybitAdapters.convertToBybitSymbol;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.marketdata.BybitPublicOrder;
+import info.bitrich.xchangestream.core.StreamingMarketDataService;
+import info.bitrich.xchangestream.service.netty.StreamingObjectMapperHelper;
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import dto.marketdata.BybitOrderbook;
+import dto.trade.BybitTrade;
+import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.marketdata.OrderBook;
+import org.knowm.xchange.dto.marketdata.OrderBookUpdate;
+import org.knowm.xchange.dto.marketdata.Trade;
+import org.knowm.xchange.instrument.Instrument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BybitStreamingMarketDataService implements StreamingMarketDataService {
+
+  private final Logger LOG = LoggerFactory.getLogger(BybitStreamingMarketDataService.class);
+  private final BybitStreamingService streamingService;
+  private final ObjectMapper mapper = StreamingObjectMapperHelper.getObjectMapper();
+  public static final String TRADE = "publicTrade.";
+  public static final String ORDERBOOK = "orderbook.";
+  public static final String TICKER = "tickers.";
+
+  private final Map<String, OrderBook> orderBookMap = new HashMap<>();
+  private long orderBookUpdateIdPrev;
+  private final Map<Instrument, PublishSubject<List<OrderBookUpdate>>>
+      orderBookUpdatesSubscriptions;
+
+  public BybitStreamingMarketDataService(BybitStreamingService streamingService) {
+    this.streamingService = streamingService;
+    this.orderBookUpdatesSubscriptions = new ConcurrentHashMap<>();
+  }
+
+  /**
+   * Linear & inverse: Level 1 data, push frequency: 10ms Level 50 data, push frequency: 20ms Level
+   * 200 data, push frequency: 100ms Level 500 data, push frequency: 100ms Spot: Level 1 data, push
+   * frequency: 10ms Level 50 data, push frequency: 20ms Level 200 data, push frequency: 200ms
+   *
+   * @param args - orderbook depth
+   **/
+  @Override
+  public Observable<OrderBook> getOrderBook(Instrument instrument, Object... args) {
+    String depth = "50";
+    if (args.length > 0 && args[0] != null) {
+      depth = args[0].toString();
+    }
+    String channelUniqueId =
+        ORDERBOOK + depth + "." + convertToBybitSymbol(instrument);
+    return streamingService
+        .subscribeChannel(channelUniqueId)
+        .flatMap(
+            jsonNode -> {
+              BybitOrderbook bybitOrderbooks =
+                  mapper.treeToValue(jsonNode, BybitOrderbook.class);
+              String type = bybitOrderbooks.getDataType();
+              if (type.equalsIgnoreCase("snapshot")) {
+                OrderBook orderBook = BybitStreamAdapters.adaptOrderBook(bybitOrderbooks, instrument);
+                orderBookUpdateIdPrev = bybitOrderbooks.getData().getU();
+                orderBookMap.put(channelUniqueId, orderBook);
+                return Observable.just(orderBook);
+              } else if (type.equalsIgnoreCase("delta")) {
+                return applyDeltaSnapshot(channelUniqueId, instrument, bybitOrderbooks);
+              }
+              return Observable.fromIterable(new LinkedList<>());
+            });
+  }
+
+  private Observable<OrderBook> applyDeltaSnapshot(String channelUniqueId, Instrument instrument,
+      BybitOrderbook bybitOrderBookUpdate) {
+    OrderBook orderBook = orderBookMap.getOrDefault(channelUniqueId, null);
+    if (orderBook == null) {
+      LOG.error("Failed to get orderBook, channelUniqueId= {}", channelUniqueId);
+      return Observable.fromIterable(new LinkedList<>());
+    }
+    orderBookUpdateIdPrev++;
+    if (orderBookUpdateIdPrev == bybitOrderBookUpdate.getData().getU()) {
+      LOG.debug("orderBookUpdate id {}, seq {} ", bybitOrderBookUpdate.getData().getU(),
+          bybitOrderBookUpdate.getData().getSeq());
+      List<BybitPublicOrder> asks = bybitOrderBookUpdate.getData().getAsk();
+      List<BybitPublicOrder> bids = bybitOrderBookUpdate.getData().getBid();
+      Date timestamp = new Date(Long.parseLong(bybitOrderBookUpdate.getTs()));
+      asks.forEach(
+          bybitPublicOrder ->
+              orderBook.update(
+                  BybitStreamAdapters.adaptOrderBookOrder(
+                      bybitPublicOrder, instrument, Order.OrderType.ASK, timestamp)));
+      bids.forEach(
+          bybitPublicOrder ->
+              orderBook.update(
+                  BybitStreamAdapters.adaptOrderBookOrder(
+                      bybitPublicOrder, instrument, Order.OrderType.BID, timestamp)));
+      if (orderBookUpdatesSubscriptions.get(instrument) != null) {
+        orderBookUpdatesSubscriptions(instrument, asks, bids, timestamp);
+      }
+      return Observable.just(orderBook);
+    } else {
+      LOG.error("orderBookUpdate id sequence failed, expected {}, in fact {}",
+          orderBookUpdateIdPrev,
+          bybitOrderBookUpdate.getData().getU());
+      // resubscribe or what here?
+      return Observable.fromIterable(new LinkedList<>());
+    }
+  }
+
+  public Observable<List<OrderBookUpdate>> getOrderBookUpdates(Instrument instrument) {
+    return orderBookUpdatesSubscriptions.computeIfAbsent(instrument, v -> PublishSubject.create());
+  }
+
+  private void orderBookUpdatesSubscriptions(
+      Instrument instrument, List<BybitPublicOrder> asks, List<BybitPublicOrder> bids, Date date) {
+    List<OrderBookUpdate> orderBookUpdates = new ArrayList<>();
+    for (BybitPublicOrder ask : asks) {
+      OrderBookUpdate o =
+          new OrderBookUpdate(
+              Order.OrderType.ASK,
+              new BigDecimal(ask.getSize()),
+              instrument,
+              new BigDecimal(ask.getPrice()),
+              date,
+              new BigDecimal(ask.getSize()));
+      orderBookUpdates.add(o);
+    }
+    for (BybitPublicOrder bid : bids) {
+      OrderBookUpdate o =
+          new OrderBookUpdate(
+              Order.OrderType.BID,
+              new BigDecimal(bid.getSize()),
+              instrument,
+              new BigDecimal(bid.getPrice()),
+              date,
+              new BigDecimal(bid.getSize()));
+      orderBookUpdates.add(o);
+    }
+    orderBookUpdatesSubscriptions.get(instrument).onNext(orderBookUpdates);
+  }
+
+  @Override
+  public Observable<Trade> getTrades(Instrument instrument, Object... args) {
+    String channelUniqueId =
+        TRADE + convertToBybitSymbol(instrument);
+
+    return streamingService
+        .subscribeChannel(channelUniqueId)
+        .filter(message -> message.has("data"))
+        .flatMap(
+            jsonNode -> {
+              List<BybitTrade> bybitTradeList =
+                  mapper.treeToValue(
+                      jsonNode.get("data"),
+                      mapper.getTypeFactory()
+                          .constructCollectionType(List.class, BybitTrade.class));
+              return Observable.fromIterable(
+                  BybitStreamAdapters.adaptTrades(bybitTradeList, instrument).getTrades());
+            });
+  }
+}
---
+++ xchange-stream-bybit/src/test/java/info/bitrich/xchangestream/bybit/BybitStreamExample.java
@@ -0,0 +1,139 @@
+package info.bitrich.xchangestream.bybit;
+
+import static org.knowm.xchange.Exchange.USE_SANDBOX;
+import static org.knowm.xchange.bybit.BybitExchange.SPECIFIC_PARAM_ACCOUNT_TYPE;
+
+import info.bitrich.xchangestream.core.StreamingExchange;
+import info.bitrich.xchangestream.core.StreamingExchangeFactory;
+import io.reactivex.disposables.Disposable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.bybit.dto.BybitCategory;
+import org.knowm.xchange.bybit.dto.account.walletbalance.BybitAccountType;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.derivative.FuturesContract;
+import org.knowm.xchange.derivative.OptionsContract;
+import org.knowm.xchange.instrument.Instrument;
+
+
+public class BybitStreamExample {
+
+  public static void main(String[] args) {
+    try {
+      spot();
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    futures();
+  }
+
+  private static final Instrument BTC_PERP = new FuturesContract("BTC/USDT/PERP");
+  private static final Instrument ETH_PERP = new CurrencyPair("ETH/USDT/PERP");
+  private static final Instrument BTC_SPOT = new CurrencyPair("BTC/USDT");
+  private static final Instrument ETH_SPOT = new CurrencyPair("ETH/USDT");
+
+  public static void spot() throws IOException {
+    ExchangeSpecification exchangeSpecification =
+        new ExchangeSpecification(BybitStreamingExchange.class);
+    exchangeSpecification.setExchangeSpecificParametersItem(USE_SANDBOX, true);
+    exchangeSpecification.setExchangeSpecificParametersItem(BybitStreamingExchange.EXCHANGE_TYPE,
+        BybitCategory.SPOT.getValue());
+    exchangeSpecification.setExchangeSpecificParametersItem(SPECIFIC_PARAM_ACCOUNT_TYPE,
+        BybitAccountType.UNIFIED);
+    StreamingExchange exchange = StreamingExchangeFactory.INSTANCE.createExchange(
+        exchangeSpecification);
+    exchange.connect().blockingAwait();
+    int count_currencyPair = 0;
+    int count_futureContractPerp = 0;
+    int count_futureContractDate = 0;
+    List<Instrument> instruments = new ArrayList<>(exchange
+        .getExchangeMetaData()
+        .getInstruments().keySet());
+    for (Instrument instrument : instruments) {
+      if (instrument instanceof CurrencyPair) {
+        count_currencyPair++;
+      }
+      if (instrument instanceof FuturesContract) {
+        count_futureContractPerp++;
+      }
+      if (instrument instanceof OptionsContract) {
+        count_futureContractDate++;
+      }
+    }
+    System.out.println("Currency pairs: " + count_currencyPair);
+    System.out.println("Futures: " + count_futureContractPerp);
+    System.out.println("Futures date: " + count_futureContractDate);
+
+    System.out.println(exchange.getMarketDataService().getTicker(BTC_SPOT));
+    System.out.println(exchange.getMarketDataService().getTicker(BTC_PERP));
+   List<Disposable> tradesDisposable = new ArrayList<>();
+    Disposable bookDisposable =
+        exchange
+            .getStreamingMarketDataService()
+            .getOrderBook(ETH_SPOT)
+            .subscribe();
+    tradesDisposable.add(exchange
+        .getStreamingMarketDataService().getTrades(ETH_SPOT).subscribe(
+            trade -> System.out.println("trade: " + trade)));
+    tradesDisposable.add(exchange
+        .getStreamingMarketDataService().getTrades(BTC_SPOT).subscribe(
+            trade -> System.out.println("trade: " + trade)));
+    try {
+      Thread.sleep(2000);
+    } catch (
+        InterruptedException ignored) {
+    }
+    bookDisposable.dispose();
+    for(Disposable disposable:tradesDisposable)
+      disposable.dispose();
+
+    try {
+      Thread.sleep(5000);
+    } catch (
+        InterruptedException ignored) {
+    }
+    exchange.disconnect().blockingAwait();
+  }
+
+
+  public static void futures() {
+    ExchangeSpecification exchangeSpecification =
+        new ExchangeSpecification(BybitStreamingExchange.class);
+    exchangeSpecification.setExchangeSpecificParametersItem(SPECIFIC_PARAM_ACCOUNT_TYPE,
+        BybitAccountType.UNIFIED);
+    exchangeSpecification.setExchangeSpecificParametersItem(BybitStreamingExchange.EXCHANGE_TYPE,
+        BybitCategory.LINEAR.getValue());
+    StreamingExchange exchange = StreamingExchangeFactory.INSTANCE.createExchange(
+        exchangeSpecification);
+    exchange.connect().blockingAwait();
+    List<Disposable> tradesDisposable = new ArrayList<>();
+    Disposable booksDisposable =
+        exchange
+            .getStreamingMarketDataService()
+            .getOrderBook(BTC_PERP)
+            .subscribe();
+
+    tradesDisposable.add(exchange
+        .getStreamingMarketDataService().getTrades(ETH_PERP).subscribe(
+            trade -> System.out.println("trade: " + trade)));
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException ignored) {
+
+    }
+    booksDisposable.dispose();
+    for(Disposable disposable:tradesDisposable)
+      disposable.dispose();
+
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException ignored) {
+
+    }
+    exchange.disconnect().blockingAwait();
+  }
+
+}
