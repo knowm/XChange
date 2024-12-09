80cbc78fbe7164fae4c6dd45e0c694afb28f20ed
Dmitri Karpovich <dmitri.karpovich@gmail.com>
2024-12-09 13:05:48 +0100

    [bitmex] Add authorized rest request

---
+++ xchange-bitmex/src/test/resources/rest/sign.js
@@ -0,0 +1,23 @@
+export function gen_sign(request) {
+  const pattern = RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
+  const method = request.method;
+  const url = request.url.tryGetSubstituted();
+  const matches =  url.match(pattern);
+
+  const path = matches[5];
+
+  let query = matches[7] || "";
+  if (query !== "") {
+    query = "?" + query;
+  }
+
+  const body = request.body.tryGetSubstituted() || "";
+  const timestamp = Math.floor(new Date(new Date().getTime() + 30000)).toFixed();
+
+  const payloadToSign = `${method}${path}${query}${timestamp}${body}`;
+  const apiSecret = request.environment.get("api_secret");
+  const sign = crypto.hmac.sha256().withTextSecret(apiSecret).updateWithText(payloadToSign).digest().toHex();
+
+  request.variables.set("timestamp", timestamp);
+  request.variables.set("sign", sign);
+}
